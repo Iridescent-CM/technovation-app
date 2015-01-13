7c97ad98c4556380472607dc7de7d8a9b29c51a5
Cory Li <coryli@mit.edu>
2015-01-13 00:55:56 -0800

    require signed_in for bg_check

--- app/controllers/bg_check_controller.rb
+++ app/controllers/bg_check_controller.rb
@@ -1,4 +1,5 @@
 class BgCheckController < ApplicationController
+  before_action :authenticate_user!
   before_action :check_user
   skip_before_filter :verify_consent
   skip_before_filter :verify_bg_check
