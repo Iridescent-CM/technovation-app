8c5ba0a82172c8e65420b7dbb26697b68c797f3a
Cassandra Xia <cssndrx@gmail.com>
2015-03-11 11:30:18 -0700

    aesthetic changes to scores page

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -3,8 +3,11 @@ class ScoresController < ApplicationController
 	def index
 		## send in a hash from team name to rubrics
 		@hash = {}
+#		@num_rubrics = 0
 		for t in current_user.teams
-			@hash[t.name] = Rubric.where("team_id = '#{t.id}'")
+			rubrics = Rubric.where("team_id = '#{t.id}'")
+			@hash[t.name] = rubrics
+#			@num_rubrics += rubrics.length
 		end
 	end
 end
