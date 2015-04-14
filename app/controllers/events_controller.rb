46b683b370581c167e5d79ce70610d458d082630
Erik Christensen <christensenep@gmail.com>
2015-04-14 19:50:19 -0400

    Fix valid_events auth requirement

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :valid_events
 
   def index
   	# if current_user.can_judge?
