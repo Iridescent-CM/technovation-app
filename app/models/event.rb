03081f5858f1a2b91bec4acb029f7f63078da182
Mike Larsson <mikelarssonftw@gmail.com>
2016-03-03 11:54:49 -0500

    Fix for #195 to only show this year's events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,9 +2,14 @@ class Event < ActiveRecord::Base
   has_many :teams
   belongs_to :region
 
-  scope :open_for_signup, -> {
+  scope :display_order, -> {
     order("name!='Virtual Judging', name")
   }
+
+  scope :open_for_signup, -> {
+    display_order.where("extract(year from whentooccur) = ?", Setting.year)
+  }
+
   scope :nonconflicting_events, ->(conflict_regions) {
     open_for_signup.where("region_id not in (?) OR region_id=-1 OR region_id IS NULL", (conflict_regions << -1))
   }
