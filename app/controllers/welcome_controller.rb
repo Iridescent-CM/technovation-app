65bd4fd7e0ad70221281903999172ae2b794121e
Mike Larsson <mikelarssonftw@gmail.com>
2016-03-10 16:17:17 -0500

    let judging users see judge-only announcements

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,9 @@ class WelcomeController < ApplicationController
   skip_before_action :verify_registered
   def index
     roles = [Announcement.roles[current_user.role], Announcement.roles['any']]
+    if current_user.judging?
+      roles.push(Announcement.roles['judge'])
+    end
     @announcements = Announcement.where(published: true, role: roles).order(created_at: :desc).limit(5)
   end
 end
