b87142d3993c7c2f74337e60f89e09897b477ba2
Marcela Fontes <mfontes@thoughtworks.com>
2016-03-21 17:09:49 -0300

    |TECH-208| Judge register email modified

--- app/mailers/signup_mailer.rb
+++ app/mailers/signup_mailer.rb
@@ -3,7 +3,7 @@ class SignupMailer < ActionMailer::Base
 
   def judge_signup_email(user)
     @user = user
-    mail(to: user.email, subject: 'Welcome to Technovation!')
+    mail(to: user.email, subject: 'Subject: Getting Started as a Technovation Judge')
   end
 
   def mentor_signup_email(user)
