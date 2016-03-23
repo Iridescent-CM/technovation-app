561d867eaf2efde86f3bd3cb43369b63fdce5439
Gabriel Caires <gcaires@thoughtworks.com>
2016-03-23 15:11:23 -0300

    Use EnvVar to configure Airbrake

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,3 +1,4 @@
 Airbrake.configure do |config|
-  config.api_key = '25c7abb3eb366a19a0743c5f04a9320e'
+  config.api_key = ENV['AIRBRAKE_API_KEY']
+  config.project_key = ENV['AIRBRAKE_PROJECT_ID']
 end
