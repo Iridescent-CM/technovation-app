7b280e187b357b7aba8b1e4ddf22e41cb3ff8981
Camilla Crispim <ccrispim@thoughtworks.com>
2016-03-30 17:35:00 -0300

    |TECH-230| Change test environment paperclip configuration

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -5,15 +5,6 @@ include WebMock::API
 describe Team, type: :model do
 
   context '.update' do
-    before do
-      default_response = {
-        status: 200,
-        body: '{"results":[]}',
-        headers: {}
-      }
-      stub_request(:any, /.*/).to_return(default_response)
-
-    end
 
     it { should validate_attachment_size(:screenshot1).less_than(100.kilobytes) }
     it { should validate_attachment_size(:screenshot2).less_than(100.kilobytes) }
