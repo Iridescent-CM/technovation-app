159a43afcbe4be88488866b0a9118f67432fb0c2
Gabriel Caires <gcaires@thoughtworks.com>
2016-03-31 11:59:07 -0300

    |TECH-230| Change tests to use the is_expected syntax

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -2,17 +2,14 @@ require 'rails_helper'
 include WebMock::API
 
 describe Team, type: :model do
+  subject { Team.new }
 
-  context '.update' do
-
-    it { should validate_attachment_size(:screenshot1).less_than(100.kilobytes) }
-    it { should validate_attachment_size(:screenshot2).less_than(100.kilobytes) }
-    it { should validate_attachment_size(:screenshot3).less_than(100.kilobytes) }
-    it { should validate_attachment_size(:screenshot4).less_than(100.kilobytes) }
-    it { should validate_attachment_size(:screenshot5).less_than(100.kilobytes) }
-    it { should validate_attachment_size(:logo).less_than(100.kilobytes) }
-
-describe Team, type: :model do
+    it { is_expected.to validate_attachment_size(:screenshot1).less_than(100.kilobytes) }
+    it { is_expected.to validate_attachment_size(:screenshot2).less_than(100.kilobytes) }
+    it { is_expected.to validate_attachment_size(:screenshot3).less_than(100.kilobytes) }
+    it { is_expected.to validate_attachment_size(:screenshot4).less_than(100.kilobytes) }
+    it { is_expected.to validate_attachment_size(:screenshot5).less_than(100.kilobytes) }
+    it { is_expected.to validate_attachment_size(:logo).less_than(100.kilobytes) }
 
   describe 'attributes' do
     subject(:team) { build(:team) }
