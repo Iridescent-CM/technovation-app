c73ccb0294be62d8c44c3db122152ca0c7cba21f
Gabriel Caires <gcaires@thoughtworks.com>
2016-04-04 14:16:10 -0300

    Paperclip use a different folder to each env

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,6 @@
 Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+if Rails.env.production?
+  Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+else
+  Paperclip::Attachment.default_options[:path] = "/#{Rails.env}/:class/:attachment/:id_partition/:style/:filename"
+end
