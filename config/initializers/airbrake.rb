dac14efeb33f8156efa97b706d2a00496be13abb
Gabriel Caires <gcaires@thoughtworks.com>
2016-04-08 14:46:36 -0300

    Fix airbrake errror

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -2,6 +2,6 @@ Airbrake.configure do |config|
   config.project_key = ENV.fetch('AIRBRAKE_API_KEY', '')
   config.project_id = ENV.fetch('AIRBRAKE_PROJECT_ID', '')
 
-  # Display debug output.
   config.logger.level = Logger::DEBUG
+  config.environment = Rails.env
 end
