4629933a6fd2e49ec954bc278e6b2aaac11a7574
Joe Sak and Marcela Fontes <joe+joemsak+mfontes@iridescentlearning.org>
2016-05-04 10:52:54 -0400

    Calculate scores for rubrics for real

--- spec/scores/calculate_scores_spec.rb
+++ spec/scores/calculate_scores_spec.rb
@@ -1,21 +1,43 @@
 require "spec_helper"
+require "ostruct"
 
 RSpec.describe "Calculate scores" do
-  points = %i{identify_problem address_problem functional external_resources
-              match_features interface description market competition
-              revenue branding pitch}
+  let(:points) { CalculateScore.scoring_attributes }
 
   it "scores zero for all zeroes" do
     attributes = Hash[points.map { |point| [point, 0] }]
-    rubric = double(:rubric, attributes.merge(score: 0))
+    rubric = TestRubric.new(attributes)
 
     CalculateScore.call(rubric)
 
     expect(rubric.score).to eq(0)
   end
 
+  it "scores one for all zeroes and a 1" do
+    attributes = Hash[points.map { |point| [point, 0] }]
+    rubric = TestRubric.new(attributes.merge(revenue: 1))
+
+    CalculateScore.call(rubric)
+
+    expect(rubric.score).to eq(1)
+  end
+
   class CalculateScore
-    def self.call(rubric)
+    class << self
+      def call(rubric)
+        rubric.score = scoring_attributes.inject(0) do |accumulator, attr|
+          accumulator + rubric.send(attr)
+        end
+      end
+
+      def scoring_attributes
+        %i{identify_problem address_problem functional external_resources
+           match_features interface description market competition
+           revenue branding pitch}
+      end
     end
+  end
+
+  class TestRubric < OpenStruct
   end
 end
