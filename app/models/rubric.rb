fa2c6b20ca0d15c5ffe9efc2d3eba8e0ac3eb057
Joe Sak and Marcela Fontes <joe+joemsak+mfontes@iridescentlearning.org>
2016-05-04 10:40:23 -0400

    Format code, remove unneeded comments

--- app/models/rubric.rb
+++ app/models/rubric.rb
@@ -18,24 +18,27 @@ class Rubric < ActiveRecord::Base
 
   def calculate_score
     score = 0
-    points = [:identify_problem, :address_problem, :functional, :external_resources, :match_features, :interface, :description, :market, :competition, :revenue, :branding, :pitch]
+    points = [:identify_problem, :address_problem, :functional, :external_resources,
+              :match_features, :interface, :description, :market, :competition,
+              :revenue, :branding, :pitch]
+
     points.each { |p| score += self[p]}
 
     if self.launched
       score += 2
     end
 
-    # deduct points for missing components
     deductions = [:pitch, :demo, :code, :description, :plan]
-    deductions.each{ |d| 
+
+    deductions.each{ |d|
       if (self.team.missing_field?(d.to_s))
         score -= 1
       end
     }
 
     deductions = [:screenshot1, :screenshot2, :screenshot3, :screenshot4, :screenshot5]
+
     if deductions.map{|d| self.team.missing_field?(d.to_s)}.all?
-      ## if all screenshots are missing then deduct 1 pt
       score -= 1
     end
 
