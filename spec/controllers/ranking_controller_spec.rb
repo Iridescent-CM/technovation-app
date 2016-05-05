a2981cd56f2cc1d7d2fdf293b750ff15a477c623
Joe Sak <joe@joesak.com>
2016-05-05 14:11:00 -0400

    Convert lazy let to eager let in RankingController spec to maybe fix random failing spec

--- spec/controllers/ranking_controller_spec.rb
+++ spec/controllers/ranking_controller_spec.rb
@@ -11,13 +11,15 @@ describe RankingController, type: :controller do
         home_country: judge_country
       )
     end
+
     let(:in_person_event) do
       create(:event, region_id: region.id, whentooccur: whentooccur_event)
     end
+
     let(:judge_event) { in_person_event }
     let(:whentooccur_event) { Faker::Date.forward(10) }
     let(:year) { whentooccur_event.year }
-    let!(:region) { create(:region, id: nil) }
+    let!(:region) { create(:region) }
     let!(:virtual_event) { create(:event, :virtual_event, whentooccur: whentooccur_event) }
     let(:judge_country) { 'CH' }
     let!(:teams) { create_list(:team, 10, region_id: region.id, year: year) }
@@ -36,7 +38,8 @@ describe RankingController, type: :controller do
     end
 
     context 'when the event is virtual' do
-      let(:regions) { create_list(:region, 10) }
+      let!(:regions) { create_list(:region, 10) }
+
       let!(:teams) do
         regions.map do |region|
           create(:team, region_id: region.id, year: year)
@@ -64,15 +67,15 @@ describe RankingController, type: :controller do
             to receive(:conflict_regions)
             .and_return double(pluck: judge_conflict_regions.map(&:id))
         end
-        
+
         it 'does not receive a region to judge' do
           RankingController.assign_judges_to_regions
           judge.reload
-          expect(judge.judging_region_id).to be_nil 
+          expect(judge.judging_region_id).to be_nil
         end
       end
     end
-    
+
     context 'when there is brazilian judge' do
       let(:judge_country) { 'BR' }
       let(:judge_event) { in_person_event_brazilian_event }
@@ -135,26 +138,27 @@ describe RankingController, type: :controller do
           end
         end
       end
-        context 'and all regions are conflict regions' do
-          let!(:non_brazilian_region) { create(:region, id: 100) }
-          let!(:non_brazilian_team) { create(:team, region: non_brazilian_region, year: year) }
-          let(:judge_conflict_regions) { south_american_regions }
-          let(:judge_event) { virtual_event }
-          let(:brazilian_team_region) { south_american_regions.sample }
-          let(:brazilian_team_division) { brazilian_team_region.division }
 
-          before do
-            allow_any_instance_of(User).
-              to receive(:conflict_regions)
-              .and_return double(pluck: judge_conflict_regions.map(&:id))
-          end
+      context 'and all regions are conflict regions' do
+        let!(:non_brazilian_region) { create(:region, id: 100) }
+        let!(:non_brazilian_team) { create(:team, region: non_brazilian_region, year: year) }
+        let(:judge_conflict_regions) { south_american_regions }
+        let(:judge_event) { virtual_event }
+        let(:brazilian_team_region) { south_american_regions.sample }
+        let(:brazilian_team_division) { brazilian_team_region.division }
 
-          it 'judge should not receive any region' do
-            RankingController.assign_judges_to_regions
-            judge.reload
-            expect(judge.judging_region_id).to be_nil
-          end
+        before do
+          allow_any_instance_of(User).
+            to receive(:conflict_regions)
+            .and_return double(pluck: judge_conflict_regions.map(&:id))
         end
+
+        it 'judge should not receive any region' do
+          RankingController.assign_judges_to_regions
+          judge.reload
+          expect(judge.judging_region_id).to be_nil
+        end
+      end
     end
   end
 end
