d08a926c504a53230e3db320581dd74a75e8565f
Joe Sak <joe@joesak.com>
2016-05-05 10:30:05 -0400

    Nitpicky code formatting yo, also TeamsHelper is really an ErrorsHelper

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,6 +1,7 @@
 module TeamsHelper
-
   def show_all_errors(errors)
-      errors.map {|field| "<strong>"+ field + "</strong>" }.to_sentence(last_word_connector: ' and ')
+    errors.map { |field|
+      "<strong>"+ field + "</strong>"
+    }.to_sentence(last_word_connector: ' and ')
   end
 end
