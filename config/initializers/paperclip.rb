62b5087217e94fdbdb1b415af49b1fbdae52d4f4
Joe Sak <joe@joesak.com>
2016-05-09 11:33:55 -0400

    |TECH #316| Write paperclip attachments in a dev/test folder that has permissions to write to

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,5 +2,5 @@ Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
 if Rails.env.production?
   Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 else
-  Paperclip::Attachment.default_options[:path] = "/#{Rails.env}/:class/:attachment/:id_partition/:style/:filename"
+  Paperclip::Attachment.default_options[:path] = "./public/#{Rails.env}/:class/:attachment/:id_partition/:style/:filename"
 end
