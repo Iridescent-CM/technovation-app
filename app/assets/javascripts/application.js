da8013b57f00b4a690aefbede8b4f96a1c939585
Joe Sak <joe@joesak.com>
2016-05-12 11:08:06 -0400

    |TECH #331| Add bootstrap tabs as first step in enabling judges to view previously judged rounds

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,3 +5,22 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+(function() {
+  var tabs = {
+    cssSelector: '.nav-tabs a',
+
+    handledEvent: 'click',
+
+    init: function() {
+      return $(document).on(this.handledEvent, this.cssSelector, this.showTab);
+    },
+
+    showTab: function(event) {
+      event.preventDefault();
+      return $(this).tab('show');
+    },
+  };
+
+  return tabs.init();
+})();
