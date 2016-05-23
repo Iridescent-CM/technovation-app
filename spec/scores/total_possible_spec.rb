db09b3ec4e2c1d1223d2a688c391a047b3554064
Joe Sak <joe@joesak.com>
2016-05-23 12:58:26 -0500

    Finish specs for total possible score

--- spec/scores/total_possible_spec.rb
+++ spec/scores/total_possible_spec.rb
@@ -9,13 +9,57 @@ RSpec.describe "total possible score" do
     expect(total_possible).to eq(0)
   end
 
-  it "returns a sum of all 1 values"
-  it "returns the sum of the maximum values"
+  it "returns a sum of all 1 values" do
+    allow(Score).to receive(:config) { all_ones_config }
+    expect(total_possible).to eq(1)
+  end
+
+  it "returns the sum of the maximum values" do
+    allow(Score).to receive(:config) { mixed_values_config }
+    expect(total_possible).to eq(8)
+  end
 
   def all_zeros_config
     {
       "category_name" => {
         "field_name" => { "values" => { "0" => 'Value explanation'  } }
+      }
+    }
+  end
+
+  def all_ones_config
+    {
+      "category_name" => {
+        "field_name" => { "values" => { "1" => 'Value explanation'  } }
+      }
+    }
+  end
+
+  def mixed_values_config
+    {
+      "category_name" => {
+        "field_name" => {
+          "values" => {
+            "1" => 'Value explanation',
+            "4" => "Value explained"
+          }
+        },
+
+        "field_name_2" => {
+          "values" => {
+            "1" => 'Value explanation',
+            "2" => "Value explained"
+          }
+        }
+      },
+
+      "category_name_2" => {
+        "field_name" => {
+          "values" => {
+            "1" => 'Value explanation',
+            "2" => "Value explained"
+          }
+        }
       }
     }
   end
