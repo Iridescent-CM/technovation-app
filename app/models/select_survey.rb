2afceffcc1a6cc177b96aa6fbbe2380767ae3285
Marcela Fontes <mfontes@thoughtworks.com>
2016-05-26 15:49:21 -0300

    |TECH #361| Refactor SelectSurvey class

--- app/models/select_survey.rb
+++ app/models/select_survey.rb
@@ -1,4 +1,22 @@
+require 'ostruct'
+
 class SelectSurvey
+
+  SurveyData = OpenStruct.new({
+    :student => OpenStruct.new({
+                pre: "https://www.surveymonkey.com/s/683JH6K",
+                post: "http://rockman.co1.qualtrics.com/SE/?SID=SV_0cankgvZeqVcy3P"
+              }),
+    :mentor => OpenStruct.new({
+                pre: "https://www.surveymonkey.com/s/6GLCHTB",
+                post: "http://rockman.co1.qualtrics.com/SE/?SID=SV_bjRy3xD4O81l6i9"
+              }),
+    :coach => OpenStruct.new({
+                pre: "https://www.surveymonkey.com/s/SV2FST7",
+                post: "http://rockman.co1.qualtrics.com/SE/?SID=SV_cN166tnJClpa7jf"
+              })
+    })
+
 
   def initialize(user, submissions)
     @user = user
@@ -6,38 +24,6 @@ class SelectSurvey
   end
 
   def link
-    Survey.new(@user, @period).link
-  end
-end
-
-
-class Survey
-  def initialize(user, period)
-    @user = user
-    @period = period
-  end
-
-  def link
-    if @period == :pre
-      case @user.role
-      when :student
-        "https://www.surveymonkey.com/s/683JH6K"
-      when :mentor
-        "https://www.surveymonkey.com/s/6GLCHTB"
-      when :coach
-        "https://www.surveymonkey.com/s/SV2FST7"
-      end
-    elsif @period == :post
-      case @user.role
-      when :student
-        "http://rockman.co1.qualtrics.com/SE/?SID=SV_0cankgvZeqVcy3P"
-      when :mentor
-        "http://rockman.co1.qualtrics.com/SE/?SID=SV_bjRy3xD4O81l6i9"
-      when :coach
-        "http://rockman.co1.qualtrics.com/SE/?SID=SV_cN166tnJClpa7jf"
-      end
-    else
-      ""
-    end
+    SurveyData[@user.role][@period]
   end
 end
