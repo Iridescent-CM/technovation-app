b99d6c4e5da580c4e8089d38a6b8527105b8ae9d
Marcela Fontes <mfontes@thoughtworks.com>
2016-05-27 17:08:03 -0300

    |TECH #361| Replaced id selector to be inside the link and corrected unreliable tests

--- spec/views/welcome/view_dashboard_spec.rb
+++ spec/views/welcome/view_dashboard_spec.rb
@@ -2,14 +2,25 @@ require 'rails_helper'
 
 RSpec.feature "view dashboard" do
   let!(:student) { create(:user, :student) }
+  let(:judge) { create(:user, :judge) }
 
-  scenario "shows pre program survey link for students" do
+  before do
     Season.open!
     Submissions.open!
-
-    sign_in(student)
-    visit root_path
-    expect(page).to have_link('', href: root_path("survey-link") )
   end
 
+  scenario "shows pre program survey link for students" do
+    sign_in(student)
+    visit root_path
+    expect(page).to have_link('survey-link')
+  end
+
+  scenario "does not show pre program survey link for judges" do
+    Quarterfinal.open!(close: Date.today + 1)
+    Semifinal.close!
+
+    sign_in(judge)
+    visit root_path
+    expect(page).not_to have_link('survey-link')
+  end
 end
