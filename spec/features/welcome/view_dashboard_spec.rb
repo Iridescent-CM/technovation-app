4c3b0d11caf8632df0cde99911970ea0ade9ae93
Camilla Crispim <ccrispim@thoughtworks.com>
2016-06-07 17:00:09 -0300

    |TECH #323| Do not show any survey link on the page if any survey toggle is on

--- spec/features/welcome/view_dashboard_spec.rb
+++ spec/features/welcome/view_dashboard_spec.rb
@@ -8,20 +8,29 @@ RSpec.feature "view dashboard" do
   before do
     Season.open!
     Submissions.open!
-    Survey.show_pre_program
   end
 
   scenario "shows pre program survey link for students" do
+    Survey.show_pre_program
     sign_in(student)
     visit root_path
     expect(page).to have_link('survey-link')
   end
 
-  scenario "does not show pre program survey link for judges" do
+  scenario "does not show survey link for judges" do
     Quarterfinal.open!(close: Date.today + 1)
     Semifinal.close!
+    Survey.show_post_program
 
     sign_in(judge)
+    visit root_path
+    expect(page).not_to have_link('survey-link')
+  end
+
+  scenario "does not show survey link when they are not visible" do
+    Survey.hide_all
+
+    sign_in(student)
     visit root_path
     expect(page).not_to have_link('survey-link')
   end
