588265cf074986903e6fce03a8be7877f406455c
Camilla Crispim <ccrispim@thoughtworks.com>
2016-06-07 10:38:00 -0300

    |TECH #323| Encapsulate show/hide survey inside Survey class

--- app/admin/toggle_controller.rb
+++ app/admin/toggle_controller.rb
@@ -2,12 +2,12 @@ class ToggleController < ActionController::Base
 
   def self.pre_program_survey
     pre_program_survey = Setting.find_by_key!('pre_program_survey')
-    pre_program_survey.update(value: pre_program_survey.value == 'true' ? 'false' : 'true')
+    pre_program_survey.value == 'true' ? Survey.hide_pre_program : Survey.show_pre_program
   end
 
   def self.post_program_survey
     post_program_survey = Setting.find_by_key!('post_program_survey')
-    post_program_survey.update(value: post_program_survey.value == 'true' ? 'false' : 'true')
+    post_program_survey.value == 'true' ? Survey.hide_post_program : Survey.show_post_program
   end
 
 end
--- app/models/survey/survey.rb
+++ app/models/survey/survey.rb
@@ -1,12 +1,25 @@
 class Survey
   class << self
     def hide_all
-      setting.reset('pre_program_survey', false)
-      setting.reset('post_program_survey', false)
+      hide_pre_program
+      hide_post_program
     end
 
     def show_pre_program
       setting.reset('pre_program_survey', true)
+      setting.reset('post_program_survey', false)
+    end
+
+    def hide_pre_program
+      setting.reset('pre_program_survey', false)
+    end
+
+    def show_post_program
+      setting.reset('post_program_survey', true)
+      setting.reset('pre_program_survey', false)
+    end
+
+    def hide_post_program
       setting.reset('post_program_survey', false)
     end
 
