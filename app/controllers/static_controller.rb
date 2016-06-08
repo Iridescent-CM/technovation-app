5a37c82bc559dd361a89f4dcca38e82b0ccecf22
Joe Sak <joe@joesak.com>
2016-06-08 17:06:45 -0500

    Continue to add sandbox view stuff

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -16,5 +16,13 @@ class StaticController < ApplicationController
     def current_season_year
       Season.current.year
     end
+
+    def scoring_rubric
+      ScoreCategory.all.map { |c|
+        [c.name,
+         c.score_attributes.flat_map(&:label),
+         c.score_attributes.flat_map { |a| a.score_values.map(&:label) }].join('<br>')
+      }.join('<br>').html_safe
+    end
   end
 end
