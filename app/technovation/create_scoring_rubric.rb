a46875bf74fc4d5dcfdbc4146ad0453f44826229
Joe Sak <joe@joesak.com>
2016-06-10 13:01:41 -0500

    Refactor the internals of CreateScoringRubric

--- app/technovation/create_scoring_rubric.rb
+++ app/technovation/create_scoring_rubric.rb
@@ -1,16 +1,27 @@
 module CreateScoringRubric
   def self.call(attributes)
-    category = ScoreCategory.find_or_create_by(name: attributes.fetch(:category))
-
-    attributes.fetch(:attributes).each do |attr|
-      attribute = category.score_attributes.build(label: attr.fetch(:label))
-
-      attr.fetch(:values).each do |value|
-        attribute.score_values.build(label: value.fetch(:label), value: value.fetch(:value))
-      end
-    end
-
+    category = create_score_category(attributes.fetch(:category))
+    build_score_attributes(category, attributes.fetch(:attributes))
     category.save
     category
+  end
+
+  private
+  def self.create_score_category(name)
+    ScoreCategory.find_or_create_by(name: name)
+  end
+
+  def self.build_score_attributes(category, attributes)
+    attributes.each do |attr|
+      attribute = category.score_attributes.build(label: attr.fetch(:label))
+      build_score_values(attribute, attr.fetch(:values))
+    end
+  end
+
+  def self.build_score_values(score_attribute, value_attributes)
+    value_attributes.each do |value|
+      score_attribute.score_values.build(label: value.fetch(:label),
+                                         value: value.fetch(:value))
+    end
   end
 end
