193f6c5e8f9109ad51d42754c1206928602fe217
Joe Sak <joe@joesak.com>
2016-06-13 15:32:41 -0500

    Update CreateAuthentication concept

--- app/technovation/create_authentication.rb
+++ app/technovation/create_authentication.rb
@@ -1,7 +1,10 @@
 module CreateAuthentication
   def self.call(attrs)
-    auth = Authentication.new(attrs)
+    auth = Authentication.new(email: attrs.fetch(:email),
+                              password: attrs.fetch(:password),
+                              password_confirmation: attrs.fetch(:password_confirmation))
     GenerateToken.(auth, :auth_token)
+    auth.build_user(roles: attrs.fetch(:roles, []))
     auth.save
     auth
   end
