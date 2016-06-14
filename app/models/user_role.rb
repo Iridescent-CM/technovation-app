7be77c77490013014b298786da15b554be949235
Joe Sak <joe@joesak.com>
2016-06-14 09:23:41 -0500

    Fix missing roles join in expertises relationship on UserRole

--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -2,6 +2,6 @@ class UserRole < ActiveRecord::Base
   belongs_to :user
   belongs_to :role
 
-  has_many :judge_expertises, -> { where(role: { name: Role.names[:judge] }) }
-  has_many :expertises, -> { where(role: { name: Role.names[:judge] }) }, through: :judge_expertises
+  has_many :judge_expertises, -> { joins(:roles).where(role: { name: Role.names[:judge] }) }
+  has_many :expertises, -> { joins(:roles).where(role: { name: Role.names[:judge] }) }, through: :judge_expertises
 end
