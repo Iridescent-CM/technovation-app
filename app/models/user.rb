dce4ca7c9b32056b50b50299319e6d6bd458f403
Joe Sak <joe@joesak.com>
2016-06-14 13:47:58 -0500

    Protect submission_ids from a blank judge_role

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,15 +10,15 @@ class User < ActiveRecord::Base
 
   validates :authentication_id, presence: true
 
+  def submission_ids
+    !!judge_role && judge_role.submission_ids
+  end
+
   def judge_role_id
     !!judge_role && judge_role.id
   end
 
   def judge_role
     user_roles.judge
-  end
-
-  def submission_ids
-    judge_role.submission_ids
   end
 end
