e7bf42b8aab7cc9a080b16cf7302e687c7cb28c3
Joe Sak <joe@joesak.com>
2016-06-14 20:16:07 -0500

    Clean up test helper file

--- test/rails_helper.rb
+++ test/rails_helper.rb
@@ -1,14 +1,10 @@
 ENV["RAILS_ENV"] = "test"
+
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"
 require "minitest/rails/capybara"
 require 'database_cleaner'
-
-# To add Capybara feature tests add `gem "minitest-rails-capybara"`
-# to the test group in the Gemfile and uncomment the following:
-# require "minitest/rails/capybara"
-
 require "minitest/pride"
 
 Dir[Rails.root.join('test/support/**/*.rb')].each { |f| require f }
@@ -16,9 +12,7 @@ Dir[Rails.root.join('test/support/**/*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-  # Add more helper methods to be used by all tests here...
 end
 
 DatabaseCleaner.strategy = :transaction
