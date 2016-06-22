6a07fd37974c1a07810a706cf7caf38470920ca7
Joe Sak <joe@joesak.com>
2016-06-22 15:32:26 -0500

    Workaround rails web form bug for checkboxes to fix validation issue in judge profile

--- app/technovation/create_authentication.rb
+++ app/technovation/create_authentication.rb
@@ -1,6 +1,11 @@
 module CreateAuthentication
   def self.call(attrs)
-    auth = Authentication.new(attrs)
+    auth_attrs = attrs.deep_merge(judge_profile_attributes: {
+      expertise_ids: attrs.fetch(:judge_profile_attributes) { { } }
+                          .fetch(:expertise_ids) { [] }
+                          .reject(&:blank?)
+    })
+    auth = Authentication.new(auth_attrs)
     GenerateToken.(auth, :auth_token)
     auth.save
     auth
