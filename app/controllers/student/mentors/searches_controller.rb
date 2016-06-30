a64053e637234271f91c54269ca997760e9165b1
Joe Sak <joe@joesak.com>
2016-06-30 16:09:10 -0500

    Paginate the mentor search results

--- app/controllers/student/mentors/searches_controller.rb
+++ app/controllers/student/mentors/searches_controller.rb
@@ -4,7 +4,7 @@ module Student
       def show
         @search_filter = SearchFilter.new(params.fetch(:search_filter) { {} })
         @expertises = Expertise.all
-        @mentors = SearchMentors.(@search_filter)
+        @mentors = SearchMentors.(@search_filter).paginate(page: params[:page])
       end
     end
   end
