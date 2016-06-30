db12210f1e94c16f32d0d965ebb2aa1d0dd8ea6a
Joe Sak <joe@joesak.com>
2016-06-30 09:32:18 -0500

    Update the find a mentor page with more details and better UI

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -1,7 +1,11 @@
 class Mentor < Account
-  default_scope { joins(:mentor_profile) }
+  default_scope { joins(:mentor_profile).includes(mentor_profile: :expertises) }
 
   scope :by_expertise_ids, ->(ids) {
     joins(mentor_profile: :expertises).where("expertises.id IN (?)", ids)
   }
+
+  def expertise_names
+    mentor_profile.expertises.collect(&:name)
+  end
 end
