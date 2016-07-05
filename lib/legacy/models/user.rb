61c9e4eb08f5a0dbcca3406f238d4805298aba8e
Joe Sak <joe@joesak.com>
2016-07-05 13:37:37 -0500

    Add convenience methods to Legacy User model

--- lib/legacy/models/user.rb
+++ lib/legacy/models/user.rb
@@ -2,5 +2,10 @@ require './lib/legacy/models/legacy_model'
 
 module Legacy
   class User < LegacyModel
+    enum role: [:student, :mentor, :coach, :judge]
+
+    def parent_name
+      [parent_first_name, parent_last_name].join(" ")
+    end
   end
 end
