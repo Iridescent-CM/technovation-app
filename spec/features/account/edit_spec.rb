34cd3b460e2c5542d759879ed71664d039ff424f
Joe Sak <joe@joesak.com>
2016-07-06 16:30:59 -0500

    Add rspect tests for edit account

---
+++ spec/features/account/edit_spec.rb
@@ -0,0 +1,45 @@
+require "rails_helper"
+
+RSpec.feature "Edit account spec" do
+  before do
+    Season.create_current
+    student = FactoryGirl.create(:student, email: "original@email.com",
+                                           password: "secret1234")
+
+    sign_in(student)
+    click_link "My Account"
+  end
+
+  scenario "my account page shows account info" do
+    expect(page).to have_content("original@email.com")
+  end
+
+  scenario "edit login info" do
+    click_link "Edit"
+    fill_in "Email", with: "something@else.com"
+    fill_in "Existing password", with: "secret1234"
+
+    click_button "Save"
+
+    expect(current_path).to eq(account_path)
+    expect(page).to have_content("something@else.com")
+  end
+
+  scenario "attempt to edit with wrong existing password" do
+    click_link "Edit"
+    fill_in "Email", with: "something@else.com"
+
+    fill_in "Existing password", with: "wrong"
+    click_button "Save"
+    expect(page).to have_css(".error", text: I18n.translate("activerecord.errors.models.account.attributes.existing_password.invalid"))
+  end
+
+  scenario "attempt to edit without existing password" do
+    click_link "Edit"
+    fill_in "Email", with: "something@else.com"
+
+    fill_in "Existing password", with: ""
+    click_button "Save"
+    expect(page).to have_css(".error", text: I18n.translate("activerecord.errors.models.account.attributes.existing_password.invalid"))
+  end
+end
