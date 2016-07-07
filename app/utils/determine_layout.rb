0fc2377ea64d4e59ebef5ca7dfe2c1c2f80aa3ed
Joe Sak <joe@joesak.com>
2016-07-06 23:12:00 -0500

    Avoid checking the database for an empty auth token

--- app/utils/determine_layout.rb
+++ app/utils/determine_layout.rb
@@ -1,7 +1,9 @@
 module DetermineLayout
   def self.call(token)
-    %w{student mentor coach judge admin}.detect { |name|
-      "#{name}_account".camelize.constantize.find_with_token(token).authenticated?
-    }
+    unless token.blank?
+      %w{student mentor coach judge admin}.detect { |name|
+        "#{name}_account".camelize.constantize.find_with_token(token).authenticated?
+      }
+    end
   end
 end
