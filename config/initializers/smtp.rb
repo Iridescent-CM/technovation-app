15b1f4fd3a560430d892da102955fb8251f846ee
Joe Sak <joe@joesak.com>
2016-07-07 09:18:09 -0500

    Add smtp config

---
+++ config/initializers/smtp.rb
@@ -0,0 +1,14 @@
+unless Rails.env.development? or Rails.env.test?
+  Rails.application.configure do
+    config.action_mailer.delivery_method = :smtp
+
+    config.action_mailer.smtp_settings = {
+      address: ENV.fetch("MAIL_ADDRESS"),
+      port: ENV.fetch("MAIL_PORT"),
+      user_name: ENV.fetch("MAIL_USER"),
+      password: ENV.fetch("MAIL_PASSWORD"),
+      authentication: :plain,
+      enable_starttls_auto: true,
+    }
+  end
+end
