183ff80878588e9c6adafa6fa36dcd7264343ca3
Joe Sak <joe@joesak.com>
2016-07-06 20:08:21 -0500

    Add sidekiq for future production active job'ing

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 
 gem 'bcrypt', '~> 3.1.7'
+gem 'sidekiq', '~> 4.1.1'
 
 gem 'jquery-rails'
 gem 'jquery-turbolinks'
--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ Bundler.require(*Rails.groups)
 
 module TechnovationApp
   class Application < Rails::Application
+    config.active_job.queue_adapter = ENV.fetch('ACTIVE_JOB_BACKEND')
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
