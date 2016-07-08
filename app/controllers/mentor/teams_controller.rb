2bd82b02e2456f5f5b164b26ad075e1167bcae2b
Joe Sak <joe@joesak.com>
2016-07-07 19:55:52 -0500

    Add a teams page for mentors to access their teams

--- app/controllers/mentor/teams_controller.rb
+++ app/controllers/mentor/teams_controller.rb
@@ -1,5 +1,9 @@
 module Mentor
   class TeamsController < MentorController
+    def index
+      @teams = current_mentor.teams
+    end
+
     def show
       @team = current_mentor.teams.find(params.fetch(:id))
       render 'student/teams/show'
