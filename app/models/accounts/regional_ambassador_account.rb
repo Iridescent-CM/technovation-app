be68f507a4fd6f2e315d59c3bc505a9540bebe39
Joe Sak <joe@joesak.com>
2016-07-20 20:32:41 -0500

    Add pending/approved/rejected scopes for regional ambassador accounts

--- app/models/accounts/regional_ambassador_account.rb
+++ app/models/accounts/regional_ambassador_account.rb
@@ -1,6 +1,10 @@
 class RegionalAmbassadorAccount < Account
   default_scope { joins(:regional_ambassador_profile) }
 
+  scope :pending, -> { where("regional_ambassador_profiles.status = ?", RegionalAmbassadorProfile.statuses[:pending]) }
+  scope :approved, -> { where("regional_ambassador_profiles.status = ?", RegionalAmbassadorProfile.statuses[:approved]) }
+  scope :rejected, -> { where("regional_ambassador_profiles.status = ?", RegionalAmbassadorProfile.statuses[:rejected]) }
+
   after_initialize :build_regional_ambassador_profile,
     if: -> { regional_ambassador_profile.blank? }
   after_create :notify_admins
