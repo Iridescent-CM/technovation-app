6ff8c0af95bef7cae9c4a2365659135315964540
Joe Sak <joe@joesak.com>
2016-07-23 13:28:45 -0500

    Add profile details to my account view

--- app/models/profiles/guidance_profile.rb
+++ app/models/profiles/guidance_profile.rb
@@ -5,4 +5,8 @@ class GuidanceProfile < ActiveRecord::Base
   has_many :expertises, through: :guidance_profile_expertises
 
   validates :school_company_name, :job_title, presence: true
+
+  def expertise_names
+    expertises.flat_map(&:name)
+  end
 end
