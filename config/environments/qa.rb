1b177b6ae21aa8d85a8b75a6b739666aee2bad3f
Joe Sak <joe@joesak.com>
2016-07-26 10:56:00 -0500

    Remove dupe env var reference

--- config/environments/qa.rb
+++ config/environments/qa.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: ENV.fetch("HOSTNAME") }
+  config.action_mailer.default_url_options = { host: ENV.fetch("HOST_DOMAIN") }
 end
