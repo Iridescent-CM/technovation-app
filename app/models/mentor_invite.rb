f49aaea296e1c985cfc00eacf2924b81bbe89368
Joe Sak <joe@joesak.com>
2016-08-09 12:24:26 -0500

    Move .accept! to MentorInvite directly

--- app/models/mentor_invite.rb
+++ app/models/mentor_invite.rb
@@ -1,6 +1,18 @@
 class MentorInvite < TeamMemberInvite
   belongs_to :invitee, class_name: "MentorAccount"
 
+  def self.accept!(token, email = nil)
+    if invite = where("invite_token = ? OR invitee_email = ?", token, email).first
+      invite.update_attributes({
+        status: :accepted,
+        invitee: Account.find_by(email: email) || invite.invitee,
+      })
+      invite
+    else
+      false
+    end
+  end
+
   def after_accept
     team.add_mentor(invitee)
   end
