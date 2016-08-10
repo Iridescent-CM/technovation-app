e7d8537652a617cfbafb840501f65f3ae2dd314c
Joe Sak <joe@joesak.com>
2016-08-10 14:16:50 -0500

    Void parental consents only on true UPDATEs of student profile parent email

--- app/models/profiles/student_profile.rb
+++ app/models/profiles/student_profile.rb
@@ -3,9 +3,10 @@ class StudentProfile < ActiveRecord::Base
 
   belongs_to :student_account, foreign_key: :account_id
 
-  after_save :resend_parental_consent,
-             :void_parental_consent,
-    if: :persisted_parent_guardian_email_changed?
+  after_validation :resend_parental_consent,
+                   :void_parental_consent,
+    on: :update,
+    if: :parent_guardian_email_changed?
 
   validates :parent_guardian_email,
             :parent_guardian_name,
@@ -16,10 +17,6 @@ class StudentProfile < ActiveRecord::Base
   validates :parent_guardian_email, email: true
 
   private
-  def persisted_parent_guardian_email_changed?
-    persisted? && parent_guardian_email_changed?
-  end
-
   def resend_parental_consent
     ParentMailer.consent_notice(student_account).deliver_later
   end
