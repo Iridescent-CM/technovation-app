26c4d120e13a406718064c018e9080ad49e6d501
Joe Sak <joe@joesak.com>
2016-08-10 15:27:18 -0500

    Wire the re-send parental consent link. #463

--- app/controllers/student/parental_consent_notices_controller.rb
+++ app/controllers/student/parental_consent_notices_controller.rb
@@ -2,6 +2,7 @@ class Student::ParentalConsentNoticesController < StudentController
   def create
     ParentMailer.consent_notice(current_student.parent_guardian_email,
                                 current_student.consent_token).deliver_later
+    flash[:success] = t("controllers.student.parental_consent_notices.create.success")
     redirect_to :back
   end
 end
