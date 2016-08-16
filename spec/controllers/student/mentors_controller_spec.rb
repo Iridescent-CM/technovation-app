5557d7f7d9e46f292e9e9e39b1e4239d5eba3944
Joe Sak <joe@joesak.com>
2016-08-16 08:48:58 -0500

    Show list of teams on mentor page for student find a mentor results. #489

---
+++ spec/controllers/student/mentors_controller_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe Student::MentorsController do
+  describe "GET #show" do
+    it "assigns the mentor" do
+      mentor = FactoryGirl.create(:mentor)
+      student = FactoryGirl.create(:student)
+
+      sign_in(student)
+      get :show, id: mentor.id
+
+      expect(assigns[:mentor]).to eq(mentor)
+    end
+  end
+end
