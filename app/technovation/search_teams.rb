61124a0380fd350d256261d7a560139bf65493fc
Joe Sak <joe@joesak.com>
2016-08-17 15:28:21 -0500

    Optimize the search query for SearchTeams. #505

--- app/technovation/search_teams.rb
+++ app/technovation/search_teams.rb
@@ -1,7 +1,11 @@
 module SearchTeams
   def self.call(filter)
     teams = if filter.nearby.present?
-              StudentAccount.near(filter.nearby, 50).collect(&:team).compact.uniq
+              student_ids = StudentAccount.near(filter.nearby, 50).collect(&:id)
+
+              Team.current
+                  .joins(:memberships)
+                  .where("memberships.member_id IN (?)", student_ids)
             else
               Team.current
             end
