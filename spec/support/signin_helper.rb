db071782c4ee849a9221ab999f819978dfee13ef
Joe Sak <joe@joesak.com>
2016-08-17 14:54:50 -0500

    Provide password fallback in signin helper

--- spec/support/signin_helper.rb
+++ spec/support/signin_helper.rb
@@ -2,7 +2,7 @@ module SigninHelper
   def sign_in(account)
     visit signin_path
     fill_in 'Email', with: account.email
-    fill_in 'Password', with: account.password
+    fill_in 'Password', with: account.password || "secret1234"
     click_button 'Sign in'
   end
 end
