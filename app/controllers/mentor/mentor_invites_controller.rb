442ce9bf2d0db5b364d4fc84a2a88178564c5f3d
Joe Sak <joe@joesak.com>
2016-08-25 17:49:33 -0500

    Cleanup mentor invite code

--- app/controllers/mentor/mentor_invites_controller.rb
+++ app/controllers/mentor/mentor_invites_controller.rb
@@ -6,22 +6,11 @@ module Mentor
 
     def update
       invite = current_mentor.mentor_invites.pending.find_by(invite_token: params.fetch(:id))
-
-      if invite_params[:status] == "accepted" and not invite.invitee.can_join_a_team?
-        decline_invitation(invite)
-      elsif invite.update_attributes(invite_params)
-        redirect_based_on_status(invite)
-      else
-        redirect_to :back, alert: t("controllers.application.general_error")
-      end
+      invite.update_attributes(invite_params)
+      redirect_based_on_status(invite)
     end
 
     private
-    def decline_invitation(invite)
-      invite.declined!
-      redirect_to mentor_dashboard_path, alert: t("controllers.team_member_invites.update.already_on_team")
-    end
-
     def redirect_based_on_status(invite)
       if invite.accepted?
         @path = mentor_team_path(invite.team)
@@ -35,9 +24,7 @@ module Mentor
     end
 
     def invite_params
-      params.require(:team_member_invite).permit(:status).tap do |p|
-        p[:invitee_id] == FindAccount.(cookies.fetch(:auth_token) { "" })
-      end
+      params.require(:team_member_invite).permit(:status)
     end
   end
 end
--- spec/controllers/mentor/mentor_invites_controller_spec.rb
+++
@@ -1,4 +0,0 @@
-require "rails_helper"
-
-RSpec.describe Mentor::MentorInvitesController do
-end
