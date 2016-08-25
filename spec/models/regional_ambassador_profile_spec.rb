08db925a5476a10d28985a2cc060837601947ae7
Joe Sak <joe@joesak.com>
2016-08-25 09:12:43 -0500

    Update RA declined email subject #550

--- spec/models/regional_ambassador_profile_spec.rb
+++ spec/models/regional_ambassador_profile_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe RegionalAmbassadorProfile do
   it "emails on decline" do
     ambassador = FactoryGirl.create(:regional_ambassador)
     ambassador.declined!
-    expect(ActionMailer::Base.deliveries.last.subject).to eq("Your Regional Ambassador application has been declined.")
+    expect(ActionMailer::Base.deliveries.last.subject).to eq("Regional Ambassador Request Follow-up")
   end
 end
