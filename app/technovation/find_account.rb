e30176582ddd6e3cc154e95db61ab9ade08aaaf7
Joe Sak <joe@joesak.com>
2016-08-25 11:15:54 -0500

    Wash, rinse, repeat: Fix FindAccount bug

--- app/technovation/find_account.rb
+++ app/technovation/find_account.rb
@@ -6,7 +6,7 @@ module FindAccount
   def self.authenticate(profile, cookies, callbacks = {})
     if current(profile, cookies).authenticated?
       true
-    elsif account = call(cookies.fetch(:auth_token) { "" }).authenticated?
+    elsif account = call(cookies.fetch(:auth_token) { "" }) && account.authenticated?
       callbacks.fetch(:unauthorized) { ->(*) { } }.call(account.type_name)
     else
       callbacks.fetch(:unauthenticated) { -> { } }.call
