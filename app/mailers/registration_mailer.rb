acb2e656bee029b935c424aa7a91f5946730e8a0
Joe Sak <joe@joesak.com>
2016-08-30 13:16:37 -0500

    Add director info to mentor welcome email. #413

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,14 +1,15 @@
 class RegistrationMailer < ApplicationMailer
   def welcome_mentor(mentor)
     @first_name = mentor.first_name
-    @contact_name = "[CONTACT NAME]"
-    @contact_position = "[CONTACT POSITION]"
-    @contact_email = "[CONTACT EMAIL]"
-    @contact_first_name = "[CONTACT FIRST NAME]"
+    @contact_name = "Allie Glotfelty"
+    @contact_position = "Events and Engagement Director"
+    @contact_email = "allie@technovationchallenge.org"
+    @contact_first_name = "Allie"
     @edit_account_url = edit_mentor_account_url
     @upload_photo_url = mentor_account_url
     @edit_bio_url = edit_mentor_account_url(anchor: "account-profile-details")
 
-    mail to: mentor.email
+    mail to: mentor.email,
+         from: @contact_email
   end
 end
