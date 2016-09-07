fcf9413c4df0e05b1fc4aef4bb35594d9c933c53
Joe Sak <joe@joesak.com>
2016-09-07 10:58:25 -0500

    Fix n/mdash references #413

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -3,6 +3,6 @@ class AdminMailer < ApplicationMailer
     @name = ambassador.full_name
     @url = admin_pending_regional_ambassadors_url
     mail to: "info@technovationchallenge.org",
-         subject: "RA Application &endash; Pending Approval".html_safe
+         subject: "RA Application &ndash; Pending Approval".html_safe
   end
 end
