e7bb1bddffa4b5a13126cb2e1cb44f9c6dc82344
Joe Sak <joe@joesak.com>
2016-09-07 09:45:30 -0500

    Update password reset mailer #411

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -1,6 +1,7 @@
 class AccountMailer < ApplicationMailer
 
   def password_reset(account)
+    @first_name = account.first_name
     @url = new_password_url(token: account.password_reset_token)
     mail to: account.email
   end
