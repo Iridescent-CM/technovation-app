8c59d76a84e11b97b2b665da39dd27f27fc9cb71
Joe Sak <joe@joesak.com>
2016-09-07 09:50:55 -0500

    Update admin email for RA applications #413

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,6 +2,7 @@ class AdminMailer < ApplicationMailer
   def pending_regional_ambassador(ambassador)
     @name = ambassador.full_name
     @url = admin_pending_regional_ambassadors_url
-    mail to: "info@technovationchallenge.org"
+    mail to: "info@technovationchallenge.org",
+         subject: "RA Application &endash; Pending Approval".html_safe
   end
 end
