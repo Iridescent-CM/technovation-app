c3a524fe65ad2272b0655acc4c54eb19aebeaac0
Joe Sak <joe@joesak.com>
2016-09-08 10:20:43 -0500

    Remove namespace option

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 if Rails.env.production? or Rails.env.qa?
   Sidekiq.configure_client do |config|
-    config.redis = { size: 3, url: ENV["REDIS_URL"], namespace: "technovation" }
+    config.redis = { size: 3, url: ENV["REDIS_URL"] }
   end
 
   Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDIS_URL'], namespace: "technovation" }
+    config.redis = { url: ENV['REDIS_URL'] }
   end
 end
