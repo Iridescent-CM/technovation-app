02c2248798ba6c3f0d46e1a1c908f6ea7d40e82e
Joe Sak <joe@joesak.com>
2016-09-08 10:08:53 -0500

    Try sidekiq config from https://bryanrite.com/heroku-puma-redis-sidekiq-and-connection-limits/ I hate myself

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,10 @@
 if Rails.env.production? or Rails.env.qa?
   Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['REDIS_URL'], size: 2 }
+    config.redis = { size: 3, url: ENV["REDIS_URL"], namespace: "technovation" }
   end
 
   Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDIS_URL'], size: 22 }
+    config.redis = { url: ENV['REDIS_URL'], namespace: "technovation" }
 
     Rails.application.config.after_initialize do
       Rails.logger.info("DB Connection Pool size for Sidekiq Server before disconnect is: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
@@ -13,8 +13,7 @@ if Rails.env.production? or Rails.env.qa?
       ActiveSupport.on_load(:active_record) do
         config = Rails.application.config.database_configuration[Rails.env]
         config['reaping_frequency'] = ENV['DATABASE_REAP_FREQ'] || 10 # seconds
-        # config['pool'] = ENV['WORKER_DB_POOL_SIZE'] || Sidekiq.options[:concurrency]
-        config['pool'] = 22
+        config['pool'] = Sidekiq.options[:concurrency]
         ActiveRecord::Base.establish_connection(config)
 
         Rails.logger.info("DB Connection Pool size for Sidekiq Server is now: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
