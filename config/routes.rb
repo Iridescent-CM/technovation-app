e15719f51a65ad616d4619e4890a4b723a373c69
Joe Sak <joe@joesak.com>
2016-09-09 16:27:49 -0500

    Add sidekiq web back

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
-  # require 'sidekiq/web'
-  # mount Sidekiq::Web => '/sidekiq'
+  mount Sidekiq::Web => '/sidekiq'
 
   namespace :student do
     get :signup, to: 'signups#new'
