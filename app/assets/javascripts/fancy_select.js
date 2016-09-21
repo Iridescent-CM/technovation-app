dd11cbe7cdfb882018759d282fb5d1c3440b30bf
Joe Sak <joe@joesak.com>
2016-09-21 14:15:04 -0400

    Fix fancy select to work with dynamic params

--- app/assets/javascripts/fancy_select.js
+++ app/assets/javascripts/fancy_select.js
@@ -7,7 +7,7 @@
 
   function initFancySelect() {
     $(document).on('change', 'select[data-reload="true"]', function(e) {
-      location.href = location.pathname + $.query.set("per_page", e.target.value).toString();
+      location.href = location.pathname + $.query.set($(e.target).data('param'), e.target.value).toString();
     });
   }
 })();
