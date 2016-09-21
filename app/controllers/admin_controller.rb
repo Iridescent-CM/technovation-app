9776fda41212748aa46fd0ab37765f17cb696c36
Joe Sak <joe@joesak.com>
2016-09-20 20:58:33 -0400

    Fix admin vulnerability

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,12 +1,5 @@
 class AdminController < ApplicationController
-  before_filter :authenticate_admin!
+  include Authenticated
 
   layout 'admin'
-
-  private
-  def authenticate_admin!
-    FindAccount.authenticate(:admin, cookies, failure: -> {
-      save_redirected_path && go_to_signin("admin")
-    })
-  end
 end
