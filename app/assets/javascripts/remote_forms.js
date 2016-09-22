20fc8448c5f35c990f706df1be9b853198925741
Joe Sak <joe@joesak.com>
2016-09-22 16:12:05 -0400

    Fix that ajax message :)

--- app/assets/javascripts/remote_forms.js
+++ app/assets/javascripts/remote_forms.js
@@ -3,13 +3,13 @@
 
   function enableRemoteCheckboxes() {
     $('form[data-remote="true"]').find('input[type="checkbox"]').on('change', function(e) {
-      var ajaxMsgElem = $('<span />').attr('id', 'currentAjaxMsg');
-      $(e.target).closest('form').append(ajaxMsgElem);
+      var ajaxMsgElem = $('<div />').attr('class', 'currentAjaxMsg');
+      $('body').prepend(ajaxMsgElem);
       $(e.target).closest('form').submit();
     });
 
     $(document).on('ajax:success', function() {
-      $('#currentAjaxMsg').addClass('alert alert-info')
+      $('.currentAjaxMsg').addClass('alert alert-info')
                           .text('Saved!')
                           .fadeIn('easein')
                           .delay(500)
