0270afae49be7dc9e1792ea9dd227e8bec9ed26e
Joe Sak <joe@joesak.com>
2016-09-22 15:46:15 -0400

    Teams can toggle their openness to mentor and student join requests #539

--- db/schema.rb
+++ db/schema.rb
@@ -315,13 +315,15 @@ ActiveRecord::Schema.define(version: 20160927145316) do
   add_index "team_member_invites", ["status"], name: "index_team_member_invites_on_status", using: :btree
 
   create_table "teams", force: :cascade do |t|
-    t.string   "name",        null: false
-    t.text     "description", null: false
-    t.integer  "division_id", null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "name",                                      null: false
+    t.text     "description",                               null: false
+    t.integer  "division_id",                               null: false
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
     t.string   "team_photo"
     t.string   "friendly_id"
+    t.boolean  "accepting_student_requests", default: true, null: false
+    t.boolean  "accepting_mentor_requests",  default: true, null: false
   end
 
   add_index "teams", ["division_id"], name: "index_teams_on_division_id", using: :btree
