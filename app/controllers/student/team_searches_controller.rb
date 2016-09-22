710d7075ec388a7b289d3ab6646f0aca9bf56f83
Joe Sak <joe@joesak.com>
2016-09-22 12:52:30 -0400

    Add 'anywhere' search to Team searches #562

--- app/controllers/student/team_searches_controller.rb
+++ app/controllers/student/team_searches_controller.rb
@@ -1,8 +1,11 @@
 module Student
   class TeamSearchesController < StudentController
     def new
+      params[:nearby] = current_student.address_details if params[:nearby].blank?
+
       @search_filter = SearchFilter.new({
-        nearby: current_student.address_details,
+        nearby: params.fetch(:nearby),
+        user: current_student,
         spot_available: true,
         page: params[:page],
       })
