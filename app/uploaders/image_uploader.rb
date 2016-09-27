ddd7c515c373afac531ea5ba5eb3619be1a49891
Joe Sak <joe@joesak.com>
2016-09-27 11:59:23 -0500

    Revert "Use the placeholder avatar for account photo fallback"
    This reverts commit 72d42f54d4c2cb76c5265171be5fc4c16a9ff7bf.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -19,7 +19,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   #
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
-    ActionController::Base.helpers.asset_path("placeholders/placeholder-avatar.svg")
+    "https://placekitten.com/g/#{default_url_endpoint_size}"
   end
 
   # Process files as they are uploaded:
