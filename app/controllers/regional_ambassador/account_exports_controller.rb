c9f0950928d0e6b3918cf28b35372637e504deea
Joe Sak <joe@joesak.com>
2016-09-27 10:10:29 -0500

    Adjust I18n paths

--- app/controllers/regional_ambassador/account_exports_controller.rb
+++ app/controllers/regional_ambassador/account_exports_controller.rb
@@ -3,8 +3,7 @@ module RegionalAmbassador
     def create
       ExportRegionalAccountsJob.perform_later(current_ambassador)
 
-      redirect_to regional_ambassador_accounts_path,
-        success: t("controllers.regional_ambassador.account_exports.create.success")
+      redirect_to regional_ambassador_accounts_path, success: t("controllers.exports.create.success")
     end
   end
 end
