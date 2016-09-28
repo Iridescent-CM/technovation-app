23403fc8d0d850c0bba478e38210c26d5618bd19
Joe Sak <joe@joesak.com>
2016-09-27 21:41:18 -0500

    Fix created_at reference for admin reg report. It should be on season registration

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -4,10 +4,10 @@ module Admin
       params[:days] = 7 if params[:days].blank?
       params[:days] = params[:days].to_i
 
-      @students = StudentAccount.current.where("accounts.created_at > ?", params[:days].days.ago)
-      @mentors = MentorAccount.current.where("accounts.created_at > ?", params[:days].days.ago)
-      @ambassadors = RegionalAmbassadorAccount.current.where("accounts.created_at > ?", params[:days].days.ago)
-      @judges = JudgeAccount.current.where("accounts.created_at > ?", params[:days].days.ago)
+      @students = StudentAccount.current.where("season_registrations.created_at > ?", params[:days].days.ago)
+      @mentors = MentorAccount.current.where("season_registrations.created_at > ?", params[:days].days.ago)
+      @ambassadors = RegionalAmbassadorAccount.current.where("season_registrations.created_at > ?", params[:days].days.ago)
+      @judges = JudgeAccount.current.where("season_registrations.created_at > ?", params[:days].days.ago)
     end
   end
 end
