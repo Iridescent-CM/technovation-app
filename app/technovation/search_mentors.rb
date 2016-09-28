c2c5180a8b6e4a6b104c801e987644172b14bf52
Joe Sak <joe@joesak.com>
2016-09-28 13:30:01 -0500

    Hot fix SearchMentors, the needs-team logic was backwards

--- app/technovation/search_mentors.rb
+++ app/technovation/search_mentors.rb
@@ -18,13 +18,13 @@ module SearchMentors
         (SELECT DISTINCT(member_id) FROM memberships
                                     WHERE memberships.member_type = 'MentorAccount'
                                     AND memberships.joinable_type = 'Team'
-                                    AND memberships.joinable_id NOT IN
+                                    AND memberships.joinable_id IN
 
-          (SELECT DISTINCT(id) FROM teams WHERE teams.id NOT IN
+          (SELECT DISTINCT(id) FROM teams WHERE teams.id IN
 
             (SELECT DISTINCT(registerable_id) FROM season_registrations
                                               WHERE season_registrations.registerable_type = 'Team'
-                                              AND season_registrations.season_id != ?)))", Season.current.year)
+                                              AND season_registrations.season_id = ?)))", Season.current.id)
     end
 
     mentors.searchable
