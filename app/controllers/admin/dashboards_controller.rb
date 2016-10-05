f085b8cd95b5457e4eded08b791921139356b09a
Joe Sak <joe@joesak.com>
2016-10-05 12:20:11 -0500

    Update admin dashboard info to show cleared students/mentor counts

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -4,11 +4,31 @@ module Admin
       params[:days] = 7 if params[:days].blank?
       params[:days] = params[:days].to_i
 
-      @accounts = Account.current.where("season_registrations.created_at > ?", params[:days].days.ago)
-      @students = @accounts.where(type: "StudentAccount")
-      @mentors = @accounts.where(type: "MentorAccount")
-      @ambassadors = @accounts.where(type: "RegionalAmbassadorAccount")
-      @judges = @accounts.where(type: "JudgeAccount")
+      accounts = Account.current.where("season_registrations.created_at > ?", params[:days].days.ago)
+
+      @students = accounts.where(type: "StudentAccount")
+      @permitted_students = StudentAccount.current
+        .joins(:parental_consent)
+        .where("parental_consents.created_at > ?", params[:days].days.ago)
+
+      @mentors = accounts.where(type: "MentorAccount")
+      @cleared_mentors = MentorAccount.current
+        .joins(:consent_waiver)
+        .includes(:background_check)
+        .where(
+          "(accounts.country = ? AND background_checks.status = ? AND background_checks.created_at > ?)
+          OR
+          (accounts.country != ? AND consent_waivers.created_at > ?)",
+          "US",
+          BackgroundCheck.statuses[:clear],
+          params[:days].days.ago,
+          "US",
+          params[:days].days.ago
+        )
+
+      @ambassadors = accounts.where(type: "RegionalAmbassadorAccount")
+
+      @judges = accounts.where(type: "JudgeAccount")
 
       @teams = Team.current.where("season_registrations.created_at > ?", params[:days].days.ago)
     end
