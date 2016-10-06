494ef462dba7f01ae0b1d35edc7c54bdfd3c8453
Joe Sak <joe@joesak.com>
2016-10-06 08:15:33 -0500

    Search only adminaccounts, 100 per page

--- app/technovation/admin/search_accounts.rb
+++ app/technovation/admin/search_accounts.rb
@@ -1,8 +1,6 @@
 module Admin
   class SearchAccounts
     def self.call(params)
-      params[:per_page] = 25 if params[:per_page].blank?
-      params[:page] = 1 if params[:page].blank?
       params[:type] = "All" if params[:type].blank?
       params[:season] = Season.current.year if params[:season].blank?
 
@@ -14,7 +12,8 @@ module Admin
         client = Swiftype::Client.new
         results = client.search(ENV.fetch('SWIFTYPE_ENGINE_SLUG'),
                                 params[:text],
-                                { per_page: params[:per_page], page: params[:page] })
+                                document_types: ["adminaccounts"],
+                                per_page: 100)
         accounts = accounts.where(id: results['adminaccounts'].collect { |h| h['external_id'] })
       end
 
