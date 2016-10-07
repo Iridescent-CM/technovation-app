0050046bb2be6bb514d5bb0009e86437a10d5a4b
Joe Sak <joe@joesak.com>
2016-10-07 12:17:18 -0500

    Flash now on photo confirmation

--- app/controllers/concerns/profile_image_upload_confirmation_controller.rb
+++ app/controllers/concerns/profile_image_upload_confirmation_controller.rb
@@ -3,7 +3,7 @@ module ProfileImageUploadConfirmationController
 
   def show
     ProcessUploadJob.perform_later(current_account, "profile_image", params.fetch(:key))
-    flash[:success] = t("controllers.accounts.show.image_processing")
+    flash.now[:success] = t("controllers.accounts.show.image_processing")
     @unprocessed_photo_url = "//s3.amazonaws.com/#{params[:bucket]}/#{params[:key]}"
     render template: "profile_image_upload_confirmations/show"
   end
