b8f05c3c336eb5b656963014fcfb370d4e947e46
Joe Sak <joe@joesak.com>
2016-10-07 12:23:51 -0500

    Remove training url from welcome mentor email #710

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -3,7 +3,6 @@ class RegistrationMailer < ApplicationMailer
     @first_name = mentor.first_name
     @infographic_url = "https://infograph.venngage.com/infograph/publish/02844b99-420b-4016-8c13-1426fc29fbe7"
     @edit_account_url = edit_mentor_account_url
-    @training_url = "http://www.iridescentacademy.org/lms/"
     @root_url = root_url
 
     mail to: mentor.email,
