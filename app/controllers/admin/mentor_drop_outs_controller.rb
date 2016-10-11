0220d728c839c9131178cc73a3d7951b7b93d2fb
Joe Sak <joe@joesak.com>
2016-10-11 15:56:15 -0500

    Add mentor drop out feature to admin/accounts/show

---
+++ app/controllers/admin/mentor_drop_outs_controller.rb
@@ -0,0 +1,10 @@
+module Admin
+  class MentorDropOutsController < AdminController
+    def create
+      mentor = MentorAccount.find(params[:id])
+      DropOutMentorJob.perform_later(mentor)
+      redirect_to admin_accounts_path,
+        success: "#{mentor.full_name} is being dropped out of Season #{Season.current.year}"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -94,6 +94,8 @@ Rails.application.routes.draw do
     resources :background_checks, only: :index
     resources :background_check_sweeps, only: :create
 
+    resource :mentor_drop_out, only: :create
+
     resources :exports, only: :create
   end
 
