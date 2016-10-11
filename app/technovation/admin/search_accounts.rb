7c3bff2950747ff13d0073885a0da23c34581b77
Joe Sak <joe@joesak.com>
2016-10-11 16:14:19 -0500

    Add advanced student querying for admins

--- app/technovation/admin/search_accounts.rb
+++ app/technovation/admin/search_accounts.rb
@@ -2,11 +2,18 @@ module Admin
   class SearchAccounts
     def self.call(params)
       params[:type] = "All" if params[:type].blank?
+      params[:parental_consent_status] = "All" if params[:parental_consent_status].blank?
       params[:season] = Season.current.year if params[:season].blank?
 
-      accounts = Account.joins(season_registrations: :season)
-                        .where("season_registrations.season_id = ?",
-                               Season.find_by(year: params[:season]))
+      klass = if params[:type] == "All"
+                Account
+              else
+                "#{params[:type]}Account".constantize
+              end
+
+      accounts = klass.joins(season_registrations: :season)
+                      .where("season_registrations.season_id = ?",
+                             Season.find_by(year: params[:season]))
 
       unless params[:text].blank?
         client = Swiftype::Client.new
@@ -17,12 +24,22 @@ module Admin
         accounts = accounts.where(id: results['adminaccounts'].collect { |h| h['external_id'] })
       end
 
-      case params[:type]
-      when "All"
-        accounts
-      else
-        accounts.where(type: "#{params[:type]}Account")
+      if params[:type] == "Student"
+        case params[:parental_consent_status]
+        when "Signed"
+          accounts = accounts.joins(:parental_consent)
+        when "Sent"
+          accounts = accounts.includes(:parental_consent)
+                             .references(:parental_consents)
+                             .where("parental_consents.id IS NULL AND student_profiles.parent_guardian_email IS NOT NULL")
+        when "Not Configured"
+          accounts = accounts.includes(:parental_consent)
+                             .references(:parental_consents)
+                             .where("parental_consents.id IS NULL AND student_profiles.parent_guardian_email IS NULL")
+        end
       end
+
+      accounts
     end
   end
 end
