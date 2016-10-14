412a8fc82ac9237834abf7e74303c97db3da283f
Joe Sak <joe@joesak.com>
2016-10-14 14:33:40 -0500

    Get  password at first step of activating emails for signup. Ensure it works for invited students too

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success
   add_flash_types :error
+  add_flash_types :error
   force_ssl if: :ssl_configured?
 
   layout :determine_layout
