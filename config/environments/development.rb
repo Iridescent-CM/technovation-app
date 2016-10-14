8cd45f670435b44153df81edf05acb2280c1c707
Joe Sak <joe@joesak.com>
2016-10-14 10:18:55 -0500

    Fix email settings for header branding

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,8 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_controller.asset_host = ENV.fetch("HOST_DOMAIN")
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
