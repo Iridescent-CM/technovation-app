0523b5da1961dc3134544a507734f6e53efad308
Joe Sak <joe@joesak.com>
2016-10-14 10:34:49 -0500

    Fix dev asset host settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,8 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :letter_opener
-  config.action_controller.asset_host = ENV.fetch("HOST_DOMAIN")
+  config.action_controller.asset_host = "http://#{ENV.fetch('HOST_DOMAIN')}"
+  config.action_mailer.asset_host = "http://#{ENV.fetch("HOST_DOMAIN")}"
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
