04ebd3ec668582b6e7db4e42c5327b8c3802f002
Joe Sak <joe@joesak.com>
2016-10-19 10:43:21 -0500

    Update the interruption page

--- spec/features/student/invalid_data_spec.rb
+++ spec/features/student/invalid_data_spec.rb
@@ -6,15 +6,15 @@ RSpec.feature "Students with invalid data" do
     student.student_profile.update_column(:parent_guardian_email, "bad-email")
 
     sign_in(student)
-    expect(page).to have_content "Hey, wait a moment! We found a problem with your profile. Your parent/guardian email appears to be invalid."
+    expect(page).to have_content "Hey, wait a moment! We detected a problem with your profile. Your parent/guardian email appears to be invalid."
     expect(page).to have_link "Update your parent/guardian email address", href: edit_student_account_path(anchor: "account-profile-details")
 
     within('.navigation__links-list') { click_link "Create a team" }
-    expect(page).to have_content "Hey, wait a moment! We found a problem with your profile. Your parent/guardian email appears to be invalid."
+    expect(page).to have_content "Hey, wait a moment! We detected a problem with your profile. Your parent/guardian email appears to be invalid."
     expect(page).to have_link "Update your parent/guardian email address", href: edit_student_account_path(anchor: "account-profile-details")
 
     within('.navigation__links-list') { click_link "Join a team" }
-    expect(page).to have_content "Hey, wait a moment! We found a problem with your profile. Your parent/guardian email appears to be invalid."
+    expect(page).to have_content "Hey, wait a moment! We detected a problem with your profile. Your parent/guardian email appears to be invalid."
     expect(page).to have_link "Update your parent/guardian email address", href: edit_student_account_path(anchor: "account-profile-details")
   end
 end
