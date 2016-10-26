8b56a14b5447aa5eedcbb8da4b24e18c396e6ef6
Joe Sak <joe@joesak.com>
2016-10-26 18:11:17 -0500

    Fix bg check sweeps due to this new dispute! status

--- app/technovation/sweep_background_checks.rb
+++ app/technovation/sweep_background_checks.rb
@@ -5,14 +5,16 @@ module SweepBackgroundChecks
 
       puts ""
 
-      if report.present? and report.status != bg_check.status
+      if report.present? and report.status != bg_check.status and bg_check.respond_to?("#{report.status}!")
         bg_check.send("#{report.status}!")
         puts "Report UPDATED TO #{report.status.upcase} for #{bg_check.account.email}"
       elsif report.present? and "engaged" == report.adjudication
         bg_check.clear!
         puts "Report UPDATED TO #{bg_check.status.upcase} for #{bg_check.account.email}"
-      elsif report.present?
+      elsif report.present? and bg_check.respond_to?("#{report.status}!")
         puts "Report STILL #{bg_check.status} for #{bg_check.account.email}"
+      elsif report.present?
+        raise "Could not call ##{report.status}! for #{bg_check.account.email}"
       else
         puts "Report NOT FOUND for #{bg_check.account.email}"
       end
