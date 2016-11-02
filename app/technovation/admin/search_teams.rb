66369a232ad54c895f27d13ee23f6c1dc20f82eb
Joe Sak <joe@joesak.com>
2016-11-02 14:09:12 -0500

    Update admin layout, fix asset compilation #775

--- app/technovation/admin/search_teams.rb
+++ app/technovation/admin/search_teams.rb
@@ -9,6 +9,20 @@ module Admin
                   .where("season_registrations.season_id = ?",
                          Season.find_by(year: params[:season]))
 
+      unless params[:text].blank?
+        results = teams.search({
+          query: {
+            query_string: {
+              query: "*#{params[:text]}*"
+            },
+          },
+          from: 0,
+          size: 10_000
+        }).results
+
+        teams = teams.where(id: results.flat_map { |r| r._source.id })
+      end
+
       case params[:division]
       when "All"
         teams
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,7 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w(
   email.css
+  admin.css
   admin.js
   technovation-typeahead.js
   Chart.bundle.js
