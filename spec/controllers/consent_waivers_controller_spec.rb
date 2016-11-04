576130d71d85c300bdc0392c931106ab1267aa7a
Joe Sak <joe@joesak.com>
2016-11-04 13:36:12 -0500

    Pass the consent waivers controller spec

--- spec/controllers/consent_waivers_controller_spec.rb
+++ spec/controllers/consent_waivers_controller_spec.rb
@@ -4,16 +4,16 @@ RSpec.describe ConsentWaiversController do
   describe "POST #create" do
     it "preserves the token on a validation error" do
       mentor = FactoryGirl.create(:mentor)
-      post :create, consent_waiver: { account_consent_token: mentor.consent_token }
-      expect(assigns[:consent_waiver].account_consent_token).to eq(mentor.consent_token)
+      post :create, consent_waiver: { account_consent_token: mentor.account.consent_token }
+      expect(assigns[:consent_waiver].account_consent_token).to eq(mentor.account.consent_token)
     end
   end
 
   describe "GET #new" do
     it "assigns the account to the consent" do
       regional_ambassador = FactoryGirl.create(:regional_ambassador)
-      get :new, token: regional_ambassador.consent_token
-      expect(assigns[:consent_waiver].account_consent_token).to eq(regional_ambassador.consent_token)
+      get :new, token: regional_ambassador.account.consent_token
+      expect(assigns[:consent_waiver].account_consent_token).to eq(regional_ambassador.account.consent_token)
     end
   end
 end
