428ee235bf5c2822ca72a0f4f5584a5ec9f3d06c
Joe Sak <joe@joesak.com>
2016-11-07 09:21:46 -0600

    Remove unecessary latitude setting on team factory

--- spec/features/mentor/find_a_team_spec.rb
+++ spec/features/mentor/find_a_team_spec.rb
@@ -13,9 +13,7 @@ RSpec.feature "Mentors find a team" do
 
   scenario "browse nearby teams" do
     mentored_team = FactoryGirl.create(:team, :with_mentor) # Creator is in Chicago
-    faraway_team = FactoryGirl.create(:team, creator_in: "Los Angeles, CA, US",
-                                             latitude: 34.052363,
-                                             longitude: -118.256551)
+    faraway_team = FactoryGirl.create(:team, creator_in: "Los Angeles, CA, US")
 
     click_link "Browse available teams"
 
