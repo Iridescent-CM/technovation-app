577bbf80f4c549bdca5c42a8cd91cf1c010defd6
Joe Sak <joe@joesak.com>
2016-11-07 13:54:41 -0600

    Fix parental consent controller test

--- spec/controllers/parental_consents_controller_spec.rb
+++ spec/controllers/parental_consents_controller_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe ParentalConsentsController do
   describe "POST #create" do
     it "preserves the token on a validation error" do
       student = FactoryGirl.create(:student, :full_profile)
-      post :create, parental_consent: { student_consent_token: student.consent_token }
-      expect(assigns[:parental_consent].student_consent_token).to eq(student.consent_token)
+      post :create, parental_consent: { student_profile_consent_token: student.consent_token }
+      expect(assigns[:parental_consent].student_profile_consent_token).to eq(student.consent_token)
     end
 
     it "emails a copy to the parent" do
       student = FactoryGirl.create(:student, :full_profile)
-      post :create, parental_consent: FactoryGirl.attributes_for(:parental_consent, student_consent_token: student.consent_token)
+      post :create, parental_consent: FactoryGirl.attributes_for(:parental_consent, student_profile_consent_token: student.consent_token)
       mail = ActionMailer::Base.deliveries.last
       expect(mail).to be_present, "no copy of parental consent was sent"
       expect(mail.to).to eq([student.reload.parent_guardian_email])
@@ -19,7 +19,7 @@ RSpec.describe ParentalConsentsController do
 
     it "notifies the student that they can move on" do
       student = FactoryGirl.create(:student, :full_profile)
-      post :create, parental_consent: FactoryGirl.attributes_for(:parental_consent, student_consent_token: student.consent_token)
+      post :create, parental_consent: FactoryGirl.attributes_for(:parental_consent, student_profile_consent_token: student.consent_token)
       mail = ActionMailer::Base.deliveries[-2]
       expect(mail).to be_present, "no notice to student about parental consent was sent"
       expect(mail.to).to eq([student.email])
@@ -31,7 +31,7 @@ RSpec.describe ParentalConsentsController do
     it "assigns the student to the consent" do
       student = FactoryGirl.create(:student, :full_profile)
       get :new, token: student.consent_token
-      expect(assigns[:parental_consent].student_consent_token).to eq(student.consent_token)
+      expect(assigns[:parental_consent].student_profile_consent_token).to eq(student.consent_token)
     end
   end
 end
