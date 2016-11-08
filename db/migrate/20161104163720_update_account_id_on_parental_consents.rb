3b64f8d46dfa5057e7b71599051cf5aa0fe5fa18
Joe Sak <joe@joesak.com>
2016-11-08 10:40:14 -0600

    Fix parental consents migration

--- db/migrate/20161104163720_update_account_id_on_parental_consents.rb
+++ db/migrate/20161104163720_update_account_id_on_parental_consents.rb
@@ -1,7 +1,35 @@
 class UpdateAccountIdOnParentalConsents < ActiveRecord::Migration
-  def change
+  def up
     remove_foreign_key :parental_consents, :accounts
+
+    ParentalConsent.find_each do |pc|
+      puts ""
+      puts "--------------------------------------------------------------------------"
+      profile = StudentProfile.find_by(account_id: pc.account_id)
+      pc.update_column(:account_id, profile.id)
+      puts "Assigned #{profile.email} student profile to Parental Consent #{pc.id}"
+      puts "--------------------------------------------------------------------------"
+      puts ""
+    end
+
     rename_column :parental_consents, :account_id, :student_profile_id
     add_foreign_key :parental_consents, :student_profiles
+  end
+
+  def down
+    remove_foreign_key :parental_consents, :student_profiles
+
+    ParentalConsent.find_each do |pc|
+      puts ""
+      puts "--------------------------------------------------------------------------"
+      account = StudentProfile.find(pc.student_profile_id).account
+      pc.update_column(:student_profile_id, account.id)
+      puts "Assigned #{account.email} account to Parental Consent #{pc.id}"
+      puts "--------------------------------------------------------------------------"
+      puts ""
+    end
+
+    rename_column :parental_consents, :student_profile_id, :account_id
+    add_foreign_key :parental_consents, :accounts
   end
 end
