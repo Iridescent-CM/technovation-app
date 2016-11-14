f500e949d7923202173a155ee8fc67614dc932ea
Joe Sak <joe@joesak.com>
2016-11-14 14:27:47 -0600

    Replace feature spec with existing controller spec #797

--- spec/controllers/regional_ambassador/profiles_controller_spec.rb
+++ spec/controllers/regional_ambassador/profiles_controller_spec.rb
@@ -16,14 +16,26 @@ RSpec.describe RegionalAmbassador::ProfilesController do
 
     it "shows users in the country region for Intn'l RAs" do
       us_student = FactoryGirl.create(:student, geocoded: "Chicago, IL")
-      riyadh_ra = FactoryGirl.create(:regional_ambassador, geocoded: "Dhurma")
+      riyadh_ra = FactoryGirl.create(:regional_ambassador,
+                                     status: :approved,
+                                     geocoded: "Dhurma")
       najran_student = FactoryGirl.create(:student, geocoded: "Najran")
+
+      pending_ra = FactoryGirl.create(:ambassador, status: :pending, geocoded: "Dhurma")
+      approved_ra = FactoryGirl.create(:ambassador, status: :approved, geocoded: "Dhurma")
+      declined_ra = FactoryGirl.create(:ambassador, status: :declined, geocoded: "Dhurma")
+      spam_ra = FactoryGirl.create(:ambassador, status: :spam, geocoded: "Dhurma")
 
       sign_in(riyadh_ra)
       get :index
 
       expect(assigns[:accounts].map(&:id)).not_to include(us_student.account_id)
+      expect(assigns[:accounts].map(&:id)).not_to include(pending_ra.account_id)
+      expect(assigns[:accounts].map(&:id)).not_to include(declined_ra.account_id)
+      expect(assigns[:accounts].map(&:id)).not_to include(spam_ra.account_id)
+
       expect(assigns[:accounts].map(&:id)).to include(riyadh_ra.account_id)
+      expect(assigns[:accounts].map(&:id)).to include(approved_ra.account_id)
       expect(assigns[:accounts].map(&:id)).to include(najran_student.account_id)
     end
   end
