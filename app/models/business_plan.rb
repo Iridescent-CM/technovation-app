3f84460505abb791a9ca30d4a2fda0a432222250
Joe Sak <joe@joesak.com>
2016-11-15 16:12:29 -0600

    Add attributes to business plan to handle uploaded file or remote url

--- app/models/business_plan.rb
+++ app/models/business_plan.rb
@@ -1,3 +1,9 @@
 class BusinessPlan < ActiveRecord::Base
   belongs_to :team_submission
+
+  mount_uploader :uploaded_file, FileProcessor
+
+  def file_url
+    uploaded_file ? uploaded_file_url : remote_file_url
+  end
 end
