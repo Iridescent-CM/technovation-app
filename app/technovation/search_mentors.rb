fdbb99c293022fcdf031d388a22022cfdedb9103
Joe Sak <joe@joesak.com>
2016-11-18 08:36:23 -0600

    Escape special characters in search query

--- app/technovation/search_mentors.rb
+++ app/technovation/search_mentors.rb
@@ -3,10 +3,12 @@ module SearchMentors
     mentors = MentorProfile.where.not(account_id: filter.user.account_id)
 
     unless filter.text.blank?
+      sanitized_text = sanitize_string_for_elasticsearch_string_query(filter.text)
+
       results = Account.joins(:mentor_profile).search(
         query: {
           query_string: {
-            query: "*#{filter.text}*"
+            query: "*#{sanitized_text}*"
           }
         },
         from: 0,
@@ -56,5 +58,25 @@ module SearchMentors
     end
 
     mentors.searchable(filter.user)
+  end
+
+  def self.sanitize_string_for_elasticsearch_string_query(str)
+    # Escape special characters
+    # http://lucene.apache.org/core/old_versioned_docs/versions/2_9_1/queryparsersyntax.html#Escaping Special Characters
+    escaped_characters = Regexp.escape('\\+-&|!(){}[]^~*?:')
+    str = str.gsub(/([#{escaped_characters}])/, '\\\\\1')
+
+    # AND, OR and NOT are used by lucene as logical operators. We need
+    # to escape them
+    ['AND', 'OR', 'NOT'].each do |word|
+      escaped_word = word.split('').map {|char| "\\#{char}" }.join('')
+      str = str.gsub(/\s*\b(#{word.upcase})\b\s*/, " #{escaped_word} ")
+    end
+
+    # Escape odd quotes
+    quote_count = str.count '"'
+    str = str.gsub(/(.*)"(.*)/, '\1\"\3') if quote_count % 2 == 1
+
+    str
   end
 end
--- app/technovation/search_teams.rb
+++ app/technovation/search_teams.rb
@@ -24,10 +24,12 @@ module SearchTeams
     end
 
     unless filter.text.blank?
+      sanitized_text = sanitize_string_for_elasticsearch_string_query(filter.text)
+
       results = teams.search({
         query: {
           query_string: {
-            query: "*#{filter.text}*"
+            query: "*#{sanitized_text}*"
           },
         },
         from: 0,
@@ -63,5 +65,25 @@ module SearchTeams
     else
       teams
     end
+  end
+
+  def self.sanitize_string_for_elasticsearch_string_query(str)
+    # Escape special characters
+    # http://lucene.apache.org/core/old_versioned_docs/versions/2_9_1/queryparsersyntax.html#Escaping Special Characters
+    escaped_characters = Regexp.escape('\\+-&|!(){}[]^~*?:')
+    str = str.gsub(/([#{escaped_characters}])/, '\\\\\1')
+
+    # AND, OR and NOT are used by lucene as logical operators. We need
+    # to escape them
+    ['AND', 'OR', 'NOT'].each do |word|
+      escaped_word = word.split('').map {|char| "\\#{char}" }.join('')
+      str = str.gsub(/\s*\b(#{word.upcase})\b\s*/, " #{escaped_word} ")
+    end
+
+    # Escape odd quotes
+    quote_count = str.count '"'
+    str = str.gsub(/(.*)"(.*)/, '\1\"\3') if quote_count % 2 == 1
+
+    str
   end
 end
