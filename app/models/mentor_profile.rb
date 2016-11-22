e203f9101de5156018e526f9023f39b1fc50a817
Joe Sak <joe@joesak.com>
2016-11-22 09:21:08 -0600

    Fix gender search #764

--- app/models/mentor_profile.rb
+++ app/models/mentor_profile.rb
@@ -17,11 +17,17 @@ class MentorProfile < ActiveRecord::Base
   }
 
   scope :by_gender_identities, ->(ids) {
-    if ids.compact.map(&:to_i).sort != [Account.genders['Male'], Account.genders['Female']].sort and ids.compact.map(&:to_i).sort != Account.genders.values.sort
+    ids = ids.compact.map(&:to_i).sort
+    male_female = [Account.genders['Male'], Account.genders['Female']].sort
+    all_genders = Account.genders.values.sort
+
+    if ids == male_female
       ids = ids + [Account.genders["Prefer not to say"], Account.genders["Non-binary"]]
-      joins(:account).where("accounts.gender IN (?)", ids.uniq)
-    else
+      joins(:account).where("accounts.gender IN (?) OR accounts.gender IS NULL", ids.uniq)
+    elsif ids == all_genders
       joins(:account).where("accounts.gender IN (?) OR accounts.gender IS NULL", ids)
+    else
+      joins(:account).where("accounts.gender IN (?)", ids)
     end
   }
 
