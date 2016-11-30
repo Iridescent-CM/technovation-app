4eebc228fe977125b1f3fbe7596560b94abe1209
Dan Cortes <danielgcortes@gmail.com>
2016-11-29 21:45:18 -0600

    Scaffolding for edit app description submission

--- app/assets/javascripts/submissions_page.js
+++ app/assets/javascripts/submissions_page.js
@@ -12,19 +12,47 @@
   var editableWrapperClass = 'ts-app-description--editable';
   var visibleCancelButtonClass = 'ts-app-description__cancel-btn--show';
 
-  var isEditable = false;
-  //var contentObj = Array.prototype.reduce
+  var contentObj = Array.prototype.reduce.call(editableContent, function(obj, node) {
+    obj[node.dataset.name] = node.innerText;
+    return obj;
+  }, {});
+
+  var tempObject = {};
 
   primaryEditableButton.addEventListener('click', makeContentEditable);
 
   function makeContentEditable() {
     primaryEditableButton.removeEventListener('click', makeContentEditable);
+    primaryEditableButton.innerText = 'Save Changes';
+
     wrapper.classList.add(editableWrapperClass);
     cancelEditableButton.classList.add(visibleCancelButtonClass);
+
+    Array.prototype.forEach.call(editableContent, function(node) {
+      node.contentEditable = true;
+    });
+
+    primaryEditableButton.addEventListener('click', saveChanges);
     cancelEditableButton.addEventListener('click', cancelEditable);
   }
 
-  function cancelEditable() {
+  function saveChanges() {
+    primaryEditableButton.removeEventListener('click', saveChanges);
+    // Submit changes to API
+    cancelEditable();
+  }
 
+  function cancelEditable() {
+    cancelEditableButton.removeEventListener('click', cancelEditable);
+
+    wrapper.classList.remove(editableWrapperClass);
+    cancelEditableButton.classList.remove(visibleCancelButtonClass);
+
+    Array.prototype.forEach.call(editableContent, function(node) {
+      node.contentEditable = false;
+    });
+
+    primaryEditableButton.innerText = 'Edit App Info';
+    primaryEditableButton.addEventListener('click', makeContentEditable);
   }
 })();
