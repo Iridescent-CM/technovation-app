ff6781a9408118075b077ce641b6bf7f54ef9ce3
Joe Sak <joe@joesak.com>
2016-12-01 13:16:26 -0600

    Replace private method call in migration

--- db/migrate/20161201145445_update_primary_location_on_teams.rb
+++ db/migrate/20161201145445_update_primary_location_on_teams.rb
@@ -1,7 +1,8 @@
 class UpdatePrimaryLocationOnTeams < ActiveRecord::Migration
   def up
     Team.where(latitude: nil, longitude: nil).find_each do |team|
-      team.update_geocoding
+      team.latitude = team.creator.latitude
+      team.longitude = team.creator.longitude
       team.save
     end
   end
