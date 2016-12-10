c8b1298c4a18481ed96d12f757f3fa787ff57be1
Dan Cortes <danielgcortes@gmail.com>
2016-12-10 11:59:05 -0600

    Saving progress

--- app/assets/javascripts/submissions_page.js
+++ app/assets/javascripts/submissions_page.js
@@ -3,122 +3,130 @@
     return;
   }
 
-  var wrapper = document.getElementById("ts-app-description-wrapper");
-  var buttonsWrapper = document.getElementById("ts-app-description-buttons-wrapper");
-  var primaryEditableButton = document.getElementById("ts-primary-editable-btn");
-  var cancelEditableButton = document.getElementById("ts-cancel-editable-btn");
-  var editableContent = wrapper.querySelectorAll('[data-editable]');
+  appNameAndDescription();
 
-  var nameField = Array.prototype.find.call(editableContent, function(node) {
-    return node.dataset.name === 'app_name';
-  });
-  var descriptionField = Array.prototype.find.call(editableContent, function(node) {
-    return node.dataset.name === 'app_description';
-  });
 
-  var editableWrapperClass = 'ts-app-description--editable';
-  var visibleCancelButtonClass = 'ts-app-description__cancel-btn--show';
+  /**
+   * Handles the editing of
+   */
+  function appNameAndDescription() {
+    var wrapper = document.getElementById("ts-app-description-wrapper");
+    var buttonsWrapper = document.getElementById("ts-app-description-buttons-wrapper");
+    var primaryEditableButton = document.getElementById("ts-primary-editable-btn");
+    var cancelEditableButton = document.getElementById("ts-cancel-editable-btn");
+    var editableContent = wrapper.querySelectorAll('[data-editable]');
 
-  var contentObj = Array.prototype.reduce.call(editableContent, function(obj, node) {
-    obj[node.dataset.name] = node.innerText;
-    return obj;
-  }, {});
-
-  var tempObject = {};
-  cloneContentObjToTempObj();
-
-  primaryEditableButton.addEventListener('click', makeContentEditable);
-
-  function makeContentEditable() {
-    primaryEditableButton.removeEventListener('click', makeContentEditable);
-    primaryEditableButton.innerText = 'Save Changes';
-
-    wrapper.classList.add(editableWrapperClass);
-    cancelEditableButton.classList.add(visibleCancelButtonClass);
-
-    Array.prototype.forEach.call(editableContent, function(node) {
-      node.contentEditable = true;
-      node.addEventListener('input', editTempObject);
+    var nameField = Array.prototype.find.call(editableContent, function(node) {
+      return node.dataset.name === 'app_name';
+    });
+    var descriptionField = Array.prototype.find.call(editableContent, function(node) {
+      return node.dataset.name === 'app_description';
     });
 
-    setTimeout(function() {
-      // If app has no name, place cursor in the app name field
-      if (!nameField.innerText) {
-        nameField.focus();
-      } else {
-        // Otherwise place cursor at the end of the description field
-        var range = document.createRange();
-        range.selectNodeContents(descriptionField);
-        range.collapse(false);
-        var selection = window.getSelection();
-        selection.removeAllRanges();
-        selection.addRange(range);
-      }
-    }, 0);
+    var editableWrapperClass = 'ts-app-description--editable';
+    var visibleCancelButtonClass = 'ts-app-description__cancel-btn--show';
 
-    primaryEditableButton.addEventListener('click', saveChanges);
-    cancelEditableButton.addEventListener('click', cancelChanges);
-  }
+    var contentObj = Array.prototype.reduce.call(editableContent, function(obj, node) {
+      obj[node.dataset.name] = node.innerText;
+      return obj;
+    }, {});
 
-  function editTempObject(e) {
-    var nodeName = e.target.dataset.name;
-    tempObject[nodeName] = e.target.innerText;
-  }
-
-  function cloneContentObjToTempObj() {
-    Object.keys(contentObj).forEach(function(key) {
-      tempObject[key] = contentObj[key];
-    });
-  }
-
-  // Submit changes to API
-  function saveChanges() {
-    primaryEditableButton.removeEventListener('click', saveChanges);
-
-    var path = wrapper.dataset.updateUrl;
-    var payload = {};
-    payload[wrapper.dataset.objectName] = tempObject;
-
-    $.ajax(path, {
-      method: 'PUT',
-      data: payload,
-      success: function(res, status) {
-        createFlashNotification('success', 'Your app description has been updated!');
-        removeContentEditable();
-      },
-      error: function(res, status) {
-        var responseText = JSON.parse(res.responseText);
-        var errorKeys = Object.keys(responseText);
-        for (var i = 0; i < errorKeys.length; i++) {
-          var currentError = errorKeys[i] + ' ' + responseText[errorKeys[i]];
-          createFlashNotification('error', currentError);
-        }
-        cancelChanges();
-      }
-    });
-
-  }
-
-  function cancelChanges() {
-    primaryEditableButton.removeEventListener('click', saveChanges);
-    cancelEditableButton.removeEventListener('click', cancelChanges);
-
+    var tempObject = {};
     cloneContentObjToTempObj();
-    Array.prototype.forEach.call(editableContent, function(node) {
-      node.innerText = contentObj[node.dataset.name];
-    });
 
-    removeContentEditable();
-  }
-
-  function removeContentEditable() {
-    wrapper.classList.remove(editableWrapperClass);
-    cancelEditableButton.classList.remove(visibleCancelButtonClass);
-
-    Array.prototype.forEach.call(editableContent, function(node) {
-      node.contentEditable = false;
-    });
-    primaryEditableButton.innerText = 'Edit App Info';
     primaryEditableButton.addEventListener('click', makeContentEditable);
+
+    function makeContentEditable() {
+      primaryEditableButton.removeEventListener('click', makeContentEditable);
+      primaryEditableButton.innerText = 'Save Changes';
+
+      wrapper.classList.add(editableWrapperClass);
+      cancelEditableButton.classList.add(visibleCancelButtonClass);
+
+      Array.prototype.forEach.call(editableContent, function(node) {
+        node.contentEditable = true;
+        node.addEventListener('input', editTempObject);
+      });
+
+      setTimeout(function() {
+        // If app has no name, place cursor in the app name field
+        if (!nameField.innerText) {
+          nameField.focus();
+        } else {
+          // Otherwise place cursor at the end of the description field
+          var range = document.createRange();
+          range.selectNodeContents(descriptionField);
+          range.collapse(false);
+          var selection = window.getSelection();
+          selection.removeAllRanges();
+          selection.addRange(range);
+        }
+      }, 0);
+
+      primaryEditableButton.addEventListener('click', saveChanges);
+      cancelEditableButton.addEventListener('click', cancelChanges);
+    }
+
+    function editTempObject(e) {
+      var nodeName = e.target.dataset.name;
+      tempObject[nodeName] = e.target.innerText;
+    }
+
+    function cloneContentObjToTempObj() {
+      Object.keys(contentObj).forEach(function(key) {
+        tempObject[key] = contentObj[key];
+      });
+    }
+
+    // Submit changes to API
+    function saveChanges() {
+      primaryEditableButton.removeEventListener('click', saveChanges);
+
+      var path = wrapper.dataset.updateUrl;
+      var payload = {};
+      payload[wrapper.dataset.objectName] = tempObject;
+
+      $.ajax(path, {
+        method: 'PUT',
+        data: payload,
+        success: function(res, status) {
+          createFlashNotification('success', 'Your app description has been updated!');
+          removeContentEditable();
+        },
+        error: function(res, status) {
+          var responseText = JSON.parse(res.responseText);
+          var errorKeys = Object.keys(responseText);
+          for (var i = 0; i < errorKeys.length; i++) {
+            var currentError = errorKeys[i] + ' ' + responseText[errorKeys[i]];
+            createFlashNotification('error', currentError);
+          }
+          cancelChanges();
+        }
+      });
+
+    }
+
+    function cancelChanges() {
+      primaryEditableButton.removeEventListener('click', saveChanges);
+      cancelEditableButton.removeEventListener('click', cancelChanges);
+
+      cloneContentObjToTempObj();
+      Array.prototype.forEach.call(editableContent, function(node) {
+        node.innerText = contentObj[node.dataset.name];
+      });
+
+      removeContentEditable();
+    }
+
+    function removeContentEditable() {
+      wrapper.classList.remove(editableWrapperClass);
+      cancelEditableButton.classList.remove(visibleCancelButtonClass);
+
+      Array.prototype.forEach.call(editableContent, function(node) {
+        node.contentEditable = false;
+      });
+      primaryEditableButton.innerText = 'Edit App Info';
+      primaryEditableButton.addEventListener('click', makeContentEditable);
+    }
   }
 })();
