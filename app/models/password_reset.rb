673b336dd41a129c506b4ed2f6e2f0369e061fce
Joe Sak <joe@joesak.com>
2016-12-12 10:48:28 -0600

    Nitpick'

--- app/models/password_reset.rb
+++ app/models/password_reset.rb
@@ -7,7 +7,6 @@ class PasswordReset
 
   def perform
     account = Account.where("lower(email) = ?", email.downcase).first
-
     account.enable_password_reset!
     AccountMailer.password_reset(account.id).deliver_later
   end
