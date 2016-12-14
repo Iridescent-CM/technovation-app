dbb022d439f0926e4361c0c99d59f8479aa0be11
Joe Sak <joe@joesak.com>
2016-12-14 14:00:38 -0600

    Bind close button event listener's this to currentModal

--- app/assets/javascripts/modalify.js
+++ app/assets/javascripts/modalify.js
@@ -18,28 +18,33 @@
 
 (function modalify() {
   var modals = document.getElementsByClassName('modalify');
+
   for (var i = 0; i < modals.length; i++) {
     var currentModal = modals[i];
     var modalContent = currentModal.innerHTML;
+
     currentModal.innerHTML = '';
+
     var modalBody = document.createElement('div');
     modalBody.classList.add('modalify__body', 'simple-card');
     modalBody.innerHTML = modalContent;
 
     var modalTopBar = document.createElement('div');
     modalTopBar.classList.add('modalify__top-bar')
+
     if (currentModal.dataset.heading) {
       var modalHeading = document.createElement('h2');
       modalHeading.innerHTML = currentModal.dataset.heading;
       modalHeading.classList.add('modalify__heading');
       modalTopBar.appendChild(modalHeading);
     }
+
     var closeButton = document.createElement('span');
     closeButton.classList.add('fa', 'fa-times', 'modalify__close');
     modalTopBar.appendChild(closeButton);
     closeButton.addEventListener('click', function() {
-      hideModal(currentModal);
-    });
+      hideModal(this);
+    }.bind(currentModal));
 
 
     var modalShade = document.createElement('div');
@@ -50,8 +55,8 @@
     currentModal.insertBefore(modalShade, modalBody);
 
     modalShade.addEventListener('click', function() {
-      hideModal(currentModal);
-    });
+      hideModal(this);
+    }.bind(currentModal));
   }
 
   var modalTriggers = document.querySelectorAll('[data-modal-trigger]');
