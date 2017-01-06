dca70ab620e181b602098f28f7093622b19fb920
Joe Sak <joe@joesak.com>
2017-01-06 12:15:09 -0600

    Fix remote forms to be about remote_checkboxes, no more word count thing

--- app/assets/javascripts/remote_forms.js
+++ app/assets/javascripts/remote_forms.js
@@ -1,27 +1,17 @@
 (function() {
   $(document).ready(enableRemoteCheckboxes);
-  $(document).ready(enableWordCount);
 
   function enableRemoteCheckboxes() {
-    $('form[data-remote="true"]').find('input[type="checkbox"]').on('change', function(e) {
+    $('form.remote_checkboxes').find('input[type="checkbox"]').on('change', function(e) {
       $(e.target).closest('form').submit();
     });
 
-    $(document).on('ajax:success', function() {
+    $('form.remote_checkboxes').on('ajax:success', function() {
       var successFlash = document.createElement('div');
       successFlash.classList.add('flash', 'flash--success');
       successFlash.innerHTML = 'Saved!';
       successFlash.dataset.timeout = 750;
       document.body.appendChild(successFlash);
-    });
-  }
-
-  function enableWordCount() {
-    $(document).on('keyup', '[data-count-words="true"]', function(e) {
-      var words = e.target.value.match(/\S+/g),
-          wordCount = words ? words.length : 0;
-
-      $('[data-word-count-show="true"]').text('Words: ' + wordCount);
     });
   }
 })();
