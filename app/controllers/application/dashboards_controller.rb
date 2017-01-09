4ab4ba15b98a71f4ef745d012f83b53d63e5fd2f
Joe Sak <joe@joesak.com>
2017-01-09 13:51:18 -0600

    Fix app dashboard redirect to consider redirected_from and last_profile_used

--- app/controllers/application/dashboards_controller.rb
+++ app/controllers/application/dashboards_controller.rb
@@ -2,7 +2,13 @@ module Application
   class DashboardsController < ApplicationController
     def show
       if current_account.authenticated?
-        redirect_to send("#{current_account.type_name}_dashboard_path")
+        last_profile_used = cookies.delete(:last_profile_used)
+
+        path = cookies.delete(:redirected_from) ||
+          (last_profile_used && public_send("#{last_profile_used}_dashboard_path")) ||
+            send("#{current_account.type_name}_dashboard_path")
+
+        redirect_to path
       else
         @signup_attempt = SignupAttempt.new(email: params[:email])
       end
