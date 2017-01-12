ecab810e93e5f1626351e40ef8c2f3f94f00dc40
Joe Sak <joe@joesak.com>
2017-01-11 18:27:20 -0600

    Fix another

--- spec/models/student_account_spec.rb
+++ spec/models/student_account_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe StudentProfile do
     it "calculates the student's age" do
       student = FactoryGirl.create(:student, date_of_birth: "Feb 29, 2008")
 
-      Timecop.freeze("March 1, 2017") do
-        expect(student.age).to eq(9)
+      Timecop.freeze("March 1, 2016 PST") do
+        expect(student.age).to eq(8)
       end
     end
   end
