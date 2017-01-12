5c3fc08c60690209ec0d660512b4092723383d82
Joe Sak <joe@joesak.com>
2017-01-12 09:40:21 -0600

    Fix admin css for viewing profiles

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -2,6 +2,8 @@
 // JQUERY IS INCLUDED HERE STRICTLY
 // AS A DEPENDENCY OF TWITTER TYPEAHEAD
 
+//= require jquery_ujs
+// RAILS VIEW HELPER DEPENDENCY
 
 //= require admin-results-tables
 //= require admin-filter-options
