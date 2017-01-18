03d87d2ad4d88d60135c2e8c7c07e65416d7eed8
Joe Sak <joe@joesak.com>
2017-01-18 11:10:13 -0600

    Fix admin paper parental consent action

--- app/controllers/admin/paper_parental_consents_controller.rb
+++ app/controllers/admin/paper_parental_consents_controller.rb
@@ -6,7 +6,7 @@ module Admin
       student.student_profile.update_columns(parent_guardian_name: "ON FILE",
                                              parent_guardian_email: "ON FILE")
 
-      sql = ActiveRecord::Base.send(:sanitize_sql_array, ["INSERT INTO parental_consents (account_id, electronic_signature, created_at, updated_at)
+      sql = ActiveRecord::Base.send(:sanitize_sql_array, ["INSERT INTO parental_consents (student_profile_id, electronic_signature, created_at, updated_at)
                                                                   VALUES (?, ?, ?, ?)", student.id, 'ON FILE', Time.current, Time.current])
       ActiveRecord::Base.connection.execute(sql)
 
