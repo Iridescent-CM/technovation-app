62b74bbaaf33b6c91196ef732f8d7fbec91f0a69
Joe Sak <joe@joesak.com>
2017-01-18 09:17:32 -0600

    Reveal team submission progress to mentors

--- spec/features/mentor/leave_team_spec.rb
+++ spec/features/mentor/leave_team_spec.rb
@@ -5,11 +5,13 @@ RSpec.feature "Mentors leave their own team" do
     mentor = FactoryGirl.create(:mentor, :on_team)
     sign_in(mentor)
 
-    click_link "My teams"
+    within(".navigation") { click_link "My teams" }
     click_link mentor.team_names.last
 
     click_link "Leave #{mentor.team_names.last}"
     expect(current_path).to eq(mentor_dashboard_path)
-    expect(page).not_to have_link("My teams")
+
+    within(".navigation") { click_link "My teams" }
+    expect(page).not_to have_link(Team.last.name)
   end
 end
