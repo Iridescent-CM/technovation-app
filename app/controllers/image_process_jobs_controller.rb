d8acb224c9bf6295e5ab887c142bd10ac09c3c9c
Joe Sak <joe@joesak.com>
2017-01-25 21:36:55 -0600

    Return the job id with the correct method call

--- app/controllers/image_process_jobs_controller.rb
+++ app/controllers/image_process_jobs_controller.rb
@@ -1,6 +1,6 @@
 module Student
   class ImageProcessJobsController < StudentController
     job = ProcessScreenshotsJob.perform_later(current_team.submission, params.fetch(:keys))
-    render json: { job_id: job.id }
+    render json: { job_id: job.job_id }
   end
 end
