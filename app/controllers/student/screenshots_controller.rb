1f0f43e52d39001298e6ada932cd72fbdb29db22
Joe Sak <joe@joesak.com>
2017-01-30 09:20:16 -0600

    Add image alt to the screenshots json

--- app/controllers/student/screenshots_controller.rb
+++ app/controllers/student/screenshots_controller.rb
@@ -1,10 +1,13 @@
 module Student
   class ScreenshotsController < StudentController
+    include ActionView::Helpers::AssetTagHelper
+
     def index
       screenshots = current_team.current_team_submission.screenshots.map do |s|
         {
           id: s.id,
           image_url: s.image_url,
+          image_alt: image_alt(s.image_url),
           sort_position: s.sort_position,
           delete_url: student_screenshot_url(s),
         }
