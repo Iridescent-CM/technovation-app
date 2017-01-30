73fbe7494d718b618d54822e79730d58ee65da14
Joe Sak <joe@joesak.com>
2017-01-30 13:49:24 -0600

    Provide a basic show page for the regional pitch event selection

--- app/controllers/student/regional_pitch_event_selections_controller.rb
+++ app/controllers/student/regional_pitch_event_selections_controller.rb
@@ -5,8 +5,12 @@ module Student
       current_team.regional_pitch_events << RegionalPitchEvent.find(params[:event_id])
       current_team.save!
 
-      redirect_to [:student, current_team.current_team_submission],
+      redirect_to [:student, :regional_pitch_event_selection],
         success: t("controllers.student.regional_pitch_event_selections.create.success")
+    end
+
+    def show
+      @regional_events = RegionalPitchEvent.available_to(current_team.current_team_submission)
     end
   end
 end
