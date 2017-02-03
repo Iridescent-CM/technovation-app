0f503d8c2546b0c47f67318f30262c9147987efa
Joe Sak <joe@joesak.com>
2017-02-03 11:19:08 -0600

    Fix the file processor to only use download token if it's available

--- app/uploaders/file_processor.rb
+++ app/uploaders/file_processor.rb
@@ -3,7 +3,11 @@ class FileProcessor < CarrierWave::Uploader::Base
   storage :fog
 
   def store_dir
-    "files/#{model.class.to_s.underscore}/#{mounted_as}/#{model.download_token}"
+    if model.respond_to?(:download_token)
+      "files/#{model.class.to_s.underscore}/#{mounted_as}/#{model.download_token}"
+    else
+      "files/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
   end
 
   def extension_white_list
