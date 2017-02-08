7ac70ab9004caf6d432d1342c71e6b35efdac927
Joe Sak <joe@joesak.com>
2017-02-07 20:31:22 -0600

    Sanitize the Business Plan remote file url too

--- app/models/business_plan.rb
+++ app/models/business_plan.rb
@@ -6,4 +6,14 @@ class BusinessPlan < ActiveRecord::Base
   def file_url
     uploaded_file ? uploaded_file_url : remote_file_url
   end
+
+  def remote_file_url=(url)
+    sanitized_url = if url.match(%r{\Ahttps?://})
+            url
+          else
+            url.sub(%r{\A(?:\w+://)?}, "http://")
+          end
+
+    super(sanitized_url)
+  end
 end
