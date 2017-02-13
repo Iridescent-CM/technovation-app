569635dbc05135f2f48b9e46af4e1373c495812a
Dan Cortes <danielgcortes@gmail.com>
2017-02-13 07:38:40 -0600

    General layout

--- app/assets/javascripts/judging_form.js
+++ app/assets/javascripts/judging_form.js
@@ -1,24 +1,107 @@
 (function judgingForm() {
-  console.log('Judge.');
   var formWrapper = document.getElementById('judging-form');
   if (!formWrapper) {
     return;
   }
-  // Question type classes: radio_buttons, text
-  var currentSectionIndex = 0;
-  var currentQuestionIndex = 0;
+  // Todo: Figure out what the active question/section is by looking
+  // at what sections have valid inputs already
+  var activeSectionIndex = 0;
+  var activeQuestionIndex = 0;
   var sections = formWrapper.getElementsByClassName('judging-form__section');
-  var currentSection = sections[currentSectionIndex];
-  var questions = currentSection.getElementsByClassName('input');
-  var currentQuestion = questions[currentQuestionIndex];
+  var activeSection = sections[activeSectionIndex];
+  var questions = activeSection.getElementsByClassName('input');
+  var activeQuestion = questions[activeQuestionIndex];
+  activeQuestion.classList.add('active'); // Not BEM cause this thing is being generated dynamically
+  activeSection.classList.add('judging-form__section--active');
 
-  currentSection.classList.add('judging-form__section--active');
-  buildCurrentQuestion();
+  generateMarkup();
+  initRangeSliders();
 
-  function buildCurrentQuestion() {
-    console.log(currentQuestion);
+  function generateMarkup() {
+    for (var i = 0; i < sections.length; i++) {
+      var currentSection = sections[i];
+      var questions = currentSection.getElementsByClassName('input');
+
+      // Sorry for the nested for loop :(
+      // This is more straightforward, IMO, than functioning it out
+      for (var i = 0; i < questions.length; i++) {
+        var currentQuestion = questions[i];
+        generateHelperInput(currentQuestion);
+      }
+    }
   }
 
-  console.log(sections.length);
+  function generateHelperInput(question) {
+    var helper = question.classList.contains('radio_buttons')
+      ? makeRadioHelper(question)
+      : makeTextareaHelper(question);
+
+    question.appendChild(helper);
+  }
+
+  function makeRadioHelper(question) {
+    var radios = question.querySelectorAll('.radio input');
+    var inputWrapper = document.createElement('div');
+    inputWrapper.classList.add('judge-helper', 'judge-helper--range');
+
+    // Range Input
+    var input = document.createElement('input');
+    input.type = 'range';
+    input.min = radios[0].value;
+    input.max = radios[radios.length - 1].value;
+    input.defaultValue = input.min;
+    inputWrapper.appendChild(input);
+
+    // Helper description text div
+    var descriptionTextEl = document.createElement('div');
+    for (var i = 0; i < radios.length; i++) {
+      descriptionTextEl.dataset['description-' + i] = radios[i].parentElement.textContent;
+    }
+    inputWrapper.appendChild(descriptionTextEl);
+
+    return inputWrapper;
+  }
+
+  function makeTextareaHelper(question) {
+    var inputWrapper = document.createElement('div');
+    inputWrapper.classList.add('judge-helper', 'judge-helper--textarea');
+    return inputWrapper;
+  }
+
+  function initRangeSliders() {
+    $('input[type="range"]').rangeslider({
+        polyfill: false,
+        // Default CSS classes
+        rangeClass: 'rangeslider',
+        disabledClass: 'rangeslider--disabled',
+        horizontalClass: 'rangeslider--horizontal',
+        verticalClass: 'rangeslider--vertical',
+        fillClass: 'rangeslider__fill',
+        handleClass: 'rangeslider__handle',
+
+        onInit: function() {
+          // Write value to drag hhandle
+          this.$handle[0].innerHTML = this.value;
+          // Make tooltip
+          var tooltip = document.createElement('div');
+          tooltip.classList.add('rangeslider__tooltip');
+          this.$range[0].parentElement.appendChild(tooltip);
+          this.tooltip = tooltip;
+          tooltip.innerHTML = 'Fabio vel iudice vincam, sunt in culpa qui officia. Curabitur blandit tempus ardua ridiculus sed magna.';
+          console.log(this);
+        },
+
+        onSlide: function(position, value) {
+          this.$handle[0].innerHTML = value;
+        },
+
+        onSlideEnd: function(position, value) {
+          // Do something
+          console.log(this.identifier);
+          // var handle = document.querySelector('#' + this.identifier + ' .rangeslider__handle');
+          // handle.innerHTML = value;
+        }
+    });
+  }
 
 })();
