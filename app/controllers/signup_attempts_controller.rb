a5fb45b260cdb0fe8239d8a7fc8c63a6cdc7ba8f
Joe Sak <joe@joesak.com>
2017-02-22 15:50:10 -0600

    Refactor signup attempt conditional paths with helpful named methods

--- app/controllers/signup_attempts_controller.rb
+++ app/controllers/signup_attempts_controller.rb
@@ -2,22 +2,17 @@ class SignupAttemptsController < ApplicationController
   before_action :require_unauthenticated
 
   def create
-    existing_attempt = SignupAttempt.where("lower(email) = ?", signup_attempt_params[:email].downcase).last
-    existing_account = Account.where("lower(email) = ?", signup_attempt_params[:email].downcase).last
-
-    @signup_attempt = SignupAttempt.new(signup_attempt_params)
-
-    if !!existing_account and existing_account.authenticate(signup_attempt_params.fetch(:password))
+    if valid_credentials_for_sign_in?
       SignIn.(existing_account, self)
 
-    elsif !!existing_account
+    elsif valid_email_for_existing_account?
       redirect_to signin_path(email: existing_account.email), error: t("controllers.signins.create.error")
 
-    elsif !!existing_attempt
+    elsif attempt_exists?
       redirect_to signup_attempt_path(existing_attempt.pending_token), notice: t("controllers.signup_attempts.create.success")
 
-    elsif @signup_attempt.save
-      redirect_to signup_attempt_path(@signup_attempt.pending_token), success: t("controllers.signup_attempts.create.success")
+    elsif current_signup_attempt.save
+      redirect_to signup_attempt_path(current_signup_attempt.pending_token), success: t("controllers.signup_attempts.create.success")
 
     else
       render "application/dashboards/show"
@@ -41,6 +36,36 @@ class SignupAttemptsController < ApplicationController
   end
 
   private
+  def valid_credentials_for_sign_in?
+     valid_email_for_existing_account? and existing_account.authenticate(signup_attempt_params.fetch(:password))
+  end
+
+  def existing_account
+    @existing_account ||= Account.where(
+      "lower(email) = ?",
+      signup_attempt_params[:email].downcase
+    ).last
+  end
+
+  def valid_email_for_existing_account?
+    !!existing_account
+  end
+
+  def attempt_exists?
+    !!existing_attempt
+  end
+
+  def existing_attempt
+   @existing_attempt ||= SignupAttempt.where(
+     "lower(email) = ?",
+     signup_attempt_params[:email].downcase
+   ).last
+  end
+
+  def current_signup_attempt
+    @signup_attempt ||= SignupAttempt.new(signup_attempt_params)
+  end
+
   def signup_attempt_params
     params.require(:signup_attempt).permit(:email, :password)
   end
