6a69637c2a0cd45ac3132611a28240314488b1ec
Joe Sak <joe@joesak.com>
2017-02-22 11:54:43 -0600

    Do NOT rescue CM info updating by subscribing a new email - RESOLVE FEB 7 INCIDENT

--- app/jobs/update_email_list_job.rb
+++ app/jobs/update_email_list_job.rb
@@ -11,13 +11,7 @@ class UpdateEmailListJob < ActiveJob::Base
       subscriber = CreateSend::Subscriber.new(auth, ENV.fetch(list_env_key), email_was)
       subscriber.update(email.strip, name, custom_fields, false)
     rescue CreateSend::BadRequest => br
-      if br.message.include?("Subscriber not in list") or
-           br.message.include?("provide a valid email address") or
-             email_was.nil?
-        SubscribeEmailListJob.perform_later(email.strip, name, list_env_key, custom_fields)
-      else
-        raise br
-      end
+      Rails.logger.info("#{email} - #{br.message}")
     end
   end
 end
