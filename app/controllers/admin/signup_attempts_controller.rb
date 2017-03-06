a3628a9e34cbadf56e2d296f7c3646ae5837eff8
Joe Sak <joe@joesak.com>
2017-03-06 15:53:50 -0600

    Add 'all' to signup attempts status filter for Admins

--- app/controllers/admin/signup_attempts_controller.rb
+++ app/controllers/admin/signup_attempts_controller.rb
@@ -1,11 +1,11 @@
 module Admin
   class SignupAttemptsController < AdminController
     def index
-      params[:status] = "pending" if params[:status].blank?
+      params[:status] = "All" if params[:status].blank?
       params[:per_page] = WillPaginate.per_page if params[:per_page].blank?
       params[:page] = 1 if params[:page].blank?
 
-      @signup_attempts = SignupAttempt.send(params[:status])
+      @signup_attempts = SignupAttempt.send(params[:status].gsub(' ', '_').downcase)
         .where("email LIKE ?", "%#{params[:text]}%")
         .page(params[:page].to_i)
         .per_page(params[:per_page].to_i)
