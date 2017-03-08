3f94250262de749b96d6297b1cf019e021d68b75
Joe Sak <joe@joesak.com>
2017-03-08 10:51:37 -0600

    Fix team member invite again on redirecting on nil invites.... ugh

--- app/controllers/concerns/team_member_invite_controller.rb
+++ app/controllers/concerns/team_member_invite_controller.rb
@@ -31,13 +31,13 @@ module TeamMemberInviteController
   def destroy
     if current_profile.team_ids.any?
       @invite = TeamMemberInvite.find_by(team_id: current_profile.team_ids,
-                                        invite_token: params.fetch(:id))
+                                         invite_token: params.fetch(:id))
       if @invite
         @invite.destroy
         redirect_to [account_type, @invite.team],
           success: t("controllers.invites.destroy.success", name: @invite.invitee_name)
       else
-        redirect_to [account_type, @invite.team],
+        redirect_to [account_type, :dashboard],
           notice: t("controllers.invites.destroy.not_found")
       end
     else
