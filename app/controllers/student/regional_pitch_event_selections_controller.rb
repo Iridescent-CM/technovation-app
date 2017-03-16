13e0f05263ea997f1a633568cfb2dab7f09dc451
Joe Sak <joe@joesak.com>
2017-03-16 11:52:20 -0500

    Add TODO ambassador mailers for teams leaving / joining RPEs

--- app/controllers/student/regional_pitch_event_selections_controller.rb
+++ app/controllers/student/regional_pitch_event_selections_controller.rb
@@ -18,20 +18,16 @@ module Student
 
     private
     def do_destroy
-      if current_team.selected_regional_pitch_event.live?
-        AmbassadorMailer.team_left_event(
-          current_team.selected_regional_pitch_event.regional_ambassador_profile.account,
-          current_team.selected_regional_pitch_event,
-          current_team
-        ).deliver_later
-      end
-
+      notify_ambassador_of_leaving_team
       current_team.regional_pitch_events.destroy_all
+
       redirect_to [:student, :dashboard],
         success: t("controllers.student.regional_pitch_event_selections.destroy.success")
     end
 
     def do_create(params)
+      notify_ambassador_of_leaving_team
+
       current_team.regional_pitch_events.destroy_all
       event = RegionalPitchEvent.find(params.fetch(:event_id))
       current_team.regional_pitch_events << event
@@ -45,6 +41,16 @@ module Student
 
       redirect_to [:student, current_team.selected_regional_pitch_event],
         success: t("controllers.student.regional_pitch_event_selections.create.success")
+    end
+
+    def notify_ambassador_of_leaving_team
+      if current_team.selected_regional_pitch_event.live?
+        AmbassadorMailer.team_left_event(
+          current_team.selected_regional_pitch_event.regional_ambassador_profile.account,
+          current_team.selected_regional_pitch_event,
+          current_team
+        ).deliver_later
+      end
     end
   end
 end
