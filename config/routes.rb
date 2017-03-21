cd8171d26dc040d42ff89b0da7f1df62ca0610d0
Joe Sak <joe@joesak.com>
2017-03-21 14:46:11 -0500

    How did this route go missing. How

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,7 @@ Rails.application.routes.draw do
     resource :team_submission_screenshot_upload_confirmation, only: :show
     resource :team_submission_file_upload_confirmation, only: :show
 
+    resources :team_submission_pitch_presentations, only: [:new, :edit]
 
     resource :regional_pitch_event_selection, only: [:show, :create, :update, :destroy]
     resources :regional_pitch_events, only: :show
