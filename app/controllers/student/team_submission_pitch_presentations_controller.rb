90011238131fff6e0c8130df51ed1c17a2f8e4f6
Joe Sak <joe@joesak.com>
2017-03-21 14:52:20 -0500

    What is going on with the pitch presentation workflow

--- app/controllers/student/team_submission_pitch_presentations_controller.rb
+++ app/controllers/student/team_submission_pitch_presentations_controller.rb
@@ -18,7 +18,7 @@ module Student
       @pitch_presentation_uploader = FileUploader.new
       @pitch_presentation_uploader.success_action_redirect = student_team_submission_file_upload_confirmation_url(
         file_attribute: :pitch_presentation,
-        back: edit_student_team_submission_pitch_presentation_path
+        back: edit_student_team_submission_pitch_presentation_path(@submission)
       )
     end
   end
