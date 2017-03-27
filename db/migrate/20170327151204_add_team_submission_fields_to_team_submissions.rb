01aa1f500d6acee80e54dc5f5ae7643497f82038
Joe Sak <joe@joesak.com>
2017-03-27 10:13:46 -0500

    Add fields for submission scoring business logic

---
+++ db/migrate/20170327151204_add_team_submission_fields_to_team_submissions.rb
@@ -0,0 +1,9 @@
+class AddTeamSubmissionFieldsToTeamSubmissions < ActiveRecord::Migration
+  def change
+    add_column :team_submissions, :submission_score_count, :integer
+    add_index :team_submissions, :submission_score_count
+    add_column :team_submissions, :judge_opened_id, :integer
+    add_column :team_submissions, :judge_opened_at, :datetime
+    add_index :team_submissions, :judge_opened_at
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322194755) do
+ActiveRecord::Schema.define(version: 20170327151204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_stat_statements"
 
   create_table "accounts", force: :cascade do |t|
     t.string   "email",                                       null: false
@@ -399,8 +400,10 @@ ActiveRecord::Schema.define(version: 20170322194755) do
     t.text     "entrepreneurship_comment"
     t.text     "pitch_comment"
     t.text     "overall_comment"
+    t.datetime "completed_at"
   end
 
+  add_index "submission_scores", ["completed_at"], name: "index_submission_scores_on_completed_at", using: :btree
   add_index "submission_scores", ["judge_profile_id"], name: "index_submission_scores_on_judge_profile_id", using: :btree
   add_index "submission_scores", ["team_submission_id"], name: "index_submission_scores_on_team_submission_id", using: :btree
 
@@ -438,9 +441,14 @@ ActiveRecord::Schema.define(version: 20170322194755) do
     t.integer  "development_platform"
     t.boolean  "source_code_file_uploaded"
     t.string   "slug"
+    t.integer  "submission_score_count"
+    t.integer  "judge_opened_id"
+    t.datetime "judge_opened_at"
   end
 
+  add_index "team_submissions", ["judge_opened_at"], name: "index_team_submissions_on_judge_opened_at", using: :btree
   add_index "team_submissions", ["stated_goal"], name: "index_team_submissions_on_stated_goal", using: :btree
+  add_index "team_submissions", ["submission_score_count"], name: "index_team_submissions_on_submission_score_count", using: :btree
 
   create_table "teams", force: :cascade do |t|
     t.string   "name",                                      null: false
