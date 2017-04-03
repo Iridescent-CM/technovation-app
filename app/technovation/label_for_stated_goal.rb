d688b2723b48268a750ea793b76d044c8524bbc7
Joe Sak <joe@joesak.com>
2017-04-03 13:36:31 -0500

    Update the SDG labels

--- app/technovation/label_for_stated_goal.rb
+++ app/technovation/label_for_stated_goal.rb
@@ -2,22 +2,22 @@ module LabelForStatedGoal
   def self.call(goal)
     case goal
     when "Poverty"
-      "Poverty Examples include: eradicating extreme poverty (measured as living under $1.25 a day), implementing social protection systems for all, and ensuring that all men and women have equal access to economic resources."
+      "This team chose Poverty as their sustainable development goal. Examples include: eradicating extreme poverty (measured as living under $1.25 a day), implementing social protection systems for all, and ensuring that all men and women have equal access to economic resources."
 
     when "Environment"
-      "Environment examples include: improving education and awareness about climate change and strengthening resilience to climate-change hazards in all countries."
+      "This team chose Environment as their sustainable development goal. Examples include: improving education and awareness about climate change and strengthening resilience to climate-change hazards in all countries."
 
     when "Peace"
-      "Peace examples include: significantly reducing violence, ending abuse of children, reducing corruption and bribery, ensuring equal access to justice for all, and ensuring public access to information."
+      "This team chose Peace as their sustainable development goal. Examples include: significantly reducing violence, ending abuse of children, reducing corruption and bribery, ensuring equal access to justice for all, and ensuring public access to information."
 
     when "Equality"
-      "Equality examples include: ending all form of discrimination against girls and women everywhere and ensuring women’s full and effective participation and equal opportunities for leadership."
+      "This team chose Equality as their sustainable development goal. Examples include: ending all form of discrimination against girls and women everywhere and ensuring women’s full and effective participation and equal opportunities for leadership."
 
     when "Education"
-      "TODO: Education label"
+      "This team chose Education as their sustainable development goal. Examples include: ensuring quality education for all, creating lifelong learning opportunities, increasing primary and secondary school completion rates."
 
     when "Health"
-      "TODO: Health label"
+      "This team chose Health as their sustainable development goal. Examples include: promoting well-being at all ages, reducing health risks, improving healthcare for women, reducing cases of diseases, addressing substance abuse."
 
     when "No goal selected!"
       "The team did not select a sustainable development goal. Score should be 0 – Incomplete."
