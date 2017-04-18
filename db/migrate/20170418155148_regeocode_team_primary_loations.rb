8d586117e24fbb710142df00d8f3fca54ac5d073
Joe Sak <joe@joesak.com>
2017-04-18 11:03:57 -0500

    Add migration to fill in team primary location info from existing data

---
+++ db/migrate/20170418155148_regeocode_team_primary_loations.rb
@@ -0,0 +1,20 @@
+class RegeocodeTeamPrimaryLoations < ActiveRecord::Migration
+  def up
+    Team.current.where("city IS NULL AND state_province IS NULL and country IS NULL").each do |team|
+      next unless team.members.any?
+
+      team.reverse_geocode
+
+      if team.city.blank?
+        m = team.members.first
+        team.city = m.city
+        team.state_province = m.state_province
+        team.country = m.country
+      end
+
+      puts "Reverse geocoding Team `#{team.name}`: #{team.primary_location}"
+
+      team.save!
+    end
+  end
+end
