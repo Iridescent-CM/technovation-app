ec66c21df59cafc1df68e8d76b73d72e0517bccd
Joe Sak <joe@joesak.com>
2017-04-18 15:23:06 -0500

    Convert params to hash for export jobs

--- app/controllers/admin/exports_controller.rb
+++ app/controllers/admin/exports_controller.rb
@@ -1,7 +1,7 @@
 module Admin
   class ExportsController < AdminController
     def create
-      ExportJob.perform_later(current_admin, params)
+      ExportJob.perform_later(current_admin, params.to_h) # TODO: research the unsupported argument type ActionController::Parameters error
       cookies.permanent[:export_email] = params[:export_email]
       redirect_to :back, success: t("controllers.exports.create.success")
     end
--- app/controllers/regional_ambassador/exports_controller.rb
+++ app/controllers/regional_ambassador/exports_controller.rb
@@ -1,7 +1,7 @@
 module RegionalAmbassador
   class ExportsController < RegionalAmbassadorController
     def create
-      RegionalExportJob.perform_later(current_ambassador, params)
+      RegionalExportJob.perform_later(current_ambassador, params.to_h) # TODO: research the unsupported argument type ActionController::Parameters error
       redirect_to :back, success: t("controllers.exports.create.success")
     end
   end
