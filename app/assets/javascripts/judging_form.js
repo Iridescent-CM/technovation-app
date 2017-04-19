515e04105986a322248edc0ba64b0e8fcab07b03
Joe Sak <joe@joesak.com>
2017-04-19 14:50:17 -0500

    Format js

--- app/assets/javascripts/judging_form.js
+++ app/assets/javascripts/judging_form.js
@@ -13,6 +13,7 @@
   var questionSelector = '.input, .question-technical-checklist';
   var questions = activeSection.querySelectorAll(questionSelector);
   var activeQuestion = questions[activeQuestionIndex];
+
   activeQuestion.classList.add('active'); // Not BEM cause this thing is being generated dynamically
   activeSection.classList.add('judging-form__section--active');
 
@@ -25,8 +26,10 @@
 
   var saveAllButton = document.getElementById('submit-all-forms');
   saveAllButton.addEventListener('click', saveProgressAll);
+
   var saveAllPending = false;
   var saveAllPendingCount = 0;
+
   $(document).ajaxSuccess(handleAjaxSave);
 
   var verifySubmissionButton = document.getElementById('verify-scores');
@@ -39,8 +42,11 @@
   }
 
   setShouldButtonsBeDisabled();
+
   generateMarkup();
+
   initRangeSliders();
+
   if (_.includes(window.location.search, "review")) {
     setFormDisplay('maximize');
   } else {
@@ -53,8 +59,10 @@
   var hasVerifiedTechnicalChecklist = (function() {
     var checkboxes = tcForm.querySelectorAll('[type="checkbox"]');
     var hasVerified = false;
+
     while (hasVerified === false) {
       var checkboxCount = checkboxes.length;
+
       for (var i = 0; i < checkboxCount; i++) {
         if (checkboxes[i].checked) {
           hasVerified = true;
@@ -64,6 +72,7 @@
     }
     return hasVerified;
   })();
+
   handleTechnicalChecklist();
 
   /**
@@ -81,6 +90,7 @@
         generateHelperInput(currentQuestion);
       }
     }
+
     makeBreadcrumbs();
     setActiveBreadcrumbs();
   }
@@ -88,20 +98,26 @@
   function makeBreadcrumbs() {
     var breadcrumbsWrapper = formWrapper.querySelector('.judging-form__breadcrumbs-wrapper');
     var container = document.createElement('div');
+
     container.classList.add('judging-form__breadcrumbs');
+
     for (var i = 0; i < sections.length; i++) {
       var currentSection = sections[i];
       var currentSectionName = currentSection
         .querySelector('h1')
         .innerText;
+
       currentSection.dataset.label = currentSectionName;
       currentSection.dataset.index = i;
       currentSection.dataset.isActive = false;
+
       var breadcrumb = document.createElement('div');
       breadcrumb.classList.add('judging-form__crumb');
+
       var labelEl = document.createElement('div');
       labelEl.classList.add('judging-form__crumb-label');
       labelEl.innerText = currentSectionName;
+
       breadcrumb.appendChild(labelEl);
       container.appendChild(breadcrumb);
     }
@@ -119,11 +135,13 @@
   function makeRadioHelper(question) {
     var radios = question.querySelectorAll('.radio input');
     var checkedValue;
+
     forEach(radios, function(r) {
       if (r.checked) {
         checkedValue = r.value;
       }
     });
+
     var inputWrapper = document.createElement('div');
     inputWrapper.classList.add('judge-helper', 'judge-helper--range');
 
@@ -138,7 +156,9 @@
     // Helper description text div
     var descriptionTextEl = document.createElement('div');
     var tickMarksWrapper = document.createElement('div');
+
     tickMarksWrapper.classList.add('judge-helper__tick-marks-wrapper');
+
     for (var i = 0; i < radios.length; i++) {
       descriptionTextEl.dataset['description-' + i] = radios[i].parentElement.textContent;
     }
@@ -162,7 +182,6 @@
     var cannedResponses = question.dataset.cannedResponses;
 
     if (!cannedResponses) return;
-
 
     var options = JSON.parse(cannedResponses);
 
@@ -198,18 +217,24 @@
     e.stopPropagation();
     saveProgress();
     questions[activeQuestionIndex].classList.remove('active');
+
     var isBeginningOfSection = activeQuestionIndex === 0;
+
     if (isBeginningOfSection) {
       activeSectionIndex = activeSectionIndex - 1;
       activeSection.classList.remove('judging-form__section--active');
+
       activeSection = sections[activeSectionIndex];
       activeSection.classList.add('judging-form__section--active');
+
       questions = activeSection.querySelectorAll(questionSelector);
       activeQuestionIndex = questions.length - 1;
     } else {
       activeQuestionIndex = activeQuestionIndex - 1;
     }
+
     questions[activeQuestionIndex].classList.add('active');
+
     setShouldButtonsBeDisabled();
     setActiveBreadcrumbs();
   }
@@ -217,38 +242,48 @@
   function goToNextQuestion(e) {
     e.stopPropagation();
     saveProgress();
+
     var isLastSection = Array.prototype.indexOf.call(sections, activeSection) === (sections.length - 1);
     var isEndOfSection = questions.length === (activeQuestionIndex + 1);
+
     if (isLastSection && isEndOfSection) {
       reviewSubmissionForm();
       return; 
     }
+
     questions[activeQuestionIndex].classList.remove('active');
     activeQuestionIndex = isEndOfSection ? 0 : activeQuestionIndex + 1;
+
     if (isEndOfSection) {
       activeSectionIndex = activeSectionIndex + 1;
       activeSection.classList.remove('judging-form__section--active');
+
       activeSection = sections[activeSectionIndex];
       activeSection.classList.add('judging-form__section--active');
+
       questions = activeSection.querySelectorAll(questionSelector);
     }
+
     questions[activeQuestionIndex].classList.add('active');
+
     setShouldButtonsBeDisabled();
     setActiveBreadcrumbs();
   }
 
   function saveProgress() {
     var submitButton = activeSection.querySelector('input[type="submit"]');
-    if (submitButton) {
+
+    if (submitButton)
       submitButton.click();
-    }
   }
 
   function saveProgressAll() {
     var submitButtons = formWrapper.querySelectorAll('input[type="submit"]');
+
     forEach(submitButtons, function(button) {
       button.click();
     });
+
     saveAllPending = true;
     saveAllButton.disabled = true;
   }
@@ -471,13 +506,16 @@
     // Make Checkboxes Fancy Again
     var checkboxes = document.getElementsByClassName('judging-tc-modal__checkbox-wrapper');
     var labelSpanClass = 'judging-tc-modal__label-text';
+
     for (var i = 0; i < checkboxes.length; i++) {
       var fancyToggle = document.createElement('span');
       fancyToggle.classList.add('fancy-toggle');
+
       var checkbox = checkboxes[i].querySelector('[type="checkbox"]');
       var label = checkbox.parentElement;
       var labelText = checkbox.parentElement.innerText;
       var labelSpan = document.createElement('span');
+
       labelSpan.innerText = labelText;
       labelSpan.classList.add(labelSpanClass);
       label.lastChild.remove();
@@ -488,13 +526,17 @@
     // If user is on touch device, change copy on the technical checklist label toggles
     function handleDetectTouch() {
       var labelSpans = document.getElementsByClassName(labelSpanClass);
+
       for (var i = 0; i < labelSpans.length; i++) {
         labelSpans[i].innerText = 'Tap to Verify';
       }
+
       var figCaptions = document.querySelectorAll('.judging-tc-modal__image-wrapper figcaption');
+
       for (var i = 0; i < figCaptions.length; i++) {
         figCaptions[i].innerText = 'Tap to expand';
       }
+
       window.removeEventListener('touchstart', handleDetectTouch);
     };
     window.addEventListener('touchstart', handleDetectTouch);
@@ -502,6 +544,7 @@
     // Paper prototype/flow chart image expand
     setTimeout(function() {
       var imageFigures = document.querySelectorAll('.judging-tc-modal__image-wrapper figure');
+
       for (var i = 0; i < imageFigures.length; i++) {
         var figure = imageFigures[i];
         figure.addEventListener('click', maximizeImage);
@@ -510,20 +553,27 @@
 
     function maximizeImage(e) {
       var figure = e.currentTarget;
+
       var positionerWrapper = document.createElement('div');
       positionerWrapper.classList.add('judging-tc-fullscreen-image');
+
       var contentWrapper = document.createElement('div');
       contentWrapper.classList.add('judging-tc-fullscreen-image__content');
+
       var closeButtonWrapper = document.createElement('div');
       closeButtonWrapper.classList.add('judging-tc-fullscreen-image__close-button-wrapper');
+
       var closeButton = document.createElement('span');
       closeButton.classList.add('fa', 'fa-times');
       closeButtonWrapper.appendChild(closeButton);
+
       var pseudoImageElement = document.createElement('div');
       pseudoImageElement.classList.add('judging-tc-fullscreen-image__pseudo-img');
       pseudoImageElement.style.backgroundImage = 'url("' + figure.firstElementChild.src + '")';
+
       contentWrapper.appendChild(closeButtonWrapper);
       contentWrapper.appendChild(pseudoImageElement);
+
       positionerWrapper.appendChild(contentWrapper);
       document.body.appendChild(positionerWrapper);
 
@@ -562,7 +612,9 @@
   function reviewSubmissionForm() {
     showToast = false;
     saveProgressAll();
+
     var submissionVerification = verifySubmission();
+
     if (submissionVerification.isValid) {
       createVerificationView();
     } else {
@@ -577,12 +629,15 @@
         error: 'Please review the Technical Checklist before continuing'
       };
     }
+
     var allSectionsHaveComments = true;
+
     forEach(sections, function(section) {
       if (section.querySelector('textarea') && !section.querySelector('textarea').value) {
         allSectionsHaveComments = false;
       }
     });
+
     if (!allSectionsHaveComments) {
       return {
         isValid: false,
@@ -612,23 +667,33 @@
       var sectionWrapper = document.createElement('div');
       var headerText = section.querySelector('h1').innerText;
       var header = document.createElement('h2');
+
       header.classList.add('verify-submission__header', 'appy-title');
       header.innerText = headerText;
       sectionWrapper.appendChild(header);
+
       var items = [];
+
       if (form) {
         var inputs = form.getElementsByClassName('input');
+
         for (var c = 0; c < inputs.length; c++) {
           var label = document.createElement('p');
           var value = document.createElement('p');
+
           label.classList.add('verify-submission__label');
           value.classList.add('verify-submission__value');
+
           var current = inputs[c];
+
           label.innerText = current.querySelector('label').innerText;
+
           var inputType = current.querySelector('input:checked') ? 'checkbox' : 'textarea';
+
           value.innerText = inputType === 'checkbox'
             ? current.querySelector('input:checked').nextSibling.textContent
             : current.querySelector('textarea').value;
+
           sectionWrapper.appendChild(label);
           sectionWrapper.appendChild(value);
         }
@@ -636,12 +701,16 @@
         // Technical Checklist
         var label = document.createElement('p');
         var value = document.createElement('p');
+
         label.classList.add('verify-submission__label');
         value.classList.add('verify-submission__value');
+
         var checkedCount = tcForm.querySelectorAll('input[type="checkbox"]:checked').length;
         var totalCount = tcForm.querySelectorAll('input[type="checkbox"]').length;
+
         label.innerText = 'Number of technical checklist items verified:';
         value.innerText = checkedCount + ' out of ' + totalCount;
+
         sectionWrapper.appendChild(label);
         sectionWrapper.appendChild(value);
       }
@@ -650,6 +719,7 @@
 
     var buttonsWrapper = document.createElement('div');
     buttonsWrapper.classList.add('verify-submission__buttons-wrapper');
+
     var submitButton = document.createElement('button');
     buttonsWrapper.appendChild(submitButton);
     submitButton.classList.add('appy-button');
