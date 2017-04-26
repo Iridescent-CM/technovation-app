203e71d7bd335f84c60c48518f90abf66625a5d5
Joe Sak <joe@joesak.com>
2017-04-26 13:52:39 -0500

    Allow apostrophes in email validator

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,6 +1,6 @@
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    if not value.match(/^[^@\.][\w\.\-]*(?:\+?[\w\.\-]+)?@[\w\.\-]+\w+$/)
+    if not value.match(/^[^@\.][\w\.\-']*(?:\+?[\w\.\-]+)?@[\w\.\-]+\w+$/)
       record.errors.add(attribute, :invalid)
     end
   end
