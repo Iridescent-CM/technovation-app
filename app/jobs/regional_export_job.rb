7503b63f9b3ceb4dd2ed5459daa5c609231a6aac
Joe Sak <joe@joesak.com>
2017-05-02 16:26:02 -0500

    Show total / possible in separate columns

--- app/jobs/regional_export_job.rb
+++ app/jobs/regional_export_job.rb
@@ -123,6 +123,7 @@ class RegionalExportJob < ActiveJob::Base
         Event\ official?
         Division
         Total
+        Possible
         Average\ completed\ live\ score
         Average\ completed\ virtual\ score
       }
@@ -138,7 +139,8 @@ class RegionalExportJob < ActiveJob::Base
           score.event_type,
           score.event_official_status,
           score.team_submission_team_division_name,
-          "#{score.total}/#{score.total_possible}",
+          score.total,
+          score.total_possible,
           score.average_completed_live_score,
           score.average_completed_virtual_score,
         ]
