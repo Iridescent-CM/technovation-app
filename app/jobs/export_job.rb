c169003e0b8cde89ece8292e0861df9808349611
Mike Larsson <mikelarssonftw@gmail.com>
2017-05-02 15:43:54 -0400

    Move scores export into existing export module and hook up to admin

--- app/jobs/export_job.rb
+++ app/jobs/export_job.rb
@@ -167,6 +167,69 @@ class ExportJob < ActiveJob::Base
     export(filepath, admin, params[:export_email])
   end
 
+  def export_scores(admin, params)
+    send("export_scores_#{params[:type]}", admin, params)
+  end
+
+  def export_scores_summary(admin, params)
+    Tempfile.open(["score-summary-", ".csv"], "./tmp/") do |fh|
+      csv = CSV.new(fh)
+      csv << %w{
+        Team\ name
+        App\ name
+        Region
+        Division
+        Sustainable\ development\ goal
+        #\ of\ incomplete\ scores
+        #\ of\ completed\ scores
+        #\ live
+        #\ virtual
+        Average\ score
+      }
+
+      TeamSubmission.current.each do |s|
+        if s.complete?
+          csv << [s.team_name, s.app_name, s.team.region_name, s.team.division.name, s.stated_goal,
+                  s.submission_scores.incomplete.count, s.submission_scores.complete.count,
+                  s.submission_scores.complete.live.count, s.submission_scores.complete.virtual.count,
+                  s.average_score]
+        end
+      end
+
+      csv.close()
+      export(fh, admin, params[:export_email])
+    end
+  end
+
+  def export_scores_detail(admin, params)
+    Tempfile.open(["score-detail-", ".csv"], "./tmp/") do |fh|
+      csv = CSV.new(fh)
+      csv << %w{
+        Team\ name
+        App\ name
+        Region
+        Division
+        Sustainable\ development\ goal
+        Judge
+        Mentor?
+        Team\ region/division\ names
+        Complete?
+        Total\ score
+      }
+
+      SubmissionScore.all.each do |s|
+        account = s.judge_profile.account
+        csv << [s.team_submission_team_name, s.team_submission_app_name,
+                s.team_submission.team.region_name, s.team_submission.team.division.name, s.team_submission.stated_goal,
+                account.email, account.mentor_profile.present?, account.team_region_division_names,
+                s.complete?, s.total]
+      end
+
+      csv.close()
+      export(fh, admin, params[:export_email])
+    end
+  end
+
   def export(filepath, admin, email)
     file = File.open(filepath)
     export = admin.exports.create!(file: file)
