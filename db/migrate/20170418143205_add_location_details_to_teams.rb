6aee9362aebbab72a9680fe0632cc286155fc883
Joe Sak <joe@joesak.com>
2017-05-03 16:10:40 -0500

    Fix migration inheritances again

--- db/migrate/20170418143205_add_location_details_to_teams.rb
+++ db/migrate/20170418143205_add_location_details_to_teams.rb
@@ -1,4 +1,4 @@
-class AddLocationDetailsToTeams < ActiveRecord::Migration
+class AddLocationDetailsToTeams < ActiveRecord::Migration[4.2]
   def change
     add_column :teams, :city, :string
     add_column :teams, :state_province, :string
