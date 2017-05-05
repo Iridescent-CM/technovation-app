c01e9d91a755c6c056fc6c21b1dcf72674a06245
Joe Sak <joe@joesak.com>
2017-05-05 15:46:15 -0500

    Remove all assets, views, routes, tests, and custom libs

--- app/technovation/reset_password_to_email.rb
+++
@@ -1,8 +0,0 @@
-module ResetPasswordToEmail
-  def self.call(email)
-    a = Account.find_by(email: email)
-    a.update_attributes(skip_existing_password: true,
-                        password: email)
-    puts "#{a.email} password is now #{email}"
-  end
-end
--- spec/factories/honor_code_agreements.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :honor_code_agreement do
-    electronic_signature "Signy McHonor"
-    agreement_confirmed true
-    account
-  end
-end
