f9485869a1e0ee8a0e32da587328b548fa68f24b
Joe Sak <joe@joesak.com>
2017-05-09 12:30:19 -0500

    Re-update average scores on admin restore

--- app/controllers/admin/submission_score_restorations_controller.rb
+++ app/controllers/admin/submission_score_restorations_controller.rb
@@ -3,6 +3,7 @@ module Admin
     def update
       submission = TeamSubmission.friendly.find(params[:id])
       submission.submission_scores.find_each(&:restore)
+      submission.update_average_scores
       redirect_back fallback_location: admin_team_submission_path(submission),
         success: "Scores restored for #{submission.app_name}"
     end
