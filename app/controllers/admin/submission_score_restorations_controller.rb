1348644cb2d94ae14376a63111c14d098b4f2fad
Joe Sak <joe@joesak.com>
2017-05-09 12:51:35 -0500

    Restore `.deleted` scores ... duh doy

--- app/controllers/admin/submission_score_restorations_controller.rb
+++ app/controllers/admin/submission_score_restorations_controller.rb
@@ -2,8 +2,7 @@ module Admin
   class SubmissionScoreRestorationsController < AdminController
     def update
       submission = TeamSubmission.friendly.find(params[:id])
-      submission.submission_scores.find_each(&:restore)
-      submission.update_average_scores
+      submission.submission_scores.deleted.find_each(&:restore)
       redirect_back fallback_location: admin_team_submission_path(submission),
         success: "Scores restored for #{submission.app_name}"
     end
