f0cc91e1264d0c7af8635d25ba5dea2837fd8fb1
Joe Sak <joe@joesak.com>
2017-05-11 14:45:41 -0500

    Remove users from schema... not sure where it came from

--- db/schema.rb
+++ db/schema.rb
@@ -551,28 +551,6 @@ ActiveRecord::Schema.define(version: 20170511160750) do
     t.index ["team_submission_id"], name: "index_technical_checklists_on_team_submission_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
   add_foreign_key "admin_profiles", "accounts"
   add_foreign_key "background_checks", "accounts"
   add_foreign_key "business_plans", "team_submissions"
