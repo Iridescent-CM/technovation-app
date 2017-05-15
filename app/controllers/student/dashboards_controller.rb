1291fe37740688c795d779f5a40d2ab7fcf1adba
Alli Colyer <allicolyer@gmail.com>
2017-05-15 11:30:56 -0700

    Clean up code

--- app/controllers/student/dashboards_controller.rb
+++ app/controllers/student/dashboards_controller.rb
@@ -1,67 +1,35 @@
 module Student
   class DashboardsController < StudentController
-
     def show
       @regional_events = RegionalPitchEvent.available_to(current_team.submission)
       @scores = current_team.submission.submission_scores.quarterfinals
 
-      @ideation_average = ideation_average
-      @technical_average = technical_average
-      @entrepreneurship_average = entrepreneurship_average
-      @pitch_average = pitch_average
-      @overall_impression_average = overall_impression_average
+      @ideation_average = category_average(:ideation)
+      @technical_average = category_average(:technical)
+      @entrepreneurship_average = category_average(:entrepreneurship)
+      @pitch_average = category_average(:pitch)
+      @overall_impression_average = category_average(:overall_impression)
       @best_category = best_category
     end
 
-    def ideation_average
-      sum = 0
-      @scores.each do |score|
-        sum += score.technical_total
+    private
+    def category_average(category)
+      sum = @scores.inject(0.0) do |acc, score|
+        acc += score.public_send("#{category}_total")
       end
-      return sum/@scores.count
-    end
-
-    def technical_average
-      sum = 0
-      @scores.each do |score|
-        sum += score.technical_total
-      end
-      return sum/@scores.count
-    end
-
-    def entrepreneurship_average
-      sum = 0
-      @scores.each do |score|
-        sum += score.entrepreneurship_total
-      end
-      return sum/@scores.count
-    end
-
-    def pitch_average
-      sum = 0
-      @scores.each do |score|
-        sum += score.entrepreneurship_total
-      end
-      return sum/@scores.count
-    end
-
-    def overall_impression_average
-      sum = 0
-      @scores.each do |score|
-        sum += score.overall_impression_total
-      end
-      return sum/@scores.count
+      (sum / @scores.count.to_f).round(2)
     end
 
     def best_category
-      adjusted_scores = {"Ideation": (ideation_average * 25/15),
-                         "Technical": technical_average,
-                         "Entrepreneurship": entrepreneurship_average,
-                         "Pitch": pitch_average,
-                         "Overall Impression": (overall_impression_average * 20/25)}
+      adjusted_scores = {
+        "Ideation": (@ideation_average * 25/15),
+        "Technical": @technical_average,
+        "Entrepreneurship":  @entrepreneurship_average,
+        "Pitch": @pitch_average,
+        "Overall Impression": (@overall_impression_average * 20/25),
+      }
 
-     return adjusted_scores.max_by{|key,value| value}[0]
+      adjusted_scores.max_by { |_,v| v }[0]
     end
-
   end
 end
