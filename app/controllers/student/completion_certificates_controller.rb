9bad5e0f20d25d34a4d2a60f31acf3c32ac3cbdc
Joe Sak <joe@joesak.com>
2017-05-31 12:14:33 -0500

    Fix prep data for student cert FillPdfs, and style the tab a little better

--- app/controllers/student/completion_certificates_controller.rb
+++ app/controllers/student/completion_certificates_controller.rb
@@ -3,7 +3,14 @@ require "fill_pdfs"
 module Student
   class CompletionCertificatesController < StudentController
     def create
-      FillPdfs.(current_student.account, :completion)
+      student_data = {
+        'Recipient Name' => current_student.full_name,
+        'app name' => current_team.submission.app_name,
+        'id' => current_student.account_id
+      }
+
+      FillPdfs.(student_data, :completion)
+
       redirect_to student_dashboard_path,
         success: "Your completion certificate is ready!"
     end
