121a2b94324f4c1fe1771a70d7e454a9c51db736
Alli Colyer <allicolyer@gmail.com>
2017-06-07 15:31:16 -0700

    code review 1

--- app/controllers/student/scores_controller.rb
+++ app/controllers/student/scores_controller.rb
@@ -2,7 +2,7 @@ module Student
   class ScoresController < StudentController
     def show
       @score = current_team.submission.submission_scores.complete.find(params[:id])
-      @division = @score.team_submission.team.division
+      @division = current_team.division
     end
   end
 end
