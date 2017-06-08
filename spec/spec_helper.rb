7ccb63c6c83a4734a0d2dfc7bb7efbc718a0fe21
Joe Sak <joe@joesak.com>
2017-06-08 17:16:55 -0500

    Provide no-rails test helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,10 @@ Dotenv.load
 require "pry"
 require "vcr_helper"
 
+Dir[Dir.pwd + '/spec/no_rails_support/**/*.rb'].each { |f| require f }
+
+include TestHelpers
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
