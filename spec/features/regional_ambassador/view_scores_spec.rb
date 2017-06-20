de2657e3986b7e204ab6782f3051fb5a8179816b
Joe Sak <joe@joesak.com>
2017-06-20 09:39:45 -0500

    Enable RA scores around feature specs

--- spec/features/regional_ambassador/view_scores_spec.rb
+++ spec/features/regional_ambassador/view_scores_spec.rb
@@ -1,6 +1,17 @@
 require "rails_helper"
 
 RSpec.feature "Regional Ambassador views scores" do
+  before do
+    @ra_scores_enabled = ENV.fetch("ENABLE_RA_SCORES") { false }
+    ENV["ENABLE_RA_SCORES"] = "yes"
+  end
+
+  after do
+    if !!@ra_scores_enabled
+      ENV["ENABLE_RA_SCORES"] = @ra_scores_enabled
+    end
+  end
+
   scenario "RA can't pick finals scores, as there is no such thing" do
     ra = FactoryGirl.create(:regional_ambassador, :approved)
     sign_in(ra)
