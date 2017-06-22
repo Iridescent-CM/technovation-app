b56dc4e5a3294903bc07db47ae875f943ed0fe7c
Joe Sak <joe@joesak.com>
2017-06-22 08:25:29 -0500

    Fix Team#add_mentor (removed) usage in Admin::TeamMembershipsController

---
+++ spec/controllers/admin/team_memberships_controller_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.describe Admin::TeamMembershipsController do
+  let(:team) { FactoryGirl.create(:team) }
+
+  before do
+    admin = FactoryGirl.create(:admin)
+    sign_in(admin)
+  end
+
+  describe "POST #create" do
+    it "can add mentors" do
+      mentor = FactoryGirl.create(:mentor)
+
+      post :create, params: {
+        team_id: team.id,
+        account_id: mentor.account_id,
+      }
+
+      expect(team.mentors).to eq([mentor])
+    end
+
+    it "can add students" do
+      student = FactoryGirl.create(:student)
+
+      post :create, params: {
+        team_id: team.id,
+        account_id: student.account_id,
+      }
+
+      expect(team.reload.students).to include(student)
+    end
+  end
+end
