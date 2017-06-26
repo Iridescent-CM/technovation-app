d9ed7f123e24f602f7d61473a136ac9e1617fb95
Joe Sak <joe@joesak.com>
2017-06-26 10:34:06 -0500

    Run webpacker dev server, install hello_react script into v3/layout

--- app/controllers/application/dashboards_controller.rb
+++ app/controllers/application/dashboards_controller.rb
@@ -1,16 +1,31 @@
 module Application
   class DashboardsController < ApplicationController
+    layout -> { choose_layout_based_on_app_version }
+
     def show
-      if current_account.authenticated?
-        last_profile_used = cookies.delete(:last_profile_used)
-
-        path = cookies.delete(:redirected_from) ||
-          (last_profile_used && public_send("#{last_profile_used}_dashboard_path")) ||
-            send("#{current_account.type_name}_dashboard_path")
-
-        redirect_to path
+      if ENV.fetch("APP_VERSION") { 2 }.to_i === 3
+        render template: 'v3/application/dashboards/show'
       else
-        @signup_attempt = SignupAttempt.new(email: params[:email])
+        if current_account.authenticated?
+          last_profile_used = cookies.delete(:last_profile_used)
+
+          path = cookies.delete(:redirected_from) ||
+            (last_profile_used && public_send("#{last_profile_used}_dashboard_path")) ||
+              send("#{current_account.type_name}_dashboard_path")
+
+          redirect_to path
+        else
+          @signup_attempt = SignupAttempt.new(email: params[:email])
+        end
+      end
+    end
+
+    private
+    def choose_layout_based_on_app_version
+      if ENV.fetch("APP_VERSION") { 2 }.to_i === 3
+        'v3/application'
+      else
+        'application'
       end
     end
   end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+  # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
@@ -52,4 +52,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.x.webpacker[:dev_server_host] = "http://localhost:8080"
 end
