8ce34a87df0ca976c763b149cc4f251f5d3e7e90
Joe Sak <joe@joesak.com>
2017-06-27 14:40:46 -0500

    Allow judging_round setting, refactor tests, allow symbols and case insensitive input

--- app/technovation/season_toggles.rb
+++ app/technovation/season_toggles.rb
@@ -1,9 +1,21 @@
 class SeasonToggles
-  VALID_TRUTHY = %w{on yes true} + [true]
-  VALID_FALSEY = %w{off no false} + [false]
+  VALID_TRUTHY = %w{on yes true}
+  VALID_FALSEY = %w{off no false}
   VALID_BOOLS = VALID_TRUTHY + VALID_FALSEY
 
+  VALID_JUDGING_ROUNDS = %w{
+    qf sf quarter_finals semi_finals quarterfinals semifinals off
+  }
+
   class << self
+    def judging_round=(value)
+      store.set(:judging_round, with_judging_round_validation(value))
+    end
+
+    def judging_round
+      store.get(:judging_round)
+    end
+
     def student_signup=(value)
       store.set(:student_signup, with_bool_validation(value))
     end
@@ -18,15 +30,31 @@ class SeasonToggles
     end
 
     def with_bool_validation(value)
-      if VALID_BOOLS.include?(value)
-        value
+      if VALID_BOOLS.include?(value.to_s.downcase)
+        value.to_s.downcase
       else
-        raise InvalidInput, "Use one of: #{VALID_BOOLS}"
+        raise_invalid_input_error(actual: value, expected: VALID_BOOLS.join('|'))
+      end
+    end
+
+    def with_judging_round_validation(value)
+      if !!value and VALID_JUDGING_ROUNDS.include?(value.to_s.downcase)
+        value.to_s.downcase
+      else
+        raise_invalid_input_error(
+          actual: value,
+          expected: VALID_JUDGING_ROUNDS.join('|')
+        )
       end
     end
 
     def convert_to_bool(value)
       VALID_TRUTHY.include?(value)
+    end
+
+    def raise_invalid_input_error(options)
+      raise InvalidInput,
+        "No toggle exists for #{options[:actual]}. Use one of: #{options[:expected]}"
     end
   end
 
--- spec/technovation/season_toggles_spec.rb
+++ spec/technovation/season_toggles_spec.rb
@@ -3,21 +3,70 @@ needs "technovation"
 require "season_toggles"
 
 RSpec.describe SeasonToggles do
+  def expect_bad_input_raises_error(options)
+    [nil, "", " ", "foo"].each do |bad|
+      expect {
+        SeasonToggles.public_send("#{options[:method]}=", bad)
+      }.to raise_error(
+        SeasonToggles::InvalidInput,
+        "No toggle exists for #{bad}. Use one of: #{options[:valid_input]}"
+      )
+    end
+  end
+
+  def expect_good_input_works(options)
+    options[:valid_input].each do |good|
+      expect {
+        SeasonToggles.public_send("#{options[:method]}=", good)
+      }.not_to raise_error
+    end
+  end
+
+  describe "#judging_round=" do
+    context "valid input" do
+      it "allows a specific set of values" do
+        expect_good_input_works(
+          method: :judging_round,
+          valid_input: SeasonToggles::VALID_JUDGING_ROUNDS +
+            %i{QF SF qF Sf QuarteRfinals quaRter_fiNals sEmifinals sEmi_finals oFf}
+        )
+      end
+
+      it "reads back #current_judging_round" do
+        SeasonToggles::VALID_JUDGING_ROUNDS.each do |jr|
+          SeasonToggles.judging_round = jr
+          expect(SeasonToggles.judging_round).to eq(jr)
+        end
+      end
+    end
+
+    context "bad input" do
+      it "raises an exception" do
+        expect_bad_input_raises_error(
+          method: :judging_round,
+          valid_input: SeasonToggles::VALID_JUDGING_ROUNDS.join('|')
+        )
+      end
+    end
+  end
+
   describe "#student_signup=" do
     context "valid input" do
       it "allows a collection of 'boolean' words and booleans" do
-        (%w{on off yes no true false} + [true, false]).each do |good|
-          expect { SeasonToggles.student_signup = good }.not_to raise_error
-        end
+        expect_good_input_works(
+          method: :student_signup,
+          valid_input: SeasonToggles::VALID_BOOLS +
+            %i{On oFf yEs nO tRue fAlse}
+        )
       end
 
       it "reads back a boolean from #student_signup?" do
-        (%w{on yes true} + [true]).each do |on|
+        SeasonToggles::VALID_TRUTHY.each do |on|
           SeasonToggles.student_signup = on
           expect(SeasonToggles.student_signup?).to be true
         end
 
-        (%w{off no false} + [false]).each do |off|
+        SeasonToggles::VALID_FALSEY.each do |off|
           SeasonToggles.student_signup = off
           expect(SeasonToggles.student_signup?).to be false
         end
@@ -25,15 +74,11 @@ RSpec.describe SeasonToggles do
     end
 
     context "bad input" do
-      it "rasies an exception" do
-        [nil, "", " ", "foo"].each do |bad|
-          expect {
-            SeasonToggles.student_signup = bad
-          }.to raise_error(
-            SeasonToggles::InvalidInput,
-            "Use one of: #{SeasonToggles::VALID_BOOLS}"
-          )
-        end
+      it "raises an exception" do
+        expect_bad_input_raises_error(
+          method: :student_signup,
+          valid_input: SeasonToggles::VALID_BOOLS.join('|')
+        )
       end
     end
   end
