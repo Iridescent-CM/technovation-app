281137807354736e85b918d2a1ea04b8bce481de
Joe Sak <joe@joesak.com>
2017-06-27 11:17:24 -0500

    Check SeasonToggles.student_signup?

--- app/technovation/season_toggles.rb
+++ app/technovation/season_toggles.rb
@@ -1,12 +1,25 @@
 require "active_model"
 
 class SeasonToggles
-  VALID_BOOLS = %w{on off yes no true false} + [true, false]
+  VALID_TRUTHY = %w{on yes true} + [true]
+  VALID_FALSEY = %w{off no false} + [false]
+  VALID_BOOLS = VALID_TRUTHY + VALID_FALSEY
 
   class << self
     def student_signup=(value)
-      if VALID_BOOLS.include?(value)
+      @@student_signup = convert_to_bool(value)
+    end
+
+    def student_signup?
+      @@student_signup
+    end
+
+    private
+    def convert_to_bool(value)
+      if VALID_TRUTHY.include?(value)
         true
+      elsif VALID_FALSEY.include?(value)
+        false
       else
         raise InvalidInput, "Use one of: #{VALID_BOOLS}"
       end
--- spec/technovation/season_toggles_spec.rb
+++ spec/technovation/season_toggles_spec.rb
@@ -9,6 +9,18 @@ RSpec.describe SeasonToggles do
           expect { SeasonToggles.student_signup = good }.not_to raise_error
         end
       end
+
+      it "reads back a boolean from #student_signup?" do
+        (%w{on yes true} + [true]).each do |on|
+          SeasonToggles.student_signup = on
+          expect(SeasonToggles).to be_student_signup
+        end
+
+        (%w{off no false} + [false]).each do |off|
+          SeasonToggles.student_signup = off
+          expect(SeasonToggles).not_to be_student_signup
+        end
+      end
     end
 
     context "bad input" do
