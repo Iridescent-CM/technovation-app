181e4e538d9d8c3913979fc73da6e1d62ea4a158
Joe Sak <joe@joesak.com>
2017-07-06 09:06:36 -0500

    Add and pass first tests for mentor access to team submissions according to toggles

---
+++ spec/features/admin/edit_team_submissions_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+RSpec.feature "Toggling editable team submissions" do
+  context "as a mentor" do
+    scenario "editing on" do
+      SeasonToggles.team_submissions_editable = true
+
+      mentor = FactoryGirl.create(:mentor)
+      team = FactoryGirl.create(:team)
+
+      TeamRosterManaging.add(team, mentor)
+      sign_in(mentor)
+
+      within("#team-submission-team-list-team-#{team.id}") do
+        expect(page).to have_link(
+          'Start the submission for this team',
+          href: new_mentor_team_submission_path(team_id: team.id)
+        )
+      end
+    end
+
+    scenario "editing off" do
+      SeasonToggles.team_submissions_editable = false
+
+      mentor = FactoryGirl.create(:mentor)
+      team = FactoryGirl.create(:team)
+
+      TeamRosterManaging.add(team, mentor)
+      sign_in(mentor)
+
+      within("#team-submission-team-list-team-#{team.id}") do
+        expect(page).to have_content("The submission deadline has passed.")
+      end
+    end
+  end
+end
