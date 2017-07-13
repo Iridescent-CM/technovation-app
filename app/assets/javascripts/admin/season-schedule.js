9e19e26733c28a8a674d7cda2039e6db19f89cc5
Joe Sak <joemsak@users.noreply.github.com>
2017-07-13 14:40:45 -0500

    Submit #1153 for QA
    * Toggle signups via admin UI
    
    * Allow admins to toggle team submission editing
    
    * Allow admins to toggle RPE selection
    
    * Allow admins to toggle scores (certificates to be merged into this soon)
    
    * Allow admins to set judging round, which now affects other settings
    
    * Improve Admin UI with JS when judging is toggled
    
    * Fix code broken by bad conflict resolution
    
    * Document js a little better hopefully
    
    * Fix js bug
    
    * Simplify choicesMade hopefully clarifying code
    
    * Simplify choicesMade hopefully clarifying code, part II
    
    * Refactor mercilessly

---
+++ app/assets/javascripts/admin/season-schedule.js
@@ -0,0 +1,124 @@
+(function() {
+  handleJudgingRoundChanges();
+
+  function handleJudgingRoundChanges() {
+    const $judgingRoundChoices = $('[name="season_toggles[judging_round]"]');
+
+    if ($judgingRoundChoices.length === 0)
+      return;
+
+    const $container = $judgingRoundChoices.closest('fieldset'),
+
+          $blockedByJudging = $('[data-blocked-by-judging]'),
+          originalTextColor = $blockedByJudging.css('color'),
+
+          store = localStorage,
+
+          choices = {
+            off: "off",
+            qf: "qf",
+            sf: "sf",
+          },
+
+          selectedChoiceOnLoad = $judgingRoundChoices.filter(function() {
+            return $(this).prop('checked');
+          })[0];
+
+    // see comments in storeOriginalValuesLocally();
+    var lastChoiceMade = selectedChoiceOnLoad.value;
+
+    if (selectedChoiceOnLoad.value !== choices.off) {
+      handleJudgingEnabled();
+    }
+
+    $judgingRoundChoices.on('change', function(e) {
+      const choice = e.target.value;
+
+      if (choice !== choices.off) {
+        handleJudgingEnabled();
+      } else {
+        handleJudgingDisabled();
+      }
+
+      // see comments in storeOriginalValuesLocally();
+      lastChoiceMade = choice;
+    });
+
+    $(window).unload(clearLocalStorage);
+
+    function handleJudgingEnabled() {
+      notifyUserOfEffect();
+      disableSettingsBlockedByJudging();
+      storeOriginalValuesLocally();
+      $blockedByJudging.prop('checked', false);
+
+      function notifyUserOfEffect() {
+        if ($container.find('.notice').length === 0) {
+          var $notice = $('<div>');
+
+          $notice.addClass('notice warning');
+          $notice.text(
+            "Turning judging on disables Submissions, Event Selection, and Scores!"
+          );
+
+          $container.append($notice);
+        }
+      }
+
+      function disableSettingsBlockedByJudging() {
+        $blockedByJudging.prop('disabled', true);
+
+        $blockedByJudging.next('label').css({
+          color: 'lightgrey',
+          fontStyle: 'italic',
+          fontSize: '0.9rem',
+          cursor: 'not-allowed',
+        });
+      }
+
+      function storeOriginalValuesLocally() {
+        if (lastChoiceMade === choices.off) {
+          $blockedByJudging.each(function() {
+            const choiceElemId = $(this).prop('id'),
+                  isChecked = $(this).prop('checked');
+
+            store[choiceElemId] = isChecked;
+          });
+        } // else
+        // judging had been on, meaning all the settings were set to false
+        // and we don't want to store that state
+      }
+    }
+
+    function handleJudgingDisabled() {
+      $container.find('.notice').remove();
+      enableSettingsBlockedByJudging();
+      restoreOriginalValues();
+      clearLocalStorage();
+
+      function enableSettingsBlockedByJudging() {
+        $blockedByJudging.removeAttr('disabled');
+
+        $blockedByJudging.next('label').css({
+          color: originalTextColor,
+          fontStyle: 'normal',
+          fontSize: '1rem',
+          cursor: 'pointer',
+        });
+      }
+
+      function restoreOriginalValues() {
+        $blockedByJudging.each(function() {
+          const originalVal = store[$(this).prop('id')];
+          $(this).prop('checked', originalVal === "true");
+        });
+      }
+    }
+
+    function clearLocalStorage() {
+      $blockedByJudging.each(function() {
+        store.removeItem($(this).prop('id'));
+      });
+    }
+  }
+})();
