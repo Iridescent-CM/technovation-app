7fcdbeb3462618a56163a8432f3331f2d85baadc
Mike Larsson <mikelarssonftw@gmail.com>
2017-07-14 16:43:39 -0400

    address review comments

--- app/technovation/season_toggles.rb
+++ app/technovation/season_toggles.rb
@@ -44,7 +44,7 @@ class SeasonToggles
 
     %w{mentor student}.each do |scope|
       define_method("#{scope}_survey_link=") do |attrs|
-        attrs[:changed_at] = Time.zone.now
+        attrs[:changed_at] = Time.current
         store.set("#{scope}_survey_link", JSON.generate(attrs.to_h))
       end
     end
@@ -53,6 +53,10 @@ class SeasonToggles
       %w{text url changed_at}.all? do |key|
         !!survey_link(scope, key) and not survey_link(scope, key).empty?
       end
+    end
+
+    def show_survey_link_modal?(scope, last_shown)
+      survey_link_available?(scope) and not survey_link(scope, "changed_at") == last_shown
     end
 
     def set_survey_link(scope, text, url)
