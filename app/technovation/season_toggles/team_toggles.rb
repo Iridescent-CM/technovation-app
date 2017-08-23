203a7e2e109a3242dd3eb6fb2468fa073c579ecd
Joe Sak <joe@joesak.com>
2017-08-23 18:59:25 -0500

    Enable bang operator of setting season toggles: team building and editing submissions

--- app/technovation/season_toggles/team_toggles.rb
+++ app/technovation/season_toggles/team_toggles.rb
@@ -21,12 +21,20 @@ class SeasonToggles
         convert_to_bool(store.get(:team_building_enabled))
       end
 
+      def team_building_enabled!
+        self.team_building_enabled = true
+      end
+
       def team_submissions_editable=(value)
         store.set(:team_submissions_editable, with_bool_validation(value))
       end
 
       def team_submissions_editable?
         convert_to_bool(store.get(:team_submissions_editable))
+      end
+
+      def team_submissions_editable!
+        self.team_submissions_editable = true
       end
     end
   end
