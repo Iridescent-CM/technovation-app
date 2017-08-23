e36b477dc94ff69a06e5f873464ce100ea7d9e95
Joe Sak <joe@joesak.com>
2017-08-23 18:59:45 -0500

    Nitpicky code formatting

--- app/controllers/student/team_submission_screenshot_upload_confirmations_controller.rb
+++ app/controllers/student/team_submission_screenshot_upload_confirmations_controller.rb
@@ -1,9 +1,16 @@
 module Student
   class TeamSubmissionScreenshotUploadConfirmationsController < StudentController
     def show
-      ProcessScreenshotsJob.perform_later(current_team.submission, params.fetch(:key))
+      ProcessScreenshotsJob.perform_later(
+        current_team.submission,
+        params.fetch(:key)
+      )
+
       flash.now[:success] = t("controllers.teams.show.image_processing")
-      @unprocessed_photo_url = "//s3.amazonaws.com/#{params[:bucket]}/#{params[:key]}"
+
+      @unprocessed_photo_url =
+        "//s3.amazonaws.com/#{params[:bucket]}/#{params[:key]}"
+
       render template: "student/team_photo_upload_confirmations/show"
     end
   end
