eb8beee438c2bdcc0dc3daf81fcc1abc674fa7cd
Joe Sak <joe@joesak.com>
2017-08-25 12:24:31 -0500

    Fix student preview of mentor profile

--- app/controllers/student/mentors_controller.rb
+++ app/controllers/student/mentors_controller.rb
@@ -1,11 +1,6 @@
 class Student::MentorsController < StudentController
   def show
-    if current_student.team.present?
-      @mentor = MentorProfile.find(params.fetch(:id))
-      @mentor_invite = MentorInvite.new
-    else
-      redirect_to student_dashboard_path,
-        alert: t("controllers.student.mentors.show.no_team")
-    end
+    @mentor = MentorProfile.find(params.fetch(:id))
+    @mentor_invite = MentorInvite.new
   end
 end
