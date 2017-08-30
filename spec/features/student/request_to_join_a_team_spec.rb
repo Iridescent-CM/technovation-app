a6bfbec960f51ec7edfe25e123f206b9e8c9cf88
Joe Sak <joemsak@users.noreply.github.com>
2017-08-30 12:06:19 -0500

    Submit #1211 for QA
    * Move shared invitation partial and leave it open to other scopes of invites
    
    * Nitpicky code formatting
    
    * Add UI to show students that they have a pending join request, lock out joining/creating while it exists

--- spec/features/student/request_to_join_a_team_spec.rb
+++ spec/features/student/request_to_join_a_team_spec.rb
@@ -1,17 +1,20 @@
 require "rails_helper"
 
-RSpec.feature "Students request to join a team", vcr: { match_requests_on: [:method, :host] }, no_es_stub: true do
-  before { SeasonToggles.team_building_enabled="yes" }
+RSpec.feature "Students request to join a team",
+  vcr: { match_requests_on: [:method, :host] },
+  no_es_stub: true do
+
+  before { SeasonToggles.team_building_enabled! }
 
   scenario "students already on a team don't see the link" do
-    student = FactoryGirl.create(:student, :on_team)
+    student = FactoryGirl.create(:student, :on_team, not_onboarded: true)
     sign_in(student)
     expect(page).not_to have_link("Join a team")
   end
 
   context "a valid student requestor" do
     let!(:team) { FactoryGirl.create(:team) } # Default is in Chicago
-    let!(:student) { FactoryGirl.create(:student) } # Default Chicago
+    let!(:student) { FactoryGirl.create(:student, not_onboarded: true) } # Default Chicago
 
     before do
       ActionMailer::Base.deliveries.clear
@@ -25,6 +28,18 @@ RSpec.feature "Students request to join a team", vcr: { match_requests_on: [:met
         "No join request email was sent"
       mail = ActionMailer::Base.deliveries.last
       expect(mail.subject).to eq("A student has asked to join your team!")
+    end
+
+    scenario "the requesting student can see their pending request" do
+      within('.join_request') do
+        expect(page).to have_content("You have asked to join a team")
+        expect(page).to have_content(team.name)
+        expect(page).to have_content(team.primary_location)
+        expect(page).to have_content(team.division_name.humanize)
+        expect(page).to have_css("img.thumbnail-md[src*='#{team.team_photo_url}']")
+      end
+
+      expect(page).not_to have_link("Join a team")
     end
 
     scenario "student accepts the request" do
@@ -41,7 +56,9 @@ RSpec.feature "Students request to join a team", vcr: { match_requests_on: [:met
       expect(mail.to).to eq([JoinRequest.last.requestor_email])
       expect(mail.subject).to eq("Your request to join #{team.name} was accepted!")
       expect(mail.body.to_s).to include("Hi #{JoinRequest.last.requestor_first_name}!")
-      expect(mail.body.to_s).to include("#{team.name} accepted your request to be a member!")
+      expect(mail.body.to_s).to include(
+        "#{team.name} accepted your request to be a member!"
+      )
 
       url = student_team_url(team,
                              host: ENV["HOST_DOMAIN"],
@@ -58,12 +75,16 @@ RSpec.feature "Students request to join a team", vcr: { match_requests_on: [:met
       click_link "My team"
       click_link "decline"
 
-      expect(ActionMailer::Base.deliveries.count).not_to be_zero, "No join request decline email was sent"
+      expect(ActionMailer::Base.deliveries.count).not_to be_zero,
+        "No join request decline email was sent"
+
       mail = ActionMailer::Base.deliveries.last
       expect(mail.to).to eq([JoinRequest.last.requestor_email])
       expect(mail.subject).to eq("Your request to join #{team.name} was declined")
       expect(mail.body).to include("Hi #{JoinRequest.last.requestor_first_name},")
-      expect(mail.body).to include("#{team.name} has declined your request to be a member.")
+      expect(mail.body).to include(
+        "#{team.name} has declined your request to be a member."
+      )
     end
   end
 end
