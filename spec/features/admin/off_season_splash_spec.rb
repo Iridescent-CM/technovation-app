4214dbc1a5f98df222def9c85297c3d18efe44c5
Joe Sak <joe@joesak.com>
2017-09-05 12:41:27 -0500

    Fix the related splash screen spec

--- spec/features/admin/off_season_splash_spec.rb
+++ spec/features/admin/off_season_splash_spec.rb
@@ -15,6 +15,6 @@ RSpec.feature "Off-season splash page" do
     SeasonToggles.disable_signups!
     visit root_path
     expect(page).not_to have_css("form#new_signup_attempt")
-    expect(page).to have_content("Registrations for the Technovation #{Season.next.year} season will open on October")
+    expect(page).to have_content("Registrations for the Technovation #{Season.next.year} season will open in the middle of October")
   end
 end
