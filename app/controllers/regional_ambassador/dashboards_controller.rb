77344777867afd8ae0a6e906c41b495f5e4106c7
Joe Sak <joe@joesak.com>
2017-09-13 09:33:00 -0500

    Take RA to accounts/index as their root path

--- app/controllers/regional_ambassador/dashboards_controller.rb
+++ app/controllers/regional_ambassador/dashboards_controller.rb
@@ -1,7 +1,7 @@
 module RegionalAmbassador
   class DashboardsController < RegionalAmbassadorController
     def show
-      redirect_to regional_ambassador_profile_path if current_ambassador.approved?
+      redirect_to regional_ambassador_accounts_path if current_ambassador.approved?
     end
   end
 end
