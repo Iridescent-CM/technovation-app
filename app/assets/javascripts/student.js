c77c94941edc29f99eaf73b30a89fb732d666b87
Joe Sak <joe@joesak.com>
2017-10-06 10:00:53 -0500

    Add jquery.sticky-kit

--- app/assets/javascripts/student.js
+++ app/assets/javascripts/student.js
@@ -1,4 +1,9 @@
 //= require application
+//= require jquery.sticky-kit.min
 
 //= require forms
 //= require search
+
+document.addEventListener("turbolinks:load", function() {
+  $(".col--sticky").stick_in_parent();
+});
