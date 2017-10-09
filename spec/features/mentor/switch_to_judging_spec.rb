46784646adbff265b96e7b5578b131e069738e94
Joe Sak <joe@joesak.com>
2017-10-09 09:41:52 -0500

    Hide 'switch to judging' while judging signups are off

---
+++ spec/features/mentor/switch_to_judging_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.feature "Mentors switch to judging mode" do
+  scenario "when judge signups are not enabled" do
+    SeasonToggles.disable_signup(:judge)
+
+    mentor = FactoryGirl.create(:mentor)
+    sign_in(mentor)
+
+    expect(page).not_to have_link("Switch to Judging")
+  end
+
+  scenario "when judge signups are enabled" do
+    SeasonToggles.enable_signup(:judge)
+
+    mentor = FactoryGirl.create(:mentor)
+    sign_in(mentor)
+
+    click_link "Switch to Judging"
+    expect(current_path).to eq(judge_dashboard_path)
+  end
+end
