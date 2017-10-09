24cda62c9c3ef497461d437d6953b29dba990fd6
Joe Sak <joe@joesak.com>
2017-10-09 11:17:32 -0500

    Override rails data-confirm with sweetalert styled confirm dialog

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,13 +4,13 @@
 //= require jquery3
 //= require jquery_ujs
 //= require chosen-jquery
+//= require sweetalert.min
 
 // ******** APP
 //
 //= require navigation
 //= require chosen-init
 //= require flash-msgs
-//= require modals
 //= require tabs
 //= require image-uploaders
 //= require location-details
@@ -21,3 +21,38 @@ $(document).ajaxSend(function(_, xhr) {
     $('meta[name="csrf-token"]').attr('content')
   );
 });
+
+//Override the default confirm dialog by rails
+$.rails.allowAction = function(link){
+  if (link.data("confirm") == undefined){
+    return true;
+  }
+  $.rails.showConfirmationDialog(link);
+  return false;
+}
+
+//User click confirm button
+$.rails.confirmed = function(link){
+  link.data("confirm", null);
+  link.trigger("click.rails");
+}
+
+//Display the confirmation dialog
+$.rails.showConfirmationDialog = function(link){
+  var positive = link.data("positive") || false;
+
+  swal({
+    text: link.data("confirm"),
+
+    dangerMode: !positive,
+
+    buttons: {
+      cancel: "No, go back",
+      confirm: "Yes, do it",
+    },
+  }).then(function(confirmed) {
+    if (confirmed) {
+      $.rails.confirmed(link);
+    }
+  });
+}
