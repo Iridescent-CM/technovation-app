32befe9d6d1c3d8fcf56fa2ca1523b79525312dc
Joe Sak <joe@joesak.com>
2017-10-13 15:17:04 -0500

    Hofix the RA section to show a 'excuse our mess' page

--- app/controllers/regional_ambassador/dashboards_controller.rb
+++ app/controllers/regional_ambassador/dashboards_controller.rb
@@ -1,7 +1,4 @@
 module RegionalAmbassador
   class DashboardsController < RegionalAmbassadorController
-    def show
-      redirect_to regional_ambassador_participants_path if current_ambassador.approved?
-    end
   end
 end
