94c99ec5868db01e85a8d43e6ed349d8e0d84997
Joe Sak <joe@joesak.com>
2017-10-19 18:31:30 -0500

    Hotfix QA migration: don't set the random icon on RA profiles.

--- db/migrate/20171019230759_set_random_profile_images_on_accounts.rb
+++ db/migrate/20171019230759_set_random_profile_images_on_accounts.rb
@@ -1,12 +1,16 @@
 class SetRandomProfileImagesOnAccounts < ActiveRecord::Migration[5.1]
   def up
-    Account.where(profile_image: ["", nil]).find_each do |a|
+    Account.left_outer_joins(:regional_ambassador_profile).where(
+      "regional_ambassador_profiles.id IS NULL AND profile_image IN (?)", ["", nil]
+    ).find_each do |a|
+
       a.update_column(
         :icon_path,
         ActionController::Base.helpers.asset_path(
           "placeholders/avatars/#{rand(1..20)}.svg"
         )
       )
+
     end
   end
 end
