add83a627c32e00e389ada48524f305997188496
Joe Sak <joe@joesak.com>
2017-10-20 15:23:06 -0500

    Hotfix the random icon setting migration - query didnt work

--- db/migrate/20171019230800_set_random_profile_images_on_accounts.rb
+++ db/migrate/20171019230800_set_random_profile_images_on_accounts.rb
@@ -1,16 +1,14 @@
 class SetRandomProfileImagesOnAccounts < ActiveRecord::Migration[5.1]
   def up
     Account.left_outer_joins(:regional_ambassador_profile).where(
-      "regional_ambassador_profiles.id IS NULL AND profile_image IN (?)", ["", nil]
+      "regional_ambassador_profiles.id IS NULL AND profile_image IS NULL"
     ).find_each do |a|
-
       a.update_column(
         :icon_path,
         ActionController::Base.helpers.asset_path(
           "placeholders/avatars/#{rand(1..24)}.svg"
         )
       )
-
     end
   end
 end
