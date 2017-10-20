7d8e662971fabbe5a42d1c23f0d2edcc54aed1c4
Joe Sak <joe@joesak.com>
2017-10-20 12:12:40 -0500

    Add back mentor js that got lost in merge conflict resolution

--- app/assets/javascripts/mentor.js
+++ app/assets/javascripts/mentor.js
@@ -4,3 +4,25 @@
 //= require forms
 //= require search
 //= require char-counter
+
+
+document.addEventListener("turbolinks:load", function() {
+  $("#background_check_candidate_ssn").mask("999-99-9999?");
+
+  $(".show-hide").on("click", "a", function(e) {
+    e.preventDefault();
+    const state = $(this).data("state"),
+          $that = $(e.target),
+          $fieldToObfuscate = $that.closest(".show-hide").find("input");
+
+    if (state === "showing") {
+      $that.text("Show");
+      $that.data("state", "hidden");
+      $fieldToObfuscate.prop("type", "password");
+    } else {
+      $that.text("Hide");
+      $that.data("state", "showing");
+      $fieldToObfuscate.prop("type", "text");
+    }
+  });
+});
