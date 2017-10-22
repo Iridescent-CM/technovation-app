498607cccf056a35521ed9b3d6d02f03bfeac87c
Joe Sak <joe@joesak.com>
2017-10-21 19:21:48 -0500

    Fix up some more admin screens

--- app/controllers/admin/participants_controller.rb
+++ app/controllers/admin/participants_controller.rb
@@ -1,42 +1,12 @@
 module Admin
   class ParticipantsController < AdminController
-    helper_method :account
-
     def index
-      params[:page] = 1 if params[:page].blank?
-      params[:per_page] = 15 if params[:per_page].blank?
-
-      @accounts = SearchAccounts.(params)
-        .page(params[:page].to_i)
-        .per_page(params[:per_page].to_i)
-
-      if @accounts.empty?
-        @accounts = @accounts.page(1)
-      end
-    end
-
-    def show
-      account
     end
 
     def edit
-      account
-      @expertises ||= Expertise.all
     end
 
     def update
-      type = params.keys.detect { |k| k =~ /_profile/ }
-
-      @profile = type.camelize.constantize.find(params[:id])
-      @account = @profile.account
-
-      if ProfileUpdating.execute(@profile, profile_params)
-        redirect_to admin_participant_path(@account),
-          success: "Account information saved"
-      else
-        @expertises ||= Expertise.all
-        render :edit
-      end
     end
 
     private
@@ -68,12 +38,6 @@ module Admin
       ).tap do |tapped|
         tapped.fetch(:account_attributes) { {} }[:skip_existing_password] = true
       end
-    end
-
-    def account
-      @account ||= Account.find(params[:id])
-      @profile ||= @account.send("#{@account.scope_name}_profile")
-      @account
     end
   end
 end
