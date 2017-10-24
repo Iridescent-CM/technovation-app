bd04033fdf33d18fc50d27ba8917277522454b49
Joe Sak <joe@joesak.com>
2017-10-24 17:07:26 -0500

    Restore previously skipped specs

--- spec/features/regional_ambassador/switch_to_mentor_spec.rb
+++ spec/features/regional_ambassador/switch_to_mentor_spec.rb
@@ -2,7 +2,6 @@ require "rails_helper"
 
 RSpec.feature "RAs switch to mentor mode" do
   scenario "an RA switches to mentor mode with a mentor profile" do
-    skip "Not there yet"
     ra = FactoryGirl.create(:regional_ambassador, :approved)
     CreateMentorProfile.(ra)
 
@@ -13,7 +12,6 @@ RSpec.feature "RAs switch to mentor mode" do
   end
 
   scenario "an RA switches to mentor mode without a mentor profile" do
-    skip "Not there yet"
     ra = FactoryGirl.create(:regional_ambassador, :approved)
 
     sign_in(ra)
@@ -23,18 +21,16 @@ RSpec.feature "RAs switch to mentor mode" do
   end
 
   scenario "an RA switches back to RA mode from mentor mode" do
-    skip "Not there yet"
     ra = FactoryGirl.create(:regional_ambassador, :approved)
 
     sign_in(ra)
     click_link "Mentor mode"
     click_link "RA mode"
 
-    expect(current_path).to eq(regional_ambassador_participants_path)
+    expect(current_path).to eq(regional_ambassador_dashboard_path)
   end
 
   scenario "a mentor without an RA profile cannot switch to RA mode" do
-    skip "Not there yet"
     mentor = FactoryGirl.create(:mentor)
     sign_in(mentor)
     expect(page).not_to have_link("RA mode")
