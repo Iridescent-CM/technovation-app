3469e81b8661a4179ac9382e3f96957d77c5dbbf
Joe Sak <joe@joesak.com>
2017-10-25 19:56:07 -0500

    Fix AdminConstraint due to cookies being signed now

--- lib/admin_constraint.rb
+++ lib/admin_constraint.rb
@@ -1,6 +1,6 @@
 class AdminConstraint
   def matches?(request)
-    request.cookie_jar['auth_token'] and
-      Account.joins(:admin_profile).exists?(auth_token: request.cookie_jar['auth_token'])
+    request.cookie_jar.signed['auth_token'] and
+      Account.joins(:admin_profile).exists?(auth_token: request.cookie_jar.signed['auth_token'])
   end
 end
