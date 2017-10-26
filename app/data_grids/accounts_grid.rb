bebb4e012f083d00227940afd4b3859d7c957585
Joe Sak <joe@joesak.com>
2017-10-26 15:12:38 -0500

    Improve AccountsGrid filter groups

--- app/data_grids/accounts_grid.rb
+++ app/data_grids/accounts_grid.rb
@@ -38,7 +38,7 @@ class AccountsGrid
     )
   end
 
-  filter :name do |value|
+  filter :name, filter_group: "text-search" do |value|
     names = value.downcase.split(' ').map { |n| I18n.transliterate(n) }
     where(
       "unaccent(lower(accounts.first_name)) ilike '%#{names.first}%' OR
@@ -46,7 +46,7 @@ class AccountsGrid
     )
   end
 
-  filter :email do |value|
+  filter :email, filter_group: "text-search" do |value|
     where("accounts.email ilike '%#{value}%'")
   end
 
@@ -63,7 +63,7 @@ class AccountsGrid
     select: ->(g) {
       CountryStateSelect.countries_collection
     },
-    filter_group: "location_data",
+    filter_group: "location-data",
     multiple: true,
     if: ->(g) { g.admin } do |values|
       clauses = values.map { |v| "accounts.country = '#{v}'" }
@@ -74,7 +74,7 @@ class AccountsGrid
     :enum,
     header: "State / Province",
     select: ->(g) { CS.get(g.country[0]).map { |s| [s[1], s[0]] } },
-    filter_group: "location_data",
+    filter_group: "location-data",
     multiple: true,
     if: ->(g) {
       g.country.one? && CS.get(g.country[0]).any?
@@ -88,7 +88,7 @@ class AccountsGrid
   filter :city,
     :enum,
     select: ->(g) { CS.get(g.country[0], g.state_province[0]) },
-    filter_group: "location_data",
+    filter_group: "location-data",
     multiple: true,
     if: ->(g) {
       g.state_province.one? && CS.get(g.country[0], g.state_province[0]).any?
@@ -123,5 +123,9 @@ class AccountsGrid
       send(value)
     end
 
-  column_names_filter(header: "More columns", multiple: true)
+  column_names_filter(
+    header: "More columns",
+    filter_group: "extra-columns",
+    multiple: true
+  )
 end
