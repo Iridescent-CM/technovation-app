e0174cafccc4fbbc480ddc7fcf5c70f9589a0704
Joe Sak <joe@joesak.com>
2017-11-07 13:02:42 -0600

    Log account id, not email, for action cable log

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -4,7 +4,7 @@ module ApplicationCable
 
     def connect
       self.current_user = find_verified_user
-      logger.add_tags 'ActionCable', current_user.email
+      logger.add_tags 'ActionCable', "Account##{current_user.id}"
     end
 
     private
