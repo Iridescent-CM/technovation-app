cc792413ff14bca30363edb9d3f5c002287c7663
Joe Sak <joe@joesak.com>
2017-11-13 13:17:40 -0600

    This circleci stuff .. it's so fun

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+cat > ~/.netrc << EOF
+machine api.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
+EOF
+
 cat >> ~/.ssh/config << EOF
 VerifyHostKeyDNS yes
 StrictHostKeyChecking no
