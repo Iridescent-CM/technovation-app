2c9b97569c51a267515eba81f6935e2ec96641fa
Joe Sak <joe@joesak.com>
2017-11-14 16:09:14 -0600

    Fix more out of date specs

--- spec/models/mentor_account_spec.rb
+++ spec/models/mentor_account_spec.rb
@@ -6,11 +6,18 @@ RSpec.describe MentorProfile do
       mentor = FactoryBot.create(:mentor)
       t1 = FactoryBot.create(:team,
                               city: "Los Angeles",
-                              state_province: "CA",
-                              division: Division.senior)
+                              state_province: "CA")
+
+      student = t1.students.first
+
+      ProfileUpdating.execute(student, account_attributes: {
+        id: student.account_id,
+        date_of_birth: 15.years.ago,
+      })
+
       TeamRosterManaging.add(t1, mentor)
 
-      t2 = FactoryBot.create(:team, division: Division.junior)
+      t2 = FactoryBot.create(:team)
       TeamRosterManaging.add(t2, mentor)
 
       expect(mentor.team_region_division_names).to match_array([
@@ -23,15 +30,22 @@ RSpec.describe MentorProfile do
       mentor = FactoryBot.create(:mentor)
       t1 = FactoryBot.create(:team,
                               city: "Los Angeles",
-                              state_province: "CA",
-                              division: Division.senior)
+                              state_province: "CA")
       TeamRosterManaging.add(t1, mentor)
 
       t2 = FactoryBot.create(:team,
                               city: "Los Angeles",
-                              state_province: "CA",
-                              division: Division.senior)
+                              state_province: "CA")
       TeamRosterManaging.add(t2, mentor)
+
+      [t1, t2].each do |team|
+        student = team.students.first
+
+        ProfileUpdating.execute(student, account_attributes: {
+          id: student.account_id,
+          date_of_birth: 15.years.ago,
+        })
+      end
 
       expect(mentor.team_region_division_names).to match_array(["US_CA,senior"])
     end
