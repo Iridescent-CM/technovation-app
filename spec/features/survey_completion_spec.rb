d29167e4c0913996bfd1e65de11080d69a77873e
Joe Sak <joe@joesak.com>
2017-11-14 11:12:59 -0600

    Ensure survey completion works with the sign in redirection logic

---
+++ spec/features/survey_completion_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.feature "Survey completion" do
+  %i{student mentor}.each do |scope|
+    scenario "redirection for #{scope} after logging in still works" do
+      user = FactoryBot.create(scope)
+
+      visit survey_completion_path
+
+      # redirect to sign in
+      fill_in 'Email', with: user.account.email
+      fill_in 'Password', with: "secret1234"
+      click_button 'Sign in'
+
+      expect(current_path).to eq(send("#{scope}_dashboard_path"))
+      expect(user.account.reload).to be_took_survey
+    end
+  end
+end
