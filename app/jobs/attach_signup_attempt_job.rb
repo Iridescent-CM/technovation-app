797c1524b9935e980c3ec29e07c46f4571adcf6a
Joe Sak <joe@joesak.com>
2017-11-15 12:50:10 -0600

    Use an account_id instead of account object in AttachSignupAttemptJob

--- app/jobs/attach_signup_attempt_job.rb
+++ app/jobs/attach_signup_attempt_job.rb
@@ -1,7 +1,8 @@
 class AttachSignupAttemptJob < ActiveJob::Base
   queue_as :default
 
-  def perform(account)
+  def perform(account_id)
+    account = Account.find(account_id)
     if attempt = SignupAttempt.find_by(email: account.email)
       attempt.update_column(:account_id, account.id)
       attempt.registered!
