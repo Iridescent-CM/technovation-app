11263889497ceaae6be340e2fdf0778af200ebea
Joe Sak <joe@joesak.com>
2017-11-22 09:59:15 -0600

    Fix a redirection spec to not care about the new param on RA dashboard ... here comes a sleu of fails on CI

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -4,26 +4,26 @@ RSpec.feature "Authentication" do
   { judge: %i{regional_ambassador student admin},
     student: %i{mentor regional_ambassador judge admin},
     mentor: %i{regional_ambassador student admin},
-    regional_ambassador: %i{judge student admin} }.each do |type, not_types|
-      not_types.each do |not_type|
-        scenario "A #{type} tries to visit a #{not_type} path" do
-          account = FactoryBot.create(type)
+    regional_ambassador: %i{judge student admin} }.each do |scope, not_scopes|
+      not_scopes.each do |not_scope|
+        scenario "A #{scope} tries to visit a #{not_scope} path" do
+          account = FactoryBot.create(scope)
 
           sign_in(account)
-          visit send("#{not_type}_dashboard_path")
+          visit send("#{not_scope}_dashboard_path")
 
-          expect(page).to have_current_path(send("#{type}_dashboard_path"))
+          expect(current_path.sub(/\?.+$/, "")).to eq(send("#{scope}_dashboard_path"))
           expect(page).to have_css(".flash", text: "You don't have permission to go there")
         end
       end
     end
 
-    %i{mentor student judge regional_ambassador admin}.each do |type|
-      scenario "A logged out user tries to visit a #{type} path" do
-        visit send("#{type}_dashboard_path")
+    %i{mentor student judge regional_ambassador admin}.each do |scope|
+      scenario "A logged out user tries to visit a #{scope} path" do
+        visit send("#{scope}_dashboard_path")
 
         expect(page).to have_current_path(signin_path)
-        expect(page).to have_css(".flash", text: "You must be signed in as #{type.indefinitize.humanize.downcase} to go there!")
+        expect(page).to have_css(".flash", text: "You must be signed in as #{scope.indefinitize.humanize.downcase} to go there!")
       end
     end
 end
