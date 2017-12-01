0a60bd40bda22bc618d55ca7b16aaa9e81a40280
Joe Sak <joe@joesak.com>
2017-12-01 09:38:43 -0600

    Lay the tested groundwork for a submission TOC

--- spec/features/student/submission_spec.rb
+++ spec/features/student/submission_spec.rb
@@ -46,4 +46,51 @@ RSpec.feature "Student team submissions" do
       href: student_team_submission_path(TeamSubmission.last)
     )
   end
+
+  scenario "Start the submission from the table of contents" do
+    student = FactoryBot.create(:student, :on_team)
+    submission = student.team.team_submissions.create!({ integrity_affirmed: true })
+    sign_in(student)
+
+
+    click_link "My team's submission"
+
+    expect(page).to have_link(
+      "Set your app's name",
+      href: edit_student_team_submission_path(submission, piece: :app_name)
+    )
+
+    expect(page).to have_link(
+      "Add your app's description",
+      href: edit_student_team_submission_path(submission, piece: :app_description)
+    )
+
+    expect(page).to have_link(
+      "Add the demo video link",
+      href: edit_student_team_submission_path(submission, piece: :demo_video_link)
+    )
+
+    expect(page).to have_link(
+      "Add the pitch video link",
+      href: edit_student_team_submission_path(submission, piece: :pitch_video_link)
+    )
+
+    expect(page).to have_link(
+      "Select the development platform that your team used",
+      href: edit_student_team_submission_path(submission, piece: :development_platform)
+    )
+
+    expect(page).to have_link(
+      "Upload your app's source code",
+      href: edit_student_team_submission_path(submission, piece: :source_code)
+    )
+
+    expect(page).to have_link(
+      "Upload screenshots of your app",
+      href: edit_student_team_submission_path(submission, piece: :screenshots)
+    )
+  end
+
+  scenario "See senior team submission pieces on TOC"
+  scenario "See live pitch event submission pieces on TOC"
 end
