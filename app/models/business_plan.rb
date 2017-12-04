1d6b173b6b9ef2ef69d8dd17ef13263993b5580d
Joe Sak <joe@joesak.com>
2017-12-04 15:45:21 -0600

    Upload business plans for team submissions

--- app/models/business_plan.rb
+++ app/models/business_plan.rb
@@ -2,18 +2,4 @@ class BusinessPlan < ActiveRecord::Base
   belongs_to :team_submission, touch: true
 
   mount_uploader :uploaded_file, FileProcessor
-
-  def file_url
-    uploaded_file ? uploaded_file_url : remote_file_url
-  end
-
-  def remote_file_url=(url)
-    sanitized_url = if url.match(%r{\Ahttps?://})
-            url
-          elsif not url.blank?
-            url.sub(%r{\A(?:\w+://)?}, "http://")
-          end
-
-    super(sanitized_url)
-  end
 end
