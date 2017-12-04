131f98870f4730cad4686a35c1f578b54ef83976
Joe Sak <joe@joesak.com>
2017-12-04 09:02:36 -0600

    Remove specs related to outdated TeamSubmission enum(:stated_goal)

--- spec/models/team_submission_spec.rb
+++ spec/models/team_submission_spec.rb
@@ -6,16 +6,6 @@ RSpec.describe TeamSubmission do
   it { should respond_to(:app_name) }
   it { should respond_to(:demo_video_link) }
   it { should respond_to(:pitch_video_link) }
-  it { should respond_to(:stated_goal) }
-  it { should respond_to(:stated_goal_explanation) }
-
-  it "has a defined list of Sustainabel Development Goals (SDG)" do
-    goals = TeamSubmission.stated_goals.keys
-
-    expect(goals).to match_array(
-      %w{Poverty Environment Peace Equality Education Health}
-    )
-  end
 
   it "prepends urls with http:// if it's not there" do
     subject.source_code_external_url = "joesak.com"
