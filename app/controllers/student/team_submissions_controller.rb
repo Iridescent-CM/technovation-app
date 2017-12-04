1ed4e3c8bd494666b123ab5abf9078737c90bef8
Joe Sak <joe@joesak.com>
2017-12-04 15:45:21 -0600

    Upload business plans for team submissions

--- app/controllers/student/team_submissions_controller.rb
+++ app/controllers/student/team_submissions_controller.rb
@@ -19,6 +19,14 @@ module Student
     def edit
       @team_submission = current_team.submission
 
+      unless @team_submission.business_plan.present?
+        @team_submission.build_business_plan
+      end
+
+      unless @team_submission.pitch_presentation.present?
+        @team_submission.build_pitch_presentation
+      end
+
       if @team_submission.present?
         begin
           render "team_submissions/pieces/#{params.fetch(:piece)}"
@@ -92,50 +100,33 @@ module Student
 
     private
     def team_submission_params
-      sub_params = if SeasonToggles.team_submissions_editable?
-                    params.require(:team_submission).permit(
-                      :integrity_affirmed,
-                      :source_code,
-                      :source_code_external_url,
-                      :app_description,
-                      :stated_goal,
-                      :stated_goal_explanation,
-                      :app_name,
-                      :demo_video_link,
-                      :pitch_video_link,
-                      :development_platform_other,
-                      :development_platform,
-                      :source_code_file_uploaded,
-                      business_plan_attributes: [
-                        :id,
-                        :remote_file_url,
-                      ],
-                      pitch_presentation_attributes: [
-                        :id,
-                        :remote_file_url,
-                      ],
-                      screenshots: [],
-                    )
-                   else
-                    params.require(:team_submission).permit(
-                      pitch_presentation_attributes: [
-                        :id,
-                        :remote_file_url,
-                      ],
-                    )
-                   end
-      sub_params.tap do |tapped|
-        if tapped[:pitch_presentation_attributes]
-          tapped[:pitch_presentation_attributes][:file_uploaded] = false
-        end
-
-        unless tapped[:source_code_external_url].blank?
-          tapped[:source_code_file_uploaded] = false
-        end
-
-        if tapped[:business_plan_attributes]
-          tapped[:business_plan_attributes][:file_uploaded] = false
-        end
+      if SeasonToggles.team_submissions_editable?
+        params.require(:team_submission).permit(
+          :integrity_affirmed,
+          :source_code,
+          :app_description,
+          :app_name,
+          :demo_video_link,
+          :pitch_video_link,
+          :development_platform_other,
+          :development_platform,
+          screenshots: [],
+          business_plan_attributes: [
+            :id,
+            :uploaded_file,
+          ],
+          pitch_presentation_attributes: [
+            :id,
+            :uploaded_file,
+          ],
+        )
+      else
+        params.require(:team_submission).permit(
+          pitch_presentation_attributes: [
+            :id,
+            :remote_file_url,
+          ],
+        )
       end
     end
   end
