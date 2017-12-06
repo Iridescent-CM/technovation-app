14d0a8139c168b8ad233858416f1208e989eda0e
Joe Sak <joe@joesak.com>
2017-12-06 14:23:02 -0600

    Install Dropzonejs-rails and enable for submission pieces: source code / business plan

--- app/assets/javascripts/student.js
+++ app/assets/javascripts/student.js
@@ -1,4 +1,5 @@
 //= require application
+//= require dropzone
 
 //= require forms
 //= require search
@@ -12,3 +13,27 @@ $(document).on("click", "#team_submissions--menu a", function(e) {
   location.href = anchor;
   scrollBy(0, -offset);
 });
+
+var defaultDropzoneOptions = {
+  maxFiles: 1,
+  dictDefaultMessage: "Drop file here to upload, or click to select a file",
+
+  init: function() {
+    this.on("success", function(file) {
+      $(".dropzone-save").fadeIn();
+      $(".after-dropzone-save").hide();
+    });
+  },
+}
+
+Dropzone.options.teamSubmissionBusinessPlanDropzone = $.extend({}, defaultDropzoneOptions, {
+  paramName: "team_submission[business_plan_attributes][uploaded_file]",
+});
+
+Dropzone.options.teamSubmissionSourcecodeDropzone = $.extend({}, defaultDropzoneOptions, {
+  paramName: "team_submission[source_code]",
+});
+
+Dropzone.autoDiscover = false;
+
+$(document).on("ready turbolinks:load", Dropzone.discover);
