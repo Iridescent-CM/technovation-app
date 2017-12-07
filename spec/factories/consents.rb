25b5935feff0f2ed52bcb0e816cbf5b4e9b93c8a
Joe Sak <joe@joesak.com>
2017-12-07 11:11:57 -0600

    Add signed trait for parental consents factories

--- spec/factories/consents.rb
+++ spec/factories/consents.rb
@@ -2,6 +2,10 @@ FactoryBot.define do
   factory :parental_consent do
     student_profile
     electronic_signature "Parenty McGee"
+
+    trait :signed do
+      status :signed
+    end
   end
 
   factory :consent_waiver do
