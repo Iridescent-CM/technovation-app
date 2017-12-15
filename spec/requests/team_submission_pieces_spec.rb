e39fcd40e2f5f07b375aae6eb71c492fa0923fa7
Joe Sak <joe@joesak.com>
2017-12-15 10:44:32 -0600

    Test new submission_pieces 'public' routes

---
+++ spec/requests/team_submission_pieces_spec.rb
@@ -0,0 +1,60 @@
+require "rails_helper"
+
+RSpec.describe "GET /team_submission_pieces/:piece" do
+  it "requires you to sign in" do
+    get "/team_submission_pieces/app_name"
+    expect(response).to redirect_to(signin_path)
+  end
+
+  it "redirects based on who signs in" do
+    mentor = FactoryBot.create(:mentor)
+
+    allow_any_instance_of(ApplicationController).to receive(
+      :current_account
+    ).and_return(mentor.account)
+
+    get "/team_submission_pieces/app_name"
+    expect(response).to redirect_to(new_mentor_team_submission_path)
+
+    student = FactoryBot.create(:student)
+
+    allow_any_instance_of(ApplicationController).to receive(
+      :current_account
+    ).and_return(student.account)
+
+    get "/team_submission_pieces/app_name"
+    expect(response).to redirect_to(new_student_team_submission_path)
+  end
+
+  it "redirects to editing existing submission pieces" do
+    student = FactoryBot.create(:student, :on_team)
+    submission = student.team.team_submissions.create!({
+      integrity_affirmed: true,
+    })
+
+    allow_any_instance_of(ApplicationController).to receive(
+      :current_account
+    ).and_return(student.account)
+
+    get "/team_submission_pieces/app_name"
+
+    expect(response).to redirect_to(
+      edit_student_team_submission_path(submission, piece: :app_name)
+    )
+
+    mentor = FactoryBot.create(:mentor, :on_team)
+    submission = mentor.teams.last.team_submissions.create!({
+      integrity_affirmed: true,
+    })
+
+    allow_any_instance_of(ApplicationController).to receive(
+      :current_account
+    ).and_return(mentor.account)
+
+    get "/team_submission_pieces/app_name"
+
+    expect(response).to redirect_to(
+      edit_mentor_team_submission_path(submission, piece: :app_name)
+    )
+  end
+end
