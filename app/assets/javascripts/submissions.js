f2597bcdc326fa25907ee699109126810e6ad969
Joe Sak <joe@joesak.com>
2017-12-18 09:09:36 -0600

    Count the words in team submission app_description, disabling submit as needed

--- app/assets/javascripts/submissions.js
+++ app/assets/javascripts/submissions.js
@@ -10,6 +10,63 @@ $(document).on("click", "#team_submissions--menu a", function(e) {
   scrollBy(0, -offset);
 });
 
+$(document).on("ready turbolinks:load", function() {
+  var $wordCounters = $("[data-word-count]");
+  $.each($wordCounters, function() {
+    var limit = $(this).data("word-count-limit");
+    $(this).find(".word-count__limit").text(limit);
+  });
+});
+
+$(document).on("click", ".actions", function(e) {
+  if ($(this).find("[disabled]").length > 0) {
+    var $wordCountEl = $(this).find("[data-word-count]");
+
+    $wordCountEl.addClass("notify");
+
+    setTimeout(function() {
+      $wordCountEl.removeClass("notify");
+    }, 200);
+
+    var $inputEl = $($wordCountEl.data("word-count")),
+        val = $inputEl.val();
+
+    $inputEl
+      .focus()
+      .val("")
+      .val(val)
+      .scrollTop($inputEl[0].scrollHeight);
+  }
+});
+
+$(document).on("input", "#team_submission_app_description", function(e) {
+  var $wordCounter = $('[data-word-count="#' + e.target.id + '"]'),
+      limit = $wordCounter.data("word-count-limit"),
+      wordCount = $(this).val().split(" ").filter(function(word) {
+        return word.length > 2;
+      }).length,
+      ratio = wordCount / parseFloat(limit),
+      $total = $wordCounter.find(".word-count__total");
+
+  $total.text(wordCount).removeClass("word-count--plenty-remaining");
+
+  if (ratio >= 0.65 && ratio < 0.9) {
+    $total.addClass("word-count--some-remaining");
+  } else if (ratio >= 0.9) {
+    $total.addClass("word-count--none-remaining");
+  } else {
+    $total.addClass("word-count--plenty-remaining");
+  }
+
+  var $submitBtn = $(this).closest("form").find("[type=submit]");
+
+  if (ratio > 1) {
+    $submitBtn.prop("disabled", true);
+  } else {
+    $submitBtn.prop("disabled", false);
+  }
+});
+
 var defaultSubmissionDropzoneOptions = {
   maxFiles: 1,
   dictDefaultMessage: "Drop file here to upload, " +
