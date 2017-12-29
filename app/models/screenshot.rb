00daf8ef76a473ea1238ef0886d101238d9ada2e
Joe Sak <joe@joesak.com>
2017-12-29 14:20:45 -0600

    Improve screenshots UX more and more

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -9,8 +9,12 @@ class Screenshot < ActiveRecord::Base
     self.sort_position = (self.class.maximum(:sort_position) || -1) + 1
   }
 
+  def self.persisted
+    select(&:persisted?)
+  end
+
   def self.max_files_remaining(submission)
-    MAX_ALLOWED - submission.screenshots.reject(&:new_record?).count
+    MAX_ALLOWED - submission.screenshots.persisted.count
   end
 
   def self.while_files_remaining(submission, &block)
@@ -21,6 +25,12 @@ class Screenshot < ActiveRecord::Base
 
   def self.while_none_remaining(submission, &block)
     unless max_files_remaining(submission) > 0
+      yield
+    end
+  end
+
+  def self.while_any(submission, &block)
+    if submission.screenshots.persisted.any?
       yield
     end
   end
