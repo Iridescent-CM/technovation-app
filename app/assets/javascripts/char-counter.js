027bc18c4c2fda7f2f44519010d470d0730a7b54
Joe Sak <joe@joesak.com>
2017-12-29 11:14:35 -0600

    Rearrange JS assets

--- app/assets/javascripts/char-counter.js
+++ app/assets/javascripts/char-counter.js
@@ -1,4 +1,4 @@
-document.addEventListener("turbolinks:load", function() {
+$(document).on("ready turbolinks:load", function() {
   $("[data-keep-count-of]").each(function() {
     var $source = $($(this).data('keep-count-of')),
         $that = $(this),
@@ -49,4 +49,80 @@ document.addEventListener("turbolinks:load", function() {
       $that.find('span:last-child').text(counted);
     }
   });
+
+  $("[data-word-count]").each(function() {
+    $(".word-count__limit").text($(this).data("word-count-limit"));
+    executeWordCounting($(this));
+  });
 });
+
+$(document).on("click", ".actions", function(e) {
+  if ($(this).find("[disabled]").length > 0) {
+    var $wordCountEl = $(this).find(".word-counter");
+
+    $wordCountEl.addClass("notify");
+
+    setTimeout(function() {
+      $wordCountEl.removeClass("notify");
+    }, 200);
+
+    var $inputEl = $("[data-word-count]"),
+        val = $inputEl.val();
+
+    $inputEl
+      .focus()
+      .val("")
+      .val(val)
+      .scrollTop($inputEl[0].scrollHeight);
+  }
+});
+
+$(document).on("input", "[data-word-count]", function(e) {
+  executeWordCounting($(this));
+});
+
+function executeWordCounting($el) {
+  var wordCount = countWords($el);
+
+  updateWordCounter($el, wordCount);
+
+  function countWords($el) {
+    return $el.val().split(" ").filter(function(word) {
+      return word.length > 2;
+    }).length;
+  }
+
+  function updateWordCounter($el, wordCount) {
+    var $total = $(".word-count__total");
+
+    $total
+      .text(wordCount)
+      .removeClass("word-count--plenty-remaining")
+      .removeClass("word-count--some-remaining")
+      .removeClass("word-count--none-remaining");
+
+    var limit = $el.data("word-count-limit"),
+        ratio = wordCount / parseFloat(limit);
+
+    if (ratio >= 0.65 && ratio < 0.9) {
+      $total.addClass("word-count--some-remaining");
+    } else if (ratio >= 0.9) {
+      $total.addClass("word-count--none-remaining");
+    } else {
+      $total.addClass("word-count--plenty-remaining");
+    }
+
+    updateSubmitButton(
+      $el.closest("form").find("[type=submit]"),
+      ratio
+    );
+
+    function updateSubmitButton(btn, ratio) {
+      if (ratio > 1) {
+        btn.prop("disabled", true);
+      } else {
+        btn.prop("disabled", false);
+      }
+    }
+  }
+}
