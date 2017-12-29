6aab4958c01ee0791a8bfe2f2fb86f032fc965f0
Joe Sak <joe@joesak.com>
2017-12-29 11:14:56 -0600

    Provide improved logic for when screenshots are or ar not remaining

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -1,4 +1,6 @@
 class Screenshot < ActiveRecord::Base
+  MAX_ALLOWED = 6
+
   belongs_to :team_submission, touch: true
 
   mount_uploader :image, ScreenshotProcessor
@@ -6,4 +8,20 @@ class Screenshot < ActiveRecord::Base
   before_create -> {
     self.sort_position = (self.class.maximum(:sort_position) || -1) + 1
   }
+
+  def self.max_files_remaining(submission)
+    MAX_ALLOWED - submission.screenshots.reject(&:new_record?).count
+  end
+
+  def self.while_files_remaining(submission, &block)
+    if max_files_remaining(submission) > 0
+      yield
+    end
+  end
+
+  def self.while_none_remaining(submission, &block)
+    unless max_files_remaining(submission) > 0
+      yield
+    end
+  end
 end
