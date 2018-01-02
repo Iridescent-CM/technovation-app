adde023aa71c44af303bb8a23f6da3ad7eda00d2
Joe Sak <joe@joesak.com>
2018-01-02 09:30:06 -0600

    WIP: sortable screenshots

---
+++ spec/controllers/mentor/team_submissions_controller_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.describe Mentor::TeamSubmissionsController do
+  describe "PATCH #update" do
+    before do
+      @editable_submissions = !!SeasonToggles.team_submissions_editable?
+      SeasonToggles.team_submissions_editable = true
+    end
+
+    after do
+      SeasonToggles.team_submissions_editable = @editable_submissions
+    end
+
+    it "can handle sorting" do
+      mentor = FactoryBot.create(:mentor, :on_team)
+      team_submission = FactoryBot.create(:submission, team: mentor.teams.first)
+
+      screenshot1 = team_submission.screenshots.create!
+      screenshot2 = team_submission.screenshots.create!
+
+      sign_in(mentor)
+
+      patch :update, params: { id: team_submission.id,
+        team_submission: { screenshots: [screenshot2.id, screenshot1.id] } }
+
+      expect(screenshot1.reload.sort_position).to eq(1)
+      expect(screenshot2.reload.sort_position).to eq(0)
+    end
+  end
+end
