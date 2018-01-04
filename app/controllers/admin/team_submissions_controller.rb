8d271d9501a8879ba15363c3a09228a07574150c
Alli Colyer <allicolyer@gmail.com>
2018-01-04 11:17:43 -0500

    configure data grid for admin team submissions

--- app/controllers/admin/team_submissions_controller.rb
+++ app/controllers/admin/team_submissions_controller.rb
@@ -1,56 +1,31 @@
-require "will_paginate/array"
-
 module Admin
   class TeamSubmissionsController < AdminController
-    def index
-      params[:page] = 1 if params[:page].blank?
-      params[:per_page] = 15 if params[:per_page].blank?
+    include DatagridUser
 
-      @team_submissions = Admin::SearchTeamSubmissions.(params)
-        .sort { |a, b| a.team_name.downcase <=> b.team_name.downcase }
-        .paginate(page: params[:page].to_i, per_page: params[:per_page].to_i)
-
-      if @team_submissions.empty?
-        @team_submissions = @team_submissions.paginate(page: 1)
-      end
-    end
+    use_datagrid with: SubmissionsGrid
 
     def show
       @team_submission = TeamSubmission.friendly.find(params[:id])
-      @team_submission.build_technical_checklist if @team_submission.technical_checklist.blank?
     end
 
     def edit
       @team_submission = TeamSubmission.friendly.find(params[:id])
-      @team_submission.build_business_plan if @team_submission.business_plan.blank?
     end
 
     def update
-      @team_submission = TeamSubmission.friendly.find(params[:id])
-
-      if @team_submission.update_attributes(team_submission_params)
-        redirect_to [:admin, @team_submission],
-          success: "Saved!"
-      else
-        render :edit
-      end
     end
 
     private
-    def team_submission_params
-      params.require(:team_submission).permit(
-        :contest_rank,
-        :app_name,
-        :app_description,
-        :demo_video_link,
-        :pitch_video_link,
-        :source_code,
-        :development_platform,
-        :development_platform_other,
-        business_plan_attributes: [
-          :id,
-          :uploaded_file,
-        ],
+    def grid_params
+      grid = (params[:submissions_grid] ||= {}).merge(
+        admin: true,
+        country: Array(params[:submissions_grid][:country]),
+        state_province: Array(params[:submissions_grid][:state_province]),
+        season: params[:submissions_grid][:season] || Season.current.year,
+      )
+
+      grid.merge(
+        column_names: detect_extra_columns(grid),
       )
     end
   end
