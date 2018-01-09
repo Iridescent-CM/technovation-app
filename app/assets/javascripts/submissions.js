b94ac0c04a8550c87cee43575fc8d86a33b37e93
Joe Sak <joe@joesak.com>
2018-01-09 09:33:18 -0600

    Add screenshots with simple modal UI on finalized submission page

--- app/assets/javascripts/submissions.js
+++ app/assets/javascripts/submissions.js
@@ -9,3 +9,9 @@ $(document).on("click", "#team_submissions--menu a", function(e) {
 
   scrollBy(0, -offset);
 });
+
+$(document).on("click", ".submission-pieces__screenshot", function(e) {
+  swal({
+    imageUrl: $(e.target).data("modalUrl"),
+  });
+});
