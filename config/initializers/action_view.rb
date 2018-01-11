85173d2707fcdaa08d3cdc5c8655006ddc9e52a7
Joe Sak <joe@joesak.com>
2018-01-11 10:48:08 -0600

    Fix form error proc to handle checkboxes gracefully

--- config/initializers/action_view.rb
+++ config/initializers/action_view.rb
@@ -1,8 +1,29 @@
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
-  if  Nokogiri::HTML::fragment(html_tag).children[0].name == "input"
+  tag = Nokogiri::HTML::fragment(html_tag).children[0]
+
+  type = tag.attributes.fetch("type") {
+    TechnovationApp::FormErrorProc::NullType.new("null")
+  }.value
+
+  html = ""
+
+  if tag.name == "input" && type != "checkbox"
     span =  %Q(<span class="error">#{Array(instance.error_message).join("\n")}</span>)
-    %Q(<div class="field_with_errors">#{html_tag}#{span}</div>).html_safe
+    html = %Q(<div class="field_with_errors">#{html_tag}#{span}</div>)
+  elsif tag.name == "input" && type == "checkbox"
+    html = html_tag
+  elsif tag.name == "label"
+    span =  %Q(<span class="error">#{Array(instance.error_message).join("\n")}</span>)
+    html = %Q(<span class="field_with_errors">#{html_tag}#{span}</span>)
   else
-    %Q(<div class="field_with_errors">#{html_tag}</div>).html_safe
+    html = %Q(<div class="field_with_errors">#{html_tag}</div>)
+  end
+
+  html.html_safe
+end
+
+module TechnovationApp
+  module FormErrorProc
+    NullType = Struct.new(:value)
   end
 end
