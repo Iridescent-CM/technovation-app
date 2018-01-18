5e20539150db794adaa0f10f079143b3c49abd83
Joe Sak <joe@joesak.com>
2018-01-18 09:29:45 -0600

    Remove guard from dupe parental consents migration - it's outdated

--- db/migrate/20171120200106_remove_dupe_parental_consents.rb
+++ db/migrate/20171120200106_remove_dupe_parental_consents.rb
@@ -1,8 +1,6 @@
 class RemoveDupeParentalConsents < ActiveRecord::Migration[5.1]
   def up
     ActiveRecord::Base.transaction do
-      uniq_student_count = StudentProfile.onboarded.uniq.count
-
       ids = ParentalConsent.nonvoid.group_by(&:student_profile_id)
 
       keep = ids.flat_map { |_, consents| consents.first }.map(&:id)
@@ -11,10 +9,6 @@ class RemoveDupeParentalConsents < ActiveRecord::Migration[5.1]
         .reject { |id| keep.include?(id) }
 
       ParentalConsent.nonvoid.where(id: throwout).destroy_all
-
-      if StudentProfile.onboarded.uniq.count != uniq_student_count
-        raise "DOES NOT MATCH UP"
-      end
     end
   end
 end
