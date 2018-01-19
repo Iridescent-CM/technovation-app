6d29010939ccb756117141dbb96c2e9d046a9293
Joe Sak <joe@joesak.com>
2018-01-19 10:18:23 -0600

    Try a handful of possible URL params to prevent youku autoplay

--- app/models/team_submission.rb
+++ app/models/team_submission.rb
@@ -386,7 +386,7 @@ class TeamSubmission < ActiveRecord::Base
       %{<iframe
           height="348"
           width="100%"
-          src="https://player.youku.com/embed/#{id}"
+          src="https://player.youku.com/embed/#{id}?play=0&auto=0&auto_play=0&autoplay=0"
           frameborder="0"
           allowfullscreen>
         </iframe>}.strip_heredoc
