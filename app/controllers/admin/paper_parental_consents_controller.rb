b0816b94279e393d7f1748ac5986e72fa981262d
Joe Sak <joe@joesak.com>
2018-01-22 09:31:48 -0600

    Fix bug in paper parental consent ON FILE for admin

--- app/controllers/admin/paper_parental_consents_controller.rb
+++ app/controllers/admin/paper_parental_consents_controller.rb
@@ -6,26 +6,16 @@ module Admin
       student.student_profile.update_columns(parent_guardian_name: "ON FILE",
                                              parent_guardian_email: "ON FILE")
 
-      sql = ActiveRecord::Base.send(
-        :sanitize_sql_array,
-        [
-          "UPDATE parental_consents
-           SET (status, electronic_signature, created_at, updated_at)
-             VALUES (?, ?, ?, ?)
-               WHERE parental_consents.student_profile_id = ?",
-         ParentalConsent.statuses[:signed],
-         'ON FILE',
-         Time.current,
-         Time.current,
-         student.id,
-        ]
+      consent = ParentalConsent.where(student_profile_id: params[:id]).last
+
+      consent.update_columns(
+        status: ParentalConsent.statuses[:signed],
+        electronic_signature: "ON FILE"
       )
 
-      ActiveRecord::Base.connection.execute(sql)
+      consent.after_signed_student_actions
 
-      ParentalConsent.last.after_signed_student_actions
-
-      redirect_to admin_profile_path(student.account),
+      redirect_to admin_participant_path(student.account),
         success: "#{student.full_name} has their paper parental consent on file."
     end
   end
