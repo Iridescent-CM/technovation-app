cc1348bfabb08c038d3165f57cf217f1037c2216
Joe Sak <joe@joesak.com>
2018-01-24 11:36:52 -0600

    Move shared submission controller code into the concern

--- app/controllers/concerns/team_submission_controller.rb
+++ app/controllers/concerns/team_submission_controller.rb
@@ -7,7 +7,7 @@ module TeamSubmissionController
 
     before_action -> {
       @submission_section = SubmissionSection.new(piece_name, self)
-    }, only: :edit
+    }, only: [:edit, :show]
   end
 
   def new
@@ -17,8 +17,73 @@ module TeamSubmissionController
     )
   end
 
+  def create
+    @team_submission = if current_team.submission.present?
+                          current_team.submission
+                        else
+                          current_team.team_submissions.build(
+                            team_submission_params
+                          )
+                        end
+
+    if @team_submission.save
+      current_team.create_activity(
+        trackable: current_account,
+        key: "submission.create",
+        recipient: @team_submission,
+      )
+      redirect_to [current_scope, @team_submission],
+        success: t("controllers.team_submissions.create.success")
+    else
+      render "team_submissions/new"
+    end
+  end
+
+  def edit
+    unless SeasonToggles.team_submissions_editable?
+      redirect_to send("#{current_scope}_dashboard_path") and return
+    end
+
+    @team_submission = current_team.submission
+    @team = current_team
+
+    if params[:attributes]
+      JSON.parse(params[:attributes]).each do |k, v|
+        @team_submission.public_send("#{k}=", v)
+      end
+
+      @team_submission.valid?
+    end
+
+    @team_submission.screenshots.build
+
+    unless @team_submission.business_plan.present?
+      @team_submission.build_business_plan
+    end
+
+    unless @team_submission.pitch_presentation.present?
+      @team_submission.build_pitch_presentation
+    end
+
+    if @team_submission.code_checklist.present?
+      @code_checklist = @team_submission.code_checklist
+    else
+      @code_checklist = @team_submission.build_code_checklist
+    end
+
+    if @team_submission.present?
+      begin
+        render "team_submissions/pieces/#{piece_name}"
+      rescue ActionView::MissingTemplate,
+              ActionController::ParameterMissing
+        redirect_to [current_scope, @team_submission]
+      end
+    else
+      redirect_to [:new, current_scope, :team_submission]
+    end
+  end
+
   def show
-    @submission_section = SubmissionSection.new(piece_name, self)
     @team_submission = TeamSubmission.friendly.find(params[:id])
     @team = @team_submission.team
 
@@ -109,6 +174,44 @@ module TeamSubmissionController
         :ideation
 
       "team_submissions/sections/#{section}"
+    end
+  end
+
+  def team_submission_params
+    if SeasonToggles.team_submissions_editable?
+      params.require(:team_submission).permit(
+        :team_id,
+        :integrity_affirmed,
+        :source_code,
+        :app_description,
+        :app_name,
+        :demo_video_link,
+        :pitch_video_link,
+        :development_platform_other,
+        :development_platform,
+        :app_inventor_app_name,
+        :app_inventor_gmail,
+        screenshots: [],
+        screenshots_attributes: [
+          :id,
+          :image,
+        ],
+        business_plan_attributes: [
+          :id,
+          :uploaded_file,
+        ],
+        pitch_presentation_attributes: [
+          :id,
+          :uploaded_file,
+        ],
+      )
+    else
+      params.require(:team_submission).permit(
+        pitch_presentation_attributes: [
+          :id,
+          :remote_file_url,
+        ],
+      )
     end
   end
 end
--- app/controllers/mentor/team_submissions_controller.rb
+++ app/controllers/mentor/team_submissions_controller.rb
@@ -4,66 +4,6 @@ module Mentor
 
     before_action :require_onboarded
 
-    def create
-      @team_submission = TeamSubmission.new(team_submission_params)
-
-      if @team_submission.save
-        current_account.create_activity(
-          trackable: current_account,
-          key: "submission.create",
-          recipient: @team_submission,
-        )
-        redirect_to mentor_team_submission_path(@team_submission),
-          success: t("controllers.team_submissions.create.success")
-      else
-        render "team_submissions/new"
-      end
-    end
-
-    def edit
-      unless SeasonToggles.team_submissions_editable?
-        redirect_to mentor_dashboard_path and return
-      end
-
-      @team_submission = TeamSubmission.friendly.find(params[:id])
-      @team = @team_submission.team
-
-      if params[:attributes]
-        JSON.parse(params[:attributes]).each do |k, v|
-          @team_submission.public_send("#{k}=", v)
-        end
-
-        @team_submission.valid?
-      end
-
-      @team_submission.screenshots.build
-
-      unless @team_submission.business_plan.present?
-        @team_submission.build_business_plan
-      end
-
-      unless @team_submission.pitch_presentation.present?
-        @team_submission.build_pitch_presentation
-      end
-
-      if @team_submission.code_checklist.present?
-        @code_checklist = @team_submission.code_checklist
-      else
-        @code_checklist = @team_submission.build_code_checklist
-      end
-
-      if @team_submission.present?
-        begin
-          render "team_submissions/pieces/#{params.fetch(:piece)}"
-        rescue ActionView::MissingTemplate,
-               ActionController::ParameterMissing
-          redirect_to mentor_team_submission_path(@team_submission)
-        end
-      else
-        redirect_to new_mentor_team_submission_path
-      end
-    end
-
     private
     def require_onboarded
       if current_mentor.onboarded?
@@ -71,44 +11,6 @@ module Mentor
       else
         redirect_to mentor_dashboard_path,
           notice: t("controllers.application.onboarding_required")
-      end
-    end
-
-    def team_submission_params
-      if SeasonToggles.team_submissions_editable?
-        params.require(:team_submission).permit(
-          :team_id,
-          :integrity_affirmed,
-          :source_code,
-          :app_description,
-          :app_name,
-          :demo_video_link,
-          :pitch_video_link,
-          :development_platform_other,
-          :development_platform,
-          :app_inventor_app_name,
-          :app_inventor_gmail,
-          screenshots: [],
-          screenshots_attributes: [
-            :id,
-            :image,
-          ],
-          business_plan_attributes: [
-            :id,
-            :uploaded_file,
-          ],
-          pitch_presentation_attributes: [
-            :id,
-            :uploaded_file,
-          ],
-        )
-      else
-        params.require(:team_submission).permit(
-          pitch_presentation_attributes: [
-            :id,
-            :remote_file_url,
-          ],
-        )
       end
     end
   end
