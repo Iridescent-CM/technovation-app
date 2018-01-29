8c03274c9b4ea5e2c8803ec5a7a381be43af2de2
Joe Sak <joe@joesak.com>
2018-01-29 16:47:17 -0600

    WIP: add event form

--- app/javascript/packs/ra_events.js
+++ app/javascript/packs/ra_events.js
@@ -1,6 +1,10 @@
 import TurbolinksAdapter from 'vue-turbolinks';
+import { Datetime } from 'vue-datetime';
+
 import Vue from 'vue/dist/vue.esm'
 import App from '../app.vue'
+
+import 'vue-datetime/dist/vue-datetime.css';
 
 Vue.use(TurbolinksAdapter)
 
@@ -8,9 +12,18 @@ document.addEventListener('turbolinks:load', () => {
   const app = new Vue({
     el: '#events',
     data: {
-      message: "Hello, RAs! The Technovation development team is " +
-               "hard at work on this section right now. " +
-               "Stay tuned for email announcements.",
+      event: {
+        name: "",
+        city: "",
+        venue_address: "",
+        divisions: [],
+        starts_at: "2018-05-05T18:00:00.000",
+        ends_at: "2018-05-05T22:00:00.000",
+      },
+
+      minDatetime: "2018-05-01T00:00:00.000Z",
+      maxDatetime: "2018-05-15T23:59:59.000Z",
+
     },
 
     computed: {
@@ -21,6 +34,7 @@ document.addEventListener('turbolinks:load', () => {
 
     components: {
       App,
+      Datetime,
     },
 
     mounted () {
