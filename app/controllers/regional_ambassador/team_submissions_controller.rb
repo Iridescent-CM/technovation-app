d9ab06ee39bda06e74203b8ef35476b9968dce5a
Alli Colyer <allicolyer@gmail.com>
2018-01-30 10:11:31 -0800

    more houndbot corrections

--- app/controllers/regional_ambassador/team_submissions_controller.rb
+++ app/controllers/regional_ambassador/team_submissions_controller.rb
@@ -13,18 +13,17 @@ module RegionalAmbassador
     end
 
     private
+    def grid_params
+      grid = (params[:submissions_grid] ||= {}).merge(
+        admin: true,
+        country: Array(params[:submissions_grid][:country]),
+        state_province: Array(params[:submissions_grid][:state_province]),
+        season: params[:submissions_grid][:season] || Season.current.year,
+      )
 
-      def grid_params
-        grid = (params[:submissions_grid] ||= {}).merge(
-          admin: true,
-          country: Array(params[:submissions_grid][:country]),
-          state_province: Array(params[:submissions_grid][:state_province]),
-          season: params[:submissions_grid][:season] || Season.current.year,
-        )
-
-        grid.merge(
-          column_names: detect_extra_columns(grid),
-        )
-      end
+      grid.merge(
+        column_names: detect_extra_columns(grid),
+      )
+    end
   end
 end
