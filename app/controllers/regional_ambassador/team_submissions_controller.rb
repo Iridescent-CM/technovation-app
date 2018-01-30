d91ee02b8688863c1dc2774b152266797ff1dddf
Alli Colyer <allicolyer@gmail.com>
2018-01-30 15:45:00 -0800

    Ra view submission (#1429)
    * remove unused files in the admin
    
    * remove unnecessary RA files
    
    * work copied over from Admin
    
    * add view button
    
    * scope in region of RA
    
    * add show template for RAs
    
    * fix admin submission view if there is no development platform chosen
    
    * allow RAs to only view submission
    
    * houndbot corrections
    
    * more houndbot corrections

--- app/controllers/regional_ambassador/team_submissions_controller.rb
+++ app/controllers/regional_ambassador/team_submissions_controller.rb
@@ -1,23 +1,29 @@
-require "will_paginate/array"
-
 module RegionalAmbassador
   class TeamSubmissionsController < RegionalAmbassadorController
-    def index
-      params[:per_page] = 15 if params[:per_page].blank?
-      params[:page] = 1 if params[:page].blank?
+    include DatagridUser
 
-      @team_submissions = RegionalAmbassador::SearchTeamSubmissions.(params, current_ambassador)
-        .sort { |a, b| a.team_name.downcase <=> b.team_name.downcase }
-        .paginate(page: params[:page].to_i, per_page: params[:per_page].to_i)
-
-      if @team_submissions.empty?
-        @team_submissions = @team_submissions.paginate(page: 1)
-      end
-    end
+    use_datagrid with: SubmissionsGrid,
+    html_scope: ->(scope, user, params) {
+        scope.in_region(user).page(params[:page])
+      },
+      csv_scope: "->(scope, user, params) { scope.in_region(user) }"
 
     def show
       @team_submission = TeamSubmission.friendly.find(params[:id])
-      @team_submission.build_technical_checklist if @team_submission.technical_checklist.blank?
+    end
+
+    private
+    def grid_params
+      grid = (params[:submissions_grid] ||= {}).merge(
+        admin: true,
+        country: Array(params[:submissions_grid][:country]),
+        state_province: Array(params[:submissions_grid][:state_province]),
+        season: params[:submissions_grid][:season] || Season.current.year,
+      )
+
+      grid.merge(
+        column_names: detect_extra_columns(grid),
+      )
     end
   end
 end
