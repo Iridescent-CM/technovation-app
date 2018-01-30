7bcb69f881cd43544ce2562187c11e70aa36255f
Alli Colyer <allicolyer@gmail.com>
2018-01-30 09:56:06 -0800

    allow RAs to only view submission

--- app/controllers/regional_ambassador/team_submissions_controller.rb
+++ app/controllers/regional_ambassador/team_submissions_controller.rb
@@ -11,25 +11,19 @@ module RegionalAmbassador
       @team_submission = TeamSubmission.friendly.find(params[:id])
     end
 
-    def edit
-      @team_submission = TeamSubmission.in_region(current_ambassador).find(params[:id])
-    end
-
-    def update
-    end
 
     private
-    def grid_params
-      grid = (params[:submissions_grid] ||= {}).merge(
-        admin: true,
-        country: Array(params[:submissions_grid][:country]),
-        state_province: Array(params[:submissions_grid][:state_province]),
-        season: params[:submissions_grid][:season] || Season.current.year,
-      )
+      def grid_params
+        grid = (params[:submissions_grid] ||= {}).merge(
+          admin: true,
+          country: Array(params[:submissions_grid][:country]),
+          state_province: Array(params[:submissions_grid][:state_province]),
+          season: params[:submissions_grid][:season] || Season.current.year,
+        )
 
-      grid.merge(
-        column_names: detect_extra_columns(grid),
-      )
+        grid.merge(
+          column_names: detect_extra_columns(grid),
+        )
+      end
   end
-end
 end
