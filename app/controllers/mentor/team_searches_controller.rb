8cc95676f584b00a3eb098e5e1385e512707e033
Joe Sak <joe@joesak.com>
2018-01-31 10:24:30 -0600

    Fix bug in mentor team search controller

--- app/controllers/mentor/team_searches_controller.rb
+++ app/controllers/mentor/team_searches_controller.rb
@@ -2,15 +2,22 @@ module Mentor
   class TeamSearchesController < MentorController
     def new
       if current_mentor.latitude.blank?
-        redirect_to mentor_location_details_path(return_to: request.fullpath),
-          notice: "Please save your location so that you can search for nearby teams"
+        redirect_to mentor_location_details_path(
+          return_to: request.fullpath
+        ),
+          notice: "Please save your location so that you can " +
+                  "search for nearby teams"
       end
 
-      params[:nearby] = current_mentor.address_details if params[:nearby].blank?
+      if params[:nearby].blank?
+        params[:nearby] = current_mentor.address_details
+      end
+
       params[:division_enums] ||= Division.names.values
 
       if params[:division_enums].respond_to?(:keys)
-        params[:division_enums] = params[:division_enums].keys.flatten.distinct
+        params[:division_enums] = params[:division_enums].keys
+                                    .flatten.uniq
       end
 
       @search_filter = SearchFilter.new({
