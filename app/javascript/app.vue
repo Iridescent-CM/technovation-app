ae95742b8280f8c837d5f4c7fe686f751d2771d7
Joe Sak <joe@joesak.com>
2018-01-31 10:19:07 -0600

    Clear out the default vue app

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,13 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
-  </div>
 </template>
 
 <script>
-export default {
-  data: function () {
-    return {
-      message: "Hello Vue!"
+  export default {
+    data: function () {
+      return { }
     }
   }
-}
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
 </style>
