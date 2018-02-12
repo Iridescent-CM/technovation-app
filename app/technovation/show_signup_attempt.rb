69cdee51fe490c02e1fd0d163575d5f6bd498097
Joe Sak <joe@joesak.com>
2018-02-12 08:18:04 -0600

    Strip signup attempt token to first 24 chars in case the email client mangled the URL

--- app/technovation/show_signup_attempt.rb
+++ app/technovation/show_signup_attempt.rb
@@ -1,6 +1,7 @@
 module ShowSignupAttempt
   def self.call(pending_token, callbacks)
-    attempt = SignupAttempt.find_by(pending_token: pending_token)
+    token = pending_token[0..23]
+    attempt = SignupAttempt.find_by(pending_token: token)
 
     if attempt.temporary_password?
       RegistrationMailer.confirm_email(attempt).deliver_later
