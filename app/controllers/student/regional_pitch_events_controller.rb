20bc729c234c98fbe1c541ad62fcb56c6f0e9c37
Alli Colyer <allicolyer@gmail.com>
2018-02-26 12:27:51 -0800

    add index view and route to show RPEs to studnet

--- app/controllers/student/regional_pitch_events_controller.rb
+++ app/controllers/student/regional_pitch_events_controller.rb
@@ -2,6 +2,11 @@ module Student
   class RegionalPitchEventsController < StudentController
     before_action :require_current_team
 
+    def index
+      @regional_events = RegionalPitchEvent.available_to(current_team.submission)
+      render template: 'regional_pitch_events/index'
+    end
+
     def show
       if params[:id] == "virtual"
         @regional_pitch_event = VirtualRegionalPitchEvent.new
