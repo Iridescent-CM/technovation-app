bc58911e3af068d0af5055992db27dee67a6ed57
Joe Sak <joe@joesak.com>
2018-03-01 14:19:52 -0600

    Allow RAs to remove team members

--- app/technovation/can_remove_team_member.rb
+++ app/technovation/can_remove_team_member.rb
@@ -1,7 +1,8 @@
 module CanRemoveTeamMember
   def self.call(account, member)
     account.admin_profile or
-      member.onboarding? or
-        account.id == member.account_id
+      account.regional_ambassador_profile or
+        member.onboarding? or
+          account.id == member.account_id
   end
 end
