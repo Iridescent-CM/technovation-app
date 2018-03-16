22bd4c89e33759acee20afaebb26bbdd6a7a7254
Joe Sak <joe@joesak.com>
2018-03-16 10:37:49 -0600

    Memoize events_available_to a loop of team submissions

--- app/controllers/mentor/regional_pitch_events_team_lists_controller.rb
+++ app/controllers/mentor/regional_pitch_events_team_lists_controller.rb
@@ -1,8 +1,24 @@
 module Mentor
   class RegionalPitchEventsTeamListsController < MentorController
+    helper_method :events_available_to,
+      :can_select_live_event?
 
     def show
       @current_teams = current_mentor.teams.current.order("teams.name")
+    end
+
+    private
+    def events_available_to(submission)
+      @events_available ||= {}
+
+      @events_available[submission.id] ||=
+        RegionalPitchEvent.available_to(submission)
+    end
+
+    def can_select_live_event?(team)
+      SeasonToggles.select_regional_pitch_event? and
+        not team.live_event? and
+          events_available_to(team.submission).any?
     end
   end
 end
