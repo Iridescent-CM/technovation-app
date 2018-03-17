1129f531dc0cf0ee658ef92809421e9e576ac05f
Joe Sak <joe@joesak.com>
2018-03-16 18:22:32 -0600

    Fix submission score factory due to seasons field

--- spec/factories/submission_scores.rb
+++ spec/factories/submission_scores.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     judge_profile
     association(:team_submission, factory: [:team_submission, :junior])
     round SubmissionScore.rounds[:quarterfinals]
+    seasons [Season.current.year]
 
     trait :complete do
       completed_at Time.current
