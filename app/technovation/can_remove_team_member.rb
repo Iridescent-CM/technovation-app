94a73ea94bce5e460a38da54d438de8e08bd8589
Joe Sak <joe@joesak.com>
2018-03-20 16:46:03 -0600

    Hide the remove link from RAs in mentor mode on team page

--- app/technovation/can_remove_team_member.rb
+++ app/technovation/can_remove_team_member.rb
@@ -1,7 +1,7 @@
 module CanRemoveTeamMember
-  def self.call(account, member)
+  def self.call(account, member, admin_ra = false)
     account.admin_profile or
-      account.regional_ambassador_profile or
+      (account.regional_ambassador_profile and admin_ra) or
         member.onboarding? or
           account.id == member.account_id
   end
