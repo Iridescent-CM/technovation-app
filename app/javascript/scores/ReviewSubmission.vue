02d85d1c63dcb7a0ec8f837880e12c2a76637b59
Joe Sak <joe@joesak.com>
2018-03-22 12:36:00 -0600

    Implement basic ReviewSubmission, get it started

--- app/javascript/scores/ReviewSubmission.vue
+++ app/javascript/scores/ReviewSubmission.vue
@@ -1,8 +1,20 @@
 <template>
-  <div>Review submission under construction</div>
+  <dl>
+    <dt>Team</dt>
+    <dd>{{ team.name }}</dd>
+
+    <dt>Submission</dt>
+    <dd>{{ submission.name }}</dd>
+  </dl>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
+  computed: mapState([
+    'team',
+    'submission',
+  ]),
 }
 </script>
