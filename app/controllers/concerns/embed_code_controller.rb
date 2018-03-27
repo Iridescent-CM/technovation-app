1d7ac841ea174215a55ed1fbab07f372235ddbf2
Joe Sak <joe@joesak.com>
2018-03-27 10:46:29 -0600

    Find any team submission embed code with the ID in the params

--- app/controllers/concerns/embed_code_controller.rb
+++ app/controllers/concerns/embed_code_controller.rb
@@ -2,7 +2,7 @@ module EmbedCodeController
   extend ActiveSupport::Concern
 
   def show
-    @team_submission = current_team.submission
+    @team_submission = TeamSubmission.friendly.find(params[:id])
     render inline: @team_submission.embed_code(params.fetch(:piece))
   end
 end
