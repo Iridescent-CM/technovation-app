2f77ae0a60a62fd5a9fb0cd95a9dae14e051b450
Joe Sak <joe@joesak.com>
2018-03-27 12:52:24 -0600

    Fix rearranged migration becuase the column exists on remote DBs

--- db/migrate/20170831151120_add_percent_complete_to_team_submissions.rb
+++ db/migrate/20170831151120_add_percent_complete_to_team_submissions.rb
@@ -1,9 +1,17 @@
 class AddPercentCompleteToTeamSubmissions < ActiveRecord::Migration[5.1]
-  def change
-    add_column :team_submissions,
-      :percent_complete,
-      :integer,
-      null: false,
-      default: 0
+  def up
+    unless column_exists?(:team_submissions, :percent_complete)
+      add_column :team_submissions,
+        :percent_complete,
+        :integer,
+        null: false,
+        default: 0
+    end
+  end
+
+  def down
+    if column_exists?(:team_submissions, :percent_complete)
+      remove_column :team_submissions, :percent_complete
+    end
   end
 end
