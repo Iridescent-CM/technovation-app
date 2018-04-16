323acdd4dbe7fdbb06d30212bafdc582862a924a
Joe Sak <joe@joesak.com>
2018-04-16 09:09:56 -0500

    Fix student profile / onboarded migration

--- db/migrate/20180410160317_add_onboarded_to_student_profiles.rb
+++ db/migrate/20180410160317_add_onboarded_to_student_profiles.rb
@@ -3,13 +3,7 @@ class AddOnboardedToStudentProfiles < ActiveRecord::Migration[5.1]
     add_column :student_profiles, :onboarded, :boolean, default: false
 
     StudentProfile.current.find_each do |s|
-      if s.signed_parental_consent.blank? or
-          s.account.email_confirmed_at.blank? or
-            s.account.latitude.blank?
-        s.update_column(:onboarded, false)
-      else
-        s.update_column(:onboarded, true)
-      end
+      s.update_column(:onboarded, s.can_be_marked_onboarded?)
     end
   end
 
