bb3a84f3cbfaf6f42deafde1ab4a163c1d523b2e
Joe Sak <joe@joesak.com>
2018-04-18 09:15:02 -0500

    Add an icon to the source code link, to conform to the style of the pitch / demo videos

--- app/javascript/judge/scores/pieces/Code.vue
+++ app/javascript/judge/scores/pieces/Code.vue
@@ -2,9 +2,14 @@
   <div class="grid__col-6">
     <h4>Code</h4>
 
-    <a :href="submission.source_code_url" target="_blank">
-      Download the source code
-    </a>
+    <p>
+      <a :href="submission.source_code_url" target="_blank">
+        <icon name="code" color="ffffff" />
+        <span>Download the source code</span>
+      </a>
+    </p>
+
+    <h4>Code Checklist</h4>
 
     <p>
       {{ team.name }} automatically earned
@@ -21,14 +26,29 @@
 
 <script>
 import { mapState } from 'vuex'
+import Icon from '../../../components/Icon'
 
 export default {
   computed: mapState(['team', 'submission']),
+
+  components: {
+    Icon,
+  },
 }
 </script>
 
 <style scoped>
 h4 {
-  margin: 0 0 1rem;
+  margin: 1rem 0 0;
+}
+
+a {
+  display: flex;
+}
+
+img,
+span {
+  align-self: center;
+  margin-right: 0.5rem;
 }
 </style>
