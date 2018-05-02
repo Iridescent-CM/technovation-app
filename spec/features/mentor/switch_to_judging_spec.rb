69117536cd452eed68192e6069f19ace7fb57faa
Joe Sak <joe@joesak.com>
2018-05-02 09:20:46 -0500

    Unskip a spec

--- spec/features/mentor/switch_to_judging_spec.rb
+++ spec/features/mentor/switch_to_judging_spec.rb
@@ -1,20 +1,7 @@
 require "rails_helper"
 
 RSpec.feature "Mentors switch to judging mode" do
-  scenario "when judge signups are not enabled" do
-    skip "Not hiding judge mode link behind season toggle"
-
-    SeasonToggles.disable_signup(:judge)
-
-    mentor = FactoryBot.create(:mentor)
-    sign_in(mentor)
-
-    expect(page).not_to have_link("Switch to Judge mode")
-  end
-
-  scenario "when judge signups are enabled" do
-    SeasonToggles.enable_signup(:judge)
-
+  scenario "mentors can switch to judge mode" do
     mentor = FactoryBot.create(:mentor)
     sign_in(mentor)
 
