f99ce7bb4828878b52f197d26149a25921380620
Joe Sak <joe@joesak.com>
2018-05-03 08:23:56 -0500

    Support saved searches in ScoresGrid

--- app/controllers/concerns/saved_search_controller.rb
+++ app/controllers/concerns/saved_search_controller.rb
@@ -19,6 +19,8 @@ module SavedSearchController
                   "#{current_scope}_team_submissions_path"
                 when "judges_grid"
                   "#{current_scope}_judges_path"
+                when "scores_grid"
+                  "#{current_scope}_scores_path"
                 else
                   raise "Param root #{@saved_search.param_root} not supported"
                 end
