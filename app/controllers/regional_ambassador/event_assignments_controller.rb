ee37c8df490c6c2cc392af0f7a249756b11e76bf
Joe Sak <joe@joesak.com>
2018-05-03 11:15:36 -0500

    Create Event Assignments in testable module

--- app/controllers/regional_ambassador/event_assignments_controller.rb
+++ app/controllers/regional_ambassador/event_assignments_controller.rb
@@ -2,48 +2,7 @@ module RegionalAmbassador
   class EventAssignmentsController < RegionalAmbassadorController
     def create
       event = RegionalPitchEvent.find(assignment_params.fetch(:event_id))
-
-      invite_params = assignment_params.fetch(:invites)
-
-      invite_params.each do |_, par|
-        # params come in strange
-        # FIXME in EventJudgeList.vue#saveAssignments form appending...
-        # FIXME in EventTeamList.vue#saveAssignments form appending...
-        opts = par.first
-
-        unless invite = opts[:scope].constantize.find_by(id: opts[:id])
-          invite = opts[:scope].constantize.create!({
-            email: opts[:email],
-            name: opts[:name],
-            profile_type: :judge,
-          });
-        end
-
-        unless invite.events.include?(event)
-          InvalidateExistingJudgeData.(invite)
-          invite.events << event
-        end
-
-        # FIXME "true" == ...
-        # make it a real boolean
-        if "true" == opts[:send_email]
-          if opts[:scope] == "Team"
-            invite.memberships.each do |membership|
-              EventMailer.invite(
-                membership.member_type,
-                membership.member_id,
-                event.id,
-              ).deliver_later
-            end
-          else
-            EventMailer.invite(
-              opts[:scope],
-              invite.id,
-              event.id,
-            ).deliver_later
-          end
-        end
-      end
+      CreateEventAssignment.(event, assignment_params)
 
       render json: {
         flash: {
