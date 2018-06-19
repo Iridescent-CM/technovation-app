3409549a8782d9b31b040f1d35bad57ddd57f706
Joe Sak <joe@joesak.com>
2018-06-19 10:50:49 -0500

    Add json payload field to Job model

---
+++ spec/models/job_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe Job do
+  it "stores a JSON payload" do
+    owner = FactoryBot.create(:mentor).account
+
+    job = Job.create!(
+      owner: owner,
+      payload: { custom: "field data" }
+    )
+
+    expect(job.payload).to eq({ "custom" => "field data" })
+  end
+end
