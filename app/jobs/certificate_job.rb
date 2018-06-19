b6234ba1f1573a20af4b930911726b778cfd332b
Joe Sak <joe@joesak.com>
2018-06-19 10:42:44 -0500

    Ensure a job is tracked in the DB for certificates

--- app/jobs/certificate_job.rb
+++ app/jobs/certificate_job.rb
@@ -3,6 +3,23 @@ require 'fill_pdfs'
 class CertificateJob < ActiveJob::Base
   queue_as :default
 
+  before_enqueue do |job|
+    account_id = job.arguments.first
+    owner = Account.find(account_id)
+
+    Job.create!(
+      job_id: job.job_id,
+      status: "queued",
+      owner: owner,
+    )
+  end
+
+  after_perform do |job|
+    if db_job = Job.find_by(job_id: job.job_id)
+      db_job.update_column(:status, "complete")
+    end
+  end
+
   def perform(account_id, team_id = nil)
     account = Account.find(account_id)
 
--- spec/jobs/certificate_job_spec.rb
+++ spec/jobs/certificate_job_spec.rb
@@ -6,9 +6,22 @@ RSpec.describe CertificateJob do
     team = mentor.current_teams.last
 
     expect {
-      CertificateJob.perform_now(mentor.account_id, team.id)
+      CertificateJob.perform_later(mentor.account_id, team.id)
     }.to change {
       mentor.current_appreciation_certificates.count
     }.from(0).to(1)
+  end
+
+  it "tracks the job in the database" do
+    mentor = FactoryBot.create(:mentor, :on_team, :complete_submission)
+    job_id = nil
+
+    expect {
+      job_id = CertificateJob.perform_later(mentor.account_id).job_id
+    }.to change {
+      Job.count
+    }.from(0).to(1)
+
+    expect(Job.last.job_id).to eq(job_id)
   end
 end
