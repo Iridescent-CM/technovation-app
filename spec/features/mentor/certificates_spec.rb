cdc1bbb49c7283484acb12307d62b68fe9e3cf5e
Joe Sak <joe@joesak.com>
2018-06-21 17:18:47 -0500

    Add thank you message for mentors with no teams / no scores

--- spec/features/mentor/certificates_spec.rb
+++ spec/features/mentor/certificates_spec.rb
@@ -5,7 +5,31 @@ RSpec.feature "Mentor certificates" do
 
   scenario "no certificates for no teams" do
     mentor = FactoryBot.create(:mentor)
+
     sign_in(mentor)
+
     expect(page).not_to have_link("Open your certificate")
+
+    expect(page).to have_content(
+      "Thank you for your participation in the #{Season.current.year} " +
+      "Technovation season. Check out the finalists at " +
+      "technovationchallenge.org/season-results"
+    )
+
+    expect(page).to have_content(
+      "The #{Season.next.year} season will open in the Fall. " +
+      "Sign up for our newsletter to stay in the loop about important dates " +
+      "and updates."
+    )
+
+    expect(page).to have_link(
+      "Sign up for our newsletter",
+      href: ENV.fetch("NEWSLETTER_URL")
+    )
+
+    expect(page).to have_link(
+      "technovationchallenge.org/season-results",
+      href: ENV.fetch("FINALISTS_URL")
+    )
   end
 end
