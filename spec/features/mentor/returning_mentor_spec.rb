264d55d1dc1f89d024e03f8938372e8a69224138
Joe Sak <joe@joesak.com>
2018-06-26 17:07:51 -0500

    Ensure previous mentors before new mentor_type field are prompted to set theirs

---
+++ spec/features/mentor/returning_mentor_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.feature "Returning mentor" do
+  scenario "asked to select their mentor_type" do
+    mentor = FactoryBot.create(:mentor)
+    mentor.update_column(:mentor_type, nil)
+
+    sign_in(mentor)
+
+    expect(page).to have_content("Please let us know what type of mentor you are")
+    select "Educator", from: "Choose your mentor type"
+    click_button "Save"
+
+    expect(mentor.reload.mentor_type).to eq("Educator")
+  end
+end
