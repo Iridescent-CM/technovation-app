4b830cbdae84617a16447caf66aa177dab2c2694
Alan Richey <adrichey@users.noreply.github.com>
2018-06-27 15:29:58 -0500

    Pie Charts Vue Refactor: Part 1 (#1566)
    * Convert pie charts over to using Vue components. Still a WIP. Need to figure out how to test Canvas elements in Jest.
    
    * Add canvas NodeJS polyfill and requirements for testing
    
    * Update README for canvas polyfill requirements
    
    * Add initializeChart tests and minor refactor to make things more readable
    
    * Refactor remaining admin UI charts to use the PieChart component
    
    * Convert remaining charts over to use PieChart Vue component
    
    * Add RA index file I missed during previous commit
    
    * Remove deprecated way of initializing pie charts
    
    * Remove unused chart-js-rails dependency from gems
    
    * Combine admin dashboard files into a singular source
    
    * Change element class to pie-chart
    
    * Adjust spec structure for readability

---
+++ app/javascript/components/PieChart.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="pie-chart">
+    <canvas :class="chartClasses"></canvas>
+  </div>
+</template>
+
+<script>
+
+import Chart from 'chart.js'
+import chroma from 'chroma-js'
+
+const defaultData = {
+  labels: [],
+  datasets: [
+    {
+      data: [],
+      backgroundColor: [],
+      hoverBackgroundColor: [],
+      urls: [],
+    }
+  ],
+}
+
+export default {
+  name: 'pie-chart',
+
+  data () {
+    return {
+      chart: null,
+      mutableChartData: {},
+    }
+  },
+
+  props: {
+    chartData: {
+      type: Object,
+      required: true,
+    },
+
+    chartClasses: {
+      type: Object,
+      default () {
+        return {
+          'quickview-charts': true,
+        }
+      },
+    },
+
+    colorRange: {
+      type: Object,
+      default () {
+        return {
+          start: 'rgb(54, 162, 235)',
+          end: 'rgb(255, 99, 132)',
+        }
+      },
+    },
+  },
+
+  mounted () {
+    this.initializeChart()
+  },
+
+  methods: {
+    initializeChart () {
+      if (this.chart !== null) {
+        this.chart.destroy()
+      }
+
+      const chartElement = this.$el.querySelector('canvas')
+      const chartContext = chartElement.getContext('2d')
+
+      const extendedChartData = Object.assign({}, defaultData, this.chartData)
+
+      // Generate colors based on number of data items in the dataset
+      Object.keys(extendedChartData.datasets).forEach((key) => {
+        const numberOfDataItems = extendedChartData.datasets[key].data.length
+        const backgroundColors = this.generateBackgroundColors(numberOfDataItems)
+
+        extendedChartData.datasets[key].hoverBackgroundColor = backgroundColors.hoverBackgroundColor
+        extendedChartData.datasets[key].backgroundColor = backgroundColors.backgroundColor
+      })
+
+      const { urls } = extendedChartData.datasets[0]
+
+      if (typeof urls !== 'undefined' && urls.length > 0)
+        chartElement.style.cursor = 'pointer'
+
+      this.chart = new Chart(chartContext, {
+        type: 'pie',
+        data: extendedChartData,
+        options: {
+          legend: {
+            position: 'bottom',
+          },
+          onClick: (evt, els) => {
+            const i = els[0]._index
+
+            if (typeof urls !== 'undefined' && urls.length > 0)
+              window.location.href = urls[i]
+          },
+        },
+      })
+
+      this.$set(this, 'mutableChartData', extendedChartData)
+    },
+
+    generateBackgroundColors (numberOfColors) {
+      const colorScale = chroma
+        .scale([
+          this.colorRange.start,
+          this.colorRange.end,
+        ])
+        .mode('hsv')
+        .colors(numberOfColors)
+
+      const backgroundColors = {
+        backgroundColor: [],
+        hoverBackgroundColor: [],
+      }
+
+      colorScale.forEach((color) => {
+        const hoverColorString = `rgba(${chroma(color).alpha(1).rgba().join(',')})`
+        backgroundColors.hoverBackgroundColor.push(hoverColorString)
+
+        const colorString = `rgba(${chroma(color).alpha(0.7).rgba().join(',')})`
+        backgroundColors.backgroundColor.push(colorString)
+      })
+
+      return backgroundColors
+    },
+  },
+}
+</script>
+
+<style scoped>
+</style>
---
+++ spec/javascript/components/PieChart.spec.js
@@ -0,0 +1,186 @@
+import { shallow } from '@vue/test-utils'
+
+import PieChart from 'components/PieChart'
+
+require('canvas')
+
+describe('PieChart Vue component', () => {
+
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallow(PieChart, {
+      propsData: {
+        chartData: {
+          labels: [
+            'With parental permission – (75%)',
+            'Without parental permission – (25%)',
+          ],
+          datasets: [
+            {
+              data: [
+                3,
+                1,
+              ],
+              urls: [
+                '/with/parental/permission',
+                '/without/parental/permission',
+              ],
+            },
+          ],
+        },
+        colorRange: {
+          start: '#fafa6e',
+          end: '#2A4858',
+        },
+      },
+    })
+  })
+
+  describe('props', () => {
+
+    it('contains valid props', () => {
+      expect(PieChart.props).toEqual({
+        chartData: {
+          type: Object,
+          required: true,
+        },
+
+        chartClasses: {
+          type: Object,
+          default: expect.any(Function)
+        },
+
+        colorRange: {
+          type: Object,
+          default: expect.any(Function)
+        },
+      })
+    })
+
+    describe('chartClasses', () => {
+      it('sets the quickview-charts class by default', () => {
+        expect(PieChart.props.chartClasses.default())
+          .toEqual({
+            'quickview-charts': true,
+          })
+      })
+    })
+
+    describe('colorRange', () => {
+      it('sets a light blue to light pink range by default', () => {
+        expect(PieChart.props.colorRange.default())
+          .toEqual({
+            start: 'rgb(54, 162, 235)',
+            end: 'rgb(255, 99, 132)',
+          })
+      })
+    })
+
+  })
+
+  describe('data', () => {
+
+    it('sets the correct initial state', () => {
+      expect(PieChart.data()).toEqual({
+        chart: null,
+        mutableChartData: {},
+      })
+    })
+
+  })
+
+  describe('methods', () => {
+
+    describe('initializeChart', () => {
+
+      it('destroys the chart if already created', () => {
+        const chartDestroySpy = spyOn(wrapper.vm.chart, 'destroy')
+
+        expect(chartDestroySpy).not.toHaveBeenCalled()
+
+        wrapper.vm.initializeChart()
+
+        expect(chartDestroySpy).toHaveBeenCalledTimes(1)
+      })
+
+      it('sets the mutableChartData by extending the chartData prop', () => {
+        expect(wrapper.vm.mutableChartData).toEqual({
+          labels: [
+            'With parental permission – (75%)',
+            'Without parental permission – (25%)',
+          ],
+          datasets: [
+            expect.objectContaining({
+              backgroundColor: [
+                "rgba(250,250,110,0.7)",
+                "rgba(42,72,88,0.7)",
+              ],
+              data: [
+                3,
+                1,
+              ],
+              hoverBackgroundColor: [
+                "rgba(250,250,110,1)",
+                "rgba(42,72,88,1)",
+              ],
+              urls: [
+                '/with/parental/permission',
+                '/without/parental/permission',
+              ],
+            })
+          ],
+        })
+      })
+
+      it('creates a chart.js instance with the correct settings', () => {
+        const chartElement = wrapper.find('canvas').element
+        const chartContext = chartElement.getContext('2d')
+
+        expect(wrapper.vm.chart.canvas).toEqual(chartElement)
+        expect(wrapper.vm.chart.ctx).toEqual(chartContext)
+        expect(wrapper.vm.chart.config).toEqual(
+          expect.objectContaining({
+            type: 'pie',
+            data: wrapper.vm.mutableChartData,
+            options: expect.objectContaining({
+              legend: expect.objectContaining({
+                position: 'bottom',
+              }),
+              onClick: expect.any(Function),
+            }),
+          })
+        )
+      })
+    })
+
+    describe('generateBackgroundColors', () => {
+
+      it('returns RGBA background and hover color scales based on number of colors passed in', () => {
+        const backgroundColors = wrapper.vm.generateBackgroundColors(6)
+
+        expect(backgroundColors).toEqual({
+          backgroundColor: [
+            'rgba(250,250,110,0.7)',
+            'rgba(161,218,97,0.7)',
+            'rgba(90,185,84,0.7)',
+            'rgba(71,153,104,0.7)',
+            'rgba(57,120,113,0.7)',
+            'rgba(42,72,88,0.7)',
+          ],
+          hoverBackgroundColor: [
+            'rgba(250,250,110,1)',
+            'rgba(161,218,97,1)',
+            'rgba(90,185,84,1)',
+            'rgba(71,153,104,1)',
+            'rgba(57,120,113,1)',
+            'rgba(42,72,88,1)',
+          ],
+        })
+      })
+
+    })
+
+  })
+
+})
