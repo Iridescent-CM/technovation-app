2bc4605a6ed7b9118cbc90917d4f183b43ecfc84
Joe Sak <joe@joesak.com>
2018-06-28 16:01:11 -0500

    Add domain to cookie store config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: ENV.fetch("COOKIE_DOMAIN")
+Rails.application.config.session_store :cookie_store,
+ key: "_technovation_challenge",
+ domain: ENV.fetch("COOKIE_DOMAIN")
