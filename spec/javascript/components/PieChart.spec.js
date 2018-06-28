6aea8dfd026cc52e6ec0fe07bb30e4ed1fe688af
Alan Richey <adrichey@users.noreply.github.com>
2018-06-28 11:47:20 -0500

    Pie Charts Vue Refactor: Part 2 (#1567)
    * Add ability to load chart data via AJAX using url prop on PieChart
    
    * Add ability to load pie chart components via AJAX
    
    * Clean up duplicate test with invalid label

--- spec/javascript/components/PieChart.spec.js
+++ spec/javascript/components/PieChart.spec.js
@@ -1,4 +1,5 @@
 import { shallow } from '@vue/test-utils'
+import axios from 'axios'
 
 import PieChart from 'components/PieChart'
 
@@ -6,29 +7,38 @@ require('canvas')
 
 describe('PieChart Vue component', () => {
 
+  const chartData = {
+    labels: [
+      'With parental permission – (75%)',
+      'Without parental permission – (25%)',
+    ],
+    data: [
+      3,
+      1,
+    ],
+    urls: [
+      '/with/parental/permission',
+      '/without/parental/permission',
+    ],
+  }
+
+  const extendedChartData = Object.assign({}, chartData, {
+    backgroundColor: [
+      "rgba(250,250,110,0.7)",
+      "rgba(42,72,88,0.7)",
+    ],
+    hoverBackgroundColor: [
+      "rgba(250,250,110,1)",
+      "rgba(42,72,88,1)",
+    ],
+  })
+
   let wrapper
 
   beforeEach(() => {
     wrapper = shallow(PieChart, {
       propsData: {
-        chartData: {
-          labels: [
-            'With parental permission – (75%)',
-            'Without parental permission – (25%)',
-          ],
-          datasets: [
-            {
-              data: [
-                3,
-                1,
-              ],
-              urls: [
-                '/with/parental/permission',
-                '/without/parental/permission',
-              ],
-            },
-          ],
-        },
+        chartData,
         colorRange: {
           start: '#fafa6e',
           end: '#2A4858',
@@ -43,19 +53,96 @@ describe('PieChart Vue component', () => {
       expect(PieChart.props).toEqual({
         chartData: {
           type: Object,
-          required: true,
+          default: expect.any(Function),
+          validator: expect.any(Function),
         },
 
         chartClasses: {
           type: Object,
-          default: expect.any(Function)
+          default: expect.any(Function),
         },
 
         colorRange: {
           type: Object,
-          default: expect.any(Function)
+          default: expect.any(Function),
+        },
+
+        url: {
+          type: String,
+          default: '',
         },
       })
+    })
+
+    describe('chartData', () => {
+      const labels = [ 'One', 'Two' ]
+      const data = [ 1, 3 ]
+      const urls = [ '/url/one', '/url/two' ]
+
+      const invalidLabels = 'string'
+      const invalidData = 'string'
+      const invalidUrls = 'string'
+
+      const dataWithoutLabels = { data, urls }
+      const labelsWithoutData = { labels, urls }
+      const labelsAndDataWithoutUrls = { labels, data }
+
+      const dataWithInvalidLabels = {
+        data,
+        urls,
+        labels: invalidLabels,
+      }
+
+      const labelsWithInvalidData = {
+        labels,
+        urls,
+        data: invalidData,
+      }
+
+      const labelsAndDataWithInvalidUrls = {
+        labels,
+        data,
+        urls: invalidUrls,
+      }
+
+      it('returns true if it is an empty object (needed for default value)', () => {
+        expect(PieChart.props.chartData.validator({})).toBe(true)
+      })
+
+      it('returns true if label, data, and url props are present and arrays', () => {
+        expect(PieChart.props.chartData.validator(chartData)).toBe(true)
+      })
+
+      it('returns true if label and data props are present and arrays', () => {
+        expect(PieChart.props.chartData.validator(labelsAndDataWithoutUrls))
+          .toBe(true)
+      })
+
+      it('returns false if data is array and labels is not present', () => {
+        expect(PieChart.props.chartData.validator(dataWithoutLabels))
+          .toBe(false)
+      })
+
+      it('returns false if labels is array and data is not present', () => {
+        expect(PieChart.props.chartData.validator(labelsWithoutData))
+          .toBe(false)
+      })
+
+      it('returns false if labels is array and data is not an array', () => {
+        expect(PieChart.props.chartData.validator(labelsWithInvalidData))
+          .toBe(false)
+      })
+
+      it('returns false if data is array and labels is not an array', () => {
+        expect(PieChart.props.chartData.validator(dataWithInvalidLabels))
+          .toBe(false)
+      })
+
+      it('returns false if optional urls property is not an array', () => {
+        expect(PieChart.props.chartData.validator(labelsAndDataWithInvalidUrls))
+          .toBe(false)
+      })
+
     })
 
     describe('chartClasses', () => {
@@ -84,7 +171,60 @@ describe('PieChart Vue component', () => {
     it('sets the correct initial state', () => {
       expect(PieChart.data()).toEqual({
         chart: null,
-        mutableChartData: {},
+        loading: true,
+        extendedChartData: {},
+      })
+    })
+
+  })
+
+  describe('mounted hook', () => {
+    beforeEach(() => {
+      axios.get.mockClear()
+      axios.mockResponseOnce('get', chartData)
+    })
+
+    it('loads the chart data via AJAX if chartData prop is empty and url is present', (done) => {
+      expect(axios.get).not.toHaveBeenCalled()
+
+      wrapper = shallow(PieChart, {
+        propsData: {
+          url: '/test/url',
+          colorRange: {
+            start: '#fafa6e',
+            end: '#2A4858',
+          },
+        },
+      })
+
+      wrapper.vm.$nextTick(() => {
+        expect(axios.get).toHaveBeenCalledWith('/test/url')
+        expect(wrapper.vm.extendedChartData).toEqual(
+          expect.objectContaining(extendedChartData)
+        )
+        done()
+      })
+    })
+
+    it('loads the chart data from chartData prop if url is not present', (done) => {
+      expect(axios.get).not.toHaveBeenCalled()
+
+      wrapper = shallow(PieChart, {
+        propsData: {
+          chartData,
+          colorRange: {
+            start: '#fafa6e',
+            end: '#2A4858',
+          },
+        },
+      })
+
+      wrapper.vm.$nextTick(() => {
+        expect(axios.get).not.toHaveBeenCalled()
+        expect(wrapper.vm.extendedChartData).toEqual(
+          expect.objectContaining(extendedChartData)
+        )
+        done()
       })
     })
 
@@ -99,50 +239,45 @@ describe('PieChart Vue component', () => {
 
         expect(chartDestroySpy).not.toHaveBeenCalled()
 
-        wrapper.vm.initializeChart()
+        wrapper.vm.initializeChart(chartData)
 
         expect(chartDestroySpy).toHaveBeenCalledTimes(1)
       })
 
-      it('sets the mutableChartData by extending the chartData prop', () => {
-        expect(wrapper.vm.mutableChartData).toEqual({
-          labels: [
-            'With parental permission – (75%)',
-            'Without parental permission – (25%)',
-          ],
-          datasets: [
-            expect.objectContaining({
-              backgroundColor: [
-                "rgba(250,250,110,0.7)",
-                "rgba(42,72,88,0.7)",
-              ],
-              data: [
-                3,
-                1,
-              ],
-              hoverBackgroundColor: [
-                "rgba(250,250,110,1)",
-                "rgba(42,72,88,1)",
-              ],
-              urls: [
-                '/with/parental/permission',
-                '/without/parental/permission',
-              ],
-            })
-          ],
-        })
+      it('sets the extendedChartData by extending the chartData prop', () => {
+        expect(wrapper.vm.extendedChartData).toEqual(
+          expect.objectContaining(extendedChartData)
+        )
       })
 
       it('creates a chart.js instance with the correct settings', () => {
         const chartElement = wrapper.find('canvas').element
         const chartContext = chartElement.getContext('2d')
 
+        const {
+          labels,
+          data,
+          backgroundColor,
+          hoverBackgroundColor,
+          urls
+        } = wrapper.vm.extendedChartData
+
         expect(wrapper.vm.chart.canvas).toEqual(chartElement)
         expect(wrapper.vm.chart.ctx).toEqual(chartContext)
         expect(wrapper.vm.chart.config).toEqual(
           expect.objectContaining({
             type: 'pie',
-            data: wrapper.vm.mutableChartData,
+            data: expect.objectContaining({
+              labels,
+              datasets: [
+                expect.objectContaining({
+                  data,
+                  backgroundColor,
+                  hoverBackgroundColor,
+                  urls,
+                })
+              ],
+            }),
             options: expect.objectContaining({
               legend: expect.objectContaining({
                 position: 'bottom',
@@ -177,6 +312,28 @@ describe('PieChart Vue component', () => {
             'rgba(42,72,88,1)',
           ],
         })
+      })
+
+    })
+
+    describe('isEmptyObject', () => {
+
+      it('returns true if object is empty', () => {
+        const object = {}
+
+        const result = wrapper.vm.isEmptyObject(object)
+
+        expect(result).toBe(true)
+      })
+
+      it('returns false if the object is not empty', () => {
+        const object = {
+          key: 'value'
+        }
+
+        const result = wrapper.vm.isEmptyObject(object)
+
+        expect(result).toBe(false)
       })
 
     })
