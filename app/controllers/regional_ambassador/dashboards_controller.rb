796e736406d7da2654aac285b0522fbeff17c2a9
Joe Sak <joe@joesak.com>
2018-06-28 15:07:17 -0500

    Wire pie chart JSON for RAs

--- app/controllers/regional_ambassador/dashboards_controller.rb
+++ app/controllers/regional_ambassador/dashboards_controller.rb
@@ -55,58 +55,10 @@ module RegionalAmbassador
         @mentors = Account.current.in_region(current_ambassador)
           .left_outer_joins(:mentor_profile)
           .where("mentor_profiles.id IS NOT NULL")
-
-        if current_ambassador.country == "US"
-          init_us_mentor_data
-        else
-          init_intl_mentor_data
-        end
-
-        @permitted_students = @students.joins(:signed_parental_consent)
-        @unpermitted_students = @students.joins(:pending_parental_consent)
-
-        @new_students = @students.where(
-          "accounts.created_at > ?",
-          Date.new(Season.current.year - 1, 10, 18)
-        )
-
-        @returning_students = @students.where(
-          "accounts.created_at < ?",
-          Date.new(Season.current.year - 1, 10, 18)
-        )
-
-        @new_mentors = @mentors.where(
-          "accounts.created_at > ?",
-          Date.new(Season.current.year - 1, 10, 18)
-        )
-
-        @returning_mentors = @mentors.where(
-          "accounts.created_at < ?",
-          Date.new(Season.current.year - 1, 10, 18)
-        )
       end
 
       render "regional_ambassador/dashboards" +
         "/show_#{current_ambassador.status}"
-    end
-
-    private
-    def init_intl_mentor_data
-      @consenting_mentors = @mentors.eager_load(:consent_waiver)
-        .where("consent_waivers.id IS NOT NULL")
-
-      @unconsenting_mentors = @mentors.eager_load(:consent_waiver)
-        .where("consent_waivers.id IS NULL")
-    end
-
-    def init_us_mentor_data
-      @cleared_mentors = @mentors.bg_check_clear.consent_signed
-
-      @signed_consent_mentors = @mentors.bg_check_submitted.consent_signed
-
-      @cleared_bg_check_mentors = @mentors.bg_check_clear.consent_not_signed
-
-      @neither_mentors = @mentors.bg_check_unsubmitted.consent_not_signed
     end
   end
 end
