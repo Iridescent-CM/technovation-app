555c4e0979433d29cc11e733e22ebf480099cb5c
Alan Richey <alan@icant.dance>
2018-06-29 15:33:20 -0500

    Tweak layout in students and mentors chart sections

--- app/javascript/admin/dashboard/components/MentorsSection.vue
+++ app/javascript/admin/dashboard/components/MentorsSection.vue
@@ -1,26 +1,30 @@
 <template>
-  <div class="tab-content" id="mentors">
+  <div id="mentors">
     <h3>Mentors <span>([TODO POPULATE THIS WITH RESULTS])</span></h3>
 
-    <h6>Background check / consent waivers</h6>
+    <div class="tab-content">
+      <h6>Background check / consent waivers</h6>
 
-    <pie-chart
-      :url="$store.getters.getChartEndpoint('onboarding_mentors')"
-      :chart-data="$store.getters.getCachedChartData('onboarding_mentors')"
-      @pieChartInitialized="addChartDataToCache"
-    />
+      <pie-chart
+        :url="$store.getters.getChartEndpoint('onboarding_mentors')"
+        :chart-data="$store.getters.getCachedChartData('onboarding_mentors')"
+        @pieChartInitialized="addChartDataToCache"
+      />
+    </div>
 
-    <h6>New vs. Returning</h6>
+    <div class="tab-content">
+      <h6>New vs. Returning</h6>
 
-    <pie-chart
-      :url="$store.getters.getChartEndpoint('returning_mentors')"
-      :chart-data="$store.getters.getCachedChartData('returning_mentors')"
-      @pieChartInitialized="addChartDataToCache"
-      :color-range="{
-        start: 'rgba(54, 162, 235, 1)',
-        end: 'rgba(255, 206, 86, 1)',
-      }"
-    />
+      <pie-chart
+        :url="$store.getters.getChartEndpoint('returning_mentors')"
+        :chart-data="$store.getters.getCachedChartData('returning_mentors')"
+        @pieChartInitialized="addChartDataToCache"
+        :color-range="{
+          start: 'rgba(54, 162, 235, 1)',
+          end: 'rgba(255, 206, 86, 1)',
+        }"
+      />
+    </div>
   </div>
 </template>
 
--- app/javascript/admin/dashboard/components/StudentsSection.vue
+++ app/javascript/admin/dashboard/components/StudentsSection.vue
@@ -1,26 +1,30 @@
 <template>
-  <div class="tab-content" id="students">
+  <div id="students">
     <h3>Students <span>([TODO POPULATE THIS WITH RESULTS])</span></h3>
 
-    <h6>Parental permission</h6>
+    <div class="tab-content">
+      <h6>Parental permission</h6>
 
-    <pie-chart
-      :url="$store.getters.getChartEndpoint('permitted_students')"
-      :chart-data="$store.getters.getCachedChartData('permitted_students')"
-      @pieChartInitialized="addChartDataToCache"
-    />
+      <pie-chart
+        :url="$store.getters.getChartEndpoint('permitted_students')"
+        :chart-data="$store.getters.getCachedChartData('permitted_students')"
+        @pieChartInitialized="addChartDataToCache"
+      />
+    </div>
 
-    <h6>New vs. Returning</h6>
+    <div class="tab-content">
+      <h6>New vs. Returning</h6>
 
-    <pie-chart
-      :url="$store.getters.getChartEndpoint('returning_students')"
-      :chart-data="$store.getters.getCachedChartData('returning_students')"
-      @pieChartInitialized="addChartDataToCache"
-      :color-range="{
-        start: 'rgba(54, 162, 235, 1)',
-        end: 'rgba(255, 206, 86, 1)',
-      }"
-    />
+      <pie-chart
+        :url="$store.getters.getChartEndpoint('returning_students')"
+        :chart-data="$store.getters.getCachedChartData('returning_students')"
+        @pieChartInitialized="addChartDataToCache"
+        :color-range="{
+          start: 'rgba(54, 162, 235, 1)',
+          end: 'rgba(255, 206, 86, 1)',
+        }"
+      />
+    </div>
   </div>
 </template>
 
