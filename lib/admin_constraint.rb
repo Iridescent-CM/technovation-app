cf02832e49fa69d6b3c7fea6b659252b9e2955c6
Joe Sak <joe@joesak.com>
2018-06-29 12:35:19 -0500

    Fix sidekiq constraint to match CookieNames implementation

--- lib/admin_constraint.rb
+++ lib/admin_constraint.rb
@@ -1,6 +1,6 @@
 class AdminConstraint
   def matches?(request)
-    request.cookie_jar.signed['auth_token'] and
-      Account.joins(:admin_profile).exists?(auth_token: request.cookie_jar.signed['auth_token'])
+    request.cookie_jar.signed[CookieNames::AUTH_TOKEN] and
+      Account.joins(:admin_profile).exists?(auth_token: request.cookie_jar.signed[CookieNames::AUTH_TOKEN])
   end
 end
