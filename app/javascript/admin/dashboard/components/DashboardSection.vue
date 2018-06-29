d646f8beb31604ecb362fbd83e23c32d50651684
Alan Richey <alan@icant.dance>
2018-06-29 14:38:26 -0500

    Create dashboard section vue component which will be extended to share functionality

---
+++ app/javascript/admin/dashboard/components/DashboardSection.vue
@@ -0,0 +1,24 @@
+<template>
+  <div />
+</template>
+
+<script>
+import PieChart from '../../../components/PieChart'
+
+export default {
+  name: 'dashboard-section',
+
+  components: {
+    PieChart,
+  },
+
+  methods: {
+    addChartDataToCache (payload) {
+      this.$store.commit('addChartDataToCache', payload)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
