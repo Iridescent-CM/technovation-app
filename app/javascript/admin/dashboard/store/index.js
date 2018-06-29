5799d3adf3ab38793172b78dbcbd29e59fbfe7cd
Alan Richey <alan@icant.dance>
2018-06-29 14:37:43 -0500

    Add getters and mutators to admin dashboard store

--- app/javascript/admin/dashboard/store/index.js
+++ app/javascript/admin/dashboard/store/index.js
@@ -6,12 +6,35 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     chartEndpoints: {},
+    cachedStates: {},
+  },
+
+  getters: {
+    getChartEndpoint: (state) => (name) => {
+      if (state.chartEndpoints[name])
+        return state.chartEndpoints[name]
+
+      return ''
+    },
+
+    getCachedChartData: (state) => (name) => {
+      const url = state.chartEndpoints[name]
+
+      if (state.cachedStates[url])
+        return state.cachedStates[url]
+
+      return {}
+    }
   },
 
   mutations: {
     addChartEndpoints (state, payload) {
       const mergedEndpoints = Object.assign({}, state.chartEndpoints, payload)
       state.chartEndpoints = payload
+    },
+
+    addChartDataToCache (state, payload) {
+      state.cachedStates[payload.url] = payload.chartData
     },
   },
 })
