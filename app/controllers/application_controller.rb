629e9eef6b45539478b20d209e900b995d2c1b22
Joe Sak <joe@joesak.com>
2018-06-29 11:05:22 -0500

    Delete old cookie session before all controller actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,6 +24,10 @@ class ApplicationController < ActionController::Base
     redirect_to timeout_error_path(back: request.fullpath)
   end
 
+  before_action -> {
+    cookies.delete("_technovation_app_session")
+  }
+
   def set_cookie(key, value, passed_options  = {})
     default_options = {
       permanent: false,
