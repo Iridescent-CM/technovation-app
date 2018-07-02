54a92b089d9d5c1df76d19f00f198639a8ef1a84
Joe Sak <joe@joesak.com>
2018-07-02 14:06:14 -0500

    Apply geocoding from the cookie LAT,LNG automatically

--- spec/controllers/student/dashboards_controller_spec.rb
+++ spec/controllers/student/dashboards_controller_spec.rb
@@ -2,6 +2,19 @@ require "rails_helper"
 
 RSpec.describe Student::DashboardsController do
   describe "GET #show" do
+    context "when visitors have lat, lng stored in the DB" do
+      it "adds the visitor's lat, lng" do
+        student = FactoryBot.create(:student, :onboarded)
+        sign_in(student)
+
+        expect {
+          get :show
+        }.to change {
+          controller.get_cookie(CookieNames::IP_GEOLOCATION)
+        }.to ([student.latitude, student.longitude])
+      end
+    end
+
     it "does not load events when RPE selection is off" do
       student = FactoryBot.create(:student, :submitted, :junior)
       event = FactoryBot.create(:event, :junior)
