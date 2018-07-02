25d5feb3d864ba88a60977c1b862340bc5659048
Joe Sak <joe@joesak.com>
2018-07-02 09:52:23 -0500

    Store the IP address to the IP_GEOLOCATION cookie

--- spec/controllers/application/dashboards_controller_spec.rb
+++ spec/controllers/application/dashboards_controller_spec.rb
@@ -3,13 +3,23 @@ require "rails_helper"
 RSpec.describe Application::DashboardsController do
   context "when visitors have a location cookie" do
     it "does not add a cookie" do
+      controller.set_cookie(CookieNames::IP_GEOLOCATION, 'something')
+
       expect {
         get :show
       }.not_to change {
-        @response.cookies[CookieNames::IP_GEOLOCATION]
+        response.cookies[CookieNames::IP_GEOLOCATION]
       }
     end
   end
 
-  context "when visitors do not have a location cookie"
+  context "when visitors do not have a location cookie" do
+    it "adds a cookie to the IP_GELOCATION key" do
+      expect {
+        get :show
+      }.to change {
+        @response.cookies[CookieNames::IP_GEOLOCATION]
+      }
+    end
+  end
 end
