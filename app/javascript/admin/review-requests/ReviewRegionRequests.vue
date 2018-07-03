cd8e8eb4153196b682b9e2786efb002dc1e1e38e
Joe Sak <joe@joesak.com>
2018-07-03 11:30:10 -0500

    Load requests from source URL

--- app/javascript/admin/review-requests/ReviewRegionRequests.vue
+++ app/javascript/admin/review-requests/ReviewRegionRequests.vue
@@ -2,19 +2,43 @@
   <div v-if="isLoading" class="loading">
     Loading...
   </div>
+
+  <div v-else-if="hasError" class="error">
+    Error.
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data () {
     return {
       isLoading: true,
+      hasError: false,
+      requests: [],
     }
+  },
+
+  props: ['sourceUrl'],
+
+  created () {
+    this.loadData()
   },
 
   methods: {
     loadData () {
-      this.isLoading = false
+      axios.get(this.sourceUrl).then(({ data }) => {
+        data.data.forEach((request) => {
+          this.requests.push(request)
+        })
+
+        this.isLoading = false
+      }).catch((err) => {
+        console.error(err)
+        this.isLoading = false
+        this.hasError = true
+      })
     },
   },
 }
--- spec/javascript/admin/ReviewRegionRequests.spec.js
+++ spec/javascript/admin/ReviewRegionRequests.spec.js
@@ -1,4 +1,5 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
+import axios from 'axios'
 
 import Vuex from 'vuex'
 import VTooltip from 'v-tooltip'
@@ -12,23 +13,84 @@ localVue.use(Vuex)
 localVue.use(VTooltip)
 localVue.use(Vue2Filters)
 
-let wrapper
-
 describe('List pending requests', () => {
   beforeEach(() => {
-    wrapper = shallowMount(
-      ReviewRegionRequests, {
-        localVue,
-      }
-    )
+    axios.get.mockClear()
   })
 
-  test('starts out loading', () => {
+  it('starts out loading', () => {
+    const wrapper = shallowMount(
+      ReviewRegionRequests, {
+        localVue,
+        propsData: {
+          sourceUrl: '/some/url',
+        },
+      }
+    )
+
     expect(wrapper.find('.loading').text()).toEqual('Loading...')
   })
 
-  test('can stop loading', () => {
-    wrapper.vm.loadData()
-    expect(wrapper.contains('.loading')).toBe(false)
+  describe('#loadData()', () => {
+    it('assigns requests from remote data', (done) => {
+      axios.mockResponseOnce("get", { data: [
+        {
+          id: "1",
+          type: "request",
+          attributes: {
+            requestor_avatar: "https://placekitten.com/g/233/233",
+            requestor_name: "RA Ambassador",
+            requestor_meta: {
+              regions: [
+                "Ontario, CA",
+                "California, US",
+                "MX"
+              ]
+            },
+            requestor_message: "I want to add these regions because...",
+            request_type: "AMBASSADOR_ADD_REGION",
+            request_status: "pending",
+            urls: {
+              patch: "http://localhost:3000/admin/requests/1"
+            }
+          }
+        },
+        {
+          id: "2",
+          type: "request",
+          attributes: {
+            requestor_avatar: "https://placekitten.com/g/250/250",
+            requestor_name: "Other Ambassador",
+            requestor_meta: {
+              regions: [
+                "Maine, US",
+                "New York, US"
+              ]
+            },
+            requestor_message: "I want to add these OTHER regions because...",
+            request_type: "AMBASSADOR_ADD_REGION",
+            request_status: "pending",
+            urls: {
+              patch: "http://localhost:3000/admin/requests/1"
+            }
+          }
+        }
+      ]})
+
+      const wrapper = shallowMount(
+        ReviewRegionRequests, {
+          localVue,
+          propsData: {
+            sourceUrl: '/some/url',
+          },
+        }
+      )
+
+      setImmediate(() => {
+        expect(wrapper.vm.requests.length).toBe(2)
+        expect(wrapper.vm.isLoading).toBe(false)
+        done()
+      })
+    })
   })
 })
