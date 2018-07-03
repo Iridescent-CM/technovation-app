54d7c0d12e5edf4f8df2c213827ea26b5374a858
Alan Richey <adrichey@users.noreply.github.com>
2018-07-03 11:47:30 -0500

    Pie Charts Vue Refactor: Part 3 (#1575)
    * Add vue router enabled application for use in admin dashboard. Still a WIP.
    
    * Restructure directories and components for the Admin Dashboard
    
    * Add a means to pass parameters from root Vue instance
    
    * Implement PieChart components inside of AdminDashboard and add store for URL storage
    
    * Update pie chart initialization event to prep for caching
    
    * Add getters and mutators to admin dashboard store
    
    * Create dashboard section vue component which will be extended to share functionality
    
    * Convert admin dashboard sections over to use extended component structure
    
    * Add store state management to admin dashboard sections
    
    * Tweak grid layout sizes in admin dashboard
    
    * Tweak layout in students and mentors chart sections
    
    * Add loading icon to the pie chart loading state
    
    * Add logic to display totals in admin dashboard
    
    * Add first test around components in admin dashboard
    
    * Add tests for DashboardSection component
    
    * Separate state functions out into testable computed properties in MentorsSection component
    
    * Fix to store state totals logic for admin dashboard
    
    * Add tests for MentorsSection Vue component
    
    * Add more markup tests for MentorsSection component
    
    * Tweak StudentsSection to increae testability and write tests for the component
    
    * Add tests for the AdminDashboard component
    
    * Add tests for admin dashboard routes
    
    * Add tests for the admin dashboard Vuex store
    
    * Add error message to chart when no data is available via reusable plugin

---
+++ app/javascript/admin/dashboard/components/AdminDashboard.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="tabs tabs-vertical grid" id="admin-dashboard">
+    <ul class="tab-menu grid__col-md-3">
+      <router-link
+        tag="li"
+        class="tab-link"
+        active-class="tabs__menu-link--active"
+        :to="{ name: 'students' }"
+      >
+        <button role="button" class="tab-button">
+          Students
+        </button>
+      </router-link>
+
+      <router-link
+        tag="li"
+        class="tab-link"
+        active-class="tabs__menu-link--active"
+        :to="{ name: 'mentors' }"
+      >
+        <button role="button" class="tab-button">
+          Mentors
+        </button>
+      </router-link>
+    </ul>
+
+    <router-view class="content grid__col-md-9"></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'admin-dashboard',
+
+  props: {
+    chartEndpoints: {
+      type: Object,
+      default () {
+        return {}
+      },
+    },
+
+    totals: {
+      type: Object,
+      default () {
+        return {}
+      },
+    },
+  },
+
+  created () {
+    this.$store.commit('addChartEndpoints', this.chartEndpoints)
+    this.$store.commit('addTotals', this.totals)
+  },
+}
+</script>
+
+<style scoped>
+</style>
---
+++ app/javascript/admin/dashboard/components/StudentsSection.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="students">
+    <h3>Students<span v-if="showTotal"> ({{ getTotal('students') }})</span></h3>
+
+    <div class="tab-content">
+      <h6>Parental permission</h6>
+
+      <pie-chart
+        :url="permittedStudentsEndpoint"
+        :chart-data="permittedStudentsChartData"
+        @pieChartInitialized="addChartDataToCache"
+      />
+    </div>
+
+    <div class="tab-content">
+      <h6>New vs. Returning</h6>
+
+      <pie-chart
+        :url="returningStudentsEndpoint"
+        :chart-data="returningStudentsChartData"
+        @pieChartInitialized="addChartDataToCache"
+        :color-range="{
+          start: 'rgba(54, 162, 235, 1)',
+          end: 'rgba(255, 206, 86, 1)',
+        }"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import DashboardSection from './DashboardSection'
+
+export default {
+  name: 'students-section',
+
+  extends: DashboardSection,
+
+  computed: {
+    permittedStudentsEndpoint () {
+      return this.$store.getters.getChartEndpoint('permitted_students')
+    },
+
+    permittedStudentsChartData () {
+      return this.$store.getters.getCachedChartData('permitted_students')
+    },
+
+    returningStudentsEndpoint () {
+      return this.$store.getters.getChartEndpoint('returning_students')
+    },
+
+    returningStudentsChartData () {
+      return this.$store.getters.getCachedChartData('returning_students')
+    },
+
+    showTotal () {
+      return this.getTotal('students') !== null
+    },
+  },
+}
+</script>
+
+<style scoped>
+</style>
