f635772c1d2e79ee61afe123cc920dd1a2a4f04b
Alan Richey <alan@icant.dance>
2018-07-06 16:31:19 -0500

    Implement first iteration of the bar chart component with tests

---
+++ app/javascript/components/BarChart.vue
@@ -0,0 +1,164 @@
+<template>
+  <div class="bar-chart">
+    <div v-if="loading">
+      <icon
+        class="spin"
+        name="spinner"
+        size="16"
+      />
+
+      <span>Loading chart...</span>
+    </div>
+
+    <canvas v-show="!loading" :class="chartClasses"></canvas>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+import Chart from 'chart.js'
+import chroma from 'chroma-js'
+
+import Icon from './Icon.vue'
+import '../utilities/chartjs-plugins'
+import { isEmptyObject } from '../utilities/utilities'
+
+export default {
+  name: 'bar-chart',
+
+  components: {
+    Icon,
+  },
+
+  data () {
+    return {
+      chart: null,
+      loading: true,
+      extendedChartData: {},
+    }
+  },
+
+  props: {
+    chartData: {
+      type: Object,
+      default () {
+        return {}
+      },
+    },
+
+    chartClasses: {
+      type: Object,
+      default () {
+        return {
+          'quickview-charts': true,
+        }
+      },
+    },
+
+    colorRange: {
+      type: Object,
+      default () {
+        return {
+          start: 'rgb(54, 162, 235)',
+          end: 'rgb(255, 99, 132)',
+        }
+      },
+    },
+
+    url: {
+      type: String,
+      default: '',
+    },
+  },
+
+  mounted () {
+    if (this.url !== '' && this.isEmptyObject(this.chartData)) {
+      axios.get(this.url)
+        .then((response) => {
+          this.initializeChart(response.data.data.attributes)
+        })
+    } else {
+      this.initializeChart(this.chartData)
+    }
+  },
+
+  methods: {
+    isEmptyObject,
+
+    initializeChart (chartData) {
+      if (this.chart !== null) {
+        this.chart.destroy()
+      }
+
+      const chartElement = this.$el.querySelector('canvas')
+      const chartContext = chartElement.getContext('2d')
+
+      const numberOfDataItems = chartData.datasets.length
+      const backgroundColors = this.generateBackgroundColors(numberOfDataItems)
+
+      const extendedChartData = Object.assign({}, chartData)
+      extendedChartData.datasets.forEach((dataset, index) => {
+        dataset.backgroundColor = backgroundColors.backgroundColor[index]
+        dataset.hoverBackgroundColor = backgroundColors.hoverBackgroundColor[index]
+      })
+
+      this.chart = new Chart(chartContext, {
+        type: 'bar',
+        data: extendedChartData,
+				options: {
+          legend: {
+            position: 'bottom',
+          },
+					scales: {
+						xAxes: [{
+							stacked: true,
+						}],
+						yAxes: [{
+							stacked: true
+						}]
+					},
+				},
+      })
+
+      this.$set(this, 'extendedChartData', extendedChartData)
+
+      // Emit event for caching AJAX response on the parent component
+      this.$emit('barChartInitialized', {
+        url: this.url,
+        chartData: this.extendedChartData
+      })
+
+      this.loading = false
+    },
+
+    generateBackgroundColors (numberOfColors) {
+      const colorScale = chroma
+        .scale([
+          this.colorRange.start,
+          this.colorRange.end,
+        ])
+        .mode('hsv')
+        .colors(numberOfColors)
+
+      const backgroundColors = {
+        backgroundColor: [],
+        hoverBackgroundColor: [],
+      }
+
+      colorScale.forEach((color) => {
+        const hoverColorString = `rgba(${chroma(color).alpha(1).rgba().join(',')})`
+        backgroundColors.hoverBackgroundColor.push(hoverColorString)
+
+        const colorString = `rgba(${chroma(color).alpha(0.7).rgba().join(',')})`
+        backgroundColors.backgroundColor.push(colorString)
+      })
+
+      return backgroundColors
+    },
+  },
+}
+</script>
+
+<style scoped>
+</style>
---
+++ spec/javascript/components/BarChart.spec.js
@@ -0,0 +1,271 @@
+import { shallowMount } from '@vue/test-utils'
+import axios from 'axios'
+
+import BarChart from 'components/BarChart'
+
+require('canvas')
+
+describe('BarChart Vue component', () => {
+  const htmlChartData = {
+    labels: [ 'US', 'Spain', 'Mexico', 'Ethiopia', 'Germany', 'India' ],
+    datasets: [
+      {
+        label: 'Students',
+        data: [ 400, 500, 600, 900, 300, 100 ],
+      },
+      {
+        label: 'Mentors',
+        data: [ 100, 200, 300, 500, 100, 64 ],
+      },
+      {
+        label: 'Judges',
+        data: [ 20, 40, 60, 80, 10, 3 ],
+      }
+    ]
+  }
+
+  const jsonChartData = {
+    data: {
+      attributes: Object.assign({}, htmlChartData),
+    },
+  }
+
+  const extendedChartData = Object.assign({}, htmlChartData)
+
+  extendedChartData.datasets[0].backgroundColor = "rgba(250,250,110,0.7)"
+  extendedChartData.datasets[1].backgroundColor = "rgba(0,0,255,0.7)"
+  extendedChartData.datasets[2].backgroundColor = "rgba(42,72,88,0.7)"
+
+  extendedChartData.datasets[0].hoverBackgroundColor = "rgba(250,250,110,1)"
+  extendedChartData.datasets[1].hoverBackgroundColor = "rgba(0,0,255,1)"
+  extendedChartData.datasets[2].hoverBackgroundColor = "rgba(42,72,88,1)"
+
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallowMount(BarChart, {
+      propsData: {
+        chartData: Object.assign({}, htmlChartData),
+        colorRange: {
+          start: '#fafa6e',
+          end: '#2A4858',
+        },
+      },
+    })
+  })
+
+  describe('props', () => {
+
+    it('contains valid props', () => {
+      expect(BarChart.props).toEqual({
+        chartData: {
+          type: Object,
+          default: expect.any(Function),
+        },
+
+        chartClasses: {
+          type: Object,
+          default: expect.any(Function),
+        },
+
+        colorRange: {
+          type: Object,
+          default: expect.any(Function),
+        },
+
+        url: {
+          type: String,
+          default: '',
+        },
+      })
+    })
+
+    describe('chartClasses', () => {
+      it('sets the quickview-charts class by default', () => {
+        expect(BarChart.props.chartClasses.default())
+          .toEqual({
+            'quickview-charts': true,
+          })
+      })
+    })
+
+    describe('colorRange', () => {
+      it('sets a light blue to light pink range by default', () => {
+        expect(BarChart.props.colorRange.default())
+          .toEqual({
+            start: 'rgb(54, 162, 235)',
+            end: 'rgb(255, 99, 132)',
+          })
+      })
+    })
+
+  })
+
+  describe('data', () => {
+
+    it('sets the correct initial state', () => {
+      expect(BarChart.data()).toEqual({
+        chart: null,
+        loading: true,
+        extendedChartData: {},
+      })
+    })
+
+  })
+
+  describe('mounted hook', () => {
+    beforeEach(() => {
+      axios.get.mockClear()
+      axios.mockResponseOnce('get', jsonChartData)
+    })
+
+    it('loads the chart data via AJAX if chartData prop is empty and url is present', (done) => {
+      expect(axios.get).not.toHaveBeenCalled()
+
+      wrapper = shallowMount(BarChart, {
+        propsData: {
+          url: '/test/url',
+          colorRange: {
+            start: '#fafa6e',
+            end: '#2A4858',
+          },
+        },
+      })
+
+      wrapper.vm.$nextTick(() => {
+        expect(axios.get).toHaveBeenCalledWith('/test/url')
+        expect(wrapper.vm.extendedChartData).toEqual(
+          expect.objectContaining(extendedChartData)
+        )
+        done()
+      })
+    })
+
+    it('loads the chart data from chartData prop if url is not present', (done) => {
+      expect(axios.get).not.toHaveBeenCalled()
+
+      wrapper = shallowMount(BarChart, {
+        propsData: {
+          chartData: Object.assign({}, htmlChartData),
+          colorRange: {
+            start: '#fafa6e',
+            end: '#2A4858',
+          },
+        },
+      })
+
+      wrapper.vm.$nextTick(() => {
+        expect(axios.get).not.toHaveBeenCalled()
+        expect(wrapper.vm.extendedChartData).toEqual(
+          expect.objectContaining(extendedChartData)
+        )
+        done()
+      })
+    })
+
+  })
+
+  describe('methods', () => {
+
+    describe('initializeChart', () => {
+
+      it('destroys the chart if already created', () => {
+        const chartDestroySpy = spyOn(wrapper.vm.chart, 'destroy')
+
+        expect(chartDestroySpy).not.toHaveBeenCalled()
+
+        wrapper.vm.initializeChart(htmlChartData)
+
+        expect(chartDestroySpy).toHaveBeenCalledTimes(1)
+      })
+
+      it('sets the extendedChartData by extending the chartData prop', () => {
+        expect(wrapper.vm.extendedChartData).toEqual(
+          expect.objectContaining(extendedChartData)
+        )
+      })
+
+      it('creates a chart.js instance with the correct settings', () => {
+        const chartElement = wrapper.find('canvas').element
+        const chartContext = chartElement.getContext('2d')
+
+        expect(wrapper.vm.chart.canvas).toEqual(chartElement)
+        expect(wrapper.vm.chart.ctx).toEqual(chartContext)
+        expect(wrapper.vm.chart.config).toEqual(
+          expect.objectContaining({
+            type: 'bar',
+            data: expect.objectContaining(wrapper.vm.extendedChartData),
+            options: expect.objectContaining({
+              legend: expect.objectContaining({
+                position: 'bottom',
+              }),
+              scales: expect.objectContaining({
+                xAxes: [
+                  expect.objectContaining({
+                    stacked: true,
+                  }),
+                ],
+                yAxes: [
+                  expect.objectContaining({
+                    stacked: true,
+                  }),
+                ]
+              }),
+            })
+          })
+        )
+      })
+    })
+
+    describe('generateBackgroundColors', () => {
+
+      it('returns RGBA background and hover color scales based on number of colors passed in', () => {
+        const backgroundColors = wrapper.vm.generateBackgroundColors(6)
+
+        expect(backgroundColors).toEqual({
+          backgroundColor: [
+            'rgba(250,250,110,0.7)',
+            'rgba(161,218,97,0.7)',
+            'rgba(90,185,84,0.7)',
+            'rgba(71,153,104,0.7)',
+            'rgba(57,120,113,0.7)',
+            'rgba(42,72,88,0.7)',
+          ],
+          hoverBackgroundColor: [
+            'rgba(250,250,110,1)',
+            'rgba(161,218,97,1)',
+            'rgba(90,185,84,1)',
+            'rgba(71,153,104,1)',
+            'rgba(57,120,113,1)',
+            'rgba(42,72,88,1)',
+          ],
+        })
+      })
+
+    })
+
+    describe('isEmptyObject', () => {
+
+      it('returns true if object is empty', () => {
+        const object = {}
+
+        const result = wrapper.vm.isEmptyObject(object)
+
+        expect(result).toBe(true)
+      })
+
+      it('returns false if the object is not empty', () => {
+        const object = {
+          key: 'value'
+        }
+
+        const result = wrapper.vm.isEmptyObject(object)
+
+        expect(result).toBe(false)
+      })
+
+    })
+
+  })
+
+})
