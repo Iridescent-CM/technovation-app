3ee78eb4a40178a8779d5bd9c0c4b32e70811ed8
Joe Sak <joe@joesak.com>
2018-07-12 12:05:20 -0500

    Populate lcoation from state from xhr response

--- app/javascript/location/components/LocationForm.vue
+++ app/javascript/location/components/LocationForm.vue
@@ -10,8 +10,26 @@
 export default {
   data () {
     return {
-
+      city: "",
+      state_code: "",
+      country_code: "",
     }
+  },
+
+  props: ['scopeName'],
+
+  computed: {
+    getCurrentLocationEndpoint () {
+      return `/${this.scopeName}/current_location`
+    },
+  },
+
+  created() {
+    window.axios.get(this.getCurrentLocationEndpoint).then(({ data }) => {
+      this.city = data.city
+      this.state_code = data.state_code
+      this.country_code = data.country_code
+    })
   },
 }
 </script>
--- spec/javascript/location/LocationForm.spec.js
+++ spec/javascript/location/LocationForm.spec.js
@@ -1,4 +1,5 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
+import axios from 'axios'
 
 import LocationForm from 'location/components/LocationForm'
 
@@ -8,12 +9,42 @@ describe('location/components/LocationForm', () => {
   let wrapper
 
   beforeEach(() => {
-    wrapper = shallowMount(LocationForm, { localVue })
+    axios.get.mockClear()
+
+    wrapper = shallowMount(LocationForm, {
+      localVue,
+      propsData: {
+        scopeName: "student",
+      },
+    })
   })
 
   it('mounts with city, state, country input fields', () => {
     expect(wrapper.contains('input[type=text]#location_city')).toBe(true)
     expect(wrapper.contains('input[type=text]#location_state')).toBe(true)
     expect(wrapper.contains('select#location_country')).toBe(true)
+  })
+
+  it('fills in the user location data from the server', (done) => {
+    axios.mockResponseOnce('get', {
+      city: "Chicago",
+      state_code: "IL",
+      country_code: "US",
+    })
+
+    const myWrapper = shallowMount(LocationForm, {
+      localVue,
+      propsData: {
+        scopeName: "student",
+      },
+    })
+
+    setImmediate(() => {
+      expect(axios.get).toHaveBeenCalledWith('/student/current_location')
+      expect(myWrapper.vm.city).toEqual("Chicago")
+      expect(myWrapper.vm.state_code).toEqual("IL")
+      expect(myWrapper.vm.country_code).toEqual("US")
+      done()
+    })
   })
 })
