58eb565929d0c555c4c20254c0bee5252ec40247
Joe Sak <joe@joesak.com>
2018-07-12 13:53:22 -0500

    Prefer axios interceptor for updating CSRF token per request on relative paths

--- app/javascript/config/axios.js
+++ app/javascript/config/axios.js
@@ -9,9 +9,17 @@ document.addEventListener('turbolinks:load', () => {
   const csrfTokenMetaTag = document.querySelector('meta[name="csrf-token"]')
 
   if (csrfTokenMetaTag) {
-    window.axios.defaults.headers.common = {
-      'X-Requested-With': 'XMLHttpRequest',
-      'X-CSRF-TOKEN' : csrfTokenMetaTag.getAttribute('content')
-    }
+    window.axios.interceptors.request.use(function (config) {
+      if (config.url.match(/^\//)) {
+        config.headers.common = {
+          'X-Requested-With': 'XMLHttpRequest',
+          'X-CSRF-TOKEN' : csrfTokenMetaTag.getAttribute('content')
+        }
+      }
+
+      return config
+    }, function (error) {
+      return Promise.reject(error);
+    })
   }
 })
