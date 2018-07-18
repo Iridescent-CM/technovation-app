ff47572e4b014ab90a3a8bf9160c6811883b46ae
Joe Sak <joe@joesak.com>
2018-07-18 11:50:28 -0500

    Improve FriendlyCountry/Subregion procedures

--- app/models/friendly_country.rb
+++ app/models/friendly_country.rb
@@ -2,18 +2,25 @@ module FriendlyCountry
   def self.call(record, options = {})
     return "" unless record.respond_to?(:country)
     return "" if record.country.nil? or record.country.strip == ""
-    return record.country if Carmen::Country.coded(record.country).blank?
+
+    country = Carmen::Country.coded(record.country) ||
+                Carmen::Country.named(record.country)
+
+    return record.country if country.blank?
 
     default_options = {
       prefix: true,
+      short_code: false,
     }
 
     merged_options = default_options.merge(options)
 
-    if merged_options[:prefix]
-      "#{record.country} - #{Carmen::Country.coded(record.country).name}"
+    if merged_options[:short_code]
+      country.code
+    elsif merged_options[:prefix]
+      "#{country.code} - #{country.name}"
     else
-      Carmen::Country.coded(record.country).name
+      country.name
     end
   end
 end
