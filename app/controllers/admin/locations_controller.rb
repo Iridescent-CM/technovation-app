3ffcaa689078221a8350cf6b1d1e496418d02daa
Joe Sak <joe@joesak.com>
2018-07-18 14:34:33 -0500

    Use variable

--- app/controllers/admin/locations_controller.rb
+++ app/controllers/admin/locations_controller.rb
@@ -2,7 +2,7 @@ module Admin
   class LocationsController < AdminController
     def update
       if account_id = params.fetch(:account_id) { false }
-        record = Account.find(params.fetch(:account_id))
+        record = Account.find(account_id)
       else
         record = Team.find(params.fetch(:team_id))
       end
