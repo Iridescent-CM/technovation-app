5aee7807221288471f60dc50bbf1b91fc463a241
Alan Richey <alan@icant.dance>
2018-07-19 13:07:11 -0500

    Implement debounce utility and test it using QuestionSection Vue component

--- app/javascript/utilities/utilities.js
+++ app/javascript/utilities/utilities.js
@@ -1,3 +1,15 @@
 export const isEmptyObject = (object) => {
   return Object.keys(object).length === 0 && object.constructor === Object
 }
+
+export const debounce = function (func, wait = 500) {
+  let timeout
+
+  return function(...args) {
+    clearTimeout(timeout)
+
+    timeout = setTimeout(() => {
+      func.apply(this, args)
+    }, wait)
+  }
+}
