280a126acc37068da54170ed83e808c648f5a1a7
Joe Sak <joe@joesak.com>
2018-07-23 17:57:45 -0500

    Convert judge special signup feature spec to system spec

--- spec/system/judge_special_signup_spec.rb
+++ spec/system/judge_special_signup_spec.rb
@@ -1,12 +1,12 @@
 require "rails_helper"
 
-RSpec.feature "Judges sign up at special link", :js do
-  scenario "visit the normal url" do
+RSpec.describe "Judges sign up at special link", :js do
+  it "visit the normal url" do
     visit judge_signup_path
     expect(current_path).to eq(root_path)
   end
 
-  scenario "visit with the special token" do
+  it "visit with the special token" do
     invitation = GlobalInvitation.create!
 
     visit judge_signup_path(invitation: invitation.token)
@@ -17,7 +17,7 @@ RSpec.feature "Judges sign up at special link", :js do
     expect(page).to have_css("#new_judge_profile input[type=password]")
   end
 
-  scenario "sign up" do
+  it "sign up" do
     invitation = GlobalInvitation.create!
 
     visit judge_signup_path(invitation: invitation.token)
@@ -29,7 +29,7 @@ RSpec.feature "Judges sign up at special link", :js do
     fill_in "Email", with: "my@email.com"
     fill_in "First name", with: "Judgey"
     fill_in "Last name", with: "McJudgeface"
-    select_date 20.years.ago, from: "Date of birth"
+    select_chosen_date 20.years.ago, from: "Date of birth"
     fill_in "Company name", with: "testing"
     fill_in "Job title", with: "test"
     fill_in "Create a password", with: "secret1234"
@@ -39,7 +39,7 @@ RSpec.feature "Judges sign up at special link", :js do
     expect(current_path).to eq(judge_dashboard_path)
   end
 
-  scenario "encounter validation errors with the special token" do
+  it "encounter validation errors with the special token" do
     invitation = GlobalInvitation.create!
 
     visit judge_signup_path(invitation: invitation.token)
