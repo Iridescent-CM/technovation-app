fd4f8db59324899005b2573138c6e2a669976ce4
Joe Sak <joe@joesak.com>
2018-07-23 14:59:15 -0500

    Add CircleCI config to support system specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -56,6 +56,16 @@ RSpec.configure do |config|
   end
 
   config.before(:each, type: :system, js: true) do
-    driven_by :selenium_chrome_headless
+    if ENV["SELENIUM_DRIVER_URL"].present?
+      driven_by :selenium,
+        using: :chrome,
+        options: {
+          browser: :remote,
+          url: ENV.fetch("SELENIUM_DRIVER_URL"),
+          desired_capabilities: :chrome,
+        }
+    else
+      driven_by :selenium_chrome_headless
+    end
   end
 end
