53c3c4d8c92708339f015dfefe7a59af8cc106b6
Joe Sak <joe@joesak.com>
2018-07-24 15:46:08 -0500

    Introduce TabLink component for convenience

---
+++ app/javascript/tabs/components/TabLink.vue
@@ -0,0 +1,23 @@
+<template>
+  <router-link
+    tag="li"
+    class="tabs__menu-link"
+    active-class="tabs__menu-link--active"
+    :to="to"
+  >
+    <button role="button" class="tabs__menu-button">
+      <slot></slot>
+    </button>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    to: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
