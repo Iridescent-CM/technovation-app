6e10f75090ef6c78baeea9a26615610eb83c217c
Joe Sak <joe@joesak.com>
2018-07-26 12:01:40 -0500

    Tidy the code for ease of reading

--- app/inputs/autocomplete_input.rb
+++ app/inputs/autocomplete_input.rb
@@ -1,15 +1,22 @@
 class AutocompleteInput < SimpleForm::Inputs::Base
   def input(wrapper_options)
-    merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
+    merged_input_options = merge_wrapper_options(
+      input_html_options,
+      wrapper_options
+    )
 
+    # interpolating a v-bind attribute
     if merged_input_options[":options".to_sym]
-      merged_input_options[":options".to_sym] = merged_input_options[":options".to_sym].to_s
+      merged_input_options[":options".to_sym] =
+        merged_input_options[":options".to_sym].to_s
     end
 
-    merged_input_options[:id] = "#{object_name.to_s}_#{attribute_name.to_s}"
-    merged_input_options[:name] = "#{object_name.to_s}[#{attribute_name.to_s}]"
+    merged_input_options[:id] = "#{object_name}_#{attribute_name}"
+    merged_input_options[:name] = "#{object_name}[#{attribute_name}]"
 
-    merged_input_options["no-options-text"] = merged_input_options[:no_options_text]
+    merged_input_options["no-options-text"] =
+      merged_input_options[:no_options_text]
+
     merged_input_options.delete(:no_options_text)
 
     template.content_tag(
