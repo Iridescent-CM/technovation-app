a34e76aa5ba4e30cfdf2277402196fdaaa417c25
Alan Richey <alan@icant.dance>
2018-07-26 14:02:54 -0500

    Add aliases for assets, pack, and vendor JS directories for Jest and Webpack

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,9 @@
 const { environment } = require('@rails/webpacker')
+const customConfig = require('./custom')
 const vue =  require('./loaders/vue')
 
 environment.loaders.append('vue', vue)
+
+environment.config.merge(customConfig)
+
 module.exports = environment
