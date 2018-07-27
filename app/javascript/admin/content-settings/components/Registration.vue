486b6b611216e5ba3d9d75582798e2b04da08c30
Alan Richey <alan@icant.dance>
2018-07-27 14:59:50 -0500

    Registration section implementation for content and settings

--- app/javascript/admin/content-settings/components/Registration.vue
+++ app/javascript/admin/content-settings/components/Registration.vue
@@ -1,12 +1,59 @@
 <template>
   <div id="registration">
     <h4>User Registations</h4>
+    <div v-for="(label, scope) in checkboxes" :key="scope">
+      <p class="inline-checkbox">
+        <input
+          v-if="judgingEnabled"
+          :id="`season_toggles_${scope}_signup`"
+          type="checkbox"
+          :value="0"
+          :disabled="true"
+        >
+        <input
+          v-else
+          :id="`season_toggles_${scope}_signup`"
+          type="checkbox"
+          v-model="$store[`${scope}_signup`]"
+        >
+        <label
+          :for="`season_toggles_${scope}_signup`"
+          :class="{ 'label--disabled': judgingEnabled }"
+        >{{ label }}</label>
+      </p>
+      <div v-if="judgingEnabled" class="notice info hint user-notice">
+        <icon name="exclamation-circle" :size="16" color="00529B" /> When judging is enabled, {{ `${scope}s` }} cannot sign up
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
+import Icon from 'components/Icon.vue'
+
 export default {
   name: 'registration-section',
+
+  components: {
+    Icon,
+  },
+
+  data () {
+    return {
+      checkboxes: {
+        student: 'Students',
+        mentor: 'Mentors',
+      },
+    }
+  },
+
+  computed: {
+    ...mapGetters([
+      'judgingEnabled',
+    ])
+  },
 }
 </script>
 
--- spec/javascript/admin/content-settings/components/Registration.spec.js
+++ spec/javascript/admin/content-settings/components/Registration.spec.js
@@ -1,17 +1,149 @@
-import { shallowMount } from '@vue/test-utils'
+import Vuex from 'vuex'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
 
+import mockStore from 'admin/content-settings/store/__mocks__'
 import Registration from 'admin/content-settings/components/Registration'
+import Icon from 'components/Icon'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
 
 describe('Admin Content & Settings - Registration component', () => {
 
   let wrapper
 
   beforeEach(() => {
-    wrapper = shallowMount(Registration)
+    wrapper = shallowMount(
+      Registration,
+      {
+        localVue,
+        store: mockStore.createMocks().store,
+      }
+    )
   })
 
   it('has a name attribute', () => {
     expect(Registration.name).toEqual('registration-section')
+  })
+
+  describe('data', () => {
+
+    it('contains the correct default data', () => {
+      expect(Registration.data()).toEqual({
+        checkboxes: {
+          student: 'Students',
+          mentor: 'Mentors',
+        },
+      })
+    })
+
+  })
+
+  describe('markup', () => {
+
+    it('contains the proper HTML based on data', () => {
+      const studentCheckbox = wrapper.find('#season_toggles_student_signup')
+      const studentCheckboxLabel = wrapper
+        .find('label[for="season_toggles_student_signup"]')
+      const mentorCheckbox = wrapper.find('#season_toggles_mentor_signup')
+      const mentorCheckboxLabel = wrapper
+        .find('label[for="season_toggles_mentor_signup"]')
+
+      expect(wrapper.vm.judgingEnabled).toBe(false)
+
+      expect(studentCheckbox.exists()).toBe(true)
+      expect(studentCheckboxLabel.exists()).toBe(true)
+      expect(mentorCheckbox.exists()).toBe(true)
+      expect(mentorCheckboxLabel.exists()).toBe(true)
+    })
+
+    it('disables checkboxes and sets values to 0 is judging is enabled', () => {
+      wrapper = shallowMount(
+        Registration,
+        {
+          localVue,
+          store: mockStore.createMocks({
+            getters: {
+              judgingEnabled: () => {
+                return true
+              },
+            },
+          }).store,
+        }
+      )
+
+      const studentCheckbox = wrapper.find('#season_toggles_student_signup')
+      const studentCheckboxLabel = wrapper
+        .find('label[for="season_toggles_student_signup"]')
+      const mentorCheckbox = wrapper.find('#season_toggles_mentor_signup')
+      const mentorCheckboxLabel = wrapper
+        .find('label[for="season_toggles_mentor_signup"]')
+
+      expect(wrapper.vm.judgingEnabled).toBe(true)
+
+      expect(studentCheckbox.attributes()).toEqual(
+        expect.objectContaining({
+          id: 'season_toggles_student_signup',
+          type: 'checkbox',
+          disabled: 'disabled',
+          value: '0',
+        })
+      )
+
+      expect(studentCheckboxLabel.attributes()).toEqual(
+        expect.objectContaining({
+          class: 'label--disabled',
+        })
+      )
+
+      expect(mentorCheckbox.attributes()).toEqual(
+        expect.objectContaining({
+          id: 'season_toggles_mentor_signup',
+          type: 'checkbox',
+          disabled: 'disabled',
+          value: '0',
+        })
+      )
+
+      expect(mentorCheckboxLabel.attributes()).toEqual(
+        expect.objectContaining({
+          class: 'label--disabled',
+        })
+      )
+    })
+
+    it('displays warning notices if judging is enabled', () => {
+      wrapper = shallowMount(
+        Registration,
+        {
+          localVue,
+          store: mockStore.createMocks({
+            getters: {
+              judgingEnabled: () => {
+                return true
+              },
+            },
+          }).store,
+        }
+      )
+
+      const notices = wrapper.findAll('.notice')
+
+      expect(wrapper.vm.judgingEnabled).toBe(true)
+      expect(notices.length).toEqual(2)
+      notices.wrappers.forEach((notice) => {
+        const props = notice.find(Icon).props()
+
+        expect(props).toEqual(
+          expect.objectContaining({
+            name: 'exclamation-circle',
+            size: 16,
+            color: '00529B',
+          })
+        )
+      })
+    })
+
   })
 
 })
