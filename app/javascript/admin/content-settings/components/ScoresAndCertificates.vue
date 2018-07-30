93e0883f2d8e9bce1a391a94a49d15d7812ce552
Alan Richey <alan@icant.dance>
2018-07-30 09:57:25 -0500

    Add markup and tests for scores and certificates section of admin content and settings

--- app/javascript/admin/content-settings/components/ScoresAndCertificates.vue
+++ app/javascript/admin/content-settings/components/ScoresAndCertificates.vue
@@ -1,12 +1,51 @@
 <template>
   <div id="scores-and-certificates">
     <h4>Scores &amp; Certificates</h4>
+
+    <p class="inline-checkbox">
+      <input
+        v-if="judgingEnabled"
+        id="season_toggles_display_scores"
+        type="checkbox"
+        :value="0"
+        :disabled="true"
+      >
+      <input
+        v-else
+        id="season_toggles_display_scores"
+        type="checkbox"
+        v-model="$store.display_scores"
+      >
+      <label
+        for="season_toggles_display_scores"
+        :class="{ 'label--disabled': judgingEnabled }"
+      >Scores &amp; Certificates Accessible</label>
+    </p>
+
+    <div v-if="judgingEnabled" class="notice info hint user-notice">
+      <icon name="exclamation-circle" :size="16" color="00529B" />
+      When judging is enabled, scores &amp; certificates cannot be displayed
+    </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
+import Icon from 'components/Icon.vue'
+
 export default {
   name: 'scores-and-certificates-section',
+
+  components: {
+    Icon,
+  },
+
+  computed: {
+    ...mapGetters([
+      'judgingEnabled',
+    ])
+  },
 }
 </script>
 
--- spec/javascript/admin/content-settings/components/ScoresAndCertificates.spec.js
+++ spec/javascript/admin/content-settings/components/ScoresAndCertificates.spec.js
@@ -3,6 +3,7 @@ import { shallowMount, createLocalVue } from '@vue/test-utils'
 
 import mockStore from 'admin/content-settings/store/__mocks__'
 import ScoresAndCertificates from 'admin/content-settings/components/ScoresAndCertificates'
+import Icon from 'components/Icon'
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
@@ -23,6 +24,90 @@ describe('Admin Content & Settings - ScoresAndCertificates component', () => {
 
   it('has a name attribute', () => {
     expect(ScoresAndCertificates.name).toEqual('scores-and-certificates-section')
+  })
+
+  describe('markup', () => {
+
+    it('contains the proper HTML based on data', () => {
+      const displayScoresCheckbox = wrapper.find('#season_toggles_display_scores')
+      const displayScoresCheckboxLabel = wrapper
+        .find('label[for="season_toggles_display_scores"]')
+
+      expect(wrapper.vm.judgingEnabled).toBe(false)
+
+      expect(displayScoresCheckbox.exists()).toBe(true)
+      expect(displayScoresCheckboxLabel.exists()).toBe(true)
+    })
+
+    it('disables checkboxes and sets values to 0 is judging is enabled', () => {
+      wrapper = shallowMount(
+        ScoresAndCertificates,
+        {
+          localVue,
+          store: mockStore.createMocks({
+            getters: {
+              judgingEnabled: () => {
+                return true
+              },
+            },
+          }).store,
+        }
+      )
+
+      const displayScoresCheckbox = wrapper.find('#season_toggles_display_scores')
+      const displayScoresCheckboxLabel = wrapper
+        .find('label[for="season_toggles_display_scores"]')
+
+      expect(wrapper.vm.judgingEnabled).toBe(true)
+
+      expect(displayScoresCheckbox.attributes()).toEqual(
+        expect.objectContaining({
+          id: 'season_toggles_display_scores',
+          type: 'checkbox',
+          disabled: 'disabled',
+          value: '0',
+        })
+      )
+
+      expect(displayScoresCheckboxLabel.attributes()).toEqual(
+        expect.objectContaining({
+          class: 'label--disabled',
+        })
+      )
+    })
+
+    it('displays warning notices if judging is enabled', () => {
+      wrapper = shallowMount(
+        ScoresAndCertificates,
+        {
+          localVue,
+          store: mockStore.createMocks({
+            getters: {
+              judgingEnabled: () => {
+                return true
+              },
+            },
+          }).store,
+        }
+      )
+
+      expect(wrapper.vm.judgingEnabled).toBe(true)
+
+      const notice = wrapper.find('.notice')
+
+      expect(notice.exists()).toBe(true)
+
+      const props = notice.find(Icon).props()
+
+      expect(props).toEqual(
+        expect.objectContaining({
+          name: 'exclamation-circle',
+          size: 16,
+          color: '00529B',
+        })
+      )
+    })
+
   })
 
 })
