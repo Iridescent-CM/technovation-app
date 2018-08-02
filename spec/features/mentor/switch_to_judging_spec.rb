66e908ef8ac13da4a7787126453f450a29bb0103
Alan Richey <alan@icant.dance>
2018-08-02 16:31:13 -0500

    Add additional tests around mentor judge switching functionality

--- spec/features/mentor/switch_to_judging_spec.rb
+++ spec/features/mentor/switch_to_judging_spec.rb
@@ -12,6 +12,16 @@ RSpec.feature "Mentors switch to judging mode" do
     expect(current_path).to eq(judge_dashboard_path)
   end
 
+  scenario "mentors with a judge profile can browse to judge profile" do
+    mentor = FactoryBot.create(:mentor, :has_judge_profile)
+
+    sign_in(mentor)
+
+    visit judge_dashboard_path
+
+    expect(current_path).to eq(judge_dashboard_path)
+  end
+
   scenario "mentors without a judge profile do not see a judge mode link" do
     mentor = FactoryBot.create(:mentor)
 
@@ -20,5 +30,16 @@ RSpec.feature "Mentors switch to judging mode" do
     expect(mentor.is_a_judge?).to be_falsey
 
     expect(page).not_to have_link("Switch to Judge mode")
+  end
+
+  scenario "mentors without a judge profile cannot browse to judge profile" do
+    mentor = FactoryBot.create(:mentor)
+
+    sign_in(mentor)
+
+    visit judge_dashboard_path
+
+    expect(current_path).to eq(mentor_dashboard_path)
+    expect(page).to have_content("You don't have permission to go there!")
   end
 end
