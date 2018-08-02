636af4793621fb12c48fb67c2dd2fd6bada01bad
Alan Richey <alan@icant.dance>
2018-08-02 15:25:48 -0500

    Remove switch to judge mode link for mentors without a judge profile

--- spec/features/mentor/switch_to_judging_spec.rb
+++ spec/features/mentor/switch_to_judging_spec.rb
@@ -1,11 +1,24 @@
 require "rails_helper"
 
 RSpec.feature "Mentors switch to judging mode" do
-  scenario "mentors can switch to judge mode" do
-    mentor = FactoryBot.create(:mentor)
+  scenario "mentors with a judge profile can switch to judge mode" do
+    mentor = FactoryBot.create(:mentor, :has_judge_profile)
+
     sign_in(mentor)
+
+    expect(mentor.is_a_judge?).to be_truthy
 
     click_link "Switch to Judge mode"
     expect(current_path).to eq(judge_dashboard_path)
+  end
+
+  scenario "mentors without a judge profile do not see a judge mode link" do
+    mentor = FactoryBot.create(:mentor)
+
+    sign_in(mentor)
+
+    expect(mentor.is_a_judge?).to be_falsey
+
+    expect(page).not_to have_link("Switch to Judge mode")
   end
 end
