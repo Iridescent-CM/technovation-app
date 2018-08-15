4766ecbb72e6d47cd28c530d9d3a21cfdb48ef46
Alan Richey <alan@icant.dance>
2018-08-15 13:27:44 -0500

    Repopulate the student, mentor, judge, and admin location forms with data

--- app/controllers/student/current_locations_controller.rb
+++ app/controllers/student/current_locations_controller.rb
@@ -2,15 +2,45 @@ module Student
   class CurrentLocationsController < StudentController
     def show
       if params.fetch(:team_id) { false }
+        state = FriendlySubregion.(
+          OpenStruct.new(
+            state_province: current_team.state_province,
+            country: current_team.country
+          ),
+          prefix: false,
+        )
+
+        country = FriendlyCountry.(
+          OpenStruct.new(country: current_team.country),
+          prefix: false,
+        )
+
         render json: {
           city: current_team.city,
+          state: state,
           state_code: current_team.state_province,
+          country: country,
           country_code: current_team.country,
         }
       else
+        state = FriendlySubregion.(
+          OpenStruct.new(
+            state_province: current_account.state_province,
+            country: current_account.country
+          ),
+          prefix: false,
+        )
+
+        country = FriendlyCountry.(
+          OpenStruct.new(country: current_account.country),
+          prefix: false,
+        )
+
         render json: {
           city: current_account.city,
+          state: state,
           state_code: current_account.state_province,
+          country: country,
           country_code: current_account.country,
         }
       end
