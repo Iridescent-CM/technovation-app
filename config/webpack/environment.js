5c7152cbe065706d5600c51a087496b272352599
Joe Sak <joe@joesak.com>
2018-08-22 17:27:51 -0500

    Try noted performance plugins from official rails webpacker readme

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -6,4 +6,25 @@ environment.loaders.append('vue', vue)
 
 environment.config.merge(customConfig)
 
+const webpack = require('webpack')
+
+environment.plugins.append(
+  'CommonsChunkVendor',
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    minChunks: (module) => {
+      // this assumes your vendor imports exist in the node_modules directory
+      return module.context && module.context.indexOf('node_modules') !== -1
+    }
+  })
+)
+
+environment.plugins.append(
+  'CommonsChunkManifest',
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'manifest',
+    minChunks: Infinity
+  })
+)
+
 module.exports = environment
