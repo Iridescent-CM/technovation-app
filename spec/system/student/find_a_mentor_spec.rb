238ce238261366ba2e8dd36aaa89c55f606849ef
Joe Sak <joe@joesak.com>
2018-08-23 12:55:59 -0500

    Clean up failing spec

--- spec/system/student/find_a_mentor_spec.rb
+++ spec/system/student/find_a_mentor_spec.rb
@@ -1,17 +1,11 @@
 require "rails_helper"
 
 RSpec.describe "Students invite mentors to join their team", :js do
-  before { SeasonToggles.team_building_enabled="yes" }
+  before { SeasonToggles.team_building_enabled! }
 
   let(:student) { FactoryBot.create(:student, :geocoded, :on_team) }
-  let!(:mentor) { FactoryBot.create(:mentor, :geocoded) }
 
-  before do
-    sign_in(student)
-    visit student_team_path(student.team)
-    click_button 'Mentors'
-    click_link "Add a mentor"
-  end
+  before { sign_in(student) }
 
   it "Student is not yet on a team" do
     student.memberships.destroy_all
@@ -20,7 +14,7 @@ RSpec.describe "Students invite mentors to join their team", :js do
     click_button "2. Build your team"
     click_button "Add a mentor to your team"
 
-    within(".completion-step__find-mentor .step-actions") do
+    within(".step-actions") do
       expect(page).not_to have_link("Search for mentors")
       expect(page).to have_content(
         "When you are on a team, you will be able to search for mentors"
