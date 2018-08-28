8f85246450482cc0c84c1c6e3fbb7a1b170cc31e
Joe Sak <joe@joesak.com>
2018-08-28 13:26:20 -0500

    Remove the extra border from embedded student components

--- app/javascript/student/components/Scores.vue
+++ app/javascript/student/components/Scores.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="tabs tabs--vertical tabs--css-only grid">
-    <div class="tabs__content grid__col-12">
+    <div class="tabs__content grid__col-12 tabs__content--embedded">
       <div class="grid">
         <div class="grid__col-8">
           <slot name="scores" />
