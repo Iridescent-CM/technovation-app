45a37c27bfaa39bc3c46386811288ed3902e4efe
Joe Sak <joe@joesak.com>
2018-09-04 17:50:53 -0500

    Fix a spec with a null background check support

--- app/null_objects/null_background_check.rb
+++ app/null_objects/null_background_check.rb
@@ -2,6 +2,7 @@ class NullBackgroundCheck < NullObject
   def id; 0; end
   def updated_at; 0; end
   def status; "none"; end
+  def new_record?; false; end
   def clear?; false; end
   def pending?; false; end
   def consider?; false; end
