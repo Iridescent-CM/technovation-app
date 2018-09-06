064e99085741c738b12aba0f884fc3ecc841b596
Joe Sak <joe@joesak.com>
2018-09-06 12:08:38 -0500

    Fix bug in mailgun response serializer for proper mailbox_verification parsing

--- app/serializers/mailgun_response_serializer.rb
+++ app/serializers/mailgun_response_serializer.rb
@@ -5,10 +5,10 @@ class MailgunResponseSerializer
     :is_role_address, :is_valid, :parts, :is_taken
 
   attribute(:mailbox_verification) do |response|
-    if response.mailbox_verification.nil?
-      nil
-    else
+    if %w(true false).include?(response.mailbox_verification)
       response.mailbox_verification === 'true'
+    else
+      nil
     end
   end
 end
