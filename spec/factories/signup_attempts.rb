d6e3128d1f91b67f73bb3bafbdcb2adaffe0e5b0
Joe Sak <joe@joesak.com>
2018-09-06 17:58:36 -0500

    Add :chicago trait to signup attempt factory

--- spec/factories/signup_attempts.rb
+++ spec/factories/signup_attempts.rb
@@ -3,5 +3,15 @@ FactoryBot.define do
     sequence(:email) { |n| "signup-attempt-#{n}@example.com" }
     password "secret1234"
     status SignupAttempt.statuses[:active]
+
+    trait :chicago do
+      city "Chicago"
+      state_code "IL"
+      country_code "US"
+
+      after(:create) do |attempt|
+        Geocoding.perform(attempt).with_save
+      end
+    end
   end
 end
