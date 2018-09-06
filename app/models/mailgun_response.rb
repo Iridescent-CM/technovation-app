f05459fce8fe5db84a6ff8244f6124fbb33e4e05
Joe Sak <joe@joesak.com>
2018-09-06 14:32:35 -0500

    Add :message to MailgunResponse, apparently

--- app/models/mailgun_response.rb
+++ app/models/mailgun_response.rb
@@ -1,7 +1,7 @@
 class MailgunResponse
   attr_accessor :id, :address, :did_you_mean, :is_disposable_address,
     :is_role_address, :is_valid, :mailbox_verification, :parts, :reason,
-    :is_taken
+    :is_taken, :message
 
   def initialize(response)
     @id = SecureRandom.hex(4)
