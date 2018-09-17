a4ec101e16a938bd59f0ec50dc6501274958a094
Alan Richey <alan@icant.dance>
2018-09-17 15:47:55 -0500

    Add student location POST route and update spec

--- app/controllers/student/locations_controller.rb
+++ app/controllers/student/locations_controller.rb
@@ -2,6 +2,16 @@ module Student
   class LocationsController < StudentController
     include LocationController
 
+    def create
+      current_account.city = location_params.fetch(:city)
+      current_account.state_province = location_params.fetch(:state)
+      current_account.country = location_params.fetch(:country)
+
+      Geocoding.perform(current_account).with_save
+
+      head 200
+    end
+
     private
     def db_record
       @db_record ||= if params.fetch(:team_id) { false }
