a8bb12d11e44ee2d464f6a2b10043b685b3e8ac5
Joe Sak <joe@joesak.com>
2018-09-17 14:09:58 -0500

    Cover israel/palestine sensitivity and location saving with a system spec

---
+++ spec/system/location/sensitive_locations_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe "Saving a location in a geopolitically sensitive area", :js do
+  before do
+    sign_in(:student)
+
+    click_button "Profile"
+    click_button "Region"
+
+    fill_in "Country", with: "Israel"
+    fill_in "State", with: "Tel Aviv"
+    fill_in "City", with: "Tel Aviv"
+
+    click_button "Next"
+  end
+
+  it "prompts the user with a choice when Israel is the country" do
+    within(".suggestions") do
+      expect(page).to have_css(".suggestion", count: 2)
+      expect(page).to have_content("Israel")
+      expect(page).to have_content("Palestine")
+    end
+  end
+
+  it "saves the changes in the database" do
+    page.find(".suggestion", text: "Israel").click
+    click_button "Confirm"
+    visit student_profile_path
+    click_button "Location"
+    expect(page).to have_content("Tel Aviv, Tel Aviv, IL")
+  end
+end
