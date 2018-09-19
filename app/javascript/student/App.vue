f397073c81fba0d4f5acb56388ebd8c45c6bf540
Joe Sak <joe@joesak.com>
2018-09-19 17:09:52 -0500

    Remove mentor e-book from student resources menu

--- app/javascript/student/App.vue
+++ app/javascript/student/App.vue
@@ -80,12 +80,6 @@ export default {
           url: 'https://www.technovationchallenge.org/judging-rubric/',
           text: 'Read the Judging Rubric',
         },
-
-        {
-          heading: 'Mentor Training',
-          url: 'https://iridescentlearning.atavist.com/technovation-mentor-training',
-          text: 'Open the Mentor Training E-book',
-        },
       ],
     }
   },
