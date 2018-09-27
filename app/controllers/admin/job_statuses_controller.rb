40bd32648d592a9e606b38abfb16d2cd2355eb48
Alan Richey <alan@icant.dance>
2018-09-27 15:02:27 -0500

    Update job statuses controller for admin for certificate button

--- app/controllers/admin/job_statuses_controller.rb
+++ app/controllers/admin/job_statuses_controller.rb
@@ -2,15 +2,7 @@ module Admin
   class JobStatusesController < AdminController
     def show
       job = Job.find_by(job_id: params.fetch(:id))
-
-      if job.status == "complete"
-        render json: {
-          status: job.status,
-          download_url: current_admin.exports.last.file_url,
-        }
-      else
-        render json: { status: job.status }
-      end
+      render json: { status: job.status, payload: job.payload }
     end
   end
 end
