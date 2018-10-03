e23ec46074890b95ce3d60b3ee8be313d2a2774b
Joe Sak <joe@joesak.com>
2018-10-03 10:53:05 -0500

    Add registration opens ImportantDate

--- app/constants/important_dates.rb
+++ app/constants/important_dates.rb
@@ -5,4 +5,11 @@ module ImportantDates
     day = Integer(ENV.fetch("DATES_MENTOR_TRAINING_DAY"))
     Date.new(year, month, day)
   end
+
+  def self.registration_opens
+    year = Integer(ENV.fetch("DATES_REGISTRATION_OPENS_YEAR"))
+    month = Integer(ENV.fetch("DATES_REGISTRATION_OPENS_MONTH"))
+    day = Integer(ENV.fetch("DATES_REGISTRATION_OPENS_DAY"))
+    Date.new(year, month, day)
+  end
 end
