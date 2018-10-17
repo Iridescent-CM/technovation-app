e33fe30a3d962cbe848bb646a6c6a92a67efa6f3
Joe Sak <joe@joesak.com>
2018-10-17 10:03:06 -0500

    Bring back the removed code, it's needed in other cases

--- app/technovation/create_mentor_profile.rb
+++ app/technovation/create_mentor_profile.rb
@@ -1,36 +1,34 @@
 module CreateMentorProfile
   def self.call(account)
-    if account.is_not_a_mentor?
-      create_mentor_profile(account)
-      true
+    if account.can_be_a_mentor?
+      attrs = setup_attributes(account)
+      account.create_mentor_profile!(attrs)
     else
       false
     end
   end
 
   private
-  def self.create_mentor_profile(account)
-    attrs = if account.regional_ambassador_profile.present?
-              {
-                school_company_name: account.regional_ambassador_profile
-                  .organization_company_name,
-                job_title: account.regional_ambassador_profile.job_title,
-                mentor_type: "Industry professional",
-              }
-            elsif account.judge_profile.present?
-              {
-                school_company_name: account.judge_profile.company_name,
-                job_title: account.judge_profile.job_title,
-                mentor_type: "Industry professional",
-              }
-            else
-              {
-                school_company_name: account.student_profile.school_name,
-                job_title: "Technovation Alumnus",
-                mentor_type: "Past Technovation student",
-              }
-            end
-
-    account.create_mentor_profile!(attrs)
+  def self.setup_attributes(account)
+    if account.regional_ambassador_profile.present?
+      {
+        school_company_name: account.regional_ambassador_profile
+          .organization_company_name,
+        job_title: account.regional_ambassador_profile.job_title,
+        mentor_type: "Industry professional",
+      }
+    elsif account.judge_profile.present?
+      {
+        school_company_name: account.judge_profile.company_name,
+        job_title: account.judge_profile.job_title,
+        mentor_type: "Industry professional",
+      }
+    else
+      {
+        school_company_name: account.student_profile.school_name,
+        job_title: "Technovation Alumnus",
+        mentor_type: "Past Technovation student",
+      }
+    end
   end
 end
