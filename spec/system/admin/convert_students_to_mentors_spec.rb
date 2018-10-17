374c5fc4ba10e50733f9f9d7aa602690c421595c
Joe Sak <joe@joesak.com>
2018-10-17 09:24:47 -0500

    Update convert mentor test to actually fire the event to test the profile creation

--- spec/system/admin/convert_students_to_mentors_spec.rb
+++ spec/system/admin/convert_students_to_mentors_spec.rb
@@ -14,17 +14,18 @@ RSpec.describe "Admin convert students to mentors" do
   end
 
   context "when the student is 14 and older" do
-    it "shows the convert button" do
+    it "shows a working convert button" do
       student = FactoryBot.create(:student, date_of_birth: 14.years.ago)
 
       sign_in(:admin)
       click_link "Participants"
       click_link "view"
 
-      expect(page).to have_link(
-        "Convert to a mentor",
-        href: admin_student_conversions_path(student_profile_id: student.id)
-      )
+      click_link "Convert to a mentor"
+
+      sign_out
+      sign_in(student)
+      expect(current_path).to eq(mentor_dashboard_path)
     end
   end
 end
