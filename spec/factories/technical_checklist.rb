e44cb7609a94cdb15313f8ef997393fd9d77a4ea
Joe Sak <joe@joesak.com>
2018-11-06 14:07:00 -0600

    Fix carrierwave related factory / spec

--- spec/factories/technical_checklist.rb
+++ spec/factories/technical_checklist.rb
@@ -6,30 +6,28 @@ FactoryBot.define do
       # does not create submission, which #completed? checks for screenshots
       used_strings { true }
       used_strings_explanation { "yep" }
+
       used_numbers { true }
       used_numbers_explanation { "yep" }
+
       used_variables { true }
       used_variables_explanation { "yep" }
+
       used_lists { true }
       used_lists_explanation { "yep" }
+
       used_local_db { true }
       used_local_db_explanation { "yep" }
+
       used_camera { true }
       used_camera_explanation { "yep" }
 
-      paper_prototype {
-        Rack::Test::UploadedFile.new(
-          File.join(Rails.root, 'spec', 'support', 'imgs', 'natasha-avatar.jpg'),
-          'image/jpg'
+      after(:create) do |tc|
+        tc.update_columns(
+          paper_prototype: "spec/support/imgs/natasha-avatar.jpg",
+          event_flow_chart: "spec/support/imgs/natasha-avatar.jpg",
         )
-      }
-
-      event_flow_chart {
-        Rack::Test::UploadedFile.new(
-          File.join(Rails.root, 'spec', 'support', 'imgs', 'natasha-avatar.jpg'),
-          'image/jpg'
-        )
-      }
+      end
     end
   end
 end
