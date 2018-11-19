09e84c2c94e01763bba6d09eff549a71977b1f8c
Alan Richey <adrichey@users.noreply.github.com>
2018-11-19 15:58:13 -0600

    Location form country responses bugfix (#1851)
    * Fix bug where country code and country were not being populated properly in current locations controllers
    
    * Use FriendlyCountry and FriendlySubregion to populate JSON responses in current location controllers

--- app/controllers/admin/current_locations_controller.rb
+++ app/controllers/admin/current_locations_controller.rb
@@ -7,25 +7,20 @@ module Admin
         record = Team.find(params.fetch(:team_id))
       end
 
-      state = FriendlySubregion.(
-        OpenStruct.new(
-          state_province: record.state_province,
-          country: record.country
-        ),
+      state = FriendlySubregion.(record, prefix: false)
+      state_code = FriendlySubregion.(record, {
         prefix: false,
-      )
+        short_code: true
+      })
 
-      country = FriendlyCountry.(
-        OpenStruct.new(country: record.country),
-        prefix: false,
-      )
+      friendly_country = FriendlyCountry.new(record)
 
       render json: {
         city: record.city,
         state: state,
-        state_code: record.state_province,
-        country: country,
-        country_code: record.country,
+        state_code: state_code,
+        country: friendly_country.country_name,
+        country_code: friendly_country.as_short_code,
       }
     end
   end
