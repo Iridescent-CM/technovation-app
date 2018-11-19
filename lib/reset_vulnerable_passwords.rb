b081a42e4e3aef4387077834bb366e857361053f
Joe Sak <joe@joesak.com>
2018-11-19 16:51:43 -0600

    Add the password reset script

---
+++ lib/reset_vulnerable_passwords.rb
@@ -0,0 +1,13 @@
+class ResetVulnerablePasswords
+  def self.perform!
+    accounts = Account.not_admin.where(
+      "created_at::date BETWEEN '2018-10-01' AND '2018-11-13'"
+    )
+
+    accounts.find_each do |account|
+      account.skip_existing_password = true
+      account.password = SecureRandom.base58(48)
+      account.regenerate_auth_token
+    end
+  end
+end
