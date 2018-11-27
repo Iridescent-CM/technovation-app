644c76f1043c4475278302ca5f0e8cffe8ae9661
Alan Richey <alan@icant.dance>
2018-11-27 16:33:48 -0600

    Add link to the team page from the the submission page in the admin UI

--- spec/system/admin/view_submissions_spec.rb
+++ spec/system/admin/view_submissions_spec.rb
@@ -26,5 +26,23 @@ RSpec.describe "Admins checking on submissions" do
         expect(page).to have_content("100% completed")
       end
     end
+
+    it "links back to the team page" do
+      submission = FactoryBot.create(:submission, :complete)
+
+      sign_in(:admin)
+      click_link "Submissions"
+      click_link submission.app_name
+
+      expect(current_path).to eq(admin_team_submission_path(submission))
+
+      within('.panel h3') do
+        expect(page).to have_content(submission.team_name)
+
+        click_link submission.team_name
+
+        expect(current_path).to eq(admin_team_path(submission.team))
+      end
+    end
   end
 end
