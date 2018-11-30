726845c71b3902050951687ea8f7b03b17693eca
Alan Richey <adrichey@users.noreply.github.com>
2018-11-30 13:54:46 -0600

    Make pending students on a team clickable within team page in admin UI (#1869)
    * Make pending students on an individual team page in the admin UI link to their profile pages
    
    * Update onboarding_member partial to check for whether we are in admin or RA dashboard

---
+++ spec/system/admin/review_teams_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.describe "Admins reviewing teams" do
+  describe "individual team view" do
+    it "links to students who are currently pending and not fully onboarded" do
+      onboarding_student = FactoryBot.create(:student, :onboarding)
+
+      team = FactoryBot.create(:team)
+
+      TeamRosterManaging.add(team, onboarding_student)
+
+      sign_in(:admin)
+
+      click_link "Teams"
+      click_link "view"
+
+      expect(current_path).to eq(admin_team_path(team))
+
+      within('.onboarding_students') do
+        expect(page).to have_content(onboarding_student.full_name)
+        click_link onboarding_student.full_name
+        expect(current_path).to eq(admin_participant_path(onboarding_student.account_id))
+      end
+    end
+  end
+end
