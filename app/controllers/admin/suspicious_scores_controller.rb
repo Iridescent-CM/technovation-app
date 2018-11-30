4f54422fe421b26bd5240f361151462af4fe8470
Alan Richey <adrichey@users.noreply.github.com>
2018-11-30 14:33:56 -0600

    Fix incorrect serializer initialization for suspicious scores (#1870)
    * Fix incorrect serializer initialization for suspicious scores
    
    * Fix serializer for suspicious scores according to Enumerable best practices

--- app/controllers/admin/suspicious_scores_controller.rb
+++ app/controllers/admin/suspicious_scores_controller.rb
@@ -2,7 +2,10 @@ module Admin
   class SuspiciousScoresController < AdminController
     def index
       suspicious_scores = SuspiciousSubmissionScores.new
-      render json: SuspiciousScoreSerializer.new(suspicious_scores).serialized_json
+      render json: SuspiciousScoreSerializer.new(
+        suspicious_scores,
+        is_collection: true
+      ).serialized_json
     end
   end
 end
