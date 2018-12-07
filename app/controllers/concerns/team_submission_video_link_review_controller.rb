7605ebef50e53712024c4385c035c1bd35fbea25
Alan Richey <alan@icant.dance>
2018-12-07 14:42:20 -0600

    Record activity throughout team submissions for RA tracking

--- app/controllers/concerns/team_submission_video_link_review_controller.rb
+++ app/controllers/concerns/team_submission_video_link_review_controller.rb
@@ -4,6 +4,14 @@ module TeamSubmissionVideoLinkReviewController
   def update
     submission = current_profile.team_submissions.current.friendly.find(video_link_params[:id])
     submission.update({ video_link_params[:piece] => video_link_params[:value] })
+
+    current_profile.team.create_activity(
+      trackable: current_profile.account,
+      key: "submission.update",
+      parameters: { piece: video_link_params[:piece] },
+      recipient: submission,
+    )
+
     redirect_to [current_scope, :team_submission_section, id: submission.to_param, section: :pitch],
       success: "You saved your #{params[:piece].humanize}!"
   end
