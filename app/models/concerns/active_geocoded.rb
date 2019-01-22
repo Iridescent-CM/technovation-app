b3bd207934b0dccd30e8686c80b6de728cf4f56c
Alan Richey <adrichey@users.noreply.github.com>
2019-01-22 14:51:06 -0600

    Location form fixes (#1934)
    * Correct country function to use fully qualified state name for more reliable functionality
    
    * Create fallback for fully qualified subregion name in ActiveGeocoded country function
    
    * Update Geocoder helpers for new API return values

--- app/models/concerns/active_geocoded.rb
+++ app/models/concerns/active_geocoded.rb
@@ -127,7 +127,10 @@ module ActiveGeocoded
       country = Carmen::Country.coded(country_code) ||
         Carmen::Country.named(country_code)
 
-      me = OpenStruct.new(address_details: [city, state_code, country].join(", "))
+      state_for_country = country.subregions.coded(state_code) ||
+        country.subregions.named(state_code)
+
+      me = OpenStruct.new(address_details: [city, state_for_country, country].join(", "))
       FriendlyCountry.new(me).country_name
     else
       super rescue country_code
