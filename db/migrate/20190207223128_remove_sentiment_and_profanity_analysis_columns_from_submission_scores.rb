079276047ebfd0ee3963e0ff0a782e2f7183f099
Alan Richey <alan@icant.dance>
2019-02-07 16:46:04 -0600

    Add migration to remove the columns for sentiment and profanity analysis from submission scores

---
+++ db/migrate/20190207223128_remove_sentiment_and_profanity_analysis_columns_from_submission_scores.rb
@@ -0,0 +1,48 @@
+class RemoveSentimentAndProfanityAnalysisColumnsFromSubmissionScores < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :submission_scores, :ideation_comment_positivity,
+      :decimal, precision: 4, scale: 3, default: 0
+    remove_column :submission_scores, :ideation_comment_negativity,
+      :decimal, precision: 4, scale: 3, default: 0
+    remove_column :submission_scores, :ideation_comment_neutrality,
+      :decimal, precision: 4, scale: 3, default: 0
+    remove_column :submission_scores, :ideation_comment_bad_word_count,
+      :integer, default: 0
+
+    remove_column :submission_scores, :technical_comment_positivity,
+      :decimal, precision: 4, scale: 3, default: 0
+    remove_column :submission_scores, :technical_comment_negativity,
+      :decimal, precision: 4, scale: 3, default: 0
+    remove_column :submission_scores, :technical_comment_neutrality,
+      :decimal, precision: 4, scale: 3, default: 0
+    remove_column :submission_scores, :technical_comment_bad_word_count,
+      :integer, default: 0
+
+    remove_column :submission_scores, :pitch_comment_positivity,
+      :decimal, precision: 4, scale: 3, default: 0
+    remove_column :submission_scores, :pitch_comment_negativity,
+      :decimal, precision: 4, scale: 3, default: 0
+    remove_column :submission_scores, :pitch_comment_neutrality,
+      :decimal, precision: 4, scale: 3, default: 0
+    remove_column :submission_scores, :pitch_comment_bad_word_count,
+      :integer, default: 0
+
+    remove_column :submission_scores, :entrepreneurship_comment_positivity,
+      :decimal, precision: 4, scale: 3, default: 0
+    remove_column :submission_scores, :entrepreneurship_comment_negativity,
+      :decimal, precision: 4, scale: 3, default: 0
+    remove_column :submission_scores, :entrepreneurship_comment_neutrality,
+      :decimal, precision: 4, scale: 3, default: 0
+    remove_column :submission_scores, :entrepreneurship_comment_bad_word_count,
+      :integer, default: 0
+
+    remove_column :submission_scores, :overall_comment_positivity,
+      :decimal, precision: 4, scale: 3, default: 0
+    remove_column :submission_scores, :overall_comment_negativity,
+      :decimal, precision: 4, scale: 3, default: 0
+    remove_column :submission_scores, :overall_comment_neutrality,
+      :decimal, precision: 4, scale: 3, default: 0
+    remove_column :submission_scores, :overall_comment_bad_word_count,
+      :integer, default: 0
+  end
+end
