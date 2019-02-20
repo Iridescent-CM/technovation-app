bf4ca7bef0cec102759e9dbe0050c360571e1c47
Mike Larsson <mikelarssonftw@gmail.com>
2019-02-20 11:39:36 -0500

    Change event capacity checking to rescue from error

--- app/controllers/mentor/regional_pitch_event_selections_controller.rb
+++ app/controllers/mentor/regional_pitch_event_selections_controller.rb
@@ -8,14 +8,17 @@ module Mentor
     def create
       event = RegionalPitchEvent.find(params[:event_id])
 
-      if event.at_team_capacity?
-        redirect_back fallback_location: mentor_regional_pitch_events_url,
-          error: t("controllers.student.regional_pitch_event_selections.create.event_full")
-      else
-        AddTeamToRegionalEvent.(event, current_team)
+      begin
 
+        AddTeamToRegionalEvent.(event, current_team)
         redirect_to mentor_regional_pitch_events_team_list_path,
           success: "#{current_team.name} is now attending #{event.name}"
+
+      rescue AddTeamToRegionalEvent::EventAtTeamCapacityError => ex
+
+        redirect_back fallback_location: mentor_regional_pitch_events_url,
+          error: t("controllers.student.regional_pitch_event_selections.create.event_full")
+
       end
     end
   end
--- app/controllers/student/regional_pitch_event_selections_controller.rb
+++ app/controllers/student/regional_pitch_event_selections_controller.rb
@@ -5,14 +5,17 @@ module Student
     def create
       event = RegionalPitchEvent.find(params.fetch(:event_id))
 
-      if event.at_team_capacity?
-        redirect_back fallback_location: student_regional_pitch_events_url,
-          error: t("controllers.student.regional_pitch_event_selections.create.event_full")
-      else
-        AddTeamToRegionalEvent.(event, current_team)
+      begin
 
+        AddTeamToRegionalEvent.(event, current_team)
         redirect_to [:student, current_team.selected_regional_pitch_event],
           success: t("controllers.student.regional_pitch_event_selections.create.success")
+
+      rescue AddTeamToRegionalEvent::EventAtTeamCapacityError => ex
+
+        redirect_back fallback_location: student_regional_pitch_events_url,
+          error: t("controllers.student.regional_pitch_event_selections.create.event_full")
+
       end
     end
   end
