cbaa764fdd5b54d578c5afa89cd28f8f1f56795b
Alan Richey <alan@icant.dance>
2019-02-25 16:42:29 -0600

    Force users to enter a location if they have not already done so. Still a WIP.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   include CookiesHelper
+  include ForceLocation
 
   protect_from_forgery with: :exception
 
---
+++ app/controllers/concerns/force_location.rb
@@ -0,0 +1,44 @@
+module ForceLocation
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :require_location
+  end
+
+  private
+  def require_location
+    return if current_scope == "admin" || current_scope == "regional_ambassador"
+
+    if logged_in_and_has_profile && invalid_location && not_on_location_form
+      redirect_to send(
+        "#{current_scope}_location_details_path",
+        return_to: send(
+          "#{current_scope}_profile_path",
+          anchor: '!location'
+        )
+      ),
+        notice: t("controllers.application.invalid_location") and return
+    end
+  end
+
+  def logged_in_and_has_profile
+    current_account.authenticated? &&
+      current_account.respond_to?("#{current_scope}_profile") &&
+        !!current_account.send("#{current_scope}_profile")
+  end
+
+  def invalid_location
+    !current_account.valid_address? || !current_account.valid_coordinates?
+  end
+
+  def not_on_location_form
+    return false if request.xhr?
+
+    original_request_path = Rails.application.routes.recognize_path(request.original_fullpath)
+    location_form_path = Rails.application.routes.recognize_path(
+      send("#{current_scope}_location_details_path")
+    )
+
+    original_request_path != location_form_path
+  end
+end
