e448eb1e80c9c7ed73ac6d88af98191d4e3b517d
Mike Larsson <mikelarssonftw@gmail.com>
2019-03-05 17:10:45 -0500

    Use new scopes in onboarding mentors data analysis

--- app/models/data_analyses/onboarding_mentors_data_analysis.rb
+++ app/models/data_analyses/onboarding_mentors_data_analysis.rb
@@ -3,36 +3,13 @@ module DataAnalyses
     def init_data
       if user.is_admin?
         @mentors = Account.current
-          .left_outer_joins(:mentor_profile)
-          .where("mentor_profiles.id IS NOT NULL")
       else
         @mentors = Account.current
-          .left_outer_joins(:mentor_profile)
-          .where("mentor_profiles.id IS NOT NULL")
           .in_region(user)
       end
 
-      #FIXME: these queries were cribbed from the datagrid, but should be shared somehow
-      @onboarded = @mentors.includes(:background_check, :consent_waiver)
-        .references(:background_checks, :consent_waivers)
-        .where(
-          "email_confirmed_at IS NOT NULL AND " +
-          "mentor_profiles.bio IS NOT NULL AND " +
-          "(training_completed_at IS NOT NULL OR date(season_registered_at) < ?) AND " +
-          "((country = 'US' AND background_checks.status = ?) OR country != 'US')",
-          ImportantDates.mentor_training_required_since,
-          BackgroundCheck.statuses[:clear]
-        )
-      @not_onboarded = @mentors.includes(:background_check, :consent_waiver)
-        .references(:background_checks, :consent_waivers)
-        .where(
-          "email_confirmed_at IS NULL OR " +
-          "mentor_profiles.bio IS NULL OR " +
-          "(training_completed_at IS NULL and date(season_registered_at) >= ?) OR " +
-          "(country = 'US' AND (background_checks.status != ? OR background_checks.status IS NULL))",
-          ImportantDates.mentor_training_required_since,
-          BackgroundCheck.statuses[:clear]
-        )
+      @onboarded = @mentors.onboarded_mentors
+      @not_onboarded = @mentors.onboarding_mentors
     end
 
     def totals
