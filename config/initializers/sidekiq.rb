4765bd8db22569a7ca6c8b30f9df2aa51404c668
Mike Larsson <mikelarssonftw@gmail.com>
2019-04-04 15:53:41 -0400

    add a sidekiq default_retries_exhausted handler to update Job status (#2084)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -17,4 +17,19 @@ Sidekiq.configure_server do |config|
       chain.add Sidekiq::Middleware::Server::Profiler
     end
   end
+
+  config.default_retries_exhausted = -> (msg, ex) do
+    job_id = msg.dig('args', 0, 'job_id')
+
+    unless job_id
+      Sidekiq.logger.warn "Unable to get job_id in default_retries_exhausted for #{msg}: #{ex}"
+      return
+    end
+
+    if db_job = Job.find_by(job_id: job_id)
+      db_job.update_column(:status, "dead")
+      Sidekiq.logger.warn "Job id=#{db_job.id} job_id=#{job_id} for " +
+                          "#{db_job.owner_type} id=#{db_job.owner_id} is dead"
+    end
+  end
 end
