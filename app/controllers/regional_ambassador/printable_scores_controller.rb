1bf3a4be82403113be157cda108d209311834a83
Alan Richey <alan@icant.dance>
2019-04-11 16:27:06 -0500

    Simplify the RPE print out logic

--- app/controllers/regional_ambassador/printable_scores_controller.rb
+++ app/controllers/regional_ambassador/printable_scores_controller.rb
@@ -9,39 +9,25 @@ module RegionalAmbassador
       ).find(params[:id])
 
       judges = @event.judges
-      teams = @event.teams.to_a
-      judge_ids = judges.map { |judge| judge.id }
 
+      judge_ids = judges.map { |judge| judge.id }
       @scores = @event.team_submissions
                 .flat_map(&:scores)
                 .reject { |score| !judge_ids.include?(score.judge_profile_id) }
 
-      if judges.any? { |j| j.assigned_teams_for_event(@event).any? }
-        @groups = []
-
-        judges.each.with_index do |judge, idx|
-          assigned_teams_for_event = judge.assigned_teams_for_event(@event).to_a
-
-          if assigned_teams_for_event.any?
-            index = @groups.find_index { |group| group[:teams] == assigned_teams_for_event }
-
-            if index.nil?
-              @groups.push({ judges: [judge], teams: assigned_teams_for_event })
-            else
-              @groups[index][:judges].push(judge)
-            end
-          else
-            index = @groups.find_index { |group| group[:teams] == teams }
-
-            if index.nil?
-              @groups.push({ judges: [judge], teams: teams })
-            else
-              @groups[index][:judges].push(judge)
-            end
-          end
+      @groups = []
+      judges.each.with_index do |judge, idx|
+        if judge.assigned_teams_for_event(@event).any?
+          teams = judge.assigned_teams_for_event(@event).to_a
+        else
+          teams = @event.teams.to_a
         end
-      else
-        @groups = [{ judges: judges, teams: teams }]
+
+        if index = @groups.find_index { |group| group[:teams] == teams }
+          @groups[index][:judges].push(judge)
+        else
+          @groups.push({ judges: [judge], teams: teams })
+        end
       end
     end
   end
