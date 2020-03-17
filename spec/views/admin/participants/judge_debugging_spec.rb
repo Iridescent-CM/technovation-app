ee000caaf529cc38bcedfcde7f035a46fb8bf4b2
Mike Larsson <mikelarssonftw@gmail.com>
2020-03-17 16:06:32 -0400

    #2411 don't show RAs the suspension status or controls on judge (#2429)
    * don't show RAs the suspension status or controls on judge
    
    * use build_stubbed in judge debugging view spec

---
+++ spec/views/admin/participants/judge_debugging_spec.rb
@@ -0,0 +1,50 @@
+require "rails_helper"
+
+RSpec.describe "admin/participants/judge_debugging", type: :view do
+  before do
+    render partial: "admin/participants/judge_debugging",
+      locals: { current_account: current_account, profile: current_profile }
+  end
+
+  let(:current_account) {
+    instance_double(
+      Account,
+      is_admin?: current_account_admin
+    )
+  }
+
+  let(:current_profiles_account) {
+    FactoryBot.build_stubbed(:account)
+  }
+  let(:current_profile) {
+    FactoryBot.build_stubbed(:judge,
+      account: current_profiles_account
+    ).judge_profile
+  }
+
+  context "as an admin" do
+    let(:current_account_admin) { true }
+    
+    it "shows judge suspended status" do
+      expect(rendered).to have_text("Suspended?")
+    end
+
+    it "shows judge suspension section" do
+      expect(rendered).to have_text("Judge suspension")
+      expect(rendered).to have_link("Suspend this judge")
+    end
+  end
+
+  context "as an RA" do
+    let(:current_account_admin) { false }
+
+    it "does not show judge suspended status" do
+      expect(rendered).not_to have_text("Suspended?")
+    end
+
+    it "does not show judge suspension section" do
+      expect(rendered).not_to have_text("Judge suspension")
+      expect(rendered).not_to have_link("Suspend this judge")
+    end
+  end
+end
