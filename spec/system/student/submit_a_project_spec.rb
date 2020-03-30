eca3077bb1001b0c36002346ffb83b9c88f49260
Mike Larsson <mikelarssonftw@gmail.com>
2020-03-30 17:37:23 -0400

    refining student submission specs

--- spec/system/student/submit_a_project_spec.rb
+++ spec/system/student/submit_a_project_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "Student submits a project", :js do
 
   before { SeasonToggles.team_submissions_editable! }
 
-  scenario "completing a submission" do
+  scenario "unable to submit prior to completing the submission" do
     submission = FactoryBot.create(:submission, :complete, :junior)
     submission.update(published_at: nil)
     submission.update(app_name: '')
@@ -12,43 +12,53 @@ RSpec.feature "Student submits a project", :js do
     student = submission.team.students.sample
 
     sign_in(student)
-    expect(page).to have_link "Your app's name"
 
     click_link "Your app's name"
-    expect(page).to have_content "App name"
 
     expect(page).to have_link "Submit for judging", class: "button--disabled"
+  end
+
+  scenario "completing and submitting the submission" do
+    submission = FactoryBot.create(:submission, :complete, :junior)
+    submission.update(published_at: nil)
+    submission.update(app_name: '')
+
+    student = submission.team.students.sample
+
+    sign_in(student)
+
+    click_link "Your app's name"
 
     fill_in "Your app's name", with: "Coolest app"
     click_button "Save this name"
-    expect(page).to have_content "Your progress has been saved"
-
-    expect(page).to have_link "Review and submit"
 
     click_link "Review and submit"
-    expect(page).to have_link "Submit now"
+
+    click_link "Submit now"
+
+    click_link "We agree"
+
+    expect(page).to have_content "Your submission has been entered for judging!"
+    expect(page).to have_link "View your complete submission"
+    expect(submission.reload).to be_published
   end
 
-  scenario "submitting a complete, unpublished submission" do
+  scenario "submitting a completed submission from the dashboard" do
     submission = FactoryBot.create(:submission, :complete, :junior)
     submission.update(published_at: nil)
 
     student = submission.team.students.sample
 
     sign_in(student)
-    expect(page).to have_link "Review and submit now!"
 
     click_link "Review and submit now!"
-    expect(page).to have_link "Submit now"
 
     click_link "Submit now"
-    expect(page).to have_text "#{Season.current.year} Technovation Honor Code"
-    expect(page).to have_link "We agree"
 
     click_link "We agree"
+
     expect(page).to have_content "Your submission has been entered for judging!"
     expect(page).to have_link "View your complete submission"
-
     expect(submission.reload).to be_published
   end
 end
