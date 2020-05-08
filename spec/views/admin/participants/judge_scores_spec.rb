f9f1eb14945746d7e51d5b12fa3c7ef709529b1e
Mike Larsson <mikelarssonftw@gmail.com>
2020-05-08 10:35:26 -0400

    show dropped info where we show admins deleted info

--- spec/views/admin/participants/judge_scores_spec.rb
+++ spec/views/admin/participants/judge_scores_spec.rb
@@ -7,14 +7,20 @@ RSpec.describe "admin/participants/judge_scores", type: :view do
 
   context "when a judge has scored a submission" do
     let(:scores) { [score_submission] }
-    let(:score_submission) { instance_double(SubmissionScore,
-      team_submission_id: 1,
-      team_name: "Amazing Team",
-      team_submission_app_name: "Amazing App",
-      total: 99,
-      total_possible: 100,
-      deleted?: score_submission_deleted) }
+    let(:score_submission) {
+      instance_double(
+        SubmissionScore,
+        team_submission_id: 1,
+        team_name: "Amazing Team",
+        team_submission_app_name: "Amazing App",
+        total: 99,
+        total_possible: 100,
+        deleted?: score_submission_deleted,
+        dropped?: score_submission_dropped
+      )
+    }
     let(:score_submission_deleted) { false }
+    let(:score_submission_dropped) { false }
 
     it "displays score summary information" do
       expect(rendered).to have_content("Score")
@@ -42,10 +48,24 @@ RSpec.describe "admin/participants/judge_scores", type: :view do
 
         it "indicates that the score has been deleted" do
           expect(rendered).to have_content("Deleted")
-          expect(rendered).to have_css(".deleted-info", text: "yes")
+          expect(rendered).to have_css(".deleted-info", text: "deleted")
         end
 
         it "displays the the deleted information with a subtle red background color" do
+          expect(rendered).to have_css(".background-color--subtle-red")
+        end
+      end
+
+      context "when the score is marked as dropped" do
+        let(:score_submission_deleted) { true }
+        let(:score_submission_dropped) { true }
+
+        it "indicates that the score has been dropped" do
+          expect(rendered).to have_content("Deleted")
+          expect(rendered).to have_css(".deleted-info", text: "dropped")
+        end
+
+        it "displays the the dropped information with a subtle red background color" do
           expect(rendered).to have_css(".background-color--subtle-red")
         end
       end
@@ -55,7 +75,7 @@ RSpec.describe "admin/participants/judge_scores", type: :view do
 
         it "indicates that the score has not been deleted" do
           expect(rendered).to have_content("Deleted")
-          expect(rendered).to have_css(".deleted-info", text: "no")
+          expect(rendered).to have_css(".deleted-info", text: "")
         end
 
         it "displays the the deleted information without a special background color" do
