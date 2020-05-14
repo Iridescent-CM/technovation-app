3ee853fd068fdecab5b3677846048eaa3be11b25
Mike Larsson <mikelarssonftw@gmail.com>
2020-05-14 16:36:50 -0400

    Add integration test for CSV export notifications

---
+++ spec/features/admin/export_a_csv_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+RSpec.feature "Admin exporting a CSV", js: true do
+  context "from the Participants page" do
+    let(:admin) { FactoryBot.create(:admin) }
+    let!(:student) { FactoryBot.create(:student, :onboarded) }
+
+    before {
+      sign_in(admin)
+      visit admin_participants_path
+    }
+
+    scenario "sees a confirmation dialog" do
+      expect(page).to have_content("1 account found")
+
+      click_link "Export current results to CSV"
+      click_button "Export now"
+
+      within("#queued-jobs") do
+        expect(page).to have_content("Your file is ready!")
+        expect(page).to have_link("Download")
+        expect(page).to have_link("I no longer need this file")
+      end
+    end
+
+    scenario "clears the confirmation dialog" do
+      click_link "Export current results to CSV"
+      click_button "Export now"
+      within("#queued-jobs") do
+        click_link "I no longer need this file"
+      end
+
+      expect(page).not_to have_css("#queued-jobs")
+    end
+  end
+end
