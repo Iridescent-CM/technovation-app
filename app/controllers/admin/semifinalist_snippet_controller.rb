497231430302c5b4f0586e5d4f209e16c474851a
Mike Larsson <mikelarssonftw@gmail.com>
2020-05-21 11:19:49 -0400

    handle nil country

--- app/controllers/admin/semifinalist_snippet_controller.rb
+++ app/controllers/admin/semifinalist_snippet_controller.rb
@@ -1,24 +1,29 @@
 module Admin
   class SemifinalistSnippetController < AdminController
     def show
-      senior_submissions_by_country = TeamSubmission
+      senior_submissions = TeamSubmission
         .current
         .semifinalist
         .filter { |sub| sub.senior_division? }
-        .group_by(&:country)
-      @senior_columns = split_into_columns(senior_submissions_by_country)
+      @senior_columns = split_into_columns(senior_submissions)
 
-      junior_submissions_by_country = TeamSubmission
+      junior_submissions = TeamSubmission
         .current
         .semifinalist
         .filter { |sub| sub.junior_division? }
-        .group_by(&:country)
-      @junior_columns = split_into_columns(junior_submissions_by_country)
+      @junior_columns = split_into_columns(junior_submissions)
 
       render content_type: "text/plain", layout: false
     end
 
     private
+
+    def group_by_country(submissions)
+      submissions
+        .group_by(&:country)
+        .map { |k, v| [k.nil? ? '' : k, v] }
+        .to_h
+    end
 
     def entry_for(country, submissions)
       friendly_name = FriendlyCountry.(
@@ -34,14 +39,14 @@ module Admin
       }
     end
 
-    def split_into_columns(subs_by_country)
+    def split_into_columns(subs)
       left_col = []
       right_col = []
 
-      total_subs = subs_by_country.values.reduce(0) { |sum, subs| sum + subs.count }
+      total_subs = subs.count
       subs_in_left_column = 0
 
-      subs_by_country.sort.each do |country, submissions|
+      group_by_country(subs).sort.each do |country, submissions|
         if subs_in_left_column < (total_subs / 2)
           left_col << entry_for(country, submissions)
           subs_in_left_column = subs_in_left_column + submissions.count
