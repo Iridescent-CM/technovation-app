8a61d7518597b3792cb296989371ce897f741fcc
Mike Larsson <mikelarssonftw@gmail.com>
2020-05-21 10:47:28 -0400

    refining the semifinalist snippet

--- app/controllers/admin/semifinalist_snippet_controller.rb
+++ app/controllers/admin/semifinalist_snippet_controller.rb
@@ -1,32 +1,52 @@
 module Admin
   class SemifinalistSnippetController < AdminController
     def show
-      senior_submissions = TeamSubmission.current.semifinalist.filter { |sub| sub.senior_division? }
-      senior_submissions_by_country = senior_submissions.group_by(&:country)
-      @senior_columns = split_columns(senior_submissions_by_country)
+      senior_submissions_by_country = TeamSubmission
+        .current
+        .semifinalist
+        .filter { |sub| sub.senior_division? }
+        .group_by(&:country)
+      @senior_columns = split_into_columns(senior_submissions_by_country)
 
-      junior_submissions = TeamSubmission.current.semifinalist.filter { |sub| sub.junior_division? }
-      junior_submissions_by_country = junior_submissions.group_by(&:country)
-      @junior_columns = split_columns(junior_submissions_by_country)
+      junior_submissions_by_country = TeamSubmission
+        .current
+        .semifinalist
+        .filter { |sub| sub.junior_division? }
+        .group_by(&:country)
+      @junior_columns = split_into_columns(junior_submissions_by_country)
 
       render content_type: "text/plain", layout: false
     end
 
     private
 
-    def split_columns(submissions_by_country)
+    def entry_for(country, submissions)
+      friendly_name = FriendlyCountry.(
+        OpenStruct.new(address_details: country),
+        prefix: false,
+        source: :country_code,
+      )
+
+      {
+        country: country,
+        friendly_name: friendly_name,
+        submissions: submissions.sort_by { |sub| sub.team_name.downcase }
+      }
+    end
+
+    def split_into_columns(subs_by_country)
       left_col = []
       right_col = []
 
-      overall_count = submissions_by_country.values.reduce(0) { |sum, subs| sum + subs.count }
-      running_count = 0
+      total_subs = subs_by_country.values.reduce(0) { |sum, subs| sum + subs.count }
+      subs_in_left_column = 0
 
-      submissions_by_country.each do |country_name, submissions|
-        if running_count < (overall_count / 2)
-          running_count = running_count + submissions.count
-          left_col << { country: country_name, submissions: submissions }
+      subs_by_country.sort.each do |country, submissions|
+        if subs_in_left_column < (total_subs / 2)
+          left_col << entry_for(country, submissions)
+          subs_in_left_column = subs_in_left_column + submissions.count
         else
-          right_col << { country: country_name, submissions: submissions }
+          right_col << entry_for(country, submissions)
         end
       end
 
