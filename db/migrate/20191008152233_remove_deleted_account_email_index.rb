001f555ac123e8ac2ab1cee2f80b896f7202a7f3
Mike Larsson <mikelarssonftw@gmail.com>
2020-07-17 11:20:31 -0400

    fix index removal breaking on CI

--- db/migrate/20191008152233_remove_deleted_account_email_index.rb
+++ db/migrate/20191008152233_remove_deleted_account_email_index.rb
@@ -1,5 +1,5 @@
 class RemoveDeletedAccountEmailIndex < ActiveRecord::Migration[5.1]
   def change
-    remove_index :accounts, :email
+    remove_index :accounts, name: "index_accounts_on_email"
   end
 end
