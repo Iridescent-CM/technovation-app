e5e3b92e388ccfa35ea1ddabbaa8b44580b70791
Mike Larsson <mikelarssonftw@gmail.com>
2020-08-28 12:09:42 -0400

    add request spec for prefilling of content into downloadable parental consent

---
+++ spec/requests/student/downloadable_parental_consent_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+RSpec.describe Student::DownloadableParentalConsentsController do
+  let(:student_account) { FactoryBot.create(:account, email: student_email_address) }
+  let(:student_email_address) { "harry@example.com"  }
+  let(:student_profile) {
+    FactoryBot.create(:student_profile, :geocoded,
+      account: student_account,
+      parent_guardian_name: parent_guardian_name,
+      parent_guardian_email: parent_guardian_email_address)
+  }
+
+  describe "GET #show" do
+    before do
+      sign_in student_profile
+      get student_downloadable_parental_consent_path
+    end
+
+    context "when no parent data has been provided" do
+      let(:parent_guardian_name) { nil }
+      let(:parent_guardian_email_address) { nil }
+
+      it "pre-fills student data" do
+        expect(response.body).to include(student_account.name)
+        expect(response.body).to include(student_email_address)
+      end
+    end
+
+    context "when parent data has been provided" do
+      let(:parent_guardian_name) { "Parent Name" }
+      let(:parent_guardian_email_address) { "parent@example.com" }
+
+      it "pre-fills parent data as well" do
+        expect(response.body).to include(parent_guardian_name)
+      end
+    end
+  end
+end
