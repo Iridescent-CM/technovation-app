193d1e34722fe950b4724a0ee71ee2e32bc0b788
Mike Larsson <mikelarssonftw@gmail.com>
2020-08-31 16:49:45 -0400

    replace cloudflare middleware from snippet with more robust gem

--- config/application.rb
+++ config/application.rb
@@ -44,9 +44,6 @@ module TechnovationApp
       g.factory_bot dir: 'spec/factories'
     end
 
-    require "#{Rails.root}/lib/cloud_flare_middleware"
-    config.middleware.insert_before(0, Rack::CloudFlareMiddleware)
-
     config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
       r301 '/users/sign_up', '/signup'
       r301 '/users/sign_in', '/signin'
