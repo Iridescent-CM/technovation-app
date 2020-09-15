8ef3b9480c051551150adb05b138dd55e57c1f7a
Mike Larsson <mikelarssonftw@gmail.com>
2020-09-15 17:29:54 -0400

    Initialize admin content and settings from ajax call, not html attr

--- app/javascript/admin/content-settings/store/actions.js
+++ app/javascript/admin/content-settings/store/actions.js
@@ -1,1 +1,16 @@
-export default {}
+import axios from 'axios' // temporarily bypassing the whole axios case converter stuff
+export default {
+  init({ commit, state}) {
+    return new Promise((resolve, reject) => {
+      axios.get('/admin/season_schedule_settings.json')
+        .then(({ data }) => {
+          if (data.data) {
+            commit('setFormData', data.data.attributes)
+            resolve()
+          } else {
+            reject()
+          }
+        }).catch(err => reject(err))
+    })
+  }
+}
