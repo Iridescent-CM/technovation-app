2347a82ce5b94e7a66528302596f44fa3c04b5bc
Mike Larsson <mikelarssonftw@gmail.com>
2020-09-15 17:38:26 -0400

    Add FIXME comment about the axios-case-converter issue

--- app/javascript/admin/content-settings/store/actions.js
+++ app/javascript/admin/content-settings/store/actions.js
@@ -1,4 +1,9 @@
-import axios from 'axios' // temporarily bypassing the whole axios case converter stuff
+import axios from 'axios'
+/* FIXME: The content-settings store speaks snake_case, and window.axios comes with
+ * an axios-case-converter transformation that transforms snake_case to camelCase,
+ * so we have to bypass that for now with a fresh import.
+ */
+
 export default {
   init({ commit, state}) {
     return new Promise((resolve, reject) => {
