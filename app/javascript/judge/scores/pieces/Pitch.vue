c694d70122d16577fe72b002aa4e5ce3be38556c
shaun <58957909+shaunxp20@users.noreply.github.com>
2020-11-09 17:09:36 -0700

    Update judge's demo video section
    I added a header for the demo video section, and changed the ordering of
    the demo video and the pitch video.
    
    Refs: #2605

--- app/javascript/judge/scores/pieces/Pitch.vue
+++ app/javascript/judge/scores/pieces/Pitch.vue
@@ -1,9 +1,30 @@
 <template>
   <div :class="solo ? 'grid__col-12' : 'grid__col-6'">
-    <h4>Pitch</h4>
 
     <div class="grid grid--bleed">
       <div :class="solo ? 'grid__col-6' : 'grid__col-12'">
+        <h4>Pitch</h4>
+        <p>
+          <a
+            href="#"
+            :data-opens-modal="`video-modal-${submission.pitch_video_id}`"
+            :data-modal-fetch="submission.pitch_video_url"
+          >
+            <icon name="lightbulb-o" color="e6e6e5" />
+            <span>Watch the pitch video</span>
+          </a>
+        </p>
+
+        <div
+          class="modal"
+          :id="`video-modal-${submission.pitch_video_id}`"
+        >
+          <div class="modal-content"></div>
+        </div>
+      </div>
+
+      <div :class="solo ? 'grid__col-6' : 'grid__col-12'">
+        <h4>Demo Video</h4>
         <p>
           <a
             href="#"
@@ -23,25 +44,6 @@
         </div>
       </div>
 
-      <div :class="solo ? 'grid__col-6' : 'grid__col-12'">
-        <p>
-          <a
-            href="#"
-            :data-opens-modal="`video-modal-${submission.pitch_video_id}`"
-            :data-modal-fetch="submission.pitch_video_url"
-          >
-            <icon name="lightbulb-o" color="e6e6e5" />
-            <span>Watch the pitch video</span>
-          </a>
-        </p>
-
-        <div
-          class="modal"
-          :id="`video-modal-${submission.pitch_video_id}`"
-        >
-          <div class="modal-content"></div>
-        </div>
-      </div>
     </div>
   </div>
 </template>
