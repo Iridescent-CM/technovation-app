bbc0c37fbe78a4ad3efc6b1939b2f0b9559edcda
shaun <58957909+shaunxp20@users.noreply.github.com>
2021-02-09 19:47:16 -0700

    Update student conversion specs
    Refs: #2354

--- spec/controllers/admin/student_conversions_controller_spec.rb
+++ spec/controllers/admin/student_conversions_controller_spec.rb
@@ -2,26 +2,26 @@ require "rails_helper"
 
 RSpec.describe Admin::StudentConversionsController do
   describe "POST #create" do
-    let(:student) { FactoryBot.create(:student, date_of_birth: 18.years.ago) }
-    let(:account) { student.account }
+    let(:student_profile) { FactoryBot.create(:student, date_of_birth: 18.years.ago) }
+    let(:account) { student_profile.account }
 
-    before { sign_in(:admin) }
-    it "preserves the student profile for historical purposes" do
-      post :create, params: { student_profile_id: student.id }
+    before do
+      sign_in(:admin)
 
-      expect {
-        student.reload
-      }.not_to raise_error
-    end
-
-    it "does not delete the account" do
-      post :create, params: { student_profile_id: student.id }
-      expect(account.reload).to be_present
+      post :create, params: { student_profile_id: student_profile.id }
+      account.reload
     end
 
     it "adds a mentor profile to the account" do
-      post :create, params: { student_profile_id: student.id }
-      expect(account.reload.mentor_profile).to be_present
+      expect(account.mentor_profile).to be_present
+    end
+
+    it "deletes the student profile on the account" do
+      expect(account.student_profile).to be_nil
+    end
+
+    it "does not delete the account" do
+      expect(account).to be_present
     end
   end
 end
