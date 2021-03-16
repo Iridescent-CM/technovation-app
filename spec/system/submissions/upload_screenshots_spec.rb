3fe14d32e8634252ebe4b09e8c9b865eb0da5272
shaun <58957909+shaunxp20@users.noreply.github.com>
2021-03-16 15:25:05 -0600

    Refactor image upload specs
    There have been some random failures since making the latest changes
    to #2850, I think this refactoring should help with that. 🤞
    
    Refs: #2734

--- spec/system/submissions/upload_screenshots_spec.rb
+++ spec/system/submissions/upload_screenshots_spec.rb
@@ -3,57 +3,102 @@ require "rails_helper"
 RSpec.describe "Uploading images to submissions", :js do
   before { SeasonToggles.team_submissions_editable! }
 
-  it "does not allow invalid file types" do
-    student = FactoryBot.create(:student, :onboarded, :on_team)
-    submission = student.team.create_submission!(integrity_affirmed: true)
+  let(:student) { FactoryBot.create(:student, :onboarded, :on_team) }
+  let(:submission) { FactoryBot.create(:submission, team: student.team) }
+
+  before do
     sign_in(student)
 
     visit student_team_submission_path(submission, piece: :screenshots)
+  end
 
-    ['jpg', 'jpeg', 'gif', 'png'].each do |good_file|
+  context "valid image file types" do
+    it "allows gifs" do
       attach_file(
         "attach-screenshots",
-        File.absolute_path("./spec/support/uploads/example.#{good_file}"),
+        File.absolute_path("./spec/support/uploads/example.gif"),
         make_visible: true
       )
+
       expect(page).not_to have_css(".flash.flash--alert")
       expect(page).to have_css(".sortable-list__item")
     end
 
-    ['bmp', 'docx', 'pdf', 'zip'].each do |bad_file|
+    it "allows pngs" do
       attach_file(
         "attach-screenshots",
-        File.absolute_path("./spec/support/uploads/example.#{bad_file}"),
+        File.absolute_path("./spec/support/uploads/example.png"),
         make_visible: true
       )
-      expect(page).to have_css(".flash.flash--alert", text: "invalid file type")
-      find(".flash .icon-close").click
+
+      expect(page).not_to have_css(".flash.flash--alert")
+      expect(page).to have_css(".sortable-list__item")
+    end
+
+    it "allows jpgs" do
+      attach_file(
+        "attach-screenshots",
+        File.absolute_path("./spec/support/uploads/example.jpg"),
+        make_visible: true
+      )
+
+      expect(page).not_to have_css(".flash.flash--alert")
+      expect(page).to have_css(".sortable-list__item")
+    end
+
+    it "allows jpegs" do
+      attach_file(
+        "attach-screenshots",
+        File.absolute_path("./spec/support/uploads/example.jpeg"),
+        make_visible: true
+      )
+
+      expect(page).not_to have_css(".flash.flash--alert")
+      expect(page).to have_css(".sortable-list__item")
     end
   end
 
-  it "does not allow invalid file types for mentors" do
-    mentor = FactoryBot.create(:mentor, :onboarded, :on_team)
-    submission = mentor.teams.last.create_submission!(integrity_affirmed: true)
-    sign_in(mentor)
-
-    visit mentor_team_submission_path(submission, piece: :screenshots)
-
-    ['jpg', 'jpeg', 'gif', 'png'].each do |good_file|
+  context "invalid image file types" do
+    it "does not allow bmps" do
       attach_file(
         "attach-screenshots",
-        File.absolute_path("./spec/support/uploads/example.#{good_file}"),
+        File.absolute_path("./spec/support/uploads/example.bmp"),
         make_visible: true
       )
-      expect(page).not_to have_css(".flash.flash--alert")
-      expect(page).to have_css(".sortable-list__item")
+
+      expect(page).to have_css(".flash.flash--alert", text: "invalid file type")
+      find(".flash .icon-close").click
     end
 
-    ['bmp', 'docx', 'pdf', 'zip'].each do |bad_file|
+    it "does not allow pdfs" do
       attach_file(
         "attach-screenshots",
-        File.absolute_path("./spec/support/uploads/example.#{bad_file}"),
+        File.absolute_path("./spec/support/uploads/example.pdf"),
         make_visible: true
       )
+
+      expect(page).to have_css(".flash.flash--alert", text: "invalid file type")
+      find(".flash .icon-close").click
+    end
+
+    it "does not allow docs" do
+      attach_file(
+        "attach-screenshots",
+        File.absolute_path("./spec/support/uploads/example.docx"),
+        make_visible: true
+      )
+
+      expect(page).to have_css(".flash.flash--alert", text: "invalid file type")
+      find(".flash .icon-close").click
+    end
+
+    it "does not allow zipped files" do
+      attach_file(
+        "attach-screenshots",
+        File.absolute_path("./spec/support/uploads/example.zip"),
+        make_visible: true
+      )
+
       expect(page).to have_css(".flash.flash--alert", text: "invalid file type")
       find(".flash .icon-close").click
     end
