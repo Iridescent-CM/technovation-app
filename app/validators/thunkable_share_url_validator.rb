3b7b3c5da487281bd20856eb5593b30099ba7666
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2021-04-17 09:44:03 -0400

    Allow projecrPage urls in team share url validator

--- app/validators/thunkable_share_url_validator.rb
+++ app/validators/thunkable_share_url_validator.rb
@@ -1,6 +1,6 @@
 class ThunkableShareUrlValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    if not value.match(/^https?:\/\/x.thunkable.com\/copy\/\w+$/)
+    if not value.match(/^https?:\/\/x.thunkable.com\/(copy|projectPage)\/\w+$/)
       record.errors.add(attribute, :invalid)
     end
   end
