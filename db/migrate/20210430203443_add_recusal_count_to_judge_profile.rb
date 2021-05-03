2f4f0e849602e65ab0d9addb402399eb45801a6f
Vivian Canales <vcnales@gmail.com>
2021-05-03 09:12:13 -0500

    Added migration file for judge recusal score count to judge profile

---
+++ db/migrate/20210430203443_add_recusal_count_to_judge_profile.rb
@@ -0,0 +1,24 @@
+class AddRecusalCountToJudgeProfile < ActiveRecord::Migration[5.2]
+  def change
+    add_column :judge_profiles, :recusal_scores_count, :integer, default: 0, null: false
+    add_index :judge_profiles, :recusal_scores_count
+
+    reversible do |direction|
+      direction.up do
+        execute <<-SQL.squish
+          update judge_profiles
+            set recusal_scores_count = (
+              select count(1)
+              from submission_scores
+              WHERE submission_scores.judge_profile_id = judge_profiles.id
+                and judge_recusal = true
+                and '2021' = any(seasons)
+            )
+        SQL
+      end
+    end
+  end
+
+
+
+end
