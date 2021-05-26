b84b9417cc7b7b2c89967617a7848af64e0ca5ba
Vivian Canales <vcnales@gmail.com>
2021-05-26 16:02:15 -0500

    Updated text for semifinalist certificate.

--- lib/fill_pdfs/semifinalist.rb
+++ lib/fill_pdfs/semifinalist.rb
@@ -7,7 +7,7 @@ module FillPdfs
     def full_text
       "For her outstanding work as a member of Technovation " +
       "#{recipient.region} team #{recipient.team_name} " +
-      "to develop the mobile application #{recipient.mobile_app_name}."
+      "to develop the mobile application #{recipient.mobile_app_name} for the #{recipient.season} Technovation Girls season."
     end
   end
 end
