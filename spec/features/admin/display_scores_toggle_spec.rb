50eacb431a00fbfbabcde08cf3ecc094000582fd
Vivian Canales <vcnales@gmail.com>
2021-06-17 13:10:42 -0500

    update to spec for toggle scores

--- spec/features/admin/display_scores_toggle_spec.rb
+++ spec/features/admin/display_scores_toggle_spec.rb
@@ -20,7 +20,7 @@ RSpec.feature "Toggling display of scores" do
 
       visit student_dashboard_path
 
-      expect(page).not_to have_content("Scores & Certificate")
+      expect(page).to have_content("Scores & Certificate")
       expect(page).not_to have_css(".button", text: "View your scores and certificate")
     end
   end
