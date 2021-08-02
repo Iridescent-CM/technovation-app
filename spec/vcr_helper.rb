56cdde1b1c5e16e7c8bca3c0a592ffe7d056df1d
shaun <58957909+shaunxp20@users.noreply.github.com>
2021-08-02 13:52:29 -0600

    Update vcr_helper
    `config.ignore_localhost = true` - this seems like a valid change, but
    at this point there are too many moving pieces to the Rails upgrade,
    which I'm trying to minimize, so that's why I'm removing it
    
    `config.allow_http_connections_when_no_cassette = true` - we definitely
    don't want this setting enabled
    
    `github-releases.githubusercontent.com` - this seems to be the new
    domain where the Chrome/Gecko drivers get downloaded from, so we want to
    allow connections to there
    
    Refs: #2872

--- spec/vcr_helper.rb
+++ spec/vcr_helper.rb
@@ -7,15 +7,14 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/cassettes"
   config.hook_into :webmock # or :fakeweb
   config.configure_rspec_metadata!
-  config.ignore_localhost = true
-  config.allow_http_connections_when_no_cassette = true
   config.ignore_request do |request|
     uri = URI(request.uri)
     LOCALHOSTS.include?(uri.host) and uri.port != 9200
   end
   config.ignore_hosts(
-    'chromedriver.storage.googleapis.com',
-    'github.com'
+    "chromedriver.storage.googleapis.com",
+    "github.com",
+    "github-releases.githubusercontent.com"
   )
 
 end
