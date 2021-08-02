fc8f8ca56a45839ad0d8862e94e027ac96cb0d4e
shaun <58957909+shaunxp20@users.noreply.github.com>
2021-08-02 16:48:49 -0600

    Update click handler in spec
    These specs are failing intermittently, after some researching I think
    this new way will work better, we'll see!
    
    Some Stack Overflow answers for reference:
    https://stackoverflow.com/a/60695813
    https://stackoverflow.com/a/52405269
    https://stackoverflow.com/a/44916498
    
    Refs: #2872

--- spec/features/judge/scoring_submissions_spec.rb
+++ spec/features/judge/scoring_submissions_spec.rb
@@ -37,6 +37,7 @@ feature "scoring submissions", js: true do
 
     then_the_review_page_displays_a_perfect_score_of 80
   end
+
   private
 
   def given_there_is_a_submission_from_a_junior_team_that_needs_scoring
@@ -58,58 +59,66 @@ feature "scoring submissions", js: true do
   end
 
   def and_scores_the_ideation_section_perfectly
-    find("#judge-scores-app li.score-question:nth-child(1) li.score-value", text: "5").click()
-    find("#judge-scores-app li.score-question:nth-child(2) li.score-value", text: "5").click()
-    find("#judge-scores-app li.score-question:nth-child(3) li.score-value", text: "5").click()
-    find("#judge-scores-app li.score-question:nth-child(4) li.score-value", text: "5").click()
+    find("#judge-scores-app li.score-question:nth-child(1) li.score-value", text: "5").click
+    find("#judge-scores-app li.score-question:nth-child(2) li.score-value", text: "5").click
+    find("#judge-scores-app li.score-question:nth-child(3) li.score-value", text: "5").click
+    find("#judge-scores-app li.score-question:nth-child(4) li.score-value", text: "5").click
     find("#judge-scores-app textarea").set("Lorem ipsum dolor sit amet consectetur adipiscing elit iaculis suspendisse natoque magna senectus, tempus nulla maecenas rutrum cursus euismod ante cras posuere proin himenaeos. Nisi primis ullamcorper penatibus vivamus dapibus, risus vel lobortis nam sed convallis, velit a cubilia hendrerit.")
   end
 
   def and_scores_the_technical_section_perfectly
-    find("#judge-scores-app a.button.btn-next", text: "Next: Technical", wait: 10).click()
+    click_next_button("Next: Technical")
 
-    find("#judge-scores-app li.score-question:nth-child(1) li.score-value", text: "5").click()
-    find("#judge-scores-app li.score-question:nth-child(2) li.score-value", text: "5").click()
-    find("#judge-scores-app li.score-question:nth-child(3) li.score-value", text: "5").click()
-    find("#judge-scores-app li.score-question:nth-child(4) li.score-value", text: "5").click()
+    find("#judge-scores-app li.score-question:nth-child(1) li.score-value", text: "5").click
+    find("#judge-scores-app li.score-question:nth-child(2) li.score-value", text: "5").click
+    find("#judge-scores-app li.score-question:nth-child(3) li.score-value", text: "5").click
+    find("#judge-scores-app li.score-question:nth-child(4) li.score-value", text: "5").click
     find("#judge-scores-app textarea").set("Lorem ipsum dolor sit amet consectetur adipiscing elit iaculis suspendisse natoque magna senectus, tempus nulla maecenas rutrum cursus euismod ante cras posuere proin himenaeos. Nisi primis ullamcorper penatibus vivamus dapibus, risus vel lobortis nam sed convallis, velit a cubilia hendrerit.")
   end
 
   def and_scores_the_pitch_section_perfectly
-    find("#judge-scores-app a.button.btn-next", text: "Next: Pitch", wait: 10).click()
+    click_next_button("Next: Pitch")
 
-    find("#judge-scores-app li.score-question:nth-child(1) li.score-value", text: "5").click()
-    find("#judge-scores-app li.score-question:nth-child(2) li.score-value", text: "5").click()
+    find("#judge-scores-app li.score-question:nth-child(1) li.score-value", text: "5").click
+    find("#judge-scores-app li.score-question:nth-child(2) li.score-value", text: "5").click
     find("#judge-scores-app textarea").set("Lorem ipsum dolor sit amet consectetur adipiscing elit iaculis suspendisse natoque magna senectus, tempus nulla maecenas rutrum cursus euismod ante cras posuere proin himenaeos. Nisi primis ullamcorper penatibus vivamus dapibus, risus vel lobortis nam sed convallis, velit a cubilia hendrerit.")
   end
 
   def and_scores_the_entrepreneurship_section_perfectly
-    find("#judge-scores-app a.button.btn-next", text: "Next: Entrepreneurship").click()
+    click_next_button("Next: Entrepreneurship")
 
-    find("#judge-scores-app li.score-question:nth-child(1) li.score-value", text: "5").click()
-    find("#judge-scores-app li.score-question:nth-child(2) li.score-value", text: "5").click()
-    find("#judge-scores-app li.score-question:nth-child(3) li.score-value", text: "5").click()
-    find("#judge-scores-app li.score-question:nth-child(4) li.score-value", text: "5").click()
+    find("#judge-scores-app li.score-question:nth-child(1) li.score-value", text: "5").click
+    find("#judge-scores-app li.score-question:nth-child(2) li.score-value", text: "5").click
+    find("#judge-scores-app li.score-question:nth-child(3) li.score-value", text: "5").click
+    find("#judge-scores-app li.score-question:nth-child(4) li.score-value", text: "5").click
     find("#judge-scores-app textarea").set("Lorem ipsum dolor sit amet consectetur adipiscing elit iaculis suspendisse natoque magna senectus, tempus nulla maecenas rutrum cursus euismod ante cras posuere proin himenaeos. Nisi primis ullamcorper penatibus vivamus dapibus, risus vel lobortis nam sed convallis, velit a cubilia hendrerit.")
   end
 
   def and_scores_the_overall_section_perfectly
-    find("#judge-scores-app a.button.btn-next", text: "Next: Overall", wait: 10).click()
+    click_next_button("Next: Overall")
 
-    find("#judge-scores-app li.score-question:nth-child(1) li.score-value", text: "5").click()
-    find("#judge-scores-app li.score-question:nth-child(2) li.score-value", text: "5").click()
+    find("#judge-scores-app li.score-question:nth-child(1) li.score-value", text: "5").click
+    find("#judge-scores-app li.score-question:nth-child(2) li.score-value", text: "5").click
     find("#judge-scores-app textarea").set("Lorem ipsum dolor sit amet consectetur adipiscing elit iaculis suspendisse natoque magna senectus, tempus nulla maecenas rutrum cursus euismod ante cras posuere proin himenaeos. Nisi primis ullamcorper penatibus vivamus dapibus, risus vel lobortis nam sed convallis, velit a cubilia hendrerit.")
 
-    find("#judge-scores-app a.button.btn-next", text: "Next: Review score", wait: 10).click()
+    click_next_button("Next: Review score")
   end
 
   def and_submits_the_score
-    click_link "Finish score"
+    finish_score_button = find("#judge-scores-app a.button", text: "Finish score")
+
+    execute_script("arguments[0].click();", finish_score_button)
   end
 
   def then_the_review_page_displays_a_perfect_score_of(perfect_score)
     click_link("review your score")
 
     expect(page).to have_content("#{perfect_score} / #{perfect_score}")
+  end
+
+  def click_next_button(button_text)
+    next_button = find("#judge-scores-app a.button.btn-next", text: button_text)
+
+    execute_script("arguments[0].click();", next_button)
   end
 end
