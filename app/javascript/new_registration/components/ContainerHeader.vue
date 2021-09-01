79ff514d062bfa3c9b8d90d648ebdbadb4dcc0e4
Vivian Canales <vcnales@gmail.com>
2021-09-01 16:43:46 -0500

    Added components needed for new registration flow

---
+++ app/javascript/new_registration/components/ContainerHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="energetic-blue">
+    <h4 class="text-left font-extrabold p-4 text-white"> {{ headerText }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContainerHeader",
+  props: {
+    headerText: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/javascript/new_registration/components/Footer.vue
@@ -0,0 +1,3 @@
+<template>
+  <p class="text-sm">&copy; 2021 Technovation. All Rights Reserved. Technovation is a 501c3 nonprofit organization in the United States.</p>
+</template>
