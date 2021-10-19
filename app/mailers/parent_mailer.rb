c2d6e2b69420c184031d08c2c453333b35371424
shaun <58957909+shaunxp20@users.noreply.github.com>
2021-10-18 22:06:25 -0600

    Send email confirmation after media consent is signed
    Refs: #3061

--- app/mailers/parent_mailer.rb
+++ app/mailers/parent_mailer.rb
@@ -28,6 +28,17 @@ class ParentMailer < ApplicationMailer
     end
   end
 
+  def confirm_media_consent_finished(student_profile)
+    return if student_profile.parent_guardian_email.blank?
+
+    @student_profile = student_profile
+    @media_consent = student_profile.media_consent
+
+    I18n.with_locale(@student_profile.account.locale) do
+      mail to: @student_profile.parent_guardian_email
+    end
+  end
+
   def thank_you(student_profile)
     return if student_profile.parent_guardian_email.blank?
 
--- app/models/media_consent.rb
+++ app/models/media_consent.rb
@@ -5,11 +5,20 @@ class MediaConsent < ActiveRecord::Base
   validates :electronic_signature, presence: true, on: :update
   validates :consent_provided, inclusion: [false, true], on: :update
 
+  after_commit :send_media_conent_confirmation_email_to_parent,
+    if: proc { |media_consent| media_consent.signed? }
+
   def signed?
     electronic_signature.present?
   end
 
   def unsigned?
     electronic_signature.blank?
+  end
+
+  private
+
+  def send_media_conent_confirmation_email_to_parent
+    ParentMailer.confirm_media_consent_finished(student_profile).deliver_later
   end
 end
