2dff1bade6db57819c7c1f7f9c714d4cbc5693c0
Vivian Canales <vcnales@gmail.com>
2021-11-05 19:22:39 -0500

    Added logic to only add class to these components if they are rendered in a view that is not the student dashboard. This is done in case these views are used in other parts of the app that are not rebranded

--- app/javascript/dashboard/components/Events.vue
+++ app/javascript/dashboard/components/Events.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="tabs tabs--vertical tabs--css-only grid">
-    <div class="tabs__content grid__col-12 tabs__content--embedded">
-      <div class="grid">
-        <div class="grid__col-8">
+  <div :class=mainVerticalTabsClass>
+    <div :class=tabClass>
+      <div :class=gridClass>
+        <div :class=gridEightClass>
           <slot name="events" />
         </div>
       </div>
@@ -13,5 +13,44 @@
 <script>
 export default {
   name: 'events',
+
+  data () {
+    return {
+      isStudentDash: false
+    }
+  },
+
+  beforeMount() {
+    let pathname = window.location.pathname
+    if (pathname === '/student/dashboard') {
+      this.isStudentDash = true
+    }
+  },
+
+  computed: {
+    mainVerticalTabsClass () {
+      if(!this.isStudentDash){
+        return 'tabs tabs--vertical tabs--css-only grid'
+      }
+    },
+
+    gridClass () {
+      if(!this.isStudentDash){
+        return 'grid'
+      }
+    },
+
+    gridEightClass () {
+      if(!this.isStudentDash){
+        return 'grid__col-8'
+      }
+    },
+
+    tabClass () {
+      if(!this.isStudentDash){
+        return 'tabs__content grid__col-12 tabs__content--embedded'
+      }
+    }
+  }
 }
 </script>
--- app/javascript/dashboard/components/Scores.vue
+++ app/javascript/dashboard/components/Scores.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="tabs tabs--vertical tabs--css-only grid">
-    <div class="tabs__content grid__col-12 tabs__content--embedded">
-      <div class="grid">
-        <div class="grid__col-12">
+  <div :class="mainVerticalTabsClass">
+    <div :class="tabClass">
+      <div :class="gridClass">
+        <div :class="gridTwelveClass">
           <slot name="scores" />
         </div>
       </div>
@@ -13,5 +13,45 @@
 <script>
 export default {
   name: 'scores',
+
+
+  data () {
+    return {
+      isStudentDash: false
+    }
+  },
+
+  beforeMount() {
+    let pathname = window.location.pathname
+    if (pathname === '/student/dashboard') {
+      this.isStudentDash = true
+    }
+  },
+
+  computed: {
+    mainVerticalTabsClass () {
+      if(!this.isStudentDash){
+        return 'tabs tabs--vertical tabs--css-only grid'
+      }
+    },
+
+    gridClass () {
+      if(!this.isStudentDash){
+        return 'grid'
+      }
+    },
+
+    gridTwelveClass () {
+      if(!this.isStudentDash){
+        return 'grid__col-12'
+      }
+    },
+
+    tabClass () {
+      if(!this.isStudentDash){
+        return 'tabs__content grid__col-12 tabs__content--embedded'
+      }
+    }
+  }
 }
 </script>
