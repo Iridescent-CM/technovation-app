ce8195ebc4581c168e6dbf2ce072927750e20081
Vivian Canales <vcnales@gmail.com>
2021-11-12 16:48:27 -0600

    Set Events component back to original file and created EventsRebrand component for the new rebranding of student dash.

--- app/javascript/dashboard/components/Events.vue
+++ app/javascript/dashboard/components/Events.vue
@@ -1,8 +1,8 @@
 <template>
-  <div :class=mainVerticalTabsClass>
-    <div :class=tabClass>
-      <div :class=gridClass>
-        <div :class=gridEightClass>
+  <div class="tabs tabs--vertical tabs--css-only grid">
+    <div class="tabs__content grid__col-12 tabs__content--embedded">
+      <div class="grid">
+        <div class="grid__col-8">
           <slot name="events" />
         </div>
       </div>
@@ -13,44 +13,5 @@
 <script>
 export default {
   name: 'events',
-
-  data () {
-    return {
-      isStudentDash: false
-    }
-  },
-
-  beforeMount() {
-    let pathname = window.location.pathname
-    if (pathname === '/student/dashboard') {
-      this.isStudentDash = true
-    }
-  },
-
-  computed: {
-    mainVerticalTabsClass () {
-      if(!this.isStudentDash){
-        return 'tabs tabs--vertical tabs--css-only grid'
-      }
-    },
-
-    gridClass () {
-      if(!this.isStudentDash){
-        return 'grid'
-      }
-    },
-
-    gridEightClass () {
-      if(!this.isStudentDash){
-        return 'grid__col-8'
-      }
-    },
-
-    tabClass () {
-      if(!this.isStudentDash){
-        return 'tabs__content grid__col-12 tabs__content--embedded'
-      }
-    }
-  }
 }
 </script>
