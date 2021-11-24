c066eef9bbe5bcf7c51f78ad187f108ba032bb13
Roxana Gonzalez <roxana@iridescentlearning.org>
2021-11-23 17:30:39 -0800

    Updated footer in contactus.vue
    Updating footers in location where i've found them

--- app/javascript/new_registration/components/ContactUs.vue
+++ app/javascript/new_registration/components/ContactUs.vue
@@ -7,19 +7,19 @@
 
     <section id="links" class="flex flex-wrap-reverse md:flex-nowrap justify-between my-12">
       <div id="icons" class="flex mx-auto md:mx-0 my-8 md: my-0 w-1/2 md:w-48 justify-between">
-        <img :src="require('social-media/fb.png')" alt="" class="social-icon">
-        <img :src="require('social-media/twitter.png')" alt="" class="social-icon">
-        <img :src="require('social-media/instagram.png')" alt="" class="social-icon">
-        <img :src="require('social-media/youtube.png')" alt="" class="social-icon">
+        <a href="https://www.facebook.com/technovationglobal"><img :src="require('social-media/fb.png')" alt="" class="social-icon"></a>
+        <a href="https://twitter.com/technovation"><img :src="require('social-media/twitter.png')" alt="" class="social-icon"></a>
+        <a href="https://www.instagram.com/technovationglobal"><img :src="require('social-media/instagram.png')" alt="" class="social-icon"></a>
+        <a href="https://www.youtube.com/technovationglobal"><img :src="require('social-media/youtube.png')" alt="" class="social-icon"></a>
       </div>
 
       <div id="site-links" class="flex flex-wrap w-full  md:mx-0 md:w-1/2 my-8 md: my-0">
-        <a href="#" class="site-link">FAQ</a>
-        <a href="#" class="site-link">Contact</a>
-        <a href="#" class="site-link">Terms of Use</a>
-        <a href="#" class="site-link">Press</a>
-        <a href="#" class="site-link">Privacy Policy</a>
-        <a href="#" class="site-link">Donate</a>
+        <a href="https://iridescentsupport.zendesk.com/hc/en-us/categories/115000091348-Technovation" class="site-link">FAQ</a>
+        <a href="mailto:support@technovation.org" class="site-link">Contact</a>
+        <a href="https://www.technovation.org/terms-of-use/" class="site-link">Terms of Use</a>
+        <a href="https://www.technovation.org/news-events/" class="site-link">Press</a>
+        <a href="https://www.technovation.org/privacy-policy/" class="site-link">Privacy Policy</a>
+        <a href="https://www.technovation.org/donate-today/" class="site-link">Donate</a>
       </div>
     </section>
   </div>
