f6424152d3dc16c53a9e164ed9b77fff20b3033c
Roxana Gonzalez <roxana@iridescentlearning.org>
2021-11-30 19:10:21 -0800

    Testing path routes for login & sign up

--- app/javascript/new_registration/components/Navbar.vue
+++ app/javascript/new_registration/components/Navbar.vue
@@ -12,8 +12,8 @@
             <a href="https://technovationchallenge.org/get-started/" class="registration-nav">Get Started</a>
             <a href="https://technovationchallenge.org/" class="registration-nav">Chapters</a>
             <a href="https://www.technovation.org/about/" class="registration-nav">About</a>
-            <a href="" class="registration-nav">Login</a>
-            <a href="" class="registration-nav">Sign Up</a>
+            <a href="/login" class="registration-nav">Login</a>
+            <a href="/signup" class="registration-nav">Sign Up</a>
             <a href="https://www.technovation.org/donate-today/" class="py-2 px-2 text-white font-black 
             rounded transition duration-300 bg-energetic-blue">DONATE</a>
           </div>
