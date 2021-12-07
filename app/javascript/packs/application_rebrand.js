10471816926dc323d4d027909f01dc118be49d03
shaun <58957909+shaunxp20@users.noreply.github.com>
2021-12-07 15:56:09 -0700

    Update stylesheet imports
    Trying to clean up our style imports a bit.
    
    Refs: #3128

--- app/javascript/packs/application_rebrand.js
+++ app/javascript/packs/application_rebrand.js
@@ -1,3 +1,8 @@
-import '../stylesheets/new_registration.css'
-import '../stylesheets/student_profile.css'
-import '../stylesheets/student_dashboard.css'
+import "tailwindcss/base";
+import "tailwindcss/components";
+import "tailwindcss/utilities";
+
+import "../stylesheets/base_forms.css"
+import "../stylesheets/new_registration.css"
+import "../stylesheets/student_profile.css"
+import "../stylesheets/student_dashboard.css"
