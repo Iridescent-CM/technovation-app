5f18b3eb95fd8efdccb9ecce07c4af4a95ce6a18
shaun <58957909+shaunxp20@users.noreply.github.com>
2021-12-07 16:08:54 -0700

    Add new documents layout
    Not sure if this is the best name for this layout, but basically it's
    used just for the consent forms right now. Anyhow, it's a very minimal
    layout for displaying documents.
    
    Refs: #3128

--- app/javascript/packs/application_rebrand.js
+++ app/javascript/packs/application_rebrand.js
@@ -3,6 +3,7 @@ import "tailwindcss/components";
 import "tailwindcss/utilities";
 
 import "../stylesheets/base_forms.css"
+import "../stylesheets/documents.css"
 import "../stylesheets/new_registration.css"
 import "../stylesheets/student_profile.css"
 import "../stylesheets/student_dashboard.css"
