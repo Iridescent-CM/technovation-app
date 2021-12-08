92b88243fdd911127a8369f988bd58a89d0b41e5
shaun <58957909+shaunxp20@users.noreply.github.com>
2021-12-07 19:47:08 -0700

    Remove unneeded stylesheet
    We shouldn't need new_registration.css outside of the registration flow.
    
    Refs: #3136

--- app/javascript/packs/application_rebrand.js
+++ app/javascript/packs/application_rebrand.js
@@ -2,6 +2,5 @@ import "../stylesheets/tailwind.css"
 
 import "../stylesheets/base_forms.css"
 import "../stylesheets/documents.css"
-import "../stylesheets/new_registration.css"
 import "../stylesheets/student_profile.css"
 import "../stylesheets/student_dashboard.css"
