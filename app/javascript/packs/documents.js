0b642f780619524e25f6e45c80370345afdb7956
shaun <58957909+shaunxp20@users.noreply.github.com>
2021-12-07 23:22:41 -0700

    Create a documents JavaScript pack
    This will be used in the new documents layout.
    
    Refs: #3128

---
+++ app/javascript/packs/documents.js
@@ -0,0 +1,6 @@
+import "../stylesheets/tailwind.css"
+
+import "../stylesheets/base_forms.css"
+import "../stylesheets/layout.css"
+import "../stylesheets/documents.css"
+import "../stylesheets/student_profile.css"
