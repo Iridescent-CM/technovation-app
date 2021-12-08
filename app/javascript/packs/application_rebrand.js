3dec56adee51e4f7d5e77e7b44fa6b3ac0b89047
shaun <58957909+shaunxp20@users.noreply.github.com>
2021-12-07 23:23:32 -0700

    Remove documents.css from rebrand pack
    We don't need this one in here anymore.
    
    Refs: #3128

--- app/javascript/packs/application_rebrand.js
+++ app/javascript/packs/application_rebrand.js
@@ -2,6 +2,5 @@ import "../stylesheets/tailwind.css"
 
 import "../stylesheets/base_forms.css"
 import "../stylesheets/layout.css"
-import "../stylesheets/documents.css"
 import "../stylesheets/student_profile.css"
 import "../stylesheets/student_dashboard.css"
