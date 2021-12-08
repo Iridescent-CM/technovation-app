622a70d075eb49788daa46b8db96e6ef2932cb30
shaun <58957909+shaunxp20@users.noreply.github.com>
2021-12-08 00:37:26 -0700

    Add documents.css to application_rebrand
    Seeing if this fixes QA,

--- app/javascript/packs/application_rebrand.js
+++ app/javascript/packs/application_rebrand.js
@@ -2,5 +2,6 @@ import "../stylesheets/tailwind.css"
 
 import "../stylesheets/base_forms.css"
 import "../stylesheets/layout.css"
+import "../stylesheets/documents.css"
 import "../stylesheets/student_profile.css"
 import "../stylesheets/student_dashboard.css"
