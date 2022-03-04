49907c8be753acb288752ef3a0f9454f53cc60df
Alexandre Conrado <alexkonrad.dev@gmail.com>
2022-03-04 18:08:26 -0300

    Removes update that put "on file" text on guardian_name and guardian_email fields

--- app/controllers/admin/paper_parental_consents_controller.rb
+++ app/controllers/admin/paper_parental_consents_controller.rb
@@ -3,13 +3,8 @@ module Admin
     def create
       student = StudentProfile.find(params[:id])
 
-      student.student_profile.update(
-        parent_guardian_name: ParentalConsent::PARENT_GUARDIAN_NAME_FOR_A_PAPER_CONSENT,
-        parent_guardian_email: ParentalConsent::PARENT_GUARDIAN_EMAIL_ADDDRESS_FOR_A_PAPER_CONSENT
-      )
-
       consent = ParentalConsent.where(student_profile_id: params[:id]).last
-
+      
       consent.update(
         status: ParentalConsent.statuses[:signed],
         electronic_signature: "ON FILE"
