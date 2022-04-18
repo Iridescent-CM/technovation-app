1c8bc38b2eb465d123b7df639bec117a3d4aa90b
Vivian Canales <vcnales@gmail.com>
2022-04-18 16:42:08 -0500

    Updated the overall component with rebrand generic container. Will most likely be altered with the new judging screens in the future.

--- app/javascript/judge/scores/sections/Overall.vue
+++ app/javascript/judge/scores/sections/Overall.vue
@@ -1,36 +1,16 @@
 <template>
-  <div class="grid">
-    <div class="grid__col-12 grid__col--bleed-y">
-      <h2>Overall Impression</h2>
-    </div>
-
-    <team-info />
-
-    <div class="grid__col-9 grid__col--bleed-y">
+  <GenericJudgingContainer
+      heading="Overall Impression"
+      section="overall"
+      next-section="review-score"
+      prev-section="entrepreneurship"
+  >
+    <template v-slot:main-content>
       <h1 class="border--b-thin-primary">{{ submission.name }}</h1>
 
       <div class="app-description" v-html="submission.description"></div>
-
-      <question-section
-        prevSection="entrepreneurship"
-        section="overall"
-        nextSection="review-score"
-        nextSectionTitle="Review score"
-      >
-        <p slot="section-summary" class="help-text">
-          To give your overall feedback to a team and help them understand how to continue
-          learning, please refer to all submission elements and the
-          <a target="_blank" href="http://technovation.staging.wpengine.com/wp-content/uploads/2021/03/Overall.png">rubric</a>.
-        </p>
-
-        <p slot="comment-tips">
-          Considering the native language and ages of the team,
-          how does this submission stand out for you? How well
-          do all the parts work together?
-        </p>
-      </question-section>
-    </div>
-  </div>
+    </template>
+  </GenericJudgingContainer>
 </template>
 
 <script>
@@ -38,11 +18,13 @@ import { mapState } from 'vuex';
 
 import TeamInfo from '../TeamInfo';
 import QuestionSection from '../QuestionSection';
+import GenericJudgingContainer from "../../components/GenericJudgingContainer";
 
 export default {
   components: {
     TeamInfo,
     QuestionSection,
+    GenericJudgingContainer
   },
 
   computed: mapState(['submission']),
