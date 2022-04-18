89b34e359781f15a7e176737ee2e1467abc14e07
Vivian Canales <vcnales@gmail.com>
2022-04-18 16:41:28 -0500

    Removed commented out code that was initially left to determine which path to take for the rebranding/new juding flow

--- app/javascript/judge/scores/sections/Ideation.vue
+++ app/javascript/judge/scores/sections/Ideation.vue
@@ -1,34 +1,4 @@
 <template>
-<!--  <div>-->
-<!--    <EnergeticContainer heading="Project Name & Description">-->
-<!--      <team-info />-->
-
-<!--      <div>-->
-<!--        <p class="font-bold text-lg">{{ submission.name }}</p>-->
-
-<!--        <div class="app-description" v-html="submission.description"></div>-->
-
-<!--        <ThickRule/>-->
-
-<!--        <question-section-->
-<!--            section="ideation"-->
-<!--            nextSection="technical"-->
-<!--        >-->
-<!--          <p slot="section-summary" class="help-text">-->
-<!--            To determine if the team has built a solution that will positively impact them and their community,-->
-<!--            refer to all submission materials and the-->
-<!--            <a target="_blank" href="http://technovation.staging.wpengine.com/wp-content/uploads/2021/03/ideation.png">rubric</a>.-->
-<!--          </p>-->
-
-<!--          <p slot="comment-tips">-->
-<!--            Be sensitive to cultural differences and-->
-<!--            focus on how well the team identified the problem-->
-<!--            and presented a solution.-->
-<!--          </p>-->
-<!--        </question-section>-->
-<!--      </div>-->
-<!--    </EnergeticContainer>-->
-<!--  </div>-->
     <div>
       <GenericJudgingContainer
           heading="Project Name & Description"
