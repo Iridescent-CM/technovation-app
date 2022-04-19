e4b526ef31515858aa6dc68bb92c7ca1b1fd61b4
shaun <58957909+shaunxp20@users.noreply.github.com>
2022-04-19 13:03:39 -0600

    Update Vue router
    Adding routes for the following sections:
    - overview
    - demo
    
    Also, adding a rough draft for the Overview component (just a copy/paste
    of one of the other components).
    
    Refs: #3342

---
+++ app/javascript/judge/scores/sections/Overview.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+      <GenericJudgingContainer
+          heading="Project Name & Description"
+          section="overview"
+          next-section="ideation"
+      >
+        <template v-slot:main-content>
+          <p class="font-bold text-lg">{{ submission.name }}</p>
+          <div v-html="submission.description"></div>
+        </template>
+      </GenericJudgingContainer>
+    </div>
+</template>
+
+<script>
+import TeamInfo from '../TeamInfo'
+import Pitch from '../pieces/Pitch'
+import QuestionSection from '../QuestionSection'
+import EnergeticContainer from "../../components/EnergeticContainer";
+import ThickRule from "../../components/ThickRule";
+
+import { mapState } from 'vuex'
+import GenericJudgingContainer from "../../components/GenericJudgingContainer";
+import Template from "../../../components/job_process/template";
+
+export default {
+  computed: mapState(['submission']),
+
+  components: {
+    Template,
+    QuestionSection,
+    TeamInfo,
+    Pitch,
+    EnergeticContainer,
+    ThickRule,
+    GenericJudgingContainer
+  },
+}
+</script>
