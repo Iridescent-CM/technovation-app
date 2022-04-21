69f74b34ad484090737fb5564bd2cbfcd823372b
Vivian Canales <vcnales@gmail.com>
2022-04-21 13:36:18 -0500

    Renamed Pitch piece to to PitchVideoLink for understanding + fixed indentation

--- app/javascript/judge/scores/pieces/PitchVideoLink.vue
+++ app/javascript/judge/scores/pieces/PitchVideoLink.vue
@@ -2,20 +2,20 @@
   <div>
     <p>
       <a
-          :href="`${submission.pitch_video_url}`"
-          :data-opens-modal="`video-modal-${submission.pitch_video_id}`"
-          :data-modal-fetch="submission.pitch_video_url"
-          @click="trackPitchVideoClick"
-          class="text-energetic-blue flex"
+        :href="`${submission.pitch_video_url}`"
+        :data-opens-modal="`video-modal-${submission.pitch_video_id}`"
+        :data-modal-fetch="submission.pitch_video_url"
+        @click="trackPitchVideoClick"
+        class="text-energetic-blue flex"
       >
-        <icon name="play-circle-o" color="0075cf"/>
+        <Icon name="play-circle-o" color="0075cf"/>
         Watch the pitch video
       </a>
     </p>
 
     <div
-        class="modal"
-        :id="`video-modal-${submission.pitch_video_id}`"
+      class="modal"
+      :id="`video-modal-${submission.pitch_video_id}`"
     >
       <div class="modal-content"></div>
     </div>
@@ -34,11 +34,6 @@ export default {
     async trackPitchVideoClick (event) {
       event.preventDefault()
       await window.axios.patch(`/judge/scores/${this.score.id}`, {submission_score: {'clicked_pitch_video': true}})
-    },
-
-    async trackDemoVideoClick (event) {
-      event.preventDefault()
-      await window.axios.patch(`/judge/scores/${this.score.id}`, {submission_score: {'clicked_demo_video': true}})
     },
   },
 
