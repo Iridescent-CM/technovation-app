4ecd57fc6c944cfd649a31f30c1792dd7cc3f8d5
shaun <58957909+shaunxp20@users.noreply.github.com>
2022-04-26 13:24:49 -0600

    Refactor JudgeQuestionsForSubmissionScore
    Refactoring this to  use the new generic/basic `JudgeQuestions` service.
    
    Refs: #3350

--- app/services/judge_questions_for_submission_score.rb
+++ app/services/judge_questions_for_submission_score.rb
@@ -1,10 +1,10 @@
 class JudgeQuestionsForSubmissionScore
-  def initialize(submission_score)
+  def initialize(submission_score, judge_questions_constructor: JudgeQuestions)
     @submission_score = submission_score
     @submission_type = submission_score.team_submission_submission_type
-    @season = submission_score.seasons.last
-    @division = submission_score.team_division_name
-    @questions = "Judging::#{season_module_name}::#{questions_class_name}".constantize.new.call
+    @questions = judge_questions_constructor
+      .new(season: submission_score.seasons.last, division: submission_score.team_division_name)
+      .call
   end
 
   def call
@@ -13,35 +13,7 @@ class JudgeQuestionsForSubmissionScore
 
   private
 
-  attr_reader :submission_score, :submission_type, :season, :division, :questions
-
-  def season_module_name
-    case season
-    when 2022
-      "TwentyTwentyTwo"
-    when 2021
-      "TwentyTwentyOne"
-    when 2020
-      "TwentyTwenty"
-    else
-      if season < 2020
-        raise "Questions for the #{season} season don't exist! Most likely the questions could be added by looking at the commit history for `quesitons.rb`."
-      else
-        raise "Questions for the #{season} season haven't been setup yet! You'll likely want to copy the previous year's queetions in '/app/services/judging/#{season - 1}/' to get them setup."
-      end
-    end
-  end
-
-  def questions_class_name
-    case division.downcase
-    when "beginner"
-      "BeginnerQuestions"
-    when "junior"
-      "JuniorQuestions"
-    when "senior"
-      "SeniorQuestions"
-    end
-  end
+  attr_reader :submission_score, :submission_type, :questions
 
   def questions_with_scores_populated
     filtered_questions.map do |question|
