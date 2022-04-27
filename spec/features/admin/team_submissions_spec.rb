06e52b86a906bb993a8d7d0fd431d0329b352034
Vivian Canales <vcnales@gmail.com>
2022-04-27 12:53:30 -0500

    Updated testing specs for business plan label change

--- spec/features/admin/team_submissions_spec.rb
+++ spec/features/admin/team_submissions_spec.rb
@@ -48,7 +48,7 @@ RSpec.feature "admin team submissions" do
     fill_in "App description", with: "A great description for the ages"
 
     attach_file(
-      "Business plan",
+      "Business Plan",
       Rails.root + "spec/support/fixtures/business_plan.pdf"
     )
 
