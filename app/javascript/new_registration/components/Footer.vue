672c71154710c92fbc0bf2dbe3928f19f8fac10c
Alexandre Conrado <alexkonrad.dev@gmail.com>
2022-05-03 13:21:43 -0300

    Makes year dynamic on site footer

--- app/javascript/new_registration/components/Footer.vue
+++ app/javascript/new_registration/components/Footer.vue
@@ -1,3 +1,13 @@
 <template>
-  <p class="text-sm">&copy; 2021 Technovation. All Rights Reserved. Technovation is a 501c3 nonprofit organization in the United States.</p>
+  <p class="text-sm">&copy; {{currentYear}} Technovation. All Rights Reserved. Technovation is a 501c3 nonprofit organization in the United States.</p>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentYear: new Date().getFullYear(),
+    };
+  }
+};
+</script>
