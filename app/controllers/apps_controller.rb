1c46c49af440a23f0b929327716203d471eb5d53
shaun <58957909+shaunxp20@users.noreply.github.com>
2022-05-04 11:41:39 -0600

    Make public app page use rebranded layout
    Refs: #3376

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -2,15 +2,19 @@ class AppsController < ApplicationController
   helper_method :current_scope,
     :determine_manifest
 
+  layout "application_rebrand"
+
   def show
     @team_submission = TeamSubmission.friendly.find(params.fetch(:id))
     @team = @team_submission.team
+
     render "team_submissions/published"
   end
 
   private
+
   def current_scope
-    'public'
+    "public"
   end
 
   def determine_manifest
