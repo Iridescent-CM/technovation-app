66819f6877769ed9ac036a83730907ff3d7a2e0c
Alexandre Conrado <alexkonrad.dev@gmail.com>
2022-05-23 13:59:36 -0300

    Adjusts Navbar responsivity

--- app/javascript/new_registration/components/Banner.vue
+++ app/javascript/new_registration/components/Banner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tg-green-stripes rubik h-36 flex items-center mb-20">
+  <div class="w-full tg-green-stripes rubik h-36 flex items-center mb-20">
     <div class="text-white sm:ml-8 lg:ml-72 text-7xl font-black">PROFILE SETUP</div>
   </div>
 </template>
--- app/javascript/new_registration/components/Navbar.vue
+++ app/javascript/new_registration/components/Navbar.vue
@@ -1,68 +1,93 @@
 <template>
+  <div>
   <nav class="bg-white">
-    <div class="max-w-7xl mx-auto px-1">
-      <div class="flex justify-between">
-          <div>
+      <!--// Desktop Navbar //-->
+      <div class="hidden md:flex max-w-7xl mx-auto px-1">
+        <div class="flex justify-between">
+            <div>
+              <a href="/" class="flex items-center py-4 px-2">
+                <img id="tg-logo" :src="require('tg-girls-logo.png')" alt="Technovation Girls Logo" />
+              </a>
+            </div>
+          <div class="flex space-x-7">
+            <div class="md:flex items-center space-x-1">
+              <a href="https://technovationchallenge.org/get-started/" class="registration-nav">Get Started</a>
+              <a href="https://technovationchallenge.org/" class="registration-nav">Chapters</a>
+              <a href="https://www.technovation.org/about/" class="registration-nav">About</a>
+              <a href="/login" class="registration-nav">Login</a>
+              <a href="/signup" class="registration-nav">Sign Up</a>
+              <a href="https://www.technovation.org/donate-today/" class="py-2 px-2 text-white font-black 
+              rounded transition duration-300 bg-energetic-blue">DONATE</a>
+            </div>
+          </div>
+
+          <div class="md:hidden flex items-center">
+            <button class="outline-none mobile-menu-button">
+              <svg class=" w-6 h-6 text-gray-500 hover:text-green-500 "
+                  x-show="!showMenu"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+              >
+                <path d="M4 6h16M4 12h16M4 18h16"></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <!--// Mob Navbar //-->
+      <div class="sm:flex md:hidden">
+        <div class="block w-full">
+          <div class="items-start float-left">
             <a href="/" class="flex items-center py-4 px-2">
               <img id="tg-logo" :src="require('tg-girls-logo.png')" alt="Technovation Girls Logo" />
             </a>
           </div>
-        <div class="flex space-x-7">
-          <div class="md:flex items-center space-x-1">
-            <a href="https://technovationchallenge.org/get-started/" class="registration-nav">Get Started</a>
-            <a href="https://technovationchallenge.org/" class="registration-nav">Chapters</a>
-            <a href="https://www.technovation.org/about/" class="registration-nav">About</a>
-            <a href="/login" class="registration-nav">Login</a>
-            <a href="/signup" class="registration-nav">Sign Up</a>
-            <a href="https://www.technovation.org/donate-today/" class="py-2 px-2 text-white font-black 
-            rounded transition duration-300 bg-energetic-blue">DONATE</a>
+
+          <div class="items-end float-right">
+            <button class="outline-none mobile-menu-button py-4 px-2" @click="() => toogleMenu()">
+              <svg class=" w-6 h-6 text-gray-500 hover:text-green-500 "
+                  x-show="!showMenu"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+              >
+                <path d="M4 6h16M4 12h16M4 18h16"></path>
+              </svg>
+            </button>
           </div>
         </div>
-
-        <div class="md:hidden flex items-center">
-          <button class="outline-none mobile-menu-button">
-            <svg class=" w-6 h-6 text-gray-500 hover:text-green-500 "
-                 x-show="!showMenu"
-                 fill="none"
-                 stroke-linecap="round"
-                 stroke-linejoin="round"
-                 stroke-width="2"
-                 viewBox="0 0 24 24"
-                 stroke="currentColor"
-            >
-              <path d="M4 6h16M4 12h16M4 18h16"></path>
-            </svg>
-          </button>
-        </div>
       </div>
-    </div>
-
-    <div class="hidden mobile-menu">
-      <ul class="">
-        <li><a href="" class="registration-nav-mobile">Get Started</a></li>
-        <li><a href="" class="registration-nav-mobile">Chapters</a></li>
-        <li><a href="" class="registration-nav-mobile">About</a></li>
-        <li><a href="" class="registration-nav-mobile">Login</a></li>
-        <li><a href="" class="registration-nav-mobile">Signup</a></li>
-        <li><a href="" class="registration-nav-mobile">DONATE</a></li>
+    </nav>
+    
+    <div class="w-full flex items-center">
+      <ul class="hidden mobile-menu w-full">
+        <li><a href="https://technovationchallenge.org/get-started/" class="registration-nav-mobile">Get Started</a></li>
+        <li><a href="https://www.technovation.org/about/" class="registration-nav-mobile">About</a></li>
+        <li><a href="/login" class="registration-nav-mobile">Login</a></li>
+        <li><a href="/signup" class="registration-nav-mobile">Sign Up</a></li>
+        <li><a href="https://www.technovation.org/donate-today/" class="registration-nav-mobile">DONATE</a></li>
       </ul>
     </div>
-  </nav>
-
+  </div>
 </template>
 
 <script>
 export default {
   name: "Navbar",
-  // mounted() {
-  //   const btn = document.querySelector("button.mobile-menu-button");
-  //   const menu = document.querySelector(".mobile-menu");
-  //
-  //   // Add Event Listeners
-  //   btn.addEventListener("click", () => {
-  //     menu.classList.toggle("hidden");
-  //   });
-  // }
+  methods: {
+    toogleMenu() {
+      const menu = document.querySelector(".mobile-menu");
+      menu.classList.toggle("hidden");
+    }
+  }
 };
 </script>
 
