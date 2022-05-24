94e3578e164294a543b3bd13419e21559e347bcf
Alexandre Conrado <alexkonrad.dev@gmail.com>
2022-05-24 12:37:58 -0300

    Reverting Thunkable URL validation

--- app/validators/thunkable_share_url_validator.rb
+++ app/validators/thunkable_share_url_validator.rb
@@ -1,6 +1,6 @@
 class ThunkableShareUrlValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    if !value.match(/^https?:\/\/x.thunkable.com\/.+$/)
+    if !value.match(/^https?:\/\/x.thunkable.com\/projects\/.+$/)
       record.errors.add(attribute, :invalid)
     end
   end
