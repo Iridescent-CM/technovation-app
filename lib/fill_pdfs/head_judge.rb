e10b5bd5b4bc0296f95fce8da5fa1377f9130131
shaun <58957909+shaunxp20@users.noreply.github.com>
2022-06-29 18:07:13 -0600

    Update 2022 certificates
    The certificates needed to be updated/saved again. I also had to create
    or update `fill_text` methods for all of the certificates.
    
    Refs: #3471

---
+++ lib/fill_pdfs/head_judge.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+module FillPdfs
+  class HeadJudge
+    include FillPdfs
+
+    def full_text
+      "For their dedication to providing valuable feedback on 6 to 10 submissions as a Head Judge."
+    end
+  end
+end
---
+++ lib/fill_pdfs/judge_advisor.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+module FillPdfs
+  class JudgeAdvisor
+    include FillPdfs
+
+    def full_text
+      "For their dedication to providing valuable feedback on 11+ submissions as a Judge Advisor."
+    end
+  end
+end
