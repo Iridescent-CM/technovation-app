cfa7fe4279b02dbf550a0df921e8b7561d54d2f8
Vivian Canales <vcnales@gmail.com>
2022-07-21 11:50:59 -0500

    Added testing spec for judging open splash

---
+++ spec/features/admin/judging_open_splash_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.feature "Judging open splash page" do
+  scenario "Judging is set to QF and judge registration is enabled" do
+    SeasonToggles.set_judging_round(:qf)
+    SeasonToggles.enable_signup(:judge)
+
+    visit root_path
+    expect(page).not_to have_css("#registration-landing")
+    expect(page).to have_content("Registration is currently open for judges")
+    expect(page).to have_content("Registration is currently closed for students and mentors")
+  end
+
+  scenario "Judging is set to QF and judge registration is enabled" do
+    SeasonToggles.set_judging_round(:qf)
+    SeasonToggles.disable_signups!
+
+    visit root_path
+    expect(page).not_to have_css("#registration-landing")
+    expect(page).to have_content("Judging is open")
+    expect(page).to have_content("Registration is currently closed for students and mentors")
+  end
+end
