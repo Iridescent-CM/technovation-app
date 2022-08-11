33cd9ff73fd5acfcafae4a55eddb8ff1b05da651
Vivian Canales <vcnales@gmail.com>
2022-08-11 15:32:04 -0500

    Updated logic for return message once demo and video links have been validated

--- app/controllers/concerns/team_submission_video_link_review_controller.rb
+++ app/controllers/concerns/team_submission_video_link_review_controller.rb
@@ -2,7 +2,6 @@ module TeamSubmissionVideoLinkReviewController
   extend ActiveSupport::Concern
 
   def update
-    msg = {success: "You saved your #{params[:piece].humanize}!"}
     submission = current_profile.team_submissions.current.friendly.find(video_link_params[:id])
 
     if submission.update({video_link_params[:piece] => video_link_params[:value]})
@@ -12,10 +11,13 @@ module TeamSubmissionVideoLinkReviewController
         parameters: {piece: video_link_params[:piece]},
         recipient: submission
       )
-    elsif submission.errors.full_messages
-      msg = {alert: submission.errors.full_messages.join(" , ")}
+      msg = {success: "You saved your #{params[:piece].humanize}!"}
     else
-      msg = {alert: "An error has occurred. Please try again later."}
+      msg = if submission.errors.full_messages
+        {alert: submission.errors.full_messages.join(" , ")}
+      else
+        {alert: "An error has occurred. Please try again later."}
+      end
     end
 
     redirect_to send("#{current_scope}_team_submission_section_path",  id: submission.to_param, section: :pitch), msg
