865dd23be115f6b610ad6a3213d58c65cdd04479
Vivian Canales <vcnales@gmail.com>
2022-09-14 16:41:30 -0500

    Changed scenario description for mentors connecting with mentors for improved clarity

--- spec/features/mentor/connect_with_mentors_spec.rb
+++ spec/features/mentor/connect_with_mentors_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.feature "Mentors connect with other mentors" do
-  scenario "A mentor has finished onboarding and views their profile for the first time" do
+  scenario "A newly onboarded mentor is not searchable by other mentors by default" do
     mentor = FactoryBot.create(:mentor, :onboarded)
 
     sign_in(mentor)
