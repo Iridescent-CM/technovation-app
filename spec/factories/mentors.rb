92eba13e532492105fdba3a42cd0edfbda1a3e85
Vivian Canales <vcnales@gmail.com>
2022-09-14 14:58:22 -0500

    Added connected_with_mentors trait to account for DB change for connect_with_mentors

--- spec/factories/mentors.rb
+++ spec/factories/mentors.rb
@@ -36,6 +36,10 @@ FactoryBot.define do
       training_completed_at { nil }
     end
 
+    trait :connected_with_mentors do
+      connect_with_mentors { true }
+    end
+
     trait :chicago do
       city { "Chicago" }
       state_province { "IL" }
