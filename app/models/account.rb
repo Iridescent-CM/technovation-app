ddaf9f2cb43acb26682431c5897e3259066ff434
Vivian Canales <vcnales@gmail.com>
2022-09-20 15:56:45 -0500

    Cleaned up profile_image_url method

--- app/models/account.rb
+++ app/models/account.rb
@@ -693,26 +693,14 @@ class Account < ActiveRecord::Base
     )
   end
 
-  def default_image_url
-    "https://placekitten.com/300/300"
-  end
-
-  def s3_profile_image_url(profile_image)
-    "https://s3.amazonaws.com/#{ENV.fetch("AWS_BUCKET_NAME")}/uploads/account/profile_image/#{id}/#{profile_image}"
-  end
-
-  def determine_profile_image_url(profile_image)
+  def profile_image_url
     if profile_image.blank?
-      default_image_url
+      "https://s3.amazonaws.com/#{ENV.fetch("AWS_BUCKET_NAME")}/placeholders/1.svg"
     elsif profile_image.include? "filestackcontent"
       profile_image
     else
-      s3_profile_image_url(profile_image)
+      "https://s3.amazonaws.com/#{ENV.fetch("AWS_BUCKET_NAME")}/uploads/account/profile_image/#{id}/#{profile_image}"
     end
-  end
-
-  def profile_image_url
-    icon_path.blank? ? determine_profile_image_url(profile_image) : icon_path
   end
 
   def can_be_a_mentor?(**options)
