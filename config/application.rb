7a418f445813fc466ca43d23f6e5269707d83967
shaun <58957909+shaunxp20@users.noreply.github.com>
2022-10-31 14:22:46 -0600

    Update demo video to technical video
    This will update all instances of "demo video" to "technical video" on
    the Rails side.
    
    Refs: #3621

--- config/application.rb
+++ config/application.rb
@@ -29,9 +29,8 @@ module TechnovationApp
 
     config.autoload_paths << Rails.root.join('app', 'models', '**/')
     config.autoload_paths << Rails.root.join("app/models/data_analyses")
-    
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
     config.i18n.default_locale = :en
     config.i18n.fallbacks = [:en]
     config.i18n.available_locales = [:en, :'es-MX']
