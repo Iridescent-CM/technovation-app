60931fe9bb3da0ffff91267d509c9e6beff6193f
Vivian Canales <vcnales@gmail.com>
2022-11-02 12:53:09 -0500

    Removed screenshot processor & uploader from screenshot model

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -1,8 +1,6 @@
 class Screenshot < ActiveRecord::Base
   belongs_to :team_submission, touch: true
 
-  mount_uploader :image, ScreenshotProcessor
-
   before_create -> {
     self.sort_position = (self.class.maximum(:sort_position) || -1) + 1
   }
