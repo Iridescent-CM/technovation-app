df6dd8cff21330428a3c362cd93552b5ff7b09f8
Vivian Canales <vcnales@gmail.com>
2022-11-02 12:53:29 -0500

    added image_url method to determine correct url for screenshots

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -8,4 +8,13 @@ class Screenshot < ActiveRecord::Base
   def self.persisted
     select(&:persisted?)
   end
+
+  def image_url
+    if image.include?("filestackcontent")
+      image
+    else
+      # This only applies to the old submission screenshots that were uploaded to S3 before 2023 season
+      "https://s3.amazonaws.com/#{ENV.fetch("AWS_BUCKET_NAME")}/uploads/screenshot/image/#{id}/#{image}"
+    end
+  end
 end
