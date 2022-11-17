a646a3f83c9c1eb468a6d74360ecc28cbe824827
shaun <58957909+shaunxp20@users.noreply.github.com>
2022-11-16 17:39:27 -0700

    Update sidekiq config
    Seeing if this will fix QA.
    
    https://stackoverflow.com/questions/66246528/herkou-redis-certificate-verify-failed-self-signed-certificate-in-certificate

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,6 +4,8 @@ Sidekiq.default_worker_options = {
 }
 
 Sidekiq.configure_server do |config|
+  config.redis = {ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE}}
+
   if database_url = ENV['DATABASE_URL']
     pool = ENV.fetch("SIDEKIQ_DB_POOL_SIZE") { 25 }
     ActiveRecord::Base.establish_connection "#{database_url}?pool=#{pool}"
