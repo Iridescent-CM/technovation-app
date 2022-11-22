1a647771ca2914c0b3b10bb9b748ed686b287757
Vivian Canales <vcnales@gmail.com>
2022-11-22 09:44:32 -0600

    Updated path to rebrand published view

--- app/controllers/student/published_team_submissions_controller.rb
+++ app/controllers/student/published_team_submissions_controller.rb
@@ -3,7 +3,7 @@ module Student
     def show
       @team_submission = current_team.submission
       @team = current_team
-      render "team_submissions/published"
+      render "team_submissions/rebrand/published"
     end
   end
 end
