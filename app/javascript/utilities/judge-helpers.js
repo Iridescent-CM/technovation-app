9d2e60f4a800477c2b4f3b5cf3f9b225eb3e57bb
shaun <58957909+shaunxp20@users.noreply.github.com>
2022-12-15 15:52:26 -0700

    Update beginner/jr/sr judging rubric links
    Refs: #3745

--- app/javascript/utilities/judge-helpers.js
+++ app/javascript/utilities/judge-helpers.js
@@ -1,11 +1,11 @@
 function getJudgingRubricLink(division){
     switch (division) {
         case 'senior':
-            return "https://technovationchallenge.org/wp-content/uploads/2021/09/Senior-Rubric-2022-Season-FINAL-9-14-21.pdf"
+            return "https://technovationchallenge.org/wp-content/uploads/2022/09/Senior-Rubric-2023-Season-FINAL.pdf"
         case 'junior':
-            return "https://technovationchallenge.org/wp-content/uploads/2021/09/Junior-Rubric-2022-Season-FINAL-9-14-21.pdf"
+            return "https://technovationchallenge.org/wp-content/uploads/2022/09/Junior-Rubric-2023-Season-FINAL.pdf"
         case 'beginner':
-            return "https://technovationchallenge.org/wp-content/uploads/2021/09/Beginner-Rubric-2022-Season-FINAL-9-14-21.pdf"
+            return "https://technovationchallenge.org/wp-content/uploads/2022/09/Beginner-Rubric-2023-Season-FINAL.pdf"
         default:
             return "https://technovationchallenge.org/curriculum/judging-rubric/"
     }
