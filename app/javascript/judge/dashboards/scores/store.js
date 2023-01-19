4e682466c6894f2706fb3830c05df828444820ce
Vivian Canales <vcnales@gmail.com>
2023-01-19 16:38:29 -0600

    WIP for assigned submission grid for RPEs

--- app/javascript/judge/dashboards/scores/store.js
+++ app/javascript/judge/dashboards/scores/store.js
@@ -1,11 +1,11 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    currentRound: 'qf',
+    currentRound: "qf",
 
     scores: {
       finished: {
@@ -21,57 +21,63 @@ export default new Vuex.Store({
 
     submissions: {},
 
-    deadline: '',
+    deadline: "",
   },
 
   getters: {
-    finishedScores (state) {
-      return state.scores.finished[state.currentRound].map(score => (
-        { ...JSON.parse(score).data.attributes }
-      ))
+    finishedScores(state) {
+      return state.scores.finished[state.currentRound].map((score) => ({
+        ...JSON.parse(score).data.attributes,
+      }));
     },
 
-    quarterFinalsScores (state) {
-      return state.scores.incomplete.qf.map(score => (
-        { ...JSON.parse(score).data.attributes }
-      ))
+    quarterFinalsScores(state) {
+      return state.scores.incomplete.qf.map((score) => ({
+        ...JSON.parse(score).data.attributes,
+      }));
     },
 
-    finishedQuarterfinalsScores (state) {
-      return state.scores.finished.qf.map(score => (
-        { ...JSON.parse(score).data.attributes }
-      ))
+    finishedQuarterfinalsScores(state) {
+      return state.scores.finished.qf.map((score) => ({
+        ...JSON.parse(score).data.attributes,
+      }));
     },
 
-    semiFinalsScores (state) {
-      return state.scores.incomplete.sf.map(score => (
-        { ...JSON.parse(score).data.attributes }
-      ))
+    semiFinalsScores(state) {
+      return state.scores.incomplete.sf.map((score) => ({
+        ...JSON.parse(score).data.attributes,
+      }));
     },
 
-    finishedSemifinalsScores (state) {
-      return state.scores.finished.sf.map(score => (
-        { ...JSON.parse(score).data.attributes }
-      ))
+    finishedSemifinalsScores(state) {
+      return state.scores.finished.sf.map((score) => ({
+        ...JSON.parse(score).data.attributes,
+      }));
     },
 
-    assignedSubmissions (state) {
-      return state.submissions
+    assignedSubmissions(state) {
+      return state.submissions;
+    },
+
+    hasSubmissionsToStart(state) {
+      return state.submissions.filter(
+        (submission) => !submission.score_started
+      );
     },
   },
 
   mutations: {
-    populateScores (state, payload) {
-      state.scores = payload
-      state.currentRound = payload.current_round
+    populateScores(state, payload) {
+      state.scores = payload;
+      state.currentRound = payload.current_round;
     },
 
-    populateSubmissions (state, payload) {
-      state.submissions = payload
+    populateSubmissions(state, payload) {
+      state.submissions = payload;
     },
 
-    deadline (state, date) {
-      state.deadline = date
+    deadline(state, date) {
+      state.deadline = date;
     },
   },
-})
+});
