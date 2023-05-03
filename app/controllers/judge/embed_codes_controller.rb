c575a167b957eb0d5e070a7d8996e9347d894efc
shaun <58957909+shaun-technovation@users.noreply.github.com>
2023-05-03 15:28:45 -0600

    Add new video clicked tracking functionality
    For some reason our existing video tracking isn't working, I have a
    hunch it's b/c the pop-up modal is intercepting/blocking the click that
    makes the request to the back-end to update the video tracking fields
    in the database. Anywho, I added some new functionality that doesn't
    require an extra HTTP request but instead hooks into the "embeds"
    controller, and looks at what is being embedded (either the pitch or
    demo/technical video) and updates the tracking field (for the specific
    submission score) in the database accordingly.
    
    Refs: #3971

--- app/controllers/judge/embed_codes_controller.rb
+++ app/controllers/judge/embed_codes_controller.rb
@@ -1,5 +1,20 @@
 module Judge
   class EmbedCodesController < JudgeController
     include EmbedCodeController
+
+    after_action :update_clicked_video_tracking, only: :show
+
+    private
+
+    def update_clicked_video_tracking
+      submission_score = current_judge.submission_scores.find_by(team_submission_id: @team_submission.id)
+
+      case params[:piece]
+      when "pitch"
+        submission_score.update(clicked_pitch_video: true)
+      when "demo"
+        submission_score.update(clicked_demo_video: true)
+      end
+    end
   end
 end
