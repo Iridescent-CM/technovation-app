6be178fe6c5aedbddeb05035634daae6dfff659d
shaun <58957909+shaun-technovation@users.noreply.github.com>
2023-05-05 11:32:59 -0600

    Fix mentors removing students tests
    Refs: #3851

--- spec/features/mentor/remove_onboarding_student_spec.rb
+++ spec/features/mentor/remove_onboarding_student_spec.rb
@@ -1,12 +1,12 @@
 require "rails_helper"
 
-RSpec.xfeature "Remove an onboarding student" do
-  scenario "remove the student" do
-    student = FactoryBot.create(:student, :onboarding)
+RSpec.feature "Metors removing students from a team" do
+  scenario "Removing an onboarding student" do
+    onboarding_student = FactoryBot.create(:onboarding_student, :onboarding)
     mentor = FactoryBot.create(:mentor, :onboarded)
     team = FactoryBot.create(:team)
 
-    TeamRosterManaging.add(team, [student, mentor])
+    TeamRosterManaging.add(team, [onboarding_student, mentor])
 
     sign_in(mentor)
     within("#find-team") { click_link team.name }
@@ -16,6 +16,6 @@ RSpec.xfeature "Remove an onboarding student" do
     end
 
     expect(team.reload.mentors).to include(mentor)
-    expect(team.students).not_to include(student)
+    expect(team.students).not_to include(onboarding_student)
   end
 end
