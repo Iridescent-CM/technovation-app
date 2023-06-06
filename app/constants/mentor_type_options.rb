d99ffd137df4a8cfd410438e750daac9619bc595
shaun <58957909+shaun-technovation@users.noreply.github.com>
2023-06-06 06:54:34 -0600

    Convert MENTOR_TYPE_OPTIONS to a hash
    This makes it clearer what value/index is associated with each mentor
    type.
    
    Refs: #3937

--- app/constants/mentor_type_options.rb
+++ app/constants/mentor_type_options.rb
@@ -1,9 +1,9 @@
 # If these change, you will need to update various TC dataclips on Heroku.
 # Alternatively, these could be moved to a table in the database and the
 # the dataclips could just be updated once to relfect the new table.
-MENTOR_TYPE_OPTIONS = %w{
-  Industry\ professional
-  Educator
-  Parent
-  Past\ Technovation\ student
+MENTOR_TYPE_OPTIONS = {
+  "Industry professional" => 0,
+  "Educator" => 1,
+  "Parent" => 2,
+  "Past Technovation student" => 3
 }
