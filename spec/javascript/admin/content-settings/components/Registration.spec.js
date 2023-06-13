9a3ddb4b40cfe52a3273d8db2e52f6ded5d265ef
Vivian Canales <vcnales@gmail.com>
2023-06-13 09:28:15 -0500

    Updated JS test

--- spec/javascript/admin/content-settings/components/Registration.spec.js
+++ spec/javascript/admin/content-settings/components/Registration.spec.js
@@ -1,198 +1,189 @@
-import Vuex from 'vuex'
-import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from "vuex";
+import { shallowMount, createLocalVue } from "@vue/test-utils";
 
-import mockStore from 'admin/content-settings/store/__mocks__'
-import Registration from 'admin/content-settings/components/Registration'
-import Icon from 'components/Icon'
+import mockStore from "admin/content-settings/store/__mocks__";
+import Registration from "admin/content-settings/components/Registration";
+import Icon from "components/Icon";
 
-const localVue = createLocalVue()
-localVue.use(Vuex)
+const localVue = createLocalVue();
+localVue.use(Vuex);
 
-describe('Admin Content & Settings - Registration component', () => {
-
-  let wrapper
+describe("Admin Content & Settings - Registration component", () => {
+  let wrapper;
 
   beforeEach(() => {
-    wrapper = shallowMount(
-      Registration,
-      {
-        localVue,
-        store: mockStore.createMocks().store,
-      }
-    )
-  })
+    wrapper = shallowMount(Registration, {
+      localVue,
+      store: mockStore.createMocks().store,
+    });
+  });
 
-  it('has a name attribute', () => {
-    expect(Registration.name).toEqual('registration-section')
-  })
+  it("has a name attribute", () => {
+    expect(Registration.name).toEqual("registration-section");
+  });
 
-  describe('data', () => {
-
-    it('contains the correct default data', () => {
+  describe("data", () => {
+    it("contains the correct default data", () => {
       expect(Registration.data()).toEqual({
         checkboxes: {
-          student: 'Students',
-          mentor: 'Mentors',
-          judge: 'Judges',
+          student: "Students",
+          mentor: "Mentors",
+          judge: "Judges",
         },
-      })
-    })
+      });
+    });
+  });
 
-  })
+  describe("markup", () => {
+    it("contains the proper HTML based on data", () => {
+      const studentCheckbox = wrapper.find("#season_toggles_student_signup");
+      const studentCheckboxLabel = wrapper.find(
+        'label[for="season_toggles_student_signup"]'
+      );
+      const mentorCheckbox = wrapper.find("#season_toggles_mentor_signup");
+      const mentorCheckboxLabel = wrapper.find(
+        'label[for="season_toggles_mentor_signup"]'
+      );
+      const judgeCheckbox = wrapper.find("#season_toggles_judge_signup");
+      const judgeCheckboxLabel = wrapper.find(
+        'label[for="season_toggles_judge_signup"]'
+      );
 
-  describe('markup', () => {
+      expect(wrapper.vm.judgingEnabled).toBe(false);
 
-    it('contains the proper HTML based on data', () => {
-      const studentCheckbox = wrapper.find('#season_toggles_student_signup')
-      const studentCheckboxLabel = wrapper
-        .find('label[for="season_toggles_student_signup"]')
-      const mentorCheckbox = wrapper.find('#season_toggles_mentor_signup')
-      const mentorCheckboxLabel = wrapper
-        .find('label[for="season_toggles_mentor_signup"]')
-      const judgeCheckbox = wrapper.find('#season_toggles_judge_signup')
-      const judgeCheckboxLabel = wrapper
-        .find('label[for="season_toggles_judge_signup"]')
+      expect(studentCheckbox.exists()).toBe(true);
+      expect(studentCheckboxLabel.exists()).toBe(true);
+      expect(mentorCheckbox.exists()).toBe(true);
+      expect(mentorCheckboxLabel.exists()).toBe(true);
+      expect(judgeCheckbox.exists()).toBe(true);
+      expect(judgeCheckboxLabel.exists()).toBe(true);
+    });
 
-      expect(wrapper.vm.judgingEnabled).toBe(false)
-
-      expect(studentCheckbox.exists()).toBe(true)
-      expect(studentCheckboxLabel.exists()).toBe(true)
-      expect(mentorCheckbox.exists()).toBe(true)
-      expect(mentorCheckboxLabel.exists()).toBe(true)
-      expect(judgeCheckbox.exists()).toBe(true)
-      expect(judgeCheckboxLabel.exists()).toBe(true)
-    })
-
-    it('disables checkboxes and sets values to 0 is judging is enabled', () => {
-      wrapper = shallowMount(
-        Registration,
-        {
-          localVue,
-          store: mockStore.createMocks({
-            getters: {
-              judgingEnabled: () => {
-                return true
-              },
+    it("disables checkboxes and sets values to 0 is judging is enabled", () => {
+      wrapper = shallowMount(Registration, {
+        localVue,
+        store: mockStore.createMocks({
+          getters: {
+            judgingEnabled: () => {
+              return true;
             },
-          }).store,
-        }
-      )
+          },
+        }).store,
+      });
 
-      const studentCheckbox = wrapper.find('#season_toggles_student_signup')
-      const studentCheckboxLabel = wrapper
-        .find('label[for="season_toggles_student_signup"]')
-      const mentorCheckbox = wrapper.find('#season_toggles_mentor_signup')
-      const mentorCheckboxLabel = wrapper
-        .find('label[for="season_toggles_mentor_signup"]')
+      const studentCheckbox = wrapper.find("#season_toggles_student_signup");
+      const studentCheckboxLabel = wrapper.find(
+        'label[for="season_toggles_student_signup"]'
+      );
+      const mentorCheckbox = wrapper.find("#season_toggles_mentor_signup");
+      const mentorCheckboxLabel = wrapper.find(
+        'label[for="season_toggles_mentor_signup"]'
+      );
 
-      expect(wrapper.vm.judgingEnabled).toBe(true)
+      expect(wrapper.vm.judgingEnabled).toBe(true);
 
       expect(studentCheckbox.attributes()).toEqual(
         expect.objectContaining({
-          id: 'season_toggles_student_signup',
-          type: 'checkbox',
-          disabled: 'disabled',
-          value: '0',
+          id: "season_toggles_student_signup",
+          type: "checkbox",
+          disabled: "disabled",
+          value: "0",
         })
-      )
+      );
 
       expect(studentCheckboxLabel.attributes()).toEqual(
         expect.objectContaining({
-          class: 'label--disabled',
+          class: "label--disabled",
         })
-      )
+      );
 
       expect(mentorCheckbox.attributes()).toEqual(
         expect.objectContaining({
-          id: 'season_toggles_mentor_signup',
-          type: 'checkbox',
-          disabled: 'disabled',
-          value: '0',
+          id: "season_toggles_mentor_signup",
+          type: "checkbox",
+          disabled: "disabled",
+          value: "0",
         })
-      )
+      );
 
       expect(mentorCheckboxLabel.attributes()).toEqual(
         expect.objectContaining({
-          class: 'label--disabled',
+          class: "label--disabled",
         })
-      )
-    })
+      );
+    });
 
-    it('allows judge registraitons when juding is enabled', () => {
-      wrapper = shallowMount(
-        Registration,
-        {
-          localVue,
-          store: mockStore.createMocks({
-            getters: {
-              judgingEnabled: () => {
-                return true
-              },
+    it("allows judge registraitons when juding is enabled", () => {
+      wrapper = shallowMount(Registration, {
+        localVue,
+        store: mockStore.createMocks({
+          getters: {
+            judgingEnabled: () => {
+              return true;
             },
-          }).store,
-        }
-      )
+            isSuperAdmin: () => {
+              return true;
+            },
+          },
+        }).store,
+      });
 
-      const judgeCheckbox = wrapper.find('#season_toggles_judge_signup')
-      const judgeCheckboxLabel = wrapper
-        .find('label[for="season_toggles_judge_signup"]')
+      const judgeCheckbox = wrapper.find("#season_toggles_judge_signup");
+      const judgeCheckboxLabel = wrapper.find(
+        'label[for="season_toggles_judge_signup"]'
+      );
 
-      expect(wrapper.vm.judgingEnabled).toBe(true)
+      expect(wrapper.vm.judgingEnabled).toBe(true);
 
       expect(judgeCheckbox.attributes()).toEqual(
         expect.objectContaining({
-          id: 'season_toggles_judge_signup',
-          type: 'checkbox',
+          id: "season_toggles_judge_signup",
+          type: "checkbox",
         })
-      )
+      );
 
       expect(judgeCheckbox.attributes()).not.toEqual(
         expect.objectContaining({
-          id: 'season_toggles_judge_signup',
-          type: 'checkbox',
-          disabled: 'disabled',
+          id: "season_toggles_judge_signup",
+          type: "checkbox",
+          disabled: "disabled",
         })
-      )
+      );
 
       expect(judgeCheckboxLabel.attributes()).not.toEqual(
         expect.objectContaining({
-          class: 'label--disabled',
+          class: "label--disabled",
         })
-      )
-    })
+      );
+    });
 
-    it('displays warning notices if judging is enabled', () => {
-      wrapper = shallowMount(
-        Registration,
-        {
-          localVue,
-          store: mockStore.createMocks({
-            getters: {
-              judgingEnabled: () => {
-                return true
-              },
+    it("displays warning notices if judging is enabled", () => {
+      wrapper = shallowMount(Registration, {
+        localVue,
+        store: mockStore.createMocks({
+          getters: {
+            judgingEnabled: () => {
+              return true;
             },
-          }).store,
-        }
-      )
+          },
+        }).store,
+      });
 
-      const notices = wrapper.findAll('.notice')
+      const notices = wrapper.findAll(".notice");
 
-      expect(wrapper.vm.judgingEnabled).toBe(true)
-      expect(notices.length).toEqual(2)
+      expect(wrapper.vm.judgingEnabled).toBe(true);
+      expect(notices.length).toEqual(2);
       notices.wrappers.forEach((notice) => {
-        const props = notice.find(Icon).props()
+        const props = notice.find(Icon).props();
 
         expect(props).toEqual(
           expect.objectContaining({
-            name: 'exclamation-circle',
+            name: "exclamation-circle",
             size: 16,
-            color: '00529B',
+            color: "00529B",
           })
-        )
-      })
-    })
-
-  })
-
-})
+        );
+      });
+    });
+  });
+});
--- spec/javascript/admin/content-settings/store/index.spec.js
+++ spec/javascript/admin/content-settings/store/index.spec.js
@@ -1,16 +1,15 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-import state from 'admin/content-settings/store/state'
-import getters from 'admin/content-settings/store/getters'
-import mutations from 'admin/content-settings/store/mutations'
-import actions from 'admin/content-settings/store/actions'
+import state from "admin/content-settings/store/state";
+import getters from "admin/content-settings/store/getters";
+import mutations from "admin/content-settings/store/mutations";
+import actions from "admin/content-settings/store/actions";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-describe('Admin Content & Settings - Vuex store', () => {
-
-  let store
+describe("Admin Content & Settings - Vuex store", () => {
+  let store;
 
   beforeEach(() => {
     store = new Vuex.Store({
@@ -18,53 +17,55 @@ describe('Admin Content & Settings - Vuex store', () => {
       getters,
       mutations,
       actions,
-    })
-  })
+    });
+  });
 
-  it('returns the initial state used by the AdminDashboard component', () => {
+  it("returns the initial state used by the AdminDashboard component", () => {
     expect(state).toEqual({
+      is_super_admin: false,
       student_signup: 0,
       mentor_signup: 0,
       judge_signup: 0,
       chapter_ambassador_signup: 0,
-      student_dashboard_text: '',
-      mentor_dashboard_text: '',
-      judge_dashboard_text: '',
-      chapter_ambassador_dashboard_text: '',
+      student_dashboard_text: "",
+      mentor_dashboard_text: "",
+      judge_dashboard_text: "",
+      chapter_ambassador_dashboard_text: "",
       student_survey_link: {
-        text: '',
-        url: '',
-        long_desc: '',
+        text: "",
+        url: "",
+        long_desc: "",
       },
       mentor_survey_link: {
-        text: '',
-        url: '',
-        long_desc: '',
+        text: "",
+        url: "",
+        long_desc: "",
       },
       team_building_enabled: 0,
       team_submissions_editable: 0,
       select_regional_pitch_event: 0,
-      judging_round: 'off',
+      judging_round: "off",
       display_scores: 0,
-    })
-  })
+    });
+  });
 
-  describe('getters', () => {
+  describe("getters", () => {
+    describe("judgingEnabled", () => {
+      it("returns false if judging is not enabled", () => {
+        expect(store.state.judging_round).toEqual("off");
+        expect(store.getters.judgingEnabled).toBe(false);
+      });
 
-    describe('judgingEnabled', () => {
-
-      it('returns false if judging is not enabled', () => {
-        expect(store.state.judging_round).toEqual('off')
-        expect(store.getters.judgingEnabled).toBe(false)
-      })
-
-      it('returns true if judging is enabled', () => {
-        store.state.judging_round = 'qf'
-        expect(store.state.judging_round).toEqual('qf')
-        expect(store.getters.judgingEnabled).toBe(true)
-      })
-
-    })
-
-  })
-})
+      it("returns true if judging is enabled", () => {
+        store.state.judging_round = "qf";
+        expect(store.state.judging_round).toEqual("qf");
+        expect(store.getters.judgingEnabled).toBe(true);
+      });
+    });
+    describe("isSuperAdmin", () => {
+      it("returns false if admin is not a super admin", () => {
+        expect(store.state.is_super_admin).toBe(false);
+      });
+    });
+  });
+});
