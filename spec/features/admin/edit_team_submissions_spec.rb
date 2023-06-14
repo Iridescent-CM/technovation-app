7977d4fd6af8afaa81b9daf1cd1f4a2f66d2527e
Vivian Canales <vcnales@gmail.com>
2023-06-14 17:33:27 -0500

    Updated test

--- spec/features/admin/edit_team_submissions_spec.rb
+++ spec/features/admin/edit_team_submissions_spec.rb
@@ -31,7 +31,7 @@ RSpec.feature "Toggling editable team submissions" do
         check "team_submission[integrity_affirmed]"
         click_button "Start now!"
 
-        expect(page).to have_css('.button', text: "Set your project's name")
+        expect(page).to have_css('.button', text: "Upload your team photo")
 
         visit mentor_dashboard_path
 
