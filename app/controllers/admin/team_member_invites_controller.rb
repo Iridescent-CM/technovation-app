2e6c1d48073124e4ab938fdf343f66e8c730b50b
shaun <58957909+shaun-technovation@users.noreply.github.com>
2023-06-23 09:41:01 -0600

    Update admin delete team member invite functionality
    This functionality was extracted to a Rails concern, so using that
    instead.
    
    Refs: #4037

--- app/controllers/admin/team_member_invites_controller.rb
+++ app/controllers/admin/team_member_invites_controller.rb
@@ -1,19 +1,5 @@
 module Admin
   class TeamMemberInvitesController < AdminController
-    def destroy
-      invite = TeamMemberInvite.find_by(invite_token: params.fetch(:id))
-
-      if invite.destroy
-        redirect_to(
-          admin_team_path(invite.team),
-          success: t("controllers.invites.destroy.success", name: invite.invitee_name)
-        )
-      else
-        redirect_to(
-          admin_team_path(invite.team),
-          notice: t("controllers.application.general_error")
-        )
-      end
-    end
+    include Admin::DeleteTeamMemberInviteConcern
   end
 end
