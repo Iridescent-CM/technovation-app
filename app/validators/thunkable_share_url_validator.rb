7a419ac21f4d764646ac3c8f5787a93a503810e7
shaun <58957909+shaun-technovation@users.noreply.github.com>
2023-07-05 15:45:42 -0600

    Update valid Thunkable URL format
    Refs: #4048

--- app/validators/thunkable_share_url_validator.rb
+++ app/validators/thunkable_share_url_validator.rb
@@ -1,6 +1,6 @@
 class ThunkableShareUrlValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    if !value.match(/^https?:\/\/x.thunkable.com\/projects\/.+$/)
+    if !value.match(/^https?:\/\/x.thunkable.com\/projectPage\/.+$/)
       record.errors.add(attribute, :invalid)
     end
   end
