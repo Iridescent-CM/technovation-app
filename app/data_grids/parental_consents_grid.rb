879af8e00fa75e892a57f97249a8304ddbf20c0b
shaun <58957909+shaun-technovation@users.noreply.github.com>
2023-08-03 12:01:49 -0600

    Feature: Add ability for admins to view uploaded paper parental consent forms
    This will add a new link/section to the admin area that will allow
    admins to view uploaded paper parental consent forms.
    
    Refs: #4126

---
+++ app/data_grids/parental_consents_grid.rb
@@ -0,0 +1,68 @@
+class ParentalConsentsGrid
+  include Datagrid
+
+  attr_accessor :admin
+
+  scope do
+    ParentalConsent.where.not(uploaded_at: nil)
+  end
+
+  column :student, mandatory: true, html: true do |parental_consent|
+    link_to(
+      parental_consent.student_profile&.account&.full_name,
+      admin_participant_path(parental_consent.student_profile&.account)
+    )
+  end
+
+  column :email, header: "Student Email Address", mandatory: true do |parental_consent|
+    parental_consent.student_profile&.account&.email
+  end
+
+  column :uploaded_at, header: "Uploaded On", mandatory: true do |parental_consent|
+    parental_consent.uploaded_at.strftime("%B %e, %Y %l:%M %p")
+  end
+
+  column :upload_approval_status, header: "Status", mandatory: true do |parental_consent|
+    parental_consent.upload_approval_status.capitalize
+  end
+
+  column :actions, mandatory: true, html: true do |parental_consent|
+    link_to(
+      "View",
+      parental_consent.uploaded_consent_form.url,
+      class: "button small",
+      target: :_blank,
+      data: {turbolinks: false}
+    )
+  end
+
+  column :upload_approved_at, header: "Approved On" do |parental_consent|
+    parental_consent.upload_approved_at&.strftime("%B %e, %Y %l:%M %p")
+  end
+
+  column :upload_rejected_at, header: "Rejected On" do |parental_consent|
+    parental_consent.upload_rejected_at&.strftime("%B %e, %Y %l:%M %p")
+  end
+
+  filter :upload_approval_status,
+    :enum,
+    header: "Status",
+    select: ParentalConsent::PAPER_CONSENT_UPLOAD_STATUSES.transform_keys(&:capitalize)
+
+  filter :season,
+    :enum,
+    select: (2015..Season.current.year).to_a.reverse,
+    filter_group: "selections",
+    html: {
+      class: "and-or-field"
+    },
+    multiple: true do |value, scope, grid|
+      scope.by_season(value)
+    end
+
+  column_names_filter(
+    header: "More columns",
+    filter_group: "more-columns",
+    multiple: true
+  )
+end
