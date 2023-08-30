22846ea60f069d9a0350e3efe127ba0b6cd31182
shaun <58957909+shaun-technovation@users.noreply.github.com>
2023-08-30 14:41:34 -0600

    Chore: Adjust consent upload functionality
    This will make it so that the consent form upload process depends all on
    the parental consent. If a parental consent is uploaded, a media consent
    can also be uploaded, but if a parental consent is not uploaded then a
    media consent won't be able to be uploaded.
    
    Refs: #4159

--- app/controllers/student/paper_consent_uploads_controller.rb
+++ app/controllers/student/paper_consent_uploads_controller.rb
@@ -4,9 +4,13 @@ module Student
       @parental_consent = current_student.parental_consent
       @media_consent = current_student.media_consent
 
-      if params[:parental_consent].blank?
-        redirect_to student_dashboard_path, error: "You forgot to upload a consent form. Please try again."
-      elsif @parental_consent.update(parental_consent_params) && @media_consent.update(media_consent_params)
+      if parental_consent_params[:uploaded_consent_form].blank?
+        redirect_to student_dashboard_path, error: "You forgot to upload your parental consent form. Please try again."
+      elsif @parental_consent.update(parental_consent_params)
+        if media_consent_params[:uploaded_consent_form].present?
+          @media_consent.update(media_consent_params)
+        end
+
         redirect_to student_dashboard_path, success: "Thank you for uploading your consent form, we will review it as soon as we can."
       else
         redirect_to student_dashboard_path, error: "The consent forms need to be an image or in PDF format, and less than #{PaperParentalConsentUploader::MAXIMUM_UPLOAD_FILE_SIZE_IN_MEGABYTES} MB. Please try again."
