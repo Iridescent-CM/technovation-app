f2e71d356f9d286af3ffeda5496ff70a5e393c75
Vivian Canales <vcnales@gmail.com>
2023-09-26 20:01:34 -0500

    Added job record creation for background check invitation sidekiq job

--- app/jobs/request_checkr_background_check_invitation_job.rb
+++ app/jobs/request_checkr_background_check_invitation_job.rb
@@ -1,6 +1,21 @@
 class RequestCheckrBackgroundCheckInvitationJob < ActiveJob::Base
   queue_as :default
 
+  before_enqueue do |job|
+    profile = job.arguments[0][:candidate]
+
+    Job.create!(
+      job_id: job.job_id,
+      status: "queued",
+      owner: profile
+    )
+  end
+
+  after_perform do |job|
+    db_job = Job.find_by(job_id: job.job_id)
+    db_job.update_column(:status, "complete")
+  end
+
   def perform(candidate:)
     CheckrApiClient::ApiClient.new.request_checkr_invitation(candidate: candidate)
   end
