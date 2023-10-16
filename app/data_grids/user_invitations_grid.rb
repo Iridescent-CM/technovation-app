7f803cbb3a17647f59d0de9b03c3b9333ec0fbe1
shaun <58957909+shaun-technovation@users.noreply.github.com>
2023-10-16 12:04:17 -0600

    Chore: Add additional registration invite columns to CSV export
    This will add the following columns to the registration invites CSV
    export:
    
    - Invitee's name
    - Invitee's email address
    - Invited by name
    - Account Id (if they registered)
    
    Refs: #4241

--- app/data_grids/user_invitations_grid.rb
+++ app/data_grids/user_invitations_grid.rb
@@ -22,6 +22,14 @@ class UserInvitationsGrid
     end
   end
 
+  column :email, header: "Invitee's Name", mandatory: true, html: false do |registration_invite|
+    registration_invite.name
+  end
+
+  column :email, header: "Invitee's Email Address", mandatory: true, html: false do |registration_invite|
+    registration_invite.email
+  end
+
   column :status, mandatory: true do |registration_invite|
     registration_invite.status.capitalize
   end
@@ -37,15 +45,25 @@ class UserInvitationsGrid
     end
   end
 
-  column :invited_by, header: "Invited By", mandatory: true, html: true do |registration_invite|
-    if registration_invite.invited_by.present?
-      link_to(
-        registration_invite.invited_by.full_name,
-        admin_participant_path(registration_invite.invited_by)
-      )
-    else
-      "-"
+  column :account, header: "Account Id", mandatory: true, html: false do |registration_invite|
+    registration_invite.account&.id
+  end
+
+  column :invited_by, header: "Invited By", mandatory: true do |registration_invite|
+    format(registration_invite.invited_by&.name) do |invited_by_name|
+      if invited_by_name.present?
+        link_to(
+          invited_by_name,
+          admin_participant_path(registration_invite.invited_by)
+        )
+      else
+        "-"
+      end
     end
+  end
+
+  column :invited_by, header: "Invited By", html: false do |registration_invite|
+    registration_invite.invited_by&.full_name
   end
 
   column :actions, mandatory: true, html: true do |registration_invite|
@@ -61,4 +79,10 @@ class UserInvitationsGrid
     :enum,
     header: "Invite Status",
     select: UserInvitation.statuses.transform_keys(&:capitalize)
+
+  column_names_filter(
+    header: "More columns",
+    filter_group: "more-columns",
+    multiple: true
+  )
 end
