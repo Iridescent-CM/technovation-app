8e616c39e79be431ae6ca31c5ed441b604b4c7e6
Vivian Canales <vcnales@gmail.com>
2023-10-18 14:37:07 -0500

    Added missing line

--- app/controllers/mentor/background_checks_controller.rb
+++ app/controllers/mentor/background_checks_controller.rb
@@ -2,7 +2,9 @@ module Mentor
   class BackgroundChecksController < MentorController
     include BackgroundCheckController
     include BackgroundCheckInvitationController
+
     private
+
     def current_profile
       current_mentor
     end
