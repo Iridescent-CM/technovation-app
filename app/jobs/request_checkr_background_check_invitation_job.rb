8ac77bdcc516ab3d8bbc96197789193ae089698c
Vivian Canales <vcnales@gmail.com>
2023-12-15 17:01:42 -0600

    Added bg check initial creating in bg check job (as placeholder)

--- app/jobs/request_checkr_background_check_invitation_job.rb
+++ app/jobs/request_checkr_background_check_invitation_job.rb
@@ -10,6 +10,12 @@ class RequestCheckrBackgroundCheckInvitationJob < ActiveJob::Base
       status: "queued",
       owner: profile
     )
+
+    bg_check = BackgroundCheck.find_or_create_by!(account: profile)
+    bg_check.update_columns(
+      status: :invitation_required,
+      state: :requesting_invitation
+    )
   end
 
   after_perform do |job|
