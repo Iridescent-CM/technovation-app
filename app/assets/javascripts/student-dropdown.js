933fe9ed6847ba73fd89fc330d7f8111739a5256
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-01-16 13:37:35 -0700

    Chore: Move dropdown nav JS to new file
    I moved the dropdown nav expand/collapse functionality into a new
    file called `dropdown_nav.js`, since the partial is also named
    `dropdown_nav`. The dropdown nav toggle functionality isn't specific to
    students which is also why I moved it.
    
    Refs: #4392

--- app/assets/javascripts/student-dropdown.js
+++ app/assets/javascripts/student-dropdown.js
@@ -1,9 +1,6 @@
-$(document).on("turbolinks:load", function() {
-    $('#cha-student-meet').click(function() {
-        $('#cha-student-intro-wrapper').toggle('slow', function() {});
-    });
-
-    $('.tw-dropdown').click(function (){
-        $('.tw-dropdown-menu').toggle();
-    });
+$(document).on("turbolinks:load", function () {
+  $("#cha-student-meet").click(function () {
+    $("#cha-student-intro-wrapper").toggle("slow", function () {});
+  });
 });
+
