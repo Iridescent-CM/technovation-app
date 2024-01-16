b71d5070af427c6b319873c91488191225edf2e7
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-01-16 10:27:31 -0700

    Feature: Add "Chapter Admin Activity"
    This will setup up the basic infrastructure for "Chapter Admin
    Activity", which is basically the old Chapter Ambassador dashboard. The
    old dashboard will be used for our new Chapter Ambassador onboarding
    process.
    
    The new `ChapterAdminController#show` action was copied from the old
    `ChapterAmbassadorDashboardsController#show` action.
    
    Refs: #4392

---
+++ app/controllers/chapter_ambassador/chapter_admin_controller.rb
@@ -0,0 +1,41 @@
+module ChapterAmbassador
+  class ChapterAdminController < ChapterAmbassadorController
+    def show
+      trackable_user_ids = Account.current
+        .not_staff
+        .in_region(current_ambassador)
+        .pluck(:id) - [current_ambassador.account_id]
+
+      trackable_team_ids = Team.current
+        .in_region(current_ambassador)
+        .pluck(:id)
+
+      @recent_activities = PublicActivity::Activity.distinct
+        .where("(activities.trackable_id IN (?) AND " \
+               "activities.trackable_type = ?) OR " \
+               "(activities.trackable_id IN (?) AND " \
+               "activities.trackable_type = ?)",
+          trackable_user_ids,
+          "Account",
+          trackable_team_ids,
+          "Team")
+        .order("created_at desc")
+        .first(20)
+
+      @top_inactive_mentors = Account.in_region(current_ambassador)
+        .not_staff
+        .inactive_mentors
+        .limit(3)
+
+      @top_inactive_students = Account.in_region(current_ambassador)
+        .not_staff
+        .inactive_students
+        .limit(3)
+
+      @top_inactive_teams = Team.in_region(current_ambassador)
+        .not_staff
+        .inactive
+        .limit(3)
+    end
+  end
+end
