716306d8104a6f12c4d8ff6e0d13ccb12bbbd027
Vivian Canales <vcnales@gmail.com>
2024-01-23 11:28:39 -0600

    Updates to ChA additions functionality based on code review

--- app/controllers/admin/chapter_ambassador_profile_additions_controller.rb
+++ app/controllers/admin/chapter_ambassador_profile_additions_controller.rb
@@ -1,5 +1,11 @@
 module Admin
   class ChapterAmbassadorProfileAdditionsController < AdminController
-    include ChapterAmbassadorProfileAdditionController
+    def create
+      account = Account.find(params.fetch(:account_id))
+      chapter_ambassador_profile_addition_result = ChapterAmbassadorProfileAddition.new(account: account).call
+
+      redirect_to admin_participant_path(account),
+        flash: chapter_ambassador_profile_addition_result.message
+    end
   end
 end
