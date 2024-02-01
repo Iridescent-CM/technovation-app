fac3bc7d0bed3412b9de88e5eb49db1f1fbc284e
Vivian Canales <vcnales@gmail.com>
2024-02-01 16:52:42 -0600

    Chore: Updates ChA background check invitation specs

--- spec/features/background_check_invitation_spec.rb
+++ spec/features/background_check_invitation_spec.rb
@@ -17,4 +17,45 @@ RSpec.feature "background check invitation" do
 
     expect(mentor.reload.background_check).to be_present
   end
+
+  scenario "Request a background check invitation as a chapter ambassador", :vcr do
+    chapter_ambassador = FactoryBot.create(
+      :chapter_ambassador,
+      :approved,
+      :geocoded,
+      account: FactoryBot.create(:account, email: "engineering+factorycha@technovation.org")
+    )
+    chapter_ambassador.background_check.destroy
+
+    sign_in(chapter_ambassador)
+
+    click_link "Background Check"
+    click_link "Submit Background Check"
+
+    expect(page).to have_link("Request background check invitation")
+    click_link "Request background check invitation"
+
+    expect(chapter_ambassador.reload.background_check).to be_present
+    expect(chapter_ambassador.background_check.internal_invitation_status).to eq("invitation_sent")
+  end
+
+  scenario "Chapter ambassadors will see an error message if their last name is less than 2 characters when trying to request a background check invitation" do
+    chapter_ambassador = FactoryBot.create(
+      :chapter_ambassador,
+      :approved,
+      :geocoded,
+      account: FactoryBot.create(:account, email: "engineering+factorycha@technovation.org", last_name: "a")
+    )
+    chapter_ambassador.background_check.destroy
+
+    sign_in(chapter_ambassador)
+
+    click_link "Background Check"
+    click_link "Submit Background Check"
+
+    expect(page).to have_link("Request background check invitation")
+    click_link "Request background check invitation"
+
+    expect(page).to have_text("Checkr requires last name to contain at least 2 alpha characters. Please edit your last name.")
+  end
 end
