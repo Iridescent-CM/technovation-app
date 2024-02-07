3d3646c1a9a6ff05ba0351b797cfe03451d89ce6
Vivian Canales <vcnales@gmail.com>
2024-02-06 21:54:45 -0600

    Added background check datagrid specs

--- spec/factories/background_checks.rb
+++ spec/factories/background_checks.rb
@@ -4,5 +4,13 @@ FactoryBot.define do
     status { BackgroundCheck.statuses[:clear] }
     report_id { "123abcREPORT_ID" }
     candidate_id { "123abcCANDIDATE_ID" }
+
+    trait :invitation_pending do
+      status { BackgroundCheck.statuses[:invitation_required] }
+      invitation_id { "123abcINVITATION_ID" }
+      invitation_status { BackgroundCheck.invitation_statuses[:pending] }
+      candidate_id { nil }
+      report_id { nil }
+    end
   end
 end
