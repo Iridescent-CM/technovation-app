dfa6d7a5748427d82e6594073e7c533e1ccd03a8
Vivian Canales <vcnales@gmail.com>
2024-02-07 15:47:37 -0600

    Updated judging rubrics

--- app/javascript/utilities/judge-helpers.js
+++ app/javascript/utilities/judge-helpers.js
@@ -1,14 +1,14 @@
-function getJudgingRubricLink(division){
-    switch (division) {
-        case 'senior':
-            return "https://technovationchallenge.org/wp-content/uploads/2022/09/Senior-Rubric-2023-Season-FINAL.pdf"
-        case 'junior':
-            return "https://technovationchallenge.org/wp-content/uploads/2022/09/Junior-Rubric-2023-Season-FINAL.pdf"
-        case 'beginner':
-            return "https://technovationchallenge.org/wp-content/uploads/2022/09/Beginner-Rubric-2023-Season-FINAL.pdf"
-        default:
-            return "https://technovationchallenge.org/curriculum/judging-rubric/"
-    }
+function getJudgingRubricLink(division) {
+  switch (division) {
+    case "senior":
+      return "https://technovationchallenge.org/wp-content/uploads/2023/07/FINAL-Senior-16-18-Rubric-2024-Season-.pdf";
+    case "junior":
+      return "https://technovationchallenge.org/wp-content/uploads/2023/07/FINAL-Junior-13-15-Rubric-2024-Season-.pdf";
+    case "beginner":
+      return "https://technovationchallenge.org/wp-content/uploads/2023/07/FINAL-Beginner-8-12-Rubric-2024-Season-.pdf";
+    default:
+      return "https://technovationchallenge.org/curriculum/judging-rubric/";
+  }
 }
 
-export { getJudgingRubricLink }
+export { getJudgingRubricLink };
