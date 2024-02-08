1545e2947b287efd21047029e43e38c85d25dc77
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-02-08 15:38:46 -0700

    Fix: Axios CSRF issue
    Recently axios was updated and some things changed/broke, this update
    will change `config.headers.common` to `config.headers`, which will
    properly include the CSRF token in the headers. The rest of the changes
    in this file are for formatting updates.
    
    Refs: #4485 #4477

--- app/javascript/config/axios.js
+++ app/javascript/config/axios.js
@@ -1,30 +1,34 @@
-import axios from 'axios'
-import applyConverters from 'axios-case-converter'
+import axios from "axios";
+import applyConverters from "axios-case-converter";
 
 // Polyfill Promise for IE11 to support axios
-require('es6-promise').polyfill()
+require("es6-promise").polyfill();
 
-window.axios = applyConverters(axios.create())
+window.axios = applyConverters(axios.create());
 
-document.addEventListener('turbolinks:load', () => {
-  const csrfTokenMetaTag = document.querySelector('meta[name="csrf-token"]')
+document.addEventListener("turbolinks:load", () => {
+  const csrfTokenMetaTag = document.querySelector('meta[name="csrf-token"]');
 
-  window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+  window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
   if (csrfTokenMetaTag) {
-    window.axios.interceptors.request.use(function (config) {
-      if (config.url && config.url.match(/^\//)) {
-        // The following headers must be in "Header-Case" because
-        // of axios-case-converter (see their documentation)
-        config.headers.common = {
-          'X-Requested-With': 'XMLHttpRequest',
-          'X-Csrf-Token' : csrfTokenMetaTag.getAttribute('content')
+    window.axios.interceptors.request.use(
+      function (config) {
+        if (config.url && config.url.match(/^\//)) {
+          // The following headers must be in "Header-Case" because
+          // of axios-case-converter (see their documentation)
+          config.headers = {
+            "X-Requested-With": "XMLHttpRequest",
+            "X-Csrf-Token": csrfTokenMetaTag.getAttribute("content"),
+          };
         }
-      }
 
-      return config
-    }, function (error) {
-      return Promise.reject(error);
-    })
+        return config;
+      },
+      function (error) {
+        return Promise.reject(error);
+      }
+    );
   }
-})
+});
+
