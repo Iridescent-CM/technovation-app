4ae178f15a6161cf157a9df64f8f15a545848f99
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-02-13 12:50:03 -0700

    UI: Add sidenav to ChA background check flow
    This will make the sidenav visible for the entire background check flow.
    
    I created a new partial
    `chapter_ambassador/dashboards/onboarding/sidenav` that can be included
    whereever we want show the sidebar.
    
    I updated the route for chapter ambassador background check show page to
    be a singular resource which will remove the id from the URL.
    
    Refs: #4495

--- app/null_objects/null_background_check.rb
+++ app/null_objects/null_background_check.rb
@@ -1,18 +1,77 @@
 class NullBackgroundCheck < NullObject
-  def id; 0; end
-  def updated_at; 0; end
-  def status; "none"; end
-  def new_record?; false; end
-  def clear?; false; end
-  def pending?; false; end
-  def consider?; false; end
-  def suspended?; false; end
-  def paranoid?; false; end
-  def invitation_status; nil; end
-  def invitation_pending?; false; end
-  def invitation_completed?; false; end
-  def invitation_expired?; false; end
-  def requesting_invitation?; false; end
-  def error?; false; end
-  def candidate_id; nil; end
+  def id
+    0
+  end
+
+  def invitation_id
+    nil
+  end
+
+  def report_id
+    nil
+  end
+
+  def updated_at
+    0
+  end
+
+  def status
+    "none"
+  end
+
+  def new_record?
+    false
+  end
+
+  def clear?
+    false
+  end
+
+  def pending?
+    false
+  end
+
+  def consider?
+    false
+  end
+
+  def suspended?
+    false
+  end
+
+  def paranoid?
+    false
+  end
+
+  def invitation_status
+    nil
+  end
+
+  def invitation_pending?
+    false
+  end
+
+  def invitation_completed?
+    false
+  end
+
+  def invitation_expired?
+    false
+  end
+
+  def invitation_sent?
+    false
+  end
+
+  def requesting_invitation?
+    false
+  end
+
+  def error?
+    false
+  end
+
+  def candidate_id
+    nil
+  end
 end
