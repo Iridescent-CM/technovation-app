cd1c48249488087f787683ae46b65eb375e56ac7
Vivian Canales <vcnales@gmail.com>
2024-02-15 09:37:49 -0600

    updated chapter specs

--- spec/features/admin/manage_chapters_spec.rb
+++ spec/features/admin/manage_chapters_spec.rb
@@ -16,4 +16,17 @@ RSpec.feature "Admins managing chapters", :js do
     expect(page).to have_content("Please use the invite button below to invite a user to be the Chapter Ambassador for this chapter.")
     expect(Chapter.count).to eq(1)
   end
+
+  scenario "Admin view all chapters" do
+    chapters = FactoryBot.create_list(:chapter, 2)
+    visit admin_chapters_path
+
+    chapters.each do |chapter|
+      expect(page).to have_selector("tr#chapter_#{chapter.id}")
+      within("tr#chapter_#{chapter.id}") do
+        expect(page).to have_link(chapter.name, href: admin_chapter_path(chapter))
+        expect(page).to have_css("td.organization_name", text: chapter.organization_name)
+      end
+    end
+  end
 end
