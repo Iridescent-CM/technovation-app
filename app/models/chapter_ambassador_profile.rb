19011cbc647ef268ee22adeae2b7fac6848a46d4
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-02-29 13:16:03 -0700

    Chore: Add ChA organization status to registration process
    This will add a new "Status with Organization" field for chapter
    ambassaadors that are registering.
    
    Refs: #4515

--- app/models/chapter_ambassador_profile.rb
+++ app/models/chapter_ambassador_profile.rb
@@ -22,6 +22,10 @@ class ChapterAmbassadorProfile < ActiveRecord::Base
   after_update :after_status_changed, if: :saved_change_to_status?
 
   enum status: %i[pending approved declined spam]
+  enum organization_status: {
+    employee: "employee",
+    volunteer: "volunteer"
+  }
 
   validates :job_title, presence: true
 
