ea6562e6411924712cb16ab8a7c98fb91c307245
Vivian Canales <vcnales@gmail.com>
2024-03-01 12:47:00 -0600

    Updated convert to cha button

--- spec/system/admin/add_chapter_ambassador_profile_to_account_spec.rb
+++ spec/system/admin/add_chapter_ambassador_profile_to_account_spec.rb
@@ -29,4 +29,18 @@ RSpec.describe "Admin add chapter ambassador profile to an account" do
       expect(page).to_not have_link("Add Chapter Ambassador account")
     end
   end
+
+  context "when a chapter ambassador views the mentor debugging section of a mentor profile" do
+    it "does not show an add chapter ambassador profile button" do
+      mentor = FactoryBot.create(:mentor)
+      chapter_ambassador = FactoryBot.create(:chapter_ambassador, :approved, intro_summary: "Here is my intro summary!")
+
+      sign_in(chapter_ambassador)
+      click_link "Participants"
+
+      visit chapter_ambassador_participant_path(mentor.account)
+
+      expect(page).to_not have_link("Add Chapter Ambassador account")
+    end
+  end
 end
