eafbb1501da4e10839fa01a206ce8afec9e89789
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-03-14 13:13:38 -0600

    Chore: Update chapter details page
    A very basic update here to add chapter name, location and summary to
    the chapter's show page. The layout will basically be the same as the
    participant's show page.
    
    On the participant's show page the location is shown via
    `#address_details`, in order to get the same functionality for chapters,
    I had to add `include ActiveGeocoded` to the chapter's model.
    
    Refs: #4562

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,4 +1,6 @@
 class Chapter < ActiveRecord::Base
+  include ActiveGeocoded
+
   has_many :chapter_ambassador_profiles
   has_many :student_profiles
   has_many :registration_invites, class_name: "UserInvitation"
