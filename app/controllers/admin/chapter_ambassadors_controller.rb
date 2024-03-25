627d4feff252eae7851c7c004dfac7067a676a4e
Vivian Canales <vcnales@gmail.com>
2024-03-25 15:09:06 -0500

    Added chapter ambassadors datagrid

--- app/controllers/admin/chapter_ambassadors_controller.rb
+++ app/controllers/admin/chapter_ambassadors_controller.rb
@@ -1,34 +1,8 @@
 module Admin
   class ChapterAmbassadorsController < AdminController
-    def index
-      params[:status] ||= :pending
-      params[:page] = 1 if params[:page].blank?
-      params[:per_page] = 15 if params[:per_page].blank?
+    include DatagridController
 
-      chapter_ambassadors = Account.joins(:chapter_ambassador_profile)
-        .where("chapter_ambassador_profiles.status = ?",
-          ChapterAmbassadorProfile.statuses[params.fetch(:status)])
-
-      unless params[:text].blank?
-        results = chapter_ambassadors.search({
-          query: {
-            query_string: {
-              query: params[:text]
-            }
-          },
-          from: 0,
-          size: 10_000
-        }).results
-
-        chapter_ambassadors = chapter_ambassadors.where(id: results.flat_map { |r| r._source.id })
-      end
-
-      @chapter_ambassadors = chapter_ambassadors.page(params[:page].to_i).per_page(params[:per_page].to_i)
-
-      if @chapter_ambassadors.empty?
-        @chapter_ambassadors = @chapter_ambassadors.page(1)
-      end
-    end
+    use_datagrid with: ChapterAmbassadorsGrid
 
     def show
       @chapter_ambassador = ChapterAmbassadorProfile.find_by(account_id: params.fetch(:id))
@@ -41,6 +15,13 @@ module Admin
       ambassador.public_send("#{params.fetch(:status)}!")
       redirect_back fallback_location: admin_chapter_ambassadors_path,
         success: "#{ambassador.full_name} was marked as #{params.fetch(:status)}"
+    end
+
+    def grid_params
+      grid = params[:chapter_ambassadors_grid] ||= {}
+      grid.merge(
+        column_names: detect_extra_columns(grid)
+      )
     end
   end
 end
