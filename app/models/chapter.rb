a17652ac17bd07c7004c481f9bca38c75542670b
Vivian Canales <vcnales@gmail.com>
2024-04-10 13:32:10 -0500

    updated view to check for primary contact

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -4,4 +4,9 @@ class Chapter < ActiveRecord::Base
   has_many :chapter_ambassador_profiles
   has_many :student_profiles
   has_many :registration_invites, class_name: "UserInvitation"
+
+  def has_primary_contact?
+    chapter_ambassador_profiles.where(is_primary_contact: true).exists?
+  end
+
 end
