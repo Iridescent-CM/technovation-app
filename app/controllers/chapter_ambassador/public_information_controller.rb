ecf3df648e01ae5d51aacf3f2942a76009014d44
Vivian Canales <vcnales@gmail.com>
2024-04-10 14:49:21 -0500

    Added edit functionality for chapter public info

--- app/controllers/chapter_ambassador/public_information_controller.rb
+++ app/controllers/chapter_ambassador/public_information_controller.rb
@@ -2,5 +2,31 @@ module ChapterAmbassador
   class PublicInformationController < ChapterAmbassadorController
     layout "chapter_ambassador_rebrand"
 
+    def update
+      @chapter = current_ambassador.chapter
+      if @chapter.update(public_information_params)
+        redirect_to chapter_ambassador_public_information_path,
+                  success: "You updated your chapter public information!"
+      else
+        flash.now[:alert] = "Error updating chapter details. Please try again later."
+        render :edit
+      end
+    end
+
+    private
+
+    def public_information_params
+      params.require(:chapter).permit(
+        :name,
+        :summary,
+        regional_links_attributes: [
+          :id,
+          :_destroy,
+          :name,
+          :value,
+          :custom_label
+        ]
+      )
+    end
   end
 end
