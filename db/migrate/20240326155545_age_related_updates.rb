4e24cc88b8ddd636d1db2ee9ed4bbb5ee7b11a57
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-04-15 07:26:45 -0600

    Chore: Update meets_minimum_age_requirement migrations
    This should ensure that this migration will only run if the
    `meets_minimum_age_requirement` column doesn't exist on QA or Preview.
    I'm pretty sure this is needed because both QA and Preview have
    this migration (one was added on QA for judges and one for chapter
    ambassadors on Preview), and this should ensure the column will only be
    added one time.
    
    Refs: #4537

--- db/migrate/20240326155545_age_related_updates.rb
+++ db/migrate/20240326155545_age_related_updates.rb
@@ -1,12 +1,13 @@
 class AgeRelatedUpdates < ActiveRecord::Migration[6.1]
-  def up
-    if Account.column_names.exclude?("meets_minimum_age_requirement")
+  Account.reset_column_information
+  if Account.column_names.exclude?("meets_minimum_age_requirement")
+    def up
       add_column :accounts, :meets_minimum_age_requirement, :boolean
       change_column_null :accounts, :date_of_birth, true
     end
-  end
 
-  def down
-    # NOOP
+    def down
+      # NOOP
+    end
   end
 end
