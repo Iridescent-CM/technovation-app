2c473e518b7134883b3110bc95b4bdc115a3f252
Vivian Canales <vcnales@gmail.com>
2024-04-15 16:52:24 -0500

    Added primary contact association to chapter model

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -2,6 +2,10 @@ class Chapter < ActiveRecord::Base
   include ActiveGeocoded
 
   has_many :chapter_ambassador_profiles
+  belongs_to :primary_contact,
+             class_name: "ChapterAmbassadorProfile",
+             foreign_key: "primary_contact_id",
+             optional: true
   has_many :student_profiles
   has_many :registration_invites, class_name: "UserInvitation"
 end
