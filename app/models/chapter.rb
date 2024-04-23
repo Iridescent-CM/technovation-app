bb47888970f9a9b7069e0625fe5c303edde94c68
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-04-23 06:37:25 -0600

    Chore: Add email validation for legal contact email address
    This will make the necessary updates to add email validation for the
    chapter's legal contact's email address.
    
    Refs: #4655

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -5,4 +5,8 @@ class Chapter < ActiveRecord::Base
   belongs_to :primary_contact, class_name: "ChapterAmbassadorProfile", foreign_key: "primary_contact_id", optional: true
   has_many :student_profiles
   has_many :registration_invites, class_name: "UserInvitation"
+
+  validates :legal_contact_email_address,
+    email: true,
+    if: -> { legal_contact_email_address.present? }
 end
