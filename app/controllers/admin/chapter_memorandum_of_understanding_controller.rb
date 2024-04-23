728775aaeffe254e33d0b31cfcd464a897d0b696
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-04-23 06:12:59 -0600

    Feature: Add ability to send MOUs to chapter legal contact
    This will add functionality so that admins can send a chapter's legal
    contact an MOU (memorandum of understanding). The MOU will be sent via a
    job which will call the DocuSign service that will send the MOU.
    
    Refs: #4655

---
+++ app/controllers/admin/chapter_memorandum_of_understanding_controller.rb
@@ -0,0 +1,17 @@
+module Admin
+  class ChapterMemorandumOfUnderstandingController < AdminController
+    def create
+      @chapter = Chapter.find(params[:chapter_id])
+
+      SendMemorandumOfUnderstandingJob.perform_later(
+        full_name: @chapter.legal_contact_full_name,
+        email_address: @chapter.legal_contact_email_address,
+        organization_name: @chapter.organization_name,
+        job_title: @chapter.legal_contact_job_title
+      )
+
+      redirect_to admin_chapter_path(@chapter),
+        success: "Successfully scheduled job to send MOU to #{@chapter.legal_contact_full_name}"
+    end
+  end
+end
