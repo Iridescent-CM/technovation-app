b37adbcdf5fa9363916be8baaacd077b451d6806
Vivian Canales <vcnales@gmail.com>
2024-04-24 19:00:20 -0500

    Added edit chapter organization headquarters location (routes/views/etc)

---
+++ spec/features/chapter_ambassador/edit_chapter_location_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.feature "Chapter ambassadors edit chapter location" do
+  let(:chapter) { FactoryBot.create(:chapter) }
+
+  before do
+    chapter_ambassador = FactoryBot.create(:chapter_ambassador, chapter: chapter)
+    sign_in(chapter_ambassador)
+    click_link "Chapter Profile"
+    click_link "Chapter Location"
+  end
+
+  scenario "Chapter ambassador edits organization headquarters location" do
+    click_link "Update chapter location"
+
+    fill_in "chapter_organization_headquarters_location", with: "123 Main St, USA"
+    click_button "Save"
+
+    expect(page).to have_css(".flash.flash--success", text: "You updated your chapter location details!")
+    expect(page).to have_content "123 Main St, USA"
+  end
+end
