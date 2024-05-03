3de5ca072bda99bbb598350483da02ce22181cfe
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-05-03 16:00:25 -0600

    Chore: Update DocuSign service to accept a legal contact
    This will update the DocuSign service, job, and specs to make it so that
    a legal contact can be passed to the service, instead of individual
    fields (full name, email address, job title, organization name). By
    using a legal contact, it will ensure that the full name and email
    address are present since those are required fields and a legal contact
    cannot exist without them.
    
    Refs: #4691

--- spec/jobs/send_memorandum_of_understanding_spec.rb
+++ spec/jobs/send_memorandum_of_understanding_spec.rb
@@ -1,25 +1,17 @@
 require "rails_helper"
 
 RSpec.describe SendMemorandumOfUnderstandingJob do
-  let(:full_name) { "Lacey Taro" }
-  let(:email_address) { "laceyt@example.com" }
-  let(:organization_name) { "Blueberry Academy" }
-  let(:job_title) { "Pokémon Trainer" }
+  let(:chapter) { instance_double(Chapter, id: 146, legal_contact: legal_contact) }
+  let(:legal_contact) { instance_double(LegalContact) }
+
+  before do
+    allow(Chapter).to receive(:find).with(chapter.id).and_return(chapter)
+  end
 
   it "calls the DocuSign service that will send the memorandum of understanding" do
     expect(Docusign::ApiClient).to receive_message_chain(:new, :send_memorandum_of_understanding)
-      .with(
-        full_name: full_name,
-        email_address: email_address,
-        organization_name: organization_name,
-        job_title: job_title
-      )
+      .with(legal_contact: chapter.legal_contact)
 
-    SendMemorandumOfUnderstandingJob.perform_now(
-      full_name: full_name,
-      email_address: email_address,
-      organization_name: organization_name,
-      job_title: job_title
-    )
+    SendMemorandumOfUnderstandingJob.perform_now(chapter_id: chapter.id)
   end
 end
