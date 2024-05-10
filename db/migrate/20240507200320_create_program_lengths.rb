889f029c3bbf289e8fc788d343e5e8d39e455022
Vivian Canales <vcnales@gmail.com>
2024-05-10 10:47:38 -0500

    removed order from program length

--- db/migrate/20240507200320_create_program_lengths.rb
+++ db/migrate/20240507200320_create_program_lengths.rb
@@ -2,7 +2,6 @@ class CreateProgramLengths < ActiveRecord::Migration[6.1]
   def change
     create_table :program_lengths do |t|
       t.string :length
-      t.integer :order
 
       t.timestamps
     end
