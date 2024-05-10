fa75b5617ead826e0ad94f4a604e2d3d8ed863c2
Vivian Canales <vcnales@gmail.com>
2024-05-10 10:47:38 -0500

    removed order from program length

--- db/migrate/20240507200453_populate_program_lengths.rb
+++ db/migrate/20240507200453_populate_program_lengths.rb
@@ -1,10 +1,10 @@
 class PopulateProgramLengths < ActiveRecord::Migration[6.1]
   def up
-    ProgramLength.create(length: "Less than 6 weeks", order: 1)
-    ProgramLength.create(length: "6-9 weeks", order: 2)
-    ProgramLength.create(length: "10-12 weeks", order: 3)
-    ProgramLength.create(length: "13-15 weeks", order: 4)
-    ProgramLength.create(length: "16+ weeks", order: 5)
+    ProgramLength.create(length: "Less than 6 weeks")
+    ProgramLength.create(length: "6-9 weeks")
+    ProgramLength.create(length: "10-12 weeks")
+    ProgramLength.create(length: "13-15 weeks")
+    ProgramLength.create(length: "16+ weeks")
   end
 
   def down
