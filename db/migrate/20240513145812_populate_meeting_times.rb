c7c017d502456c6b4406b1c5ff195c7d143868af
Vivian Canales <vcnales@gmail.com>
2024-05-13 23:46:09 -0500

    removed order from org types, meeting times, and meeting facilitators tables

--- db/migrate/20240513145812_populate_meeting_times.rb
+++ db/migrate/20240513145812_populate_meeting_times.rb
@@ -1,9 +1,9 @@
 class PopulateMeetingTimes < ActiveRecord::Migration[6.1]
   def up
-    MeetingTime.create(time: "During school", order: 1)
-    MeetingTime.create(time: "After school", order: 2)
-    MeetingTime.create(time: "In the evenings", order: 3)
-    MeetingTime.create(time: "On the weekend", order: 4)
+    MeetingTime.create(time: "During school")
+    MeetingTime.create(time: "After school")
+    MeetingTime.create(time: "In the evenings")
+    MeetingTime.create(time: "On the weekend")
   end
 
   def down
