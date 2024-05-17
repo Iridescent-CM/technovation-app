579cb57ab75875f761ac611029b22b46e303b677
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-05-17 12:23:41 -0600

    Chore: Refactor DocuSign API client to be more flexible
    I extracted the common parts for making an API call to DocuSign into a
    new private method called `send_document_to`, which takes a `signer`
    and `params` (which contains all of the specific details). All of the
    specific details for sending an MOU to a legal contact have also been
    extracted, and this will get passed to `send_document_to`. These changes
    will allow for chapter ambassador profiles to be more easily used in
    this service.
    
    Refs: #4697

--- app/services/docusign/api_client.rb
+++ app/services/docusign/api_client.rb
@@ -23,115 +23,10 @@ module Docusign
     end
 
     def send_memorandum_of_understanding(legal_contact:)
-      response = client.post(
-        "v2.1/accounts/#{api_account_id}/envelopes",
-        {
-          templateId: ENV.fetch("DOCUSIGN_MEMORANDUM_OF_UNDERSTANDING_TEMPLATE_ID"),
-          templateRoles: [
-            {
-              name: legal_contact.full_name,
-              email: legal_contact.email_address,
-              roleName: "signer",
-              tabs: {
-                textTabs: [
-                  {
-                    documentId: 1,
-                    pageNumber: 1,
-                    required: true,
-                    font: "Georgia",
-                    fontSize: "Size10",
-                    italic: true,
-                    underline: true,
-                    value: legal_contact.full_name,
-                    width: 100,
-                    height: 23,
-                    xPosition: 394,
-                    yPosition: 152
-                  },
-                  {
-                    documentId: 1,
-                    pageNumber: 1,
-                    font: "Georgia",
-                    fontSize: "Size10",
-                    italic: true,
-                    underline: true,
-                    value: legal_contact.chapter.organization_name,
-                    width: 12,
-                    height: 23,
-                    xPosition: 98,
-                    yPosition: 180
-                  },
-                  {
-                    anchorString: "Name of Organization (if applicable):",
-                    anchorXOffset: 190,
-                    anchorYOffset: -11,
-                    font: "Georgia",
-                    fontSize: "Size12",
-                    value: legal_contact.chapter.organization_name,
-                    width: 200
-                  },
-                  {
-                    anchorString: "Title:",
-                    anchorXOffset: 30,
-                    anchorYOffset: -11,
-                    font: "Georgia",
-                    fontSize: "Size12",
-                    required: false,
-                    value: legal_contact.job_title,
-                    width: 155
-                  }
-                ],
-                signHereTabs: [
-                  {
-                    anchorString: "Signature:",
-                    anchorXOffset: 65,
-                    anchorYOffset: -7
-                  }
-                ],
-                dateSignedTabs: [
-                  {
-                    anchorString: "Date:",
-                    anchorXOffset: 30,
-                    anchorYOffset: -10,
-                    font: "Georgia",
-                    fontSize: "Size12"
-                  }
-                ],
-                fullNameTabs: [
-                  {
-                    anchorString: "Name:",
-                    anchorXOffset: 35,
-                    anchorYOffset: -9,
-                    font: "Georgia",
-                    fontSize: "Size12"
-                  }
-                ]
-              }
-            }
-          ],
-          status: "sent"
-        }.to_json
+      send_document_to(
+        signer: legal_contact,
+        params: params_for_legal_contact(legal_contact)
       )
-
-      response_body = JSON.parse(response.body, symbolize_names: true)
-
-      if response.success?
-        legal_contact.documents.create(
-          full_name: legal_contact.full_name,
-          email_address: legal_contact.email_address,
-          active: true,
-          docusign_envelope_id: response_body[:envelopeId]
-        )
-
-        Result.new(success?: true)
-      else
-        error = "[DOCUSIGN] Error sending MOU - #{response_body[:message]}"
-
-        logger.error(error)
-        error_notifier.notify(error)
-
-        Result.new(success?: false, message: "There was an error trying to send the MOU, please check the logs for more info.")
-      end
     end
 
     private
@@ -139,5 +34,121 @@ module Docusign
     attr_reader :client, :api_account_id, :logger, :error_notifier
 
     Result = Struct.new(:success?, :message, keyword_init: true)
+
+    def send_document_to(signer:, params:)
+      response = client.post(
+        "v2.1/accounts/#{api_account_id}/envelopes",
+        params
+      )
+
+      response_body = JSON.parse(response.body, symbolize_names: true)
+
+      if response.success?
+        signer.documents.create(
+          full_name: signer.full_name,
+          email_address: signer.email_address,
+          active: true,
+          docusign_envelope_id: response_body[:envelopeId]
+        )
+
+        Result.new(success?: true)
+      else
+        error = "[DOCUSIGN] Error sending document - #{response_body[:message]}"
+
+        logger.error(error)
+        error_notifier.notify(error)
+
+        Result.new(success?: false, message: "There was an error trying to send your document, please check the logs for more info.")
+      end
+    end
+
+    def params_for_legal_contact(legal_contact)
+      {
+        templateId: ENV.fetch("DOCUSIGN_MEMORANDUM_OF_UNDERSTANDING_TEMPLATE_ID"),
+        templateRoles: [
+          {
+            name: legal_contact.full_name,
+            email: legal_contact.email_address,
+            roleName: "signer",
+            tabs: {
+              textTabs: [
+                {
+                  documentId: 1,
+                  pageNumber: 1,
+                  required: true,
+                  font: "Georgia",
+                  fontSize: "Size10",
+                  italic: true,
+                  underline: true,
+                  value: legal_contact.full_name,
+                  width: 100,
+                  height: 23,
+                  xPosition: 394,
+                  yPosition: 152
+                },
+                {
+                  documentId: 1,
+                  pageNumber: 1,
+                  font: "Georgia",
+                  fontSize: "Size10",
+                  italic: true,
+                  underline: true,
+                  value: legal_contact.chapter.organization_name,
+                  width: 12,
+                  height: 23,
+                  xPosition: 98,
+                  yPosition: 180
+                },
+                {
+                  anchorString: "Name of Organization (if applicable):",
+                  anchorXOffset: 190,
+                  anchorYOffset: -11,
+                  font: "Georgia",
+                  fontSize: "Size12",
+                  value: legal_contact.chapter.organization_name,
+                  width: 200
+                },
+                {
+                  anchorString: "Title:",
+                  anchorXOffset: 30,
+                  anchorYOffset: -11,
+                  font: "Georgia",
+                  fontSize: "Size12",
+                  required: false,
+                  value: legal_contact.job_title,
+                  width: 155
+                }
+              ],
+              signHereTabs: [
+                {
+                  anchorString: "Signature:",
+                  anchorXOffset: 65,
+                  anchorYOffset: -7
+                }
+              ],
+              dateSignedTabs: [
+                {
+                  anchorString: "Date:",
+                  anchorXOffset: 30,
+                  anchorYOffset: -10,
+                  font: "Georgia",
+                  fontSize: "Size12"
+                }
+              ],
+              fullNameTabs: [
+                {
+                  anchorString: "Name:",
+                  anchorXOffset: 35,
+                  anchorYOffset: -9,
+                  font: "Georgia",
+                  fontSize: "Size12"
+                }
+              ]
+            }
+          }
+        ],
+        status: "sent"
+      }.to_json
+    end
   end
 end
