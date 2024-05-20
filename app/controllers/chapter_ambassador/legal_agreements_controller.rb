625c645406526d08e0a916f49258065287348029
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-05-20 13:36:53 -0600

    Feature: Chapter ambassador legal agreements
    This will setup all the necessary pieces for chapter ambassador legal
    agreements.
    
    Refs: #4697

---
+++ app/controllers/chapter_ambassador/legal_agreements_controller.rb
@@ -0,0 +1,12 @@
+module ChapterAmbassador
+  class LegalAgreementsController < ChapterAmbassadorController
+    def create
+      SendChapterAmbassadorLegalAgreementJob.perform_now(
+        chapter_ambassador_profile_id: current_ambassador.id
+      )
+
+      redirect_to chapter_ambassador_mou_path,
+        success: "You should receive an email from DocuSign soon. If you do not receive this email or you are having issues please reach out to #{ENV.fetch("HELP_EMAIL")}"
+    end
+  end
+end
