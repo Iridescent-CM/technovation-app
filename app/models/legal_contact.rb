56ea60316bf577e2ec49638ad702994ce774721e
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-05-21 07:46:00 -0600

    UI: Display "Seasons MOU Valid For" in admin
    This will display "Seasons MOU Valid For" on the chapter show page in
    the admin area.
    
    Refs: #4717

--- app/models/legal_contact.rb
+++ app/models/legal_contact.rb
@@ -1,4 +1,6 @@
 class LegalContact < ActiveRecord::Base
+  NUMBER_OF_SEASONS_LEGAL_AGREEMENT_IS_VALID_FOR = 3
+
   belongs_to :chapter, optional: true
 
   has_one :legal_document, -> { where(active: true) }, class_name: "Document", as: :signer
@@ -6,4 +8,12 @@ class LegalContact < ActiveRecord::Base
 
   validates :email_address, presence: true, email: true
   validates :full_name, presence: true
+
+  def seasons_legal_agreement_is_valid_for
+    if legal_document&.season_signed.present?
+      Array(legal_document.season_signed...(legal_document.season_signed + NUMBER_OF_SEASONS_LEGAL_AGREEMENT_IS_VALID_FOR))
+    else
+      []
+    end
+  end
 end
