b4b07e0d16082e6c78b5b2a7f83fc1c47db20831
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-05-21 09:54:00 -0600

    Chore: Schedule legal agreement jobs to be sent later
    This will change `perform_now` to `perform_later` for the legal
    agreements. I was testing locally with `perform_now` and forgot to
    update them.
    
    Refs: #4697

--- app/controllers/admin/chapter_memorandum_of_understanding_controller.rb
+++ app/controllers/admin/chapter_memorandum_of_understanding_controller.rb
@@ -3,7 +3,7 @@ module Admin
     def create
       @chapter = Chapter.find(params[:chapter_id])
 
-      SendMemorandumOfUnderstandingJob.perform_now(chapter_id: @chapter.id)
+      SendMemorandumOfUnderstandingJob.perform_later(chapter_id: @chapter.id)
 
       redirect_to admin_chapter_path(@chapter),
         success: "Successfully scheduled job to send MOU to #{@chapter.legal_contact.full_name}"
