87861e91c28afaf20a1453d1dfa1ad6c192d62cd
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-05-22 09:24:20 -0600

    Chore: Add legal agreement columns to chapter's datagrid
    This will add these extra columns to the chapter's datagrid:
    
    - Legal agreement status
    - Legal agreement valid for
    
    Refs: #4718

--- app/data_grids/chapters_grid.rb
+++ app/data_grids/chapters_grid.rb
@@ -32,6 +32,18 @@ class ChaptersGrid
 
   filter(:visible_on_map, :xboolean)
 
+  column :legal_agreement_status do
+    if legal_document.present?
+      legal_document.signed? ? "Signed" : "Not signed"
+    else
+      "Not sent"
+    end
+  end
+
+  column :legal_agreement_valid_for do
+    legal_contact&.seasons_legal_agreement_is_valid_for&.join(", ")
+  end
+
   column :visible_on_map, header: "Visible on map"
 
   column :actions, mandatory: true, html: true do |chapter|
