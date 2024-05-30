65a5986ab87d9e2159db6f5e214885bc1aec2be0
Vivian Canales <vcnales@gmail.com>
2024-05-29 21:25:45 -0500

    Added functionality to update the viewed_community_connections field

--- app/controllers/chapter_ambassador/community_connections_controller.rb
+++ app/controllers/chapter_ambassador/community_connections_controller.rb
@@ -1,5 +1,13 @@
 module ChapterAmbassador
   class CommunityConnectionsController < ChapterAmbassadorController
     layout "chapter_ambassador_rebrand"
+
+    after_action :update_viewed_community_connections, only: :show
+
+    private
+
+    def update_viewed_community_connections
+      current_ambassador.update(viewed_community_connections: true)
+    end
   end
 end
