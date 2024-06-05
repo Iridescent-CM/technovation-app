94aebb55b8f4e9731ce63b9d371c2f74b8bf9673
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-06-05 15:31:42 -0600

    Chore: Add method to determine if chapter program info is complete
    This will add a new method to tell us if the chapter program information
    has been completed or not.
    
    Refs: #4689

--- app/models/chapter_program_information.rb
+++ app/models/chapter_program_information.rb
@@ -14,4 +14,21 @@ class ChapterProgramInformation < ActiveRecord::Base
 
   has_many :chapter_program_information_meeting_facilitators
   has_many :meeting_facilitators, through: :chapter_program_information_meeting_facilitators
+
+  def complete?
+    [
+      child_safeguarding_policy_and_process,
+      team_structure,
+      external_partnerships,
+      start_date,
+      program_model,
+      number_of_low_income_or_underserved_calculation,
+      program_length,
+      participant_count_estimate,
+      low_income_estimate,
+      organization_types,
+      meeting_times,
+      meeting_facilitators
+    ].all?(&:present?)
+  end
 end
