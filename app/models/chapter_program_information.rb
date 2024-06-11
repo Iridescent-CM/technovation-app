94d611ed79b452ea437a814f1b09ce0c6e3dce0e
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-06-10 20:38:46 -0600

    Chore: Delete associated chapter data when chapter is deleted
    This will delete the associated chapter data when a chapter is deleted.
    
    We may want to revisit this approach (we might wan to anonymize the
    data, or deactivate the chapter...) in the future.
    
    Refs: #4689

--- app/models/chapter_program_information.rb
+++ app/models/chapter_program_information.rb
@@ -6,13 +6,13 @@ class ChapterProgramInformation < ActiveRecord::Base
   belongs_to :participant_count_estimate
   belongs_to :low_income_estimate
 
-  has_many :chapter_program_information_organization_types
+  has_many :chapter_program_information_organization_types, dependent: :destroy
   has_many :organization_types, through: :chapter_program_information_organization_types
 
-  has_many :chapter_program_information_meeting_times
+  has_many :chapter_program_information_meeting_times, dependent: :destroy
   has_many :meeting_times, through: :chapter_program_information_meeting_times
 
-  has_many :chapter_program_information_meeting_facilitators
+  has_many :chapter_program_information_meeting_facilitators, dependent: :destroy
   has_many :meeting_facilitators, through: :chapter_program_information_meeting_facilitators
 
   def complete?
