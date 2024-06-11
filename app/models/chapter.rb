179d69e187e17e91f77c8aa88f534dda9b32ad36
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-06-10 20:38:46 -0600

    Chore: Delete associated chapter data when chapter is deleted
    This will delete the associated chapter data when a chapter is deleted.
    
    We may want to revisit this approach (we might wan to anonymize the
    data, or deactivate the chapter...) in the future.
    
    Refs: #4689

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -4,12 +4,12 @@ class Chapter < ActiveRecord::Base
   belongs_to :primary_contact, class_name: "ChapterAmbassadorProfile", foreign_key: "primary_contact_id", optional: true
 
   has_one :legal_contact, dependent: :destroy
-  has_one :chapter_program_information
+  has_one :chapter_program_information, dependent: :destroy
 
   has_many :chapter_ambassador_profiles
-  has_many :chapter_links, class_name: "RegionalLink"
+  has_many :chapter_links, class_name: "RegionalLink", dependent: :destroy
   has_many :student_profiles
-  has_many :registration_invites, class_name: "UserInvitation"
+  has_many :registration_invites, class_name: "UserInvitation", dependent: :destroy
 
   accepts_nested_attributes_for :chapter_links, reject_if: ->(attrs) {
     attrs.reject { |k, _| k.to_s == "custom_label" }.values.any?(&:blank?)
