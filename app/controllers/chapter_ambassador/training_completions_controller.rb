972e518fbceae7a43a9b8c04ace22279223c7695
Vivian Canales <vcnales@gmail.com>
2024-06-13 13:26:45 -0500

    updated training completions controller to mark training complete

--- app/controllers/chapter_ambassador/training_completions_controller.rb
+++ app/controllers/chapter_ambassador/training_completions_controller.rb
@@ -3,5 +3,12 @@ module ChapterAmbassador
     skip_before_action :require_chapter_and_chapter_ambassador_onboarded
 
     layout "chapter_ambassador_rebrand"
+
+    def show
+      current_ambassador.complete_training!
+      redirect_to chapter_ambassador_dashboard_path,
+        success: "Thank you for completing the checkpoint!"
+    end
+
   end
 end
