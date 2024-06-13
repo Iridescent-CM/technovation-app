127eca3e7c3983b4b3848723c32c0fa8cfc4b470
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-06-13 06:58:11 -0600

    Chore: Remove unused ChA controller
    This controller was added as a placeholder, but isn't being used.
    
    Refs: #4689

--- app/controllers/chapter_ambassador/program_information_controller.rb
+++
@@ -1,7 +0,0 @@
-module ChapterAmbassador
-  class ProgramInformationController < ChapterAmbassadorController
-    skip_before_action :require_chapter_and_chapter_ambassador_onboarded
-
-    layout "chapter_ambassador_rebrand"
-  end
-end
