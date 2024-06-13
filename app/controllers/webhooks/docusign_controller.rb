ac0c4ad56d5363c0339d291c1e86f02898179388
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-06-13 11:32:20 -0600

    Feature: HMAC security for DocuSign webhook payloads
    This will add a new service that will verify webhook payloads with the
    HMAC standard. This service should be able to be reused for future
    webhooks.
    
    For DocuSign, the payload will get verified before being saved to the
    database and being processed by the background job.
    
    Refs: #4716

--- app/controllers/webhooks/docusign_controller.rb
+++ app/controllers/webhooks/docusign_controller.rb
@@ -4,4 +4,22 @@ class Webhooks::DocusignController < Webhooks::BaseController
 
     head :ok
   end
+
+  private
+
+  def verify_webhook_payload
+    if VerifyWebhookPayload
+        .new(
+          payload: request.body.read,
+          secret: ENV.fetch("DOCUSIGN_HMAC_SECRET_KEY"),
+          signature: request.headers["X-Docusign-Signature-1"]
+        )
+        .call
+        .success?
+
+      save_webhook_payload
+    else
+      head :ok
+    end
+  end
 end
