5e785dc8c49b799c85d10c98e59566449af27665
Vivian Canales <vcnales@gmail.com>
2024-06-14 15:48:08 -0500

    Updated full text for 2024 certificates

--- lib/fill_pdfs/certified_judge.rb
+++ lib/fill_pdfs/certified_judge.rb
@@ -5,7 +5,7 @@ module FillPdfs
     include FillPdfs
 
     def full_text
-      "For their dedication to providing valuable feedback on 5 submissions as a Certified Judge in the #{recipient.season} season."
+      "For their dedication to providing personalized and actionable feedback on 5 technology based projects in the Technovation Girls #{recipient.season} program season. This feedback will further enrich Technovation Girls participants' learning journeys."
     end
   end
 end
--- lib/fill_pdfs/head_judge.rb
+++ lib/fill_pdfs/head_judge.rb
@@ -6,9 +6,9 @@ module FillPdfs
 
     def full_text
       if recipient.account.judge_profile.live_event?
-        "For their participation in a Regional Pitch Event in the #{recipient.season} season."
+        "For their dedication to providing personalized and actionable feedback on technology based projects during a Regional Pitch Event in the Technovation Girls #{recipient.season} program season. This feedback will further enrich Technovation Girls participants' learning journeys."
       else
-        "For their dedication to providing valuable feedback on 6 to 10 submissions as a Head Judge in the #{recipient.season} season."
+        "For their dedication to providing personalized and actionable feedback on 6-10 technology based projects in the Technovation Girls #{recipient.season} program season. This feedback will further enrich Technovation Girls participants' learning journeys."
       end
     end
   end
