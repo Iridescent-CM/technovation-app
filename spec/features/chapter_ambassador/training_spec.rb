0742d2fb2ce25f9a0b184aadd338c6343baa0d88
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-06-18 13:00:25 -0600

    Chore: Add :training_completed_at trait for ChA factory
    This will add a new :training_not_completed trait to the chapter
    ambassador factory. This new trait needs to be used for both existing
    chapter ambassador training specs.
    
    Refs: #4779

--- spec/features/chapter_ambassador/training_spec.rb
+++ spec/features/chapter_ambassador/training_spec.rb
@@ -1,20 +1,20 @@
 require "rails_helper"
 
 RSpec.feature "Chapter Ambassador Training" do
-  scenario "Visiting the training completions endpoint marks training as complete and displays a thank you message" do
-    chapter_ambassador = FactoryBot.create(:chapter_ambassador)
+  let(:chapter_ambassador) { FactoryBot.create(:chapter_ambassador, :training_not_completed) }
 
+  before do
     sign_in(chapter_ambassador)
+  end
+
+  scenario "Visiting the training completions endpoint marks training as complete and displays a thank you message" do
     visit chapter_ambassador_training_completion_path
 
-    expect(chapter_ambassador.training_completed?).to eq(true)
+    expect(chapter_ambassador.reload.training_completed?).to eq(true)
     expect(page).to have_text("Thank you for completing the checkpoint!")
   end
 
   scenario "Visiting the training endpoint when training hasn't been completed displays a link to take the training" do
-    chapter_ambassador = FactoryBot.create(:chapter_ambassador, training_completed_at: nil)
-
-    sign_in(chapter_ambassador)
     visit chapter_ambassador_training_path
 
     expect(page).to have_text("Click the link below to proceed to the training checkpoint.")
