d98a0eff6468b32c2769cb7fe08e354e9a58854c
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-06-18 10:56:40 -0600

    Chore: Update chapter ambassador training specs
    This will hopefully make it clearer what is being tested in each
    scenario.
    
    The structure of these specs had to change because for the second
    scenario we need to use a chapter ambassador who hasn't completed
    training yet.
    
    Refs: #4779

--- spec/features/chapter_ambassador/training_spec.rb
+++ spec/features/chapter_ambassador/training_spec.rb
@@ -1,18 +1,20 @@
 require "rails_helper"
 
-RSpec.feature "Chapter Ambassador can complete training checkpoint" do
-  let(:chapter_ambassador) { FactoryBot.create(:chapter_ambassador) }
+RSpec.feature "Chapter Ambassador Training" do
+  scenario "Visiting the training completions endpoint marks training as complete and displays a thank you message" do
+    chapter_ambassador = FactoryBot.create(:chapter_ambassador)
 
-  before do
     sign_in(chapter_ambassador)
-  end
-
-  scenario "Chapter ambassador has completed training" do
     visit chapter_ambassador_training_completion_path
+
+    expect(chapter_ambassador.training_completed?).to eq(true)
     expect(page).to have_text("Thank you for completing the checkpoint!")
   end
 
-  scenario "Chapter ambassador has not completed training" do
+  scenario "Visiting the training endpoint when training hasn't been completed displays a link to take the training" do
+    chapter_ambassador = FactoryBot.create(:chapter_ambassador, training_completed_at: nil)
+
+    sign_in(chapter_ambassador)
     visit chapter_ambassador_training_path
 
     expect(page).to have_text("Click the link below to proceed to the training checkpoint.")
