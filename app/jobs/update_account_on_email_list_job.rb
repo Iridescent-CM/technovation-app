96f355b1d2375f31ea91f75df2d0c7ba8c18be84
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-07-09 07:25:59 -0600

    Chore: Remove calls to Mailchimp
    This will remove the calls to Mailchimp, the Mailchip
    service/functionality is still there though.
    
    Refs: #4385

--- app/jobs/update_account_on_email_list_job.rb
+++ app/jobs/update_account_on_email_list_job.rb
@@ -4,7 +4,6 @@ class UpdateAccountOnEmailListJob < ActiveJob::Base
   def perform(account_id:, currently_subscribed_as:)
     account = Account.find(account_id)
 
-    Mailchimp::MailingList.new.update(account: account, currently_subscribed_as: currently_subscribed_as)
     Salesforce::ApiClient.new.update_contact(account: account)
   end
 end
