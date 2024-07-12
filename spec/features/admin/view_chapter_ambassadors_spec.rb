3149db9882fb6c451c87c3c04596af7669a39c50
Vivian Canales <vcnales@gmail.com>
2024-07-11 21:10:07 -0500

    added tests for viewing chapter details on chapter ambassador participant detailed page

---
+++ spec/features/admin/view_chapter_ambassadors_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.feature "Admins view Chapter Ambassador participant details" do
+  scenario "when Chapter Ambassador is assigned to a chapter, chapter name and organization are displayed" do
+    chapter_ambassador = FactoryBot.create(:chapter_ambassador)
+    chapter = chapter_ambassador.chapter
+
+    sign_in(:admin)
+
+    click_link "Participants"
+    within("tr#account_#{chapter_ambassador.account_id}") { click_link "view" }
+
+    expect(current_path).to eq(admin_participant_path(chapter_ambassador.account))
+
+    expect(page).to have_content "Chapter (Program name)"
+    expect(page).to have_link chapter.name, href: admin_chapter_path(chapter)
+    expect(page).to have_content "Chapter Organization"
+    expect(page).to have_content "#{chapter.organization_name}"
+  end
+
+  scenario "when Chapter Ambassador is not assigned to a chapter, chapter name and organization are not displayed" do
+    chapter_ambassador = FactoryBot.create(:chapter_ambassador, :not_assigned_to_chapter)
+
+    sign_in(:admin)
+
+    click_link "Participants"
+    within("tr#account_#{chapter_ambassador.account_id}") { click_link "view" }
+
+    expect(current_path).to eq(admin_participant_path(chapter_ambassador.account))
+
+    expect(page).to have_content "Chapter"
+    expect(page).to have_content "Not assigned to a chapter"
+  end
+end
