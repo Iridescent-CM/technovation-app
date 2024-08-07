ba6a2b734b6be4b625db38bbaad810b680d7ca93
Vivian Canales <vcnales@gmail.com>
2024-08-06 21:06:05 -0500

    Added complete onboarding tasks to the onboarding tasks concern

--- app/controllers/concerns/onboarding_tasks_concern.rb
+++ app/controllers/concerns/onboarding_tasks_concern.rb
@@ -4,4 +4,8 @@ module OnboardingTasksConcern
   def incomplete_onboarding_tasks
     required_onboarding_tasks.reject { |_, completed| completed }.keys
   end
+
+  def complete_onboarding_tasks
+    required_onboarding_tasks.select { |_, completed| completed }.keys
+  end
 end
