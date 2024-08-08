ad650a480d376266ebaf808ab4e692b9acbbe302
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-08-08 14:12:22 -0600

    Chore: Conditionallty hide chapter affiliation agreement buttons
    When a job is scheduled to send or void a chapter affiliation agreement,
    we'll redirect back to the chapter's page with a new URL parameter
    `affiliation_agreement_job_scheduled`, the send and void buttons will
    not be displayed if this URL parameter is present.
    
    Refs: #4917

--- app/controllers/admin/chapter_affiliation_agreement_controller.rb
+++ app/controllers/admin/chapter_affiliation_agreement_controller.rb
@@ -9,7 +9,7 @@ module Admin
       else
         SendChapterAffiliationAgreementJob.perform_later(chapter_id: @chapter.id)
 
-        redirect_to admin_chapter_path(@chapter),
+        redirect_to admin_chapter_path(@chapter, affiliation_agreement_job_scheduled: 1),
           success: "Successfully scheduled job to send a Chapter Affiliation Agreement to #{@chapter.legal_contact.full_name}"
       end
     end
@@ -19,7 +19,7 @@ module Admin
 
       VoidDocumentJob.perform_later(document_id: chapter.affiliation_agreement.id)
 
-      redirect_to admin_chapter_path(chapter),
+      redirect_to admin_chapter_path(chapter, affiliation_agreement_job_scheduled: 1),
         success: "Successfully scheduled job to void the Chapter Affiliation Agreement for #{chapter.legal_contact.full_name}"
     end
   end
