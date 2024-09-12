093c304b050a1ac1618481068fe5ece900aa375b
Vivian Canales <vcnales@gmail.com>
2024-09-12 13:15:09 -0500

    updated methods to check for chapter id parameter

--- app/controllers/admin/locations_controller.rb
+++ app/controllers/admin/locations_controller.rb
@@ -5,10 +5,12 @@ module Admin
     private
 
     def db_record
-      @db_record ||= if account_id = params.fetch(:account_id) { false }
-        Account.find(account_id)
-      else
+      @db_record ||= if params.has_key?(:account_id)
+        Account.find(params.fetch(:account_id))
+      elsif params.has_key?(:team_id)
         Team.find(params.fetch(:team_id))
+      elsif params.has_key?(:chapter_id)
+        Chapter.find(params.fetch(:chapter_id))
       end
     end
   end
