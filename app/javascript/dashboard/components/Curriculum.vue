fb49d7a0b383d5e06a2d92b7d11889db4f94b7e9
Vivian Canales <vcnales@gmail.com>
2024-09-17 12:25:48 -0500

    Update curriculum links

--- app/javascript/dashboard/components/Curriculum.vue
+++ app/javascript/dashboard/components/Curriculum.vue
@@ -5,61 +5,80 @@
         <div class="grid__col-12">
           <h1>Curriculum</h1>
           <p>
-            Learn how to make your project as strong as possible by following these lessons.
-            Most participants spend around 40+ hours working on their ideas.
+            Learn how to make your project as strong as possible by following
+            these lessons. Most participants spend around 40+ hours working on
+            their ideas.
           </p>
 
-          <h3 class="margin--t-xlarge margin--b-none">Beginner Division</h3>
+          <h3 class="margin--t-xlarge margin--b-none">Students</h3>
           <p>
-            <a href="https://beginner.technovationchallenge.org/" class="button" target="_blank">
-              Go to Beginner Lessons
+            <a
+              href="https://technovationchallenge.org/technovation-curriculum/"
+              class="button"
+              target="_blank"
+            >
+              Go to Student Curriculum
             </a>
           </p>
 
-          <h3 class="margin--t-xlarge margin--b-none">Junior and Senior Divisions</h3>
+          <h3 class="margin--t-xlarge margin--b-none">Mentors</h3>
           <p>
-            <a href="https://technovationchallenge.org/curriculum-intro/registered/new/" class="button" target="_blank">
-              Go to Junior/Senior Lessons
-            </a>
-          </p>
-
-          <h3 class="margin--t-xlarge margin--b-none">Mentor Curriculum</h3>
-          <p>
-            <a href="https://technovationchallenge.org/mentor-curriculum/" class="button" target="_blank">
+            <a
+              href="https://technovationchallenge.org/mentor-curriculum/"
+              class="button"
+              target="_blank"
+            >
               Go to Mentor Curriculum
             </a>
           </p>
 
           <h3 class="margin--t-xlarge margin--b-none">Judging</h3>
-          <p class="margin--t-none">Visit these helpful links to prepare your project for judging:</p>
+          <p class="margin--t-none">
+            Visit these helpful links to prepare your project for judging:
+          </p>
 
           <ul>
             <li>
-              <a href="https://technovationchallenge.org/submission-guidelines/" target="_blank">
+              <a
+                href="https://technovationchallenge.org/submission-guidelines/"
+                target="_blank"
+              >
                 Submission Guidelines (updated each season)
               </a>
             </li>
 
             <li>
-              <a href="https://technovationchallenge.org/curriculum/judging-rubric/" target="_blank">
+              <a
+                href="https://technovationchallenge.org/curriculum/judging-rubric/"
+                target="_blank"
+              >
                 Judging Rubrics
               </a>
             </li>
 
             <li>
-              <a href="https://technovationchallenge.org/program-timeline" target="_blank">
+              <a
+                href="https://technovationchallenge.org/program-timeline"
+                target="_blank"
+              >
                 Suggested Milestones
               </a>
             </li>
 
             <li>
-              <a href="https://technovationchallenge.org/award-structure/" target="_blank">
+              <a
+                href="https://technovationchallenge.org/award-structure/"
+                target="_blank"
+              >
                 Awards Page
               </a>
             </li>
 
             <li>
-              <a href="https://www.technovation.org/app-gallery/" target="_blank">
+              <a
+                href="https://www.technovation.org/app-gallery/"
+                target="_blank"
+              >
                 Apps and AI Inventions from Past Teams
               </a>
             </li>
@@ -72,6 +91,6 @@
 
 <script>
 export default {
-  name: 'curriculum',
-}
+  name: "curriculum",
+};
 </script>
