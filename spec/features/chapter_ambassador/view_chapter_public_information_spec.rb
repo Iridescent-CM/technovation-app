c7961bd3deaff7bf66df268a9ff3632371abc1f2
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-09-24 12:35:46 -0600

    Chore: Refactor chapter assignments
    This will change how chapter assignments are done, a chapter assignment
    used to be one-to-one - one profile type assigned to one chapter. This
    new way will associate accounts (not profiles) to chapters, and will
    allow accounts to be able to be associated to multiple chapters.
    
    Since chapters are now associated to accounts, the other big change is that a
    chapter's primary contact will now be associated to an account instead
    of a chapter ambassador profile.
    
    The rest of the changes here are to support those two^ changes.
    
    Refs: #5024

--- spec/features/chapter_ambassador/view_chapter_public_information_spec.rb
+++ spec/features/chapter_ambassador/view_chapter_public_information_spec.rb
@@ -1,10 +1,9 @@
 require "rails_helper"
 
 RSpec.feature "Chapter Ambassadors can click on chapter profile tab" do
-  let(:chapter) { FactoryBot.create(:chapter) }
+  let(:chapter_ambassador) { FactoryBot.create(:chapter_ambassador) }
 
   scenario "All Chapter Details links are visible in the side navigation" do
-    chapter_ambassador = FactoryBot.create(:chapter_ambassador, chapter: chapter)
     sign_in(chapter_ambassador)
 
     click_link "Chapter Profile"
@@ -17,14 +16,13 @@ RSpec.feature "Chapter Ambassadors can click on chapter profile tab" do
   end
 
   scenario "A Chapter Ambassador assigned to a chapter can view chapter public information" do
-    chapter_ambassador = FactoryBot.create(:chapter_ambassador, chapter: chapter)
     sign_in(chapter_ambassador)
 
     click_link "Chapter Profile"
     click_link "Public Info"
 
-    expect(page).to have_content(chapter.organization_name)
-    expect(page).to have_content(chapter.name)
+    expect(page).to have_content(chapter_ambassador.chapter.organization_name)
+    expect(page).to have_content(chapter_ambassador.chapter.name)
     expect(page).to have_content("This chapter is displayed on the map of chapters on the Technovation website")
   end
 
