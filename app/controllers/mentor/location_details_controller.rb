ca6a619a3b383fa4cec3e6b24dfa5b22ff2281da
Vivian Canales <vcnales@gmail.com>
2024-09-24 10:08:47 -0500

    updated mentor location to use rebranded form

--- app/controllers/mentor/location_details_controller.rb
+++ app/controllers/mentor/location_details_controller.rb
@@ -1,6 +1,7 @@
 module Mentor
   class LocationDetailsController < MentorController
     helper_method :current_profile
+    layout "application_rebrand"
 
     def show
       render template: "location_details/show"
