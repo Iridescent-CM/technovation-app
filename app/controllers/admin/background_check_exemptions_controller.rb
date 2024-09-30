640b2f2c821142238f020350421c446da3d77358
Vivian Canales <vcnales@gmail.com>
2024-09-30 10:28:33 -0500

    Added activity tracking for background check exemption

--- app/controllers/admin/background_check_exemptions_controller.rb
+++ app/controllers/admin/background_check_exemptions_controller.rb
@@ -4,6 +4,14 @@ module Admin
       @account = Account.find(params[:participant_id])
 
       if @account.grant_background_check_exemption
+        @account.create_activity(
+          key: "account.update",
+          owner: current_account,
+          params: {
+            changes:
+              {background_check_exemption: @account.saved_change_to_background_check_exemption}
+          }
+        )
         redirect_to admin_participant_path(@account),
           success: "Background check exemption granted"
       else
@@ -16,6 +24,14 @@ module Admin
       @account = Account.find(params[:participant_id])
 
       if @account.revoke_background_check_exemption
+        @account.create_activity(
+          key: "account.update",
+          owner: current_account,
+          params: {
+            changes:
+              {background_check_exemption: @account.saved_change_to_background_check_exemption}
+          }
+        )
         redirect_to admin_participant_path(@account),
           success: "Background check exemption revoked"
       else
