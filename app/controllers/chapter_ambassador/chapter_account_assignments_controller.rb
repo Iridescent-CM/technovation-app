91ca5980d7819d6b142185f06a3799353b6c952c
Vivian Canales <vcnales@gmail.com>
2024-10-24 21:36:22 -0500

    Added route and controller for chapter account assignment for ChAs

---
+++ app/controllers/chapter_ambassador/chapter_account_assignments_controller.rb
@@ -0,0 +1,28 @@
+module ChapterAmbassador
+  class ChapterAccountAssignmentsController < ChapterAmbassadorController
+    def create
+      account = Account.find(params.fetch(:account_id))
+
+      account.chapter_assignments.where(season: Season.current.year).delete_all
+      account.update(no_chapter_selected: nil)
+
+      if chapter_account_assignment_params.fetch(:chapter_id).present?
+        account.chapter_assignments.create(
+          profile: account.mentor_profile.presence || account.student_profile,
+          chapter_id: chapter_account_assignment_params.fetch(:chapter_id),
+          season: Season.current.year,
+          primary: true
+        )
+      end
+
+      redirect_to chapter_ambassador_participants_path,
+        success: "Successfully assigned #{account.full_name} to a your chapter"
+    end
+
+    private
+
+    def chapter_account_assignment_params
+      params.require(:chapter_account_assignment).permit(:chapter_id)
+    end
+  end
+end
