7ac02c08cba25dc0921a7dfb41fed4f734318eed
Vivian Canales <vcnales@gmail.com>
2024-11-06 16:48:53 -0600

    updated meet your cha section text

--- app/controllers/student/mentor_searches_controller.rb
+++ app/controllers/student/mentor_searches_controller.rb
@@ -1,5 +1,7 @@
 module Student
   class MentorSearchesController < StudentController
+    include RequireParentalConsentSigned
+    include RequireLocationIsSet
     include ::MentorSearchesController
   end
 end
