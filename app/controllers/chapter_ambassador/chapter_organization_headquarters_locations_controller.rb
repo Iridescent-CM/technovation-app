fb3d5702a68c6f537c3e72b97d4b83857e3bbe1b
Vivian Canales <vcnales@gmail.com>
2024-11-06 13:19:24 -0600

    update chapter hq location naming/text

--- app/controllers/chapter_ambassador/chapter_organization_headquarters_locations_controller.rb
+++ app/controllers/chapter_ambassador/chapter_organization_headquarters_locations_controller.rb
@@ -6,18 +6,18 @@ module ChapterAmbassador
 
     def update
       @chapter = current_ambassador.chapter
-      if @chapter.update(chapter_location_params)
+      if @chapter.update(chapter_organization_headquarters_location_params)
         redirect_to chapter_ambassador_chapter_location_path,
-          success: "You updated your chapter location details!"
+          success: "You updated your chapter organization headquarters location details!"
       else
-        flash.now[:alert] = "Error updating chapter location details."
+        flash.now[:alert] = "Error updating chapter organization headquarters location details."
         render :edit
       end
     end
 
     private
 
-    def chapter_location_params
+    def chapter_organization_headquarters_location_params
       params.require(:chapter).permit(
         :organization_headquarters_location
       )
