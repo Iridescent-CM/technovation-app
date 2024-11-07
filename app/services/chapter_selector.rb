85229fa7a4b5d503a8008a688f4a5fff47041a5b
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-11-07 16:32:00 -0700

    Feature: Chapter selection revamp
    A pretty big refactoring that will create 8 different permutations of
    the chapter selection screen, for both students and mentors:
    
    - Chapters found only in state/province
    - Chapters found in state/province AND country
      - State/province chapters will be displayed by default
      - There will now be a button to display additional chapters in
        someone's country
    - Chapters found only in the country
      - No chapters will be displayed by default, they will have to click a
        button to display additional chapters in their country
    - No chapters found
    
    And a few other updates:
    
    - Adding mentor mentoring options to no chapters available screen
    - Updating some of the text
    
    Refs: #5138 #5151 #5152

--- app/services/chapter_selector.rb
+++ app/services/chapter_selector.rb
@@ -4,32 +4,44 @@ class ChapterSelector
   end
 
   def call
-    find_chapters_by_state_province.presence || find_chapters_by_country
+    {
+      chapters_in_state_province: find_chapters_in_state_province,
+      chapters_in_country: find_chapters_in_country
+    }
   end
 
   private
 
   attr_accessor :account
 
-  def find_chapters_by_state_province
-    select_chapters_where({
-      state_province: account.state_code,
-      country: account.country_code
-    })
+  def find_chapters_in_state_province
+    select_chapters(
+      where: {
+        state_province: account.state_code,
+        country: account.country_code
+      }
+    )
   end
 
-  def find_chapters_by_country
-    select_chapters_where({
-      country: account.country_code
-    })
+  def find_chapters_in_country
+    select_chapters(
+      where: {
+        country: account.country_code
+      },
+      where_not: {
+        state_province: account.state_code
+      }
+    )
   end
 
-  def select_chapters_where(where_clause)
+  def select_chapters(where:, where_not: {})
     Chapter
       .joins(legal_contact: :chapter_affiliation_agreement)
       .includes(legal_contact: :chapter_affiliation_agreement)
       .includes(:primary_contact)
       .where(chapter_affiliation_agreement: {status: ["signed", "off-platform"]})
-      .where(where_clause)
+      .where(where)
+      .where.not(where_not)
+      .order(:name)
   end
 end
