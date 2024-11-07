a00c0189a37a2468c268403eca92b2059a3cb7cc
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-11-07 16:32:00 -0700

    Feature: Chapter selection revamp
    A pretty big refactoring that will create 8 different permutations of
    the chapter selection screen, for both students and mentors:
    
    - Chapters found only in state/province
    - Chapters found in state/province AND country
      - State/province chapters will be displayed by default
      - There will now be a button to display additional chapters in
        someone's country
    - Chapters found only in the country
      - No chapters will be displayed by default, they will have to click a
        button to display additional chapters in their country
    - No chapters found
    
    And a few other updates:
    
    - Adding mentor mentoring options to no chapters available screen
    - Updating some of the text
    
    Refs: #5138 #5151 #5152

--- app/controllers/chapter_account_assignments_controller.rb
+++ app/controllers/chapter_account_assignments_controller.rb
@@ -3,7 +3,10 @@ class ChapterAccountAssignmentsController < ApplicationController
 
   def new
     @chapter_assignment = ChapterAccountAssignment.new
-    @chapters = ChapterSelector.new(account: current_account).call
+
+    all_chapters = ChapterSelector.new(account: current_account).call
+    @primary_chapters = all_chapters[:chapters_in_state_province]
+    @additional_chapters = all_chapters[:chapters_in_country]
   end
 
   def create
