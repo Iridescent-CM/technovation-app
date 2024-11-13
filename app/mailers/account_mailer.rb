488f659165f12d64e0a4ea82f352f2558c933414
shaun <58957909+shaun-technovation@users.noreply.github.com>
2024-11-13 16:28:51 -0700

    Chore: Email mentors when they're assigned to a chapter
    This will send a notification email to a mentor after they have been
    assigned to a chapter.
    
    Refs: #5160

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -62,4 +62,13 @@ class AccountMailer < ApplicationMailer
       mail to: account.email
     end
   end
+
+  def chapter_assigned(account)
+    @first_name = account.first_name
+    @chapter_name = account.current_chapter.name
+
+    I18n.with_locale(account.locale) do
+      mail to: account.email
+    end
+  end
 end
--- app/mailers/student_mailer.rb
+++ app/mailers/student_mailer.rb
@@ -6,13 +6,4 @@ class StudentMailer < ApplicationMailer
       mail to: account.email
     end
   end
-
-  def chapter_assigned(account)
-    @first_name = account.first_name
-    @chapter_name = account.current_chapter.name
-
-    I18n.with_locale(account.locale) do
-      mail to: account.email
-    end
-  end
 end
