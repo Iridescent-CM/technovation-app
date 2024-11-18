0265ce7c533e9a01d89f10d8e7ae3e268b43c9c4
Vivian Canales <vcnales@gmail.com>
2024-11-18 17:13:53 -0600

    Added club specs

---
+++ spec/controllers/admin/clubs_controller_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+RSpec.describe Admin::ClubsController do
+  before do
+    sign_in(:admin)
+  end
+  describe "POST #create" do
+    context "with valid attributes" do
+      it "creates a new club" do
+        expect {
+          post :create, params: {
+            club: {name: "Hello World"}
+          }
+        }.to change { Club.count }.by(1)
+      end
+
+      it "redirects to the created club" do
+        post :create, params: {
+          club: {name: "Hello World Again"}
+        }
+        expect(response).to redirect_to(admin_club_path(Club.last))
+      end
+    end
+
+    context "with invalid attributes" do
+      it "does not create a new club" do
+        expect {
+          post :create, params: {
+            club: {name: ""}
+          }
+        }.not_to change { Club.count }
+      end
+
+    end
+  end
+end
