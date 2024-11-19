02f9a5558fbf3e989a65a56bf9886cc54c3d3c0d
Vivian Canales <vcnales@gmail.com>
2024-11-19 10:33:07 -0600

    Added club id to location logic

--- app/controllers/admin/locations_controller.rb
+++ app/controllers/admin/locations_controller.rb
@@ -11,6 +11,8 @@ module Admin
         Team.find(params.fetch(:team_id))
       elsif params.has_key?(:chapter_id)
         Chapter.find(params.fetch(:chapter_id))
+      elsif params.has_key?(:club_id)
+        Club.find(params.fetch(:club_id))
       end
     end
   end
