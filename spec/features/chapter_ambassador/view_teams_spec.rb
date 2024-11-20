4e26f794973df1f8b831fe1ab9821c3fe28f91a9
Vivian Canales <vcnales@gmail.com>
2024-11-20 16:12:13 -0600

    Added test

---
+++ spec/features/chapter_ambassador/view_teams_spec.rb
@@ -0,0 +1,53 @@
+require "rails_helper"
+
+RSpec.feature "Chapter Ambassador viewing their teams" do
+  let(:chapter) { FactoryBot.create(:chapter, :chicago, :onboarded) }
+  let(:chapter_ambassador) { FactoryBot.create(:chapter_ambassador, :not_assigned_to_chapter) }
+
+  before do
+    chapter_ambassador.chapter_assignments.create(
+      account: chapter_ambassador.account,
+      chapter: chapter,
+      season: Season.current.year
+    )
+  end
+
+  scenario "displays teams of students assigned to their chapter" do
+    affiliated_student = FactoryBot.create(:student, :chicago)
+    affiliated_student.chapter_assignments.create(
+      account: affiliated_student.account,
+      chapter: chapter,
+      season: Season.current.year
+    )
+
+    team = FactoryBot.create(:team)
+    TeamRosterManaging.add(team, affiliated_student)
+
+    affiliated_student_2 = FactoryBot.create(:student, :chicago)
+    affiliated_student_2.chapter_assignments.create(
+      account: affiliated_student_2.account,
+      chapter: chapter,
+      season: Season.current.year
+    )
+
+    team_2 = FactoryBot.create(:team)
+    TeamRosterManaging.add(team_2, affiliated_student_2)
+
+    sign_in(chapter_ambassador)
+    visit(chapter_ambassador_teams_path)
+
+    expect(page).to have_content(affiliated_student.team.name)
+    expect(page).to have_content(affiliated_student_2.team.name)
+  end
+
+  scenario "does not display teams of students not assigned to their chapter" do
+    affiliated_student = FactoryBot.create(:student, :brazil)
+    team = FactoryBot.create(:team)
+    TeamRosterManaging.add(team, affiliated_student)
+
+    sign_in(chapter_ambassador)
+    visit(chapter_ambassador_teams_path)
+
+    expect(page).not_to have_content(affiliated_student.team.name)
+  end
+end
