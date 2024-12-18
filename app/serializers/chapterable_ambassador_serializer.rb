f4a26690e77828eb95ad64cc3b42be488fdc27cb
Vivian Canales <vcnales@gmail.com>
2024-12-17 20:38:26 -0600

    Added chapterable ambassador serializer

---
+++ app/serializers/chapterable_ambassador_serializer.rb
@@ -0,0 +1,16 @@
+class ChapterableAmbassadorSerializer
+  include FastJsonapi::ObjectSerializer
+  set_key_transform :camel_lower
+
+  set_id :random_id
+
+  attributes :name
+
+  attribute(:api_root) do |account|
+    "#{account.scope_name}"
+  end
+
+  attribute(:avatar_url) do |account|
+    account.profile_image_url
+  end
+end
