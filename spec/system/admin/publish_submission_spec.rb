863cda2fd0e7d186b1210653d46c7a89b3a479ac
shaun <58957909+shaun-technovation@users.noreply.github.com>
2025-01-03 15:12:26 -0700

    Feature: Allow super admins to publish an individual submission
    This will allow super admins to publish an individual submission.
    
    The "Publish" button will only be displayed if the submission only needs
    to be published.
    
    Refs: #4707

---
+++ spec/system/admin/publish_submission_spec.rb
@@ -0,0 +1,84 @@
+require "rails_helper"
+
+RSpec.describe "Publishing a submission", :js do
+  context "when a submission only needs to be published" do
+    let!(:submission) { FactoryBot.create(:submission, :only_needs_to_submit) }
+
+    context "as a super admin" do
+      before do
+        sign_in(:super_admin)
+      end
+
+      it "displays a link to publish the submission" do
+        click_link "Submissions"
+        click_link submission.app_name
+
+        within(".edit-team-submission-buttons") do
+          expect(page).to have_link("Publish")
+        end
+      end
+
+      it "successfully publishes a submission" do
+        click_link "Submissions"
+        click_link submission.app_name
+
+        within(".edit-team-submission-buttons") do
+          click_link "Publish"
+        end
+
+        click_button "Yes, do it"
+
+        expect(submission.reload).to be_published
+      end
+    end
+
+    context "as an admin" do
+      before do
+        sign_in(:admin)
+      end
+
+      it "does not display a link to publish the submission" do
+        click_link "Submissions"
+        click_link submission.app_name
+
+        within(".edit-team-submission-buttons") do
+          expect(page).not_to have_link("Publish")
+        end
+      end
+    end
+  end
+
+  context "when a submission is incomplete (it needs more than just publishing)" do
+    let!(:incomplete_submission) { FactoryBot.create(:submission, :incomplete) }
+
+    context "as a super admin" do
+      before do
+        sign_in(:super_admin)
+      end
+
+      it "does not display a link to publish the submission" do
+        click_link "Submissions"
+        click_link incomplete_submission.app_name
+
+        within(".edit-team-submission-buttons") do
+          expect(page).not_to have_link("Publish")
+        end
+      end
+    end
+
+    context "as an admin" do
+      before do
+        sign_in(:admin)
+      end
+
+      it "does not display a link to publish the submission" do
+        click_link "Submissions"
+        click_link incomplete_submission.app_name
+
+        within(".edit-team-submission-buttons") do
+          expect(page).not_to have_link("Publish")
+        end
+      end
+    end
+  end
+end
