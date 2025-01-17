27fd8529e633c58e6363f4c79745358cbde49915
Vivian Canales <vcnales@gmail.com>
2025-01-17 08:16:19 -0600

    add location columns and filters on clubs grid

--- app/data_grids/clubs_grid.rb
+++ app/data_grids/clubs_grid.rb
@@ -16,11 +16,36 @@ class ClubsGrid
 
   column :name, header: "Name", mandatory: true
 
+  column :city
+
+  column :state_province, header: "State" do
+    FriendlySubregion.call(self, prefix: false)
+  end
+
+  column :country do
+    FriendlyCountry.new(self).country_name
+  end
+
   column :actions, mandatory: true, html: true do |club|
     link_to("View",
       admin_club_path(club),
       class: "button small gray",
       data: {turbolinks: false})
+  end
+
+  filter :country,
+    :enum,
+    header: "Country",
+    select: ->(g) {
+      CountryStateSelect.countries_collection
+    },
+    filter_group: "more-specific",
+    multiple: true,
+    data: {
+      placeholder: "Select or start typing..."
+    } do |values|
+    clauses = values.flatten.map { |v| "clubs.country = '#{v}'" }
+    where(clauses.join(" OR "))
   end
 
   column_names_filter(
