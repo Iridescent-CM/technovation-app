f42875b0f5d2cee75c898fea5cf7b1c319ca730d
shaun <58957909+shaun-technovation@users.noreply.github.com>
2025-01-28 12:10:34 -0700

    Chore: Remove unused ambassador controller actions
    These actions aren't being used, there aren't even "views" for these
    actions.
    
    Refs: #5320

--- app/controllers/ambassador/teams_controller.rb
+++ app/controllers/ambassador/teams_controller.rb
@@ -6,30 +6,5 @@ module Ambassador
         current_ambassador.current_chapterable.id
       ).find(params[:id])
     end
-
-    def edit
-      @team = Team.by_chapterable(
-        current_ambassador.chapterable_type,
-        current_ambassador.current_chapter.id
-      ).find(params[:id])
-    end
-
-    def update
-      @team = Team.by_chapter(current_ambassador.current_chapter.id).find(params[:id])
-
-      if TeamUpdating.execute(@team, team_params)
-        redirect_to chapter_ambassador_team_path, success: "Team changes saved!"
-      else
-        redirect_to chapter_ambassador_team_path, error: "Error saving team photo. Please try again later."
-      end
-    end
-
-    private
-
-    def team_params
-      params.require(:team).permit(
-        :team_photo
-      )
-    end
   end
 end
