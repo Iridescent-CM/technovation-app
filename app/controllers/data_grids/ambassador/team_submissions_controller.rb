643ad6ac392c606f4245ca2a03012a817448b2dc
Vivian Canales <vcnales@gmail.com>
2025-01-30 15:42:29 -0600

    added team submissions datagrid controller

---
+++ app/controllers/data_grids/ambassador/team_submissions_controller.rb
@@ -0,0 +1,51 @@
+module DataGrids::Ambassador
+  class TeamSubmissionsController < AmbassadorController
+    include DatagridController
+
+    layout "ambassador"
+
+    use_datagrid with: SubmissionsGrid,
+
+      html_scope: ->(scope, user, params) {
+        scope
+          .by_chapterable(
+            user.chapterable_type,
+            user.current_chapterable.id
+          )
+          .distinct
+          .page(params[:page])
+      },
+
+      csv_scope: "->(scope, user, params) {
+        scope
+          .by_chapterable(
+            user.chapterable_type,
+            user.current_chapter.id
+          )
+          .distinct
+      }"
+
+
+    private
+
+    def grid_params
+      grid = (params[:submissions_grid] ||= {}).merge(
+        admin: false,
+        allow_state_search: current_ambassador.country_code != "US",
+        country: [current_ambassador.country_code],
+        state_province: (
+          if current_ambassador.country_code == "US"
+            [current_ambassador.state_province]
+          else
+            Array(params[:submissions_grid][:state_province])
+          end
+        ),
+        season: params[:submissions_grid][:season] || Season.current.year
+      )
+
+      grid.merge(
+        column_names: detect_extra_columns(grid)
+      )
+    end
+  end
+end
