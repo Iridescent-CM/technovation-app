015a65fc55ce2b86a51aa603dfb76f6f3cefad77
Vivian Canales <vcnales@gmail.com>
2025-02-05 20:13:18 -0600

    update test

--- spec/controllers/admin/chapterable_account_assignments_controller_spec.rb
+++ spec/controllers/admin/chapterable_account_assignments_controller_spec.rb
@@ -2,7 +2,9 @@ require "rails_helper"
 
 RSpec.describe Admin::ChapterableAccountAssignmentsController do
   let(:club_ambassador) { FactoryBot.create(:club_ambassador) }
+  let(:chapter_ambassador) { FactoryBot.create(:chapter_ambassador) }
   let(:club) { FactoryBot.create(:club) }
+  let(:chapter) { FactoryBot.create(:chapter) }
 
   before do
     sign_in(:admin)
@@ -49,6 +51,36 @@ RSpec.describe Admin::ChapterableAccountAssignmentsController do
         }
 
         expect(club_ambassador.account.reload.current_club).to be_empty
+      end
+    end
+
+    context "when a Club Ambassador is assigned to a chapter" do
+      it "does not create a chapterable assignment" do
+        post :create, params: {
+          account_id: club_ambassador.account.id,
+          chapterable_account_assignment: {
+            club_id: nil,
+            chapter_id: chapter.id,
+            chapterable_type: "Chapter"
+          }
+        }
+
+        expect(club_ambassador.account.reload.current_chapterable_assignment).to be_empty
+      end
+    end
+
+    context "when a Chapter Ambassador is assigned to a club" do
+      it "does not create a chapterable assignment" do
+        post :create, params: {
+          account_id: chapter_ambassador.account.id,
+          chapterable_account_assignment: {
+            club_id: club.id,
+            chapter_id: nil,
+            chapterable_type: "Club"
+          }
+        }
+
+        expect(club_ambassador.account.reload.current_chapterable_assignment).to be_empty
       end
     end
   end
