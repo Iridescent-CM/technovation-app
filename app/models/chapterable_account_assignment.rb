f15ce6dca2d7ceb6bbc785f06ff3988bf04c50a5
Vivian Canales <vcnales@gmail.com>
2025-02-05 20:12:57 -0600

    Added validation for chapterable assignment type

--- app/models/chapterable_account_assignment.rb
+++ app/models/chapterable_account_assignment.rb
@@ -14,4 +14,18 @@ class ChapterableAccountAssignment < ApplicationRecord
   scope :clubs, -> {
     where(chapterable_type: "Club")
   }
+
+  validate :ambassador_chapterable_assignment_type
+
+  private
+
+  def ambassador_chapterable_assignment_type
+    type = chapterable_type.capitalize
+
+    if account.is_a_club_ambassador? && type == "Chapter"
+      errors.add(:base, "Club ambassadors cannot be assigned to a chapter.")
+    elsif account.is_an_ambassador? && type == "Club"
+      errors.add(:base, "Chapter ambassadors cannot be assigned to a club.")
+    end
+  end
 end
