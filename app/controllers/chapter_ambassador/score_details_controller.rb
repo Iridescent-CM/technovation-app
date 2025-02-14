1d8db21e1a558a03112d698c9d10957c6888819d
shaun <58957909+shaun-technovation@users.noreply.github.com>
2025-02-14 12:58:10 -0700

    Feature: Scores datagrid for clubs and refactoring
    This will:
    
    - make the scores datagrid available for club ambassadors
    - add `by_chapterable` functionality for scores
    - combine the controller and views for chapter and club ambassadors
    - combine the datagrid views for admins, chapter and club ambassadors
    - remove the old/used controllers and views
    - update the links on the scores datagrid to accommodate for club
      ambassador
    - update the specs for chapter ambassadors viewing scores
      - assigning the students associated with the submission to the chapter
        of the chapter ambassador
    
    Refs: #5165

--- app/controllers/chapter_ambassador/score_details_controller.rb
+++
@@ -1,8 +0,0 @@
-module ChapterAmbassador
-  class ScoreDetailsController < ChapterAmbassadorController
-    def show
-      @submission = TeamSubmission.includes(:team, :submission_scores)
-        .find(params.fetch(:id))
-    end
-  end
-end
