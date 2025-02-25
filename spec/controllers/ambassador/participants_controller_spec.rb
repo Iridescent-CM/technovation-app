6530b62187ec4e8fcdb81417ed9aacc274eb7c27
shaun <58957909+shaun-technovation@users.noreply.github.com>
2025-02-25 13:14:41 -0700

    Security: Restrict which participants can be viewed with the national view
    If a chapter ambassador has "national view", this will ensure that the
    chapter ambassador can only view participants in their region, where the
    country code of the participant matches the chapters country code.
    
    Refs: #5373

--- spec/controllers/ambassador/participants_controller_spec.rb
+++ spec/controllers/ambassador/participants_controller_spec.rb
@@ -3,15 +3,38 @@ require "rails_helper"
 RSpec.describe Ambassador::ParticipantsController do
   describe "GET #show" do
     context "as a chapter ambassador" do
+      let(:chapter_ambassador) do
+        FactoryBot.create(
+          :chapter_ambassador,
+          :not_assigned_to_chapter,
+          national_view: national_view
+        )
+      end
+      let(:brazil_chapter) do
+        FactoryBot.create(
+          :chapter,
+          :brazil,
+          primary_contact: chapter_ambassador.account
+        )
+      end
+      let(:national_view) { false }
+
       before do
-        sign_in(:chapter_ambassador)
+        chapter_ambassador.chapterable_assignments.create(
+          chapterable: brazil_chapter,
+          account: chapter_ambassador.account,
+          season: Season.current.year,
+          primary: true
+        )
+
+        sign_in(chapter_ambassador)
       end
 
       context "when viewing participants with `search_in_region` set to a truthy value" do
         let(:search_in_region) { 1 }
 
-        context "when viewing a judge" do
-          let(:judge) { FactoryBot.create(:judge) }
+        context "when viewing a judge in the same region" do
+          let(:judge) { FactoryBot.create(:judge, :brazil) }
 
           before do
             get :show, params: {
@@ -22,6 +45,19 @@ RSpec.describe Ambassador::ParticipantsController do
 
           it "returns an OK 200 success status code" do
             expect(response.status).to eq(200)
+          end
+        end
+
+        context "when viewing a judge in a different region" do
+          let(:judge) { FactoryBot.create(:judge, :chicago) }
+
+          it "raises an 'ActiveRecord::RecordNotFound' error" do
+            expect {
+              get :show, params: {
+                id: judge.account.id,
+                search_in_region: search_in_region
+              }
+            }.to raise_error(ActiveRecord::RecordNotFound)
           end
         end
 
@@ -46,6 +82,36 @@ RSpec.describe Ambassador::ParticipantsController do
               get :show, params: {
                 id: student.account.id,
                 search_in_region: search_in_region
+              }
+            }.to raise_error(ActiveRecord::RecordNotFound)
+          end
+        end
+      end
+
+      context "when a chapter ambassador has the 'national view' ability" do
+        let(:national_view) { true }
+
+        context "when viewing a participant in the chapter ambassador's region" do
+          let(:brazil_student) { FactoryBot.create(:student, :brazil) }
+
+          before do
+            get :show, params: {
+              id: brazil_student.account.id
+            }
+          end
+
+          it "returns an OK 200 success status code" do
+            expect(response.status).to eq(200)
+          end
+        end
+
+        context "when viewing a participant that is not in the chapter ambassador's region" do
+          let(:chicago_student) { FactoryBot.create(:student, :chicago) }
+
+          it "raises an 'ActiveRecord::RecordNotFound' error" do
+            expect {
+              get :show, params: {
+                id: chicago_student.account.id
               }
             }.to raise_error(ActiveRecord::RecordNotFound)
           end
