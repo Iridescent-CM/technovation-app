89848d75bbc95ad6da3f42b64eb2c29688c24273
Vivian Canales <vcnales@gmail.com>
2025-03-06 11:37:22 -0600

    changed destroy_all to delete_all

--- db/migrate/20250211205130_remove_club_ambassador_from_mentor_profile_types.rb
+++ db/migrate/20250211205130_remove_club_ambassador_from_mentor_profile_types.rb
@@ -2,7 +2,7 @@ class RemoveClubAmbassadorFromMentorProfileTypes < ActiveRecord::Migration[6.1]
   def up
     club_ambassador_mentor_type = MentorType.find_by(name: "Club Ambassador", order: 6)
 
-    club_ambassador_mentor_type.mentor_profile_mentor_types.destroy_all
+    club_ambassador_mentor_type.mentor_profile_mentor_types.delete_all
     club_ambassador_mentor_type.delete
   end
 
