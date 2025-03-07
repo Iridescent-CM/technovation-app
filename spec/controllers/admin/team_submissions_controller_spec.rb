c3f7ca6c6c28f8b84979015b9a9c626452ad4648
Vivian Canales <vcnales@gmail.com>
2025-03-06 18:56:53 -0600

    Added test for return to judging pool

--- spec/controllers/admin/team_submissions_controller_spec.rb
+++ spec/controllers/admin/team_submissions_controller_spec.rb
@@ -10,4 +10,19 @@ RSpec.describe Admin::TeamSubmissionsController do
       }.to change { Export.count }.by(1)
     end
   end
+
+  describe "PATCH #return_to_judging_pool" do
+    let(:team_submission) { FactoryBot.create(:submission, removed_from_judging_pool: true) }
+    let(:admin) { FactoryBot.create(:admin) }
+
+    it "sets removed from judging pool to false" do
+      sign_in(admin)
+
+      patch :return_to_judging_pool, params: {team_submission_id: team_submission.id}
+
+      team_submission.reload
+      expect(team_submission.removed_from_judging_pool).to be false
+      expect(team_submission.returned_by_id).to eq(admin.account.id)
+    end
+  end
 end
