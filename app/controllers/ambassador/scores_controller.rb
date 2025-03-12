e7f5a10eab4694f3b91a2ba8014d6c3a8c14e13c
shaun <58957909+shaun-technovation@users.noreply.github.com>
2025-03-12 15:03:44 -0600

    Security: Restrict access viewing scores
    Before this change an ambassador could view any score on our platform by
    manipulating the score id in the URL.
    
    With this change:
    
    - If an ambassador has the "national view" they should only be able to
      view a score from their region
    
    - If an ambassador doesn't have "national view" they should only be able
      to view scores from participants/submissions assigned to their chapter or
      club
    
    Refs: #5391

--- app/controllers/ambassador/scores_controller.rb
+++ app/controllers/ambassador/scores_controller.rb
@@ -1,7 +1,18 @@
 module Ambassador
   class ScoresController < AmbassadorController
     def show
-      @score = SubmissionScore.find(params.fetch(:id))
+      @score = if current_ambassador.national_view?
+        SubmissionScore
+          .in_region(current_ambassador.chapter)
+          .find(params.fetch(:id))
+      else
+        SubmissionScore
+          .by_chapterable(
+            current_ambassador.chapterable_type,
+            current_ambassador.current_chapterable.id
+          )
+          .find(params.fetch(:id))
+      end
 
       render "admin/scores/show"
     end
