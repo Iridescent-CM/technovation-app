75402a980f9a1a87060121a733af0f73f8d3c085
shaun <58957909+shaun-technovation@users.noreply.github.com>
2025-03-25 13:32:35 -0600

    Chore: Ensure unique chapterable assignments when mentor joins team
    This will extract the chapterable assignment functionality out the
    controllers and move it into a new service.
    
    This new service will only make a chapterable assignment for the mentor
    if one doesn't exist yet; i.e. it shouldn't create duplicate chapterable
    assignments.
    
    Refs: #5430

---
+++ app/services/mentor_to_team_chapterable_assigner.rb
@@ -0,0 +1,42 @@
+class MentorToTeamChapterableAssigner
+  def initialize(mentor_profile:, team:)
+    @mentor_profile = mentor_profile
+    @team = team
+  end
+
+  def call
+    chapterables_not_assigned_to_mentor.each do |chapterable|
+      mentor_profile.account.chapterable_assignments.find_or_create_by(
+        profile: mentor_profile,
+        chapterable: chapterable,
+        season: Season.current.year,
+        primary: false
+      )
+    end
+  end
+
+  private
+
+  attr_accessor :mentor_profile, :team
+
+  def chapterables_not_assigned_to_mentor
+    @chapterables_not_assigned_to_mentor ||= student_chapterable_assignments.uniq.reject do |student_chapterable|
+      mentor_chapterable_assignments.any? do |mentor_chapterable|
+        mentor_chapterable.chapterable_type == student_chapterable.chapterable_type &&
+          mentor_chapterable.chapterable_id == student_chapterable.chapterable_id
+      end
+    end
+      .reject(&:blank?)
+      .map(&:chapterable)
+  end
+
+  def mentor_chapterable_assignments
+    @mentor_chapterable_assignments ||= mentor_profile.account.current_chapterable_assignments
+  end
+
+  def student_chapterable_assignments
+    @student_chapterable_assignments ||= team.students.flat_map do |student_chapterable|
+      student_chapterable.account.current_chapterable_assignment
+    end
+  end
+end
