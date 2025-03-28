db3678e0e6368273d48661cd3137bf81f776354d
shaun <58957909+shaun-technovation@users.noreply.github.com>
2025-03-28 13:16:30 -0600

    Chore: Limit which team participants an ambassador can view
    When an ambassador is viewing a team, this will determine whether or not
    to display a link to view team participant detail.
    
    Refs: #5245

--- spec/system/chapter_ambassador/view_team_submission_from_team_page_spec.rb
+++ spec/system/chapter_ambassador/view_team_submission_from_team_page_spec.rb
@@ -1,22 +1,53 @@
 require "rails_helper"
 
-RSpec.describe "chapter ambassadors visiting a team page" do
+RSpec.describe "Chapter ambassadors visiting a team page" do
   let(:chapter_ambassador) { FactoryBot.create(:chapter_ambassador) }
+  let(:team) { FactoryBot.create(:team, :submitted) }
+  let(:student_in_chapter) do
+    FactoryBot.create(
+      :student,
+      :chicago,
+      :not_assigned_to_chapter,
+      first_name: "Harmony",
+      last_name: "Bear"
+    )
+  end
+  let(:student_not_in_chapter) do
+    FactoryBot.create(
+      :student,
+      :chicago,
+      first_name: "Cheer",
+      last_name: "Bear"
+    )
+  end
+
+  before do
+    student_in_chapter.chapterable_assignments.create(
+      account: student_in_chapter.account,
+      chapterable: chapter_ambassador.current_chapter,
+      season: Season.current.year,
+      primary: true
+    )
+
+    TeamRosterManaging.add(team, student_in_chapter)
+    TeamRosterManaging.add(team, student_not_in_chapter)
+
+    sign_in(chapter_ambassador)
+    visit chapter_ambassador_team_path(team)
+  end
+
+  describe "Viewing students on a team" do
+    it "displays a link for the student that belongs to the chapter ambassador's chapter" do
+      expect(page).to have_link(student_in_chapter.full_name)
+    end
+
+    it "does not display a link for the student that does not belong to the chapter ambassador's chapter" do
+      expect(page).not_to have_link(student_not_in_chapter.full_name)
+    end
+  end
 
   describe "Clicking through to the submission" do
-    it "works" do
-      team = FactoryBot.create(:team, :submitted)
-
-      affiliated_student = FactoryBot.create(:student, :chicago, :not_assigned_to_chapter)
-      affiliated_student.chapterable_assignments.create(
-        account: affiliated_student.account,
-        chapterable: chapter_ambassador.current_chapter,
-        season: Season.current.year
-      )
-      TeamRosterManaging.add(team, affiliated_student)
-
-      sign_in(chapter_ambassador)
-      visit chapter_ambassador_team_path(team)
+    it "displays the submission" do
       click_link team.submission.app_name
 
       expect(current_path).to eq(chapter_ambassador_team_submission_path(team.submission))
