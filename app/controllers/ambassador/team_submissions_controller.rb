ef92227c5c632414e1ee6e92f9fa8e96eebbb48d
Vivian Canales <vcnales@gmail.com>
2025-04-09 12:27:29 -0500

    Updated ambassador team submission controller to include national view conditional

--- app/controllers/ambassador/team_submissions_controller.rb
+++ app/controllers/ambassador/team_submissions_controller.rb
@@ -3,10 +3,15 @@ module Ambassador
     layout "ambassador"
 
     def show
-      @team_submission = TeamSubmission.by_chapterable(
-        current_ambassador.chapterable_type,
-        current_ambassador.current_chapterable.id
-      ).friendly.find(params[:id])
+      @team_submission = if current_ambassador.national_view?
+        TeamSubmission.in_region(current_ambassador.chapter)
+          .friendly.find(params[:id])
+      else
+        TeamSubmission.by_chapterable(
+          current_ambassador.chapterable_type,
+          current_ambassador.current_chapterable.id
+        ).friendly.find(params[:id])
+      end
     end
   end
 end
