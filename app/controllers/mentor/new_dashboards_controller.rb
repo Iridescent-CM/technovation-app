8e33e22f542c3569df026cc73173f843e932c942
Vivian Canales <vcnales@gmail.com>
2025-04-29 11:42:08 -0500

    Added route, controller, and view for new mentor dashboard

---
+++ app/controllers/mentor/new_dashboards_controller.rb
@@ -0,0 +1,20 @@
+module Mentor
+  class NewDashboardsController < MentorController
+    include LocationStorageController
+
+    layout "mentor_rebrand"
+
+    private
+
+    def create_judge_mentor_on_dashboard
+      return if current_session.authenticated?
+      # Chapter ambassador/Admin Logged in as someone else
+
+      if CreateMentorProfile.call(current_account)
+        flash.now[:success] = t(
+          "controllers.mentor.dashboards.show.mentor_profile_created"
+        )
+      end
+    end
+  end
+end
