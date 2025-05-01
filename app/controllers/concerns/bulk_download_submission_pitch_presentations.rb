a6c4f6cfa6aadb4f66ea5d432268230f1dd0ff30
shaun <58957909+shaun-technovation@users.noreply.github.com>
2025-05-01 13:27:14 -0600

    Chore: Update functionality to bulk download RPE pitch presentations
    A few tweaks to the functionality that will allow admins and ambassadors
    to download pitch presentations:
    
    - The bulk download button will only be displayed if there are
      presentations to download
    - Added some spacing around the download buttons
    - Updated the filename being downloaded, it was replacing the period
      in the file extension with an underscore, e.g. .pdf would become _pdf
    
    Refs: #5539

--- app/controllers/concerns/bulk_download_submission_pitch_presentations.rb
+++ app/controllers/concerns/bulk_download_submission_pitch_presentations.rb
@@ -4,10 +4,10 @@ module BulkDownloadSubmissionPitchPresentations
   def bulk_download_submission_pitch_presentations
     event = RegionalPitchEvent.find(params[:event_id])
     pitch_presentations = event.team_submissions.map do |submission|
-      if submission.pitch_presentation.present?
+      if submission.pitch_presentation.url.present?
         [
           submission.pitch_presentation.url,
-          "#{submission.team.name.downcase.parameterize(separator: "_")}-#{submission.pitch_presentation.file.filename.downcase.parameterize(separator: "_")}"
+          "#{submission.team.name}-#{submission.pitch_presentation.file.filename.downcase.gsub(/\s+/, "_")}"
         ]
       end
     end
