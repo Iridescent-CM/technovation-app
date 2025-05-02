f2adcc2c377a0818c43608afa01b2592a1316615
shaun <58957909+shaun-technovation@users.noreply.github.com>
2025-05-02 14:49:32 -0600

    Chore: Only bulk download pitch presentations if they exist
    This will add a new scope `with_pitch_presentations` that will be used
    when bulk downloading pitch presentations, so that only the submissions
    with pitch presentations will get downloaded.
    
    Refs: #5539

--- app/controllers/concerns/bulk_download_submission_pitch_presentations.rb
+++ app/controllers/concerns/bulk_download_submission_pitch_presentations.rb
@@ -3,13 +3,11 @@ module BulkDownloadSubmissionPitchPresentations
 
   def bulk_download_submission_pitch_presentations
     event = RegionalPitchEvent.find(params[:event_id])
-    pitch_presentations = event.team_submissions.map do |submission|
-      if submission.pitch_presentation.url.present?
-        [
-          submission.pitch_presentation.url,
-          "#{submission.team.name}-#{submission.pitch_presentation.file.filename.downcase.gsub(/\s+/, "_")}"
-        ]
-      end
+    pitch_presentations = event.team_submissions.with_pitch_presentations.map do |submission|
+      [
+        submission.pitch_presentation.url,
+        "#{submission.team.name}-#{submission.pitch_presentation.file.filename.downcase.gsub(/\s+/, "_")}"
+      ]
     end
 
     zipline(
