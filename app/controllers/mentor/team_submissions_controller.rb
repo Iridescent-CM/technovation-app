380b7ee6e10a69ee25c4eeb919a942196d35e05c
Vivian Canales <vcnales@gmail.com>
2025-05-07 11:00:00 -0500

    Added index action and index view for mentor team submissions

--- app/controllers/mentor/team_submissions_controller.rb
+++ app/controllers/mentor/team_submissions_controller.rb
@@ -4,6 +4,10 @@ module Mentor
 
     before_action :require_onboarded
 
+    def index
+      @current_teams = current_mentor.teams.current.order("teams.name")
+    end
+
     private
 
     def require_onboarded
