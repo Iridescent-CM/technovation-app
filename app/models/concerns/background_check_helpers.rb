b03f278a9bdcb60b085ba35a5b07d7bfc36828ba
shaun <58957909+shaun-technovation@users.noreply.github.com>
2025-05-09 15:45:59 -0600

    Feature: Club ambassador background check and onboarding groundwork
    Basic overview of the changes here:
    
    - the onboarding steps will be added to the club ambassador dashboard
      and to the admin area
    - groundwork for all club ambassador onboarding steps
    - viewed_community_connections database migration for club ambassadors
    - background checks for club ambassadors
      - extracting common background check functionality so it can be reused
    
    There are some important background check changes here:
    
    - `in_background_check_country?` for chapter ambassadors only had US
      listed, but chapter ambassadors from all countries are required to
      pass a background check, so I set this value to true
    - `background_check_exemption?`, I updated this to check for the
      exemption on the `account` and also added if a condition for if the
      participant is not in a background check country
    - `requires_background_check?`, a required background check is the
      opposite of an exemption, it's required if you're not exempt and in
      a background check country
    
    Refs: #5528 #5537 #5565 #5583

---
+++ app/models/concerns/background_check_helpers.rb
@@ -0,0 +1,25 @@
+module BackgroundCheckHelpers
+  extend ActiveSupport::Concern
+
+  def requires_background_check?
+    in_background_check_country? && !background_check_exempt?
+  end
+
+  def background_check_exempt?
+    !in_background_check_country? || account.background_check_exemption?
+  end
+
+  def background_check_exempt_or_complete?
+    background_check_exempt? || background_check_complete?
+  end
+
+  def background_check_complete?
+    return true if !requires_background_check?
+
+    background_check.present? && background_check.clear?
+  end
+
+  def in_background_check_country?
+    true
+  end
+end
