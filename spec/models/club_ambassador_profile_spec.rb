91d18901fdee82474c2dc728ef01cb188be1fdff
shaun <58957909+shaun-technovation@users.noreply.github.com>
2025-05-09 15:45:59 -0600

    Feature: Club ambassador background check and onboarding groundwork
    Basic overview of the changes here:
    
    - the onboarding steps will be added to the club ambassador dashboard
      and to the admin area
    - groundwork for all club ambassador onboarding steps
    - viewed_community_connections database migration for club ambassadors
    - background checks for club ambassadors
      - extracting common background check functionality so it can be reused
    
    There are some important background check changes here:
    
    - `in_background_check_country?` for chapter ambassadors only had US
      listed, but chapter ambassadors from all countries are required to
      pass a background check, so I set this value to true
    - `background_check_exemption?`, I updated this to check for the
      exemption on the `account` and also added if a condition for if the
      participant is not in a background check country
    - `requires_background_check?`, a required background check is the
      opposite of an exemption, it's required if you're not exempt and in
      a background check country
    
    Refs: #5528 #5537 #5565 #5583

--- spec/models/club_ambassador_profile_spec.rb
+++ spec/models/club_ambassador_profile_spec.rb
@@ -3,10 +3,12 @@ require "rails_helper"
 RSpec.describe ClubAmbassadorProfile do
   let(:club_ambassador_profile) do
     FactoryBot.create(:club_ambassador_profile,
-      training_completed_at: training_completed_at)
+      training_completed_at: training_completed_at,
+      viewed_community_connections: viewed_community_connections)
   end
 
   let(:training_completed_at) { Time.now }
+  let(:viewed_community_connections) { true }
 
   context "callbacks" do
     context "#after_update" do
@@ -14,22 +16,48 @@ RSpec.describe ClubAmbassadorProfile do
         before do
           allow(club_ambassador_profile).to receive(:account)
             .and_return(account)
+
+          allow(account).to receive(:background_check)
+            .and_return(background_check)
         end
 
         let(:account) do
           instance_double(Account,
+            country_code: "US",
             email_confirmed?: email_address_confirmed,
+            background_check_exemption?: false,
             marked_for_destruction?: false,
             valid?: true)
         end
 
         let(:email_address_confirmed) { true }
+        let(:background_check) { instance_double(BackgroundCheck, clear?: background_check_cleared) }
+        let(:background_check_cleared) { true }
 
         context "when all onboarding steps have been completed" do
           let(:email_address_confirmed) { true }
+          let(:background_check_cleared) { true }
+          let(:training_completed_at) { Time.now }
+          let(:viewed_community_connections) { true }
+
+          before do
+            club_ambassador_profile.save
+          end
 
           it "returns true" do
             expect(club_ambassador_profile.onboarded?).to eq(true)
+          end
+        end
+
+        context "when the background check has not been cleared" do
+          let(:background_check_cleared) { false }
+
+          before do
+            club_ambassador_profile.save
+          end
+
+          it "returns false" do
+            expect(club_ambassador_profile.onboarded?).to eq(false)
           end
         end
 
@@ -38,7 +66,17 @@ RSpec.describe ClubAmbassadorProfile do
             club_ambassador_profile.update(training_completed_at: false)
           end
 
-          xit "returns false" do
+          it "returns false" do
+            expect(club_ambassador_profile.onboarded?).to eq(false)
+          end
+        end
+
+        context "when the community connections page has not been viewed" do
+          before do
+            club_ambassador_profile.update(viewed_community_connections: false)
+          end
+
+          it "returns false" do
             expect(club_ambassador_profile.onboarded?).to eq(false)
           end
         end
