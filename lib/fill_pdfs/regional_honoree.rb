47f3e9a06f8bf5d952ca495ec8c9815693d13b91
Vivian Canales <vcnales@gmail.com>
2025-05-13 13:46:48 -0500

    Added full_text method for additional certificate types

---
+++ lib/fill_pdfs/regional_honoree.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+module FillPdfs
+  class RegionalHonoree
+    include FillPdfs
+
+    def full_text
+      "For their outstanding work as a member of Technovation #{recipient.region}, team #{recipient.team_name}, to develop the project #{recipient.mobile_app_name} in the #{recipient.season} season."
+    end
+  end
+end
