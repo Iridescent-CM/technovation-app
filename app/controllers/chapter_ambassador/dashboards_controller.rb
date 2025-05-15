00eddbd0ffd82ff2c00219a05f9614468f8ef337
shaun <58957909+shaun-technovation@users.noreply.github.com>
2025-05-15 15:27:46 -0600

    Feature: Restrict access to Club Admin Activity
    This will restrict club ambassadors from accessing Club Admin Activity
    until both the club ambassador and the club are fully onboarded.
    
    To do this, I renamed `require_chapter_and_chapter_ambassador_onboarded`
    to `require_chapterable_and_ambassador_onboarded` to make it more
    generic and reusable.
    
    I also added a missing `has_many :chapterable_assignments` to
    `ClubAmbassadorProfile` and also updated `has_many :current_clubs` on
    `Account` to reference clubs instead of chapters. Great catch on both of
    those, @viviancan!
    
    Refs: #5538

--- app/controllers/chapter_ambassador/dashboards_controller.rb
+++ app/controllers/chapter_ambassador/dashboards_controller.rb
@@ -1,6 +1,6 @@
 module ChapterAmbassador
   class DashboardsController < ChapterAmbassadorController
-    skip_before_action :require_chapter_and_chapter_ambassador_onboarded
+    skip_before_action :require_chapterable_and_ambassador_onboarded
 
     layout "chapter_ambassador_rebrand"
 
