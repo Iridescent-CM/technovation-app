dfa0da43f5c7c430c377a072e3b6209a7da2899f
shaun <58957909+shaun-technovation@users.noreply.github.com>
2025-05-29 15:05:27 -0600

    Chore: Consolidate ChA and CA background check functionality
    This will refactor chapter ambassador and club ambassador background
    check functionality so that the controller and views are shared between
    them (instead of having a separate version for each of them). The routes
    will still be separate though.
    
    Refs: #5537

--- app/controllers/concerns/background_check_invitation_controller.rb
+++ app/controllers/concerns/background_check_invitation_controller.rb
@@ -2,18 +2,17 @@ module BackgroundCheckInvitationController
   extend ActiveSupport::Concern
 
   def create_invitation
-
     result = CheckrApiClient::BackgroundCheckInvitationValidator.new(account: current_account).call
 
     if result.error?
-      redirect_to send("#{current_scope}_dashboard_path"),
+      redirect_to send(:"#{current_scope}_dashboard_path"),
         alert: result.error_message
     elsif result.requires_background_check_invitation?
       RequestCheckrBackgroundCheckInvitationJob.perform_later(account_id: current_account.id)
-      redirect_to send("#{current_scope}_dashboard_path"),
+      redirect_to send(:"#{current_scope}_dashboard_path"),
         success: t("controllers.background_checks.invite.success")
     else
-      redirect_to send("#{current_scope}_dashboard_path"),
+      redirect_to send(:"#{current_scope}_dashboard_path"),
         alert: t("controllers.application.unauthorized")
     end
   end
