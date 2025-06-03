bdd9bac6a58d9332b44bc139c093ff66b05cf224
Vivian Canales <vcnales@gmail.com>
2025-06-03 15:09:12 -0500

    Updated test

--- spec/controllers/ambassador/training_completion_controller_spec.rb
+++ spec/controllers/ambassador/training_completion_controller_spec.rb
@@ -1,11 +1,17 @@
 require "rails_helper"
 
-RSpec.describe ChapterAmbassador::TrainingCompletionsController do
+RSpec.describe Ambassador::TrainingCompletionController do
   describe "GET #show" do
     it "records the chapter ambassador's training completion time" do
       sign_in(:chapter_ambassador)
       get :show
       expect(ChapterAmbassadorProfile.last.training_completed_at).not_to be_nil
+    end
+
+    it "records the club ambassador's training completion time" do
+      sign_in(:club_ambassador)
+      get :show
+      expect(ClubAmbassadorProfile.last.training_completed_at).not_to be_nil
     end
   end
 end
