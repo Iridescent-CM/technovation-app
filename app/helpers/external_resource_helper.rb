d396724570e305711544b3f4e95b3b50b0e39fe0
Vivian Canales <vcnales@gmail.com>
2025-06-03 14:51:53 -0500

    Updated external url method to be generic for ambassadors and removed module link method

--- app/helpers/external_resource_helper.rb
+++ app/helpers/external_resource_helper.rb
@@ -1,21 +1,20 @@
 module ExternalResourceHelper
-  def external_chapter_ambassador_training_checkpoint_link(account)
-    base_url = ENV.fetch("CHAPTER_AMBASSADOR_TRAINING_CHECKPOINT_BASE_URL")
+  def external_ambassador_training_checkpoint_link(account)
+    base_url = ENV.fetch("#{current_scope.upcase}_TRAINING_CHECKPOINT_BASE_URL")
+
+    organization_value = account.chapter_ambassador? ?
+      account.chapter_ambassador_profile.chapter&.organization_name : account.club_ambassador_profile.club&.name
 
     params = {
       uid_value: account.id,
       full_name_value: account.full_name,
       email_value: account.email,
-      organization_value: account.chapter_ambassador_profile.chapter&.organization_name,
+      organization_value: organization_value,
       city_value: account.city,
       state_province_value: account.state_province,
       country_value: account.country
     }
 
     "#{base_url}?#{params.to_query}"
-  end
-
-  def external_chapter_ambassador_training_module_link(module_number)
-    ENV.fetch("CHAPTER_AMBASSADOR_TRAINING_MODULE_#{module_number}_URL", "")
   end
 end
