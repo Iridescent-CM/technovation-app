98be7b4ce7d9c1e7c980113802b2dc905839f5e8
shaun <58957909+shaun-technovation@users.noreply.github.com>
2025-06-05 08:29:10 -0600

    Chore: Remove onboarding requirement for club ambassador profile section
    By default all club ambassador sections are locked down by default
    (requiring the club ambassador to be fully onboarded before they can
    be accessed) and need to be explicitly opted out of the onboarding
    requirement, the change here will make the club ambassador profile
    section available w/o requiring onboarding.
    
    Refs: #5538

--- app/controllers/club_ambassador/profiles_controller.rb
+++ app/controllers/club_ambassador/profiles_controller.rb
@@ -2,6 +2,8 @@ module ClubAmbassador
   class ProfilesController < AmbassadorController
     include ProfileController
 
+    skip_before_action :require_chapterable_and_ambassador_onboarded
+
     layout "club_ambassador_rebrand"
 
     def profile_params
