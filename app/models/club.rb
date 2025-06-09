8c999aee8715da41e9a99e23a54ab6ebf9b88a01
shaun <58957909+shaun-technovation@users.noreply.github.com>
2025-06-09 14:08:33 -0600

    Chore: Display outstanding club ambassador onboarding steps
    This will display outstanding club ambassador and club onboarding steps
    on the club ambassador's dashboard; this approach mirrors the existing
    chapter ambassador approach.
    
    Refs: #5605

--- app/models/club.rb
+++ app/models/club.rb
@@ -5,7 +5,6 @@ class Club < ActiveRecord::Base
   include StatusHelpers
 
   include Casting::Client
-
   delegate_missing_methods
 
   belongs_to :primary_contact, class_name: "Account", foreign_key: "primary_account_id", optional: true
