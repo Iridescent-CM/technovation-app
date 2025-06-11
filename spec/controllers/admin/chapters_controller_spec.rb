69e33ad286bfc6457706493efba053cf19fe5547
shaun <58957909+shaun-technovation@users.noreply.github.com>
2025-06-11 15:30:58 -0600

    Chore: Add newly created chapter to the current season
    This will make it so that a newly created chapter will get assigned to
    the current season.
    
    Refs: #5636

--- spec/controllers/admin/chapters_controller_spec.rb
+++ spec/controllers/admin/chapters_controller_spec.rb
@@ -13,6 +13,14 @@ RSpec.describe Admin::ChaptersController do
       }.to change { Chapter.count }.by(1)
     end
 
+    it "adds the current season to the newly created chapter" do
+      post :create, params: {
+        chapter: {organization_name: "Coding Zone"}
+      }
+
+      expect(Chapter.last.seasons).to include(Season.current.year)
+    end
+
     it "redirects to the created chapter" do
       post :create, params: {
         chapter: {organization_name: "Hello World Again"}
