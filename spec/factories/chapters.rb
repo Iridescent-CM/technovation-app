5ddafe791db2518eb32f24a691feecf8126e50be
shaun <58957909+shaun-technovation@users.noreply.github.com>
2025-06-12 10:46:40 -0600

    Chore: Remove inactive chapters from the chapter selection process
    This will remove inactive chapters (chapters not participating in the
    current season) from the chapter selection process.
    
    Refs: #5615

--- spec/factories/chapters.rb
+++ spec/factories/chapters.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     sequence(:name) { |n| "FactoryBot Program #{n}" }
     sequence(:summary) { |n| "FactoryBot Summary #{n}" }
     sequence(:organization_name) { |n| "FactoryBot Organization #{n}" }
+    seasons { [Season.current.year] }
     visible_on_map { true }
     sequence(:organization_headquarters_location) { |n| "FactoryBot Location #{n}" }
     chapter_links { build_list :chapter_link, 3 }
