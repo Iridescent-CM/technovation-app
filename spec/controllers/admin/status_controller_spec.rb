534a02faae05f28b8e59cb1dcac8e530a2783982
Vivian Canales <vcnales@gmail.com>
2025-06-23 11:51:22 -0500

    Updated test

--- spec/controllers/admin/status_controller_spec.rb
+++ spec/controllers/admin/status_controller_spec.rb
@@ -1,36 +1,20 @@
 require "rails_helper"
 
-RSpec.describe Admin::Ambassadors::SeasonStatusController do
-  describe "PATCH #update" do
-
-    before do
-      sign_in(:admin)
-    end
-
+RSpec.describe Admin::Chapterables::StatusController do
+  before do
+    sign_in(:admin)
+  end
+  describe "PATCH #active" do
     context "When marking a chapter active" do
       let(:chapter) { FactoryBot.create(:chapter) }
 
       it "adds the current season to the chapter" do
-        patch :update, params: {chapter_id: chapter.id, active: true}
+        patch :active, params: {chapter_id: chapter.id}
         expect(chapter.reload.seasons).to include(Season.current.year)
       end
 
       it "redirects to the chapter admin page" do
-        patch :update, params: {chapter_id: chapter.id, active: true}
-        expect(response).to redirect_to(admin_chapter_path(chapter))
-      end
-    end
-
-    context "When marking a chapter inactive" do
-      let(:chapter) { FactoryBot.create(:chapter, :current) }
-
-      it "removes the current season from the chapter" do
-        patch :update, params: {chapter_id: chapter.id, active: false}
-        expect(chapter.reload.seasons).not_to include(Season.current.year)
-      end
-
-      it "redirects to the chapter admin page" do
-        patch :update, params: {chapter_id: chapter.id, active: false}
+        patch :active, params: {chapter_id: chapter.id}
         expect(response).to redirect_to(admin_chapter_path(chapter))
       end
     end
@@ -39,13 +23,29 @@ RSpec.describe Admin::Ambassadors::SeasonStatusController do
       let(:club) { FactoryBot.create(:club) }
 
       it "adds the current season to the club" do
-        patch :update, params: {club_id: club.id, active: true}
+        patch :active, params: {club_id: club.id}
         expect(club.reload.seasons).to include(Season.current.year)
       end
 
       it "redirects to the club admin page" do
-        patch :update, params: {club_id: club.id, active: true}
+        patch :active, params: {club_id: club.id}
         expect(response).to redirect_to(admin_club_path(club))
+      end
+    end
+  end
+
+  describe "PATCH #inactive" do
+    context "When marking a chapter inactive" do
+      let(:chapter) { FactoryBot.create(:chapter, :current) }
+
+      it "removes the current season from the chapter" do
+        patch :inactive, params: {chapter_id: chapter.id}
+        expect(chapter.reload.seasons).not_to include(Season.current.year)
+      end
+
+      it "redirects to the chapter admin page" do
+        patch :inactive, params: {chapter_id: chapter.id}
+        expect(response).to redirect_to(admin_chapter_path(chapter))
       end
     end
 
@@ -53,12 +53,12 @@ RSpec.describe Admin::Ambassadors::SeasonStatusController do
       let(:club) { FactoryBot.create(:club, :current) }
 
       it "removes the current season from the club" do
-        patch :update, params: {club_id: club.id, active: false}
+        patch :inactive, params: {club_id: club.id}
         expect(club.reload.seasons).not_to include(Season.current.year)
       end
 
       it "redirects to the club admin page" do
-        patch :update, params: {club_id: club.id, active: false}
+        patch :inactive, params: {club_id: club.id}
         expect(response).to redirect_to(admin_club_path(club))
       end
     end
