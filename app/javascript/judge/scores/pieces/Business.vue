de5f3ce55d974025ab147575b4c64b28acaec361
Vivian Canales <vcnales@gmail.com>
2025-07-16 13:55:44 -0500

    Updated business plan to business canvas

--- app/javascript/judge/scores/pieces/Business.vue
+++ app/javascript/judge/scores/pieces/Business.vue
@@ -1,53 +1,51 @@
 <template>
   <div>
-    <template v-if="team.division === 'senior' || team.division ===  'junior'">
+    <template v-if="team.division === 'senior' || team.division === 'junior'">
       <a
         :href="submission.business_plan_url"
         target="_blank"
         @click="trackBusinessPlanDownload"
         class="text-energetic-blue text-3xl"
       >
-        <Icon
-            name="file-o"
-            class="inline"
-            color="0075cf"
-        />
+        <Icon name="file-o" class="inline" color="0075cf" />
         Read the <span>{{ documentType }}</span> plan
       </a>
     </template>
 
     <template v-else>
-      Beginner Division teams do not upload a business plan.
+      Beginner Division teams do not upload a business canvas.
     </template>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState } from "vuex";
 import Icon from "../../../components/Icon";
 
 export default {
   computed: {
-    ...mapState(['score', 'submission', 'team']),
+    ...mapState(["score", "submission", "team"]),
 
-    documentType () {
+    documentType() {
       switch (this.team.division) {
-        case 'senior':
-          return 'business'
-          break
-        case 'junior':
-          return 'user adoption'
-          break
+        case "senior":
+          return "business";
+          break;
+        case "junior":
+          return "user adoption";
+          break;
       }
-    }
-  },
-  components: {
-    Icon
-  },
-  methods: {
-    async trackBusinessPlanDownload () {
-      await window.axios.patch(`/judge/scores/${this.score.id}`, {submission_score: {'downloaded_business_plan': true}})
     },
   },
-}
+  components: {
+    Icon,
+  },
+  methods: {
+    async trackBusinessPlanDownload() {
+      await window.axios.patch(`/judge/scores/${this.score.id}`, {
+        submission_score: { downloaded_business_plan: true },
+      });
+    },
+  },
+};
 </script>
