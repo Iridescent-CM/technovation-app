05b25df8b4bf845e8fded44d5e37b2c603ff2d12
Vivian Canales <vcnales@gmail.com>
2025-08-12 14:31:36 -0500

    Added migration to add "code_org_app_lab_project_url" field to team submission table

--- db/structure.sql
+++ db/structure.sql
@@ -2440,7 +2440,8 @@ CREATE TABLE public.team_submissions (
     removed_from_judging_pool boolean DEFAULT false,
     returned_to_judging_pool_by_account_id integer,
     ai_usage boolean,
-    ethics_description character varying
+    ethics_description character varying,
+    code_org_app_lab_project_url character varying
 );
 
 
@@ -5148,6 +5149,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20250730184414'),
 ('20250805025429'),
 ('20250806213828'),
-('20250807161706');
+('20250807161706'),
+('20250812160844');
 
 
