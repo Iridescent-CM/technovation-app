2133e6c8b5c189400f0967a83d7cb20fb1aefe27
Vivian Canales <vcnales@gmail.com>
2025-09-04 18:12:04 -0500

    Updated/removed old bg check work and rebrand references

--- app/controllers/concerns/background_check_controller.rb
+++ app/controllers/concerns/background_check_controller.rb
@@ -1,12 +1,4 @@
 module BackgroundCheckController
-  def new
-    if current_profile.background_check_candidate_id.present?
-      redirect_to send("#{current_scope}_background_check_path", id: current_profile.background_check_candidate_id)
-    else
-      @candidate = BackgroundCheckCandidate.new(account: current_account)
-    end
-  end
-
   def show
     if current_profile.background_check.invitation_id.present?
       InvitationChecking.new(current_account).execute
@@ -16,38 +8,5 @@ module BackgroundCheckController
       BackgroundChecking.new(current_profile.background_check).execute
     end
     @background_check = current_profile.background_check
-  end
-
-  def create
-    @candidate = BackgroundCheckCandidate.new(candidate_params)
-
-    if @candidate.submit
-      current_profile.account.update(background_check_attributes: {
-        candidate_id: @candidate.id,
-        report_id: @candidate.report_id
-      })
-      redirect_to send("#{current_scope}_dashboard_path"),
-        success: "Thank you for submitting your background check."
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def candidate_params
-    params.require(:background_check_candidate).permit(
-      :first_name,
-      :middle_name,
-      :last_name,
-      :email,
-      :zipcode,
-      :date_of_birth,
-      :ssn,
-      :driver_license_state,
-      :copy_requested
-    ).tap do |tapped|
-      tapped[:driver_license_state] = tapped[:driver_license_state].strip.upcase
-    end
   end
 end
