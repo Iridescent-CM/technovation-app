a1f183f186dda3bdbba5b6bee3bc119ab391325b
Vivian Canales <vcnales@gmail.com>
2025-09-18 18:19:33 -0500

    Test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,7 +30,7 @@ Capybara.register_driver :selenium_chrome_headless do |app|
   options.add_argument("--headless=new")
   options.add_argument("--no-sandbox")
   options.add_argument("--disable-dev-shm-usage")
-  options.add_argument("--window-size=1920,1080")
+  options.add_argument("--window-size=-2400,-2400")
 
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
