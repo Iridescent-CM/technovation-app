e0d5f386c820b96565e3d7c3bc5847e5aea69c96
Vivian Canales <vcnales@gmail.com>
2025-09-29 20:38:23 -0500

    Added/updated student dashboard controllers

--- app/controllers/student/team_creation_controller.rb
+++ app/controllers/student/team_creation_controller.rb
@@ -1,5 +1,5 @@
 module Student
-  class CurriculumController < StudentController
+  class TeamCreationController < StudentController
     include RequireParentalConsentSigned
     include RequireLocationIsSet
   end
