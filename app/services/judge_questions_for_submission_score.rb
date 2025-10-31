5efa144ccc185b54be609d38d26a7f3802632a7b
shaun <58957909+shaun-technovation@users.noreply.github.com>
2025-10-31 14:56:19 -0600

    Chore: Bifurcate AI-related judging question
    This will update the "judge question" service to return a different
    question based on if the submission being judged used AI or not
    (based on the `ai_usage` question).
    
    Refs: #5904

--- app/services/judge_questions_for_submission_score.rb
+++ app/services/judge_questions_for_submission_score.rb
@@ -1,6 +1,7 @@
 class JudgeQuestionsForSubmissionScore
   def initialize(submission_score, judge_questions_constructor: JudgeQuestions)
     @submission_score = submission_score
+    @submission_ai_usage = submission_score.team_submission_ai_usage
     @submission_type = submission_score.team_submission_submission_type
     @questions = judge_questions_constructor
       .new(season: submission_score.seasons.last, division: submission_score.team_division_name)
@@ -13,7 +14,7 @@ class JudgeQuestionsForSubmissionScore
 
   private
 
-  attr_reader :submission_score, :submission_type, :questions
+  attr_reader :submission_score, :submission_type, :submission_ai_usage, :questions
 
   def questions_with_scores_populated
     filtered_questions.map do |question|
@@ -25,7 +26,9 @@ class JudgeQuestionsForSubmissionScore
 
   def filtered_questions
     questions.delete_if do |question|
-      question.section == "demo" && question.submission_type.to_s != submission_type.to_s
+      question.section == "demo" &&
+        (question.submission_type.to_s != submission_type.to_s) ||
+        (!question.ai_usage.nil? && question.ai_usage != submission_ai_usage)
     end
   end
 end
