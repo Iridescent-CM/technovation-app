e32fa223d20b964b0235d93d07ac67249ed3356f
Vivian Canales <vcnales@gmail.com>
2025-11-10 17:11:11 -0600

    Added event teams controller + turbo stream templates for create & destroy action

---
+++ app/controllers/chapter_ambassador/event_teams_controller.rb
@@ -0,0 +1,64 @@
+module ChapterAmbassador
+  class EventTeamsController < ChapterAmbassadorController
+    def create
+      @event = RegionalPitchEvent.in_region(current_ambassador).find(params[:event_id])
+      @team = Team.find(params[:team_id])
+
+      AddTeamToRegionalEvent.call(@event, @team)
+      @event = RegionalPitchEvent
+        .includes(
+         teams: [
+           :division,
+           :submission
+         ]
+        )
+        .find(params[:event_id])
+      @available_teams = load_available_teams_for_event(@event)
+
+      respond_to do |format|
+        format.html {
+          redirect_to chapter_ambassador_event_path(@event),
+            notice: "#{@team.name} added to #{@event.name}."
+        }
+        format.turbo_stream
+      end
+    end
+
+    def destroy
+      @event = RegionalPitchEvent.in_region(current_ambassador).find(params[:event_id])
+      @team = Team.find(params[:id])
+
+      if @event.attendees.include?(@team)
+        InvalidateExistingJudgeData.call(@team, removing: true, event: @event)
+
+        @team.memberships.each do |membership|
+          EventMailer.notify_removed(
+            membership.member_type,
+            membership.member_id,
+            @event.id,
+            team_name: @team.name
+          ).deliver_later
+        end
+      end
+
+      @event = RegionalPitchEvent
+        .includes(
+          :judges, :user_invitations,
+          teams: [
+           :division,
+           :submission
+         ]
+        )
+      .find(params[:event_id])
+      @available_teams = load_available_teams_for_event(@event)
+
+      respond_to do |format|
+        format.html {
+          redirect_to chapter_ambassador_event_path(@event),
+          notice: "Team removed"
+        }
+        format.turbo_stream
+      end
+    end
+  end
+end
