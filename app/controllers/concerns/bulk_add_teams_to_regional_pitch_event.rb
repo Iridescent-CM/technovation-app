93451d1fef486c8f19e3818ef60cfbe24db75299
shaun <58957909+shaun-technovation@users.noreply.github.com>
2025-11-21 16:48:21 -0700

    Chore: Ensure uploaded CSV headers are formatted correctly
    This will use the `required_keys` option to ensure the uploaded CSV has
    a "Team Id" header/column.
    
    Refs: #5912

--- app/controllers/concerns/bulk_add_teams_to_regional_pitch_event.rb
+++ app/controllers/concerns/bulk_add_teams_to_regional_pitch_event.rb
@@ -9,15 +9,24 @@ module BulkAddTeamsToRegionalPitchEvent
         ]
       )
       .find(params[:event_id])
-    SmarterCSV.process(params[:csv_file], {chunk_size: 100}) do |chunk|
-      team_ids = chunk.map do |row|
-        row[:team_id] if row[:team_id].present?
-      end.compact
 
-      result = DataProcessors::AssignTeamsToRegionalPitchEvent.new(team_ids:, rpe_id: @event.id).call
+    begin
+      SmarterCSV.process(params[:csv_file], {
+        chunk_size: 100,
+        required_keys: [:team_id]
+      }) do |chunk|
+        team_ids =
+          chunk.map do |row|
+            row[:team_id] if row[:team_id].present?
+          end.compact
 
-      flash.now[:alert] = result.results
-      render "admin/regional_pitch_events/show"
+        result = DataProcessors::AssignTeamsToRegionalPitchEvent.new(team_ids:, rpe_id: @event.id).call
+
+        flash.now[:alert] = result.results
+        render "admin/regional_pitch_events/show"
+      end
+    rescue SmarterCSV::MissingKeys
+      redirect_to chapter_ambassador_event_path(@event), error: 'Please ensure the CSV contains a "Team Id" header.'
     end
   end
 end
