061ec5fea2f4d4ad20c61cd82a25f5c5ab365341
Vivian Canales <vcnales@gmail.com>
2025-12-03 11:13:18 -0600

    Added twilio ruby sdk + backend logic to send parental consent sms

--- app/controllers/student/parental_consent_notices_controller.rb
+++ app/controllers/student/parental_consent_notices_controller.rb
@@ -2,9 +2,9 @@ class Student::ParentalConsentNoticesController < StudentController
   def create
     if profile_params
       current_student.assign_attributes(profile_params)
-      current_student.validate_parent_email
+      current_student.validate_parent_contact_information
 
-      unless current_student.validate_parent_email && current_student.save
+      unless current_student.validate_parent_contact_information && current_student.save
         render :new and return
       end
     else
@@ -23,7 +23,8 @@ class Student::ParentalConsentNoticesController < StudentController
     if params.fetch(:student_profile) { false }
       params.require(:student_profile).permit(
         :parent_guardian_email,
-        :parent_guardian_name
+        :parent_guardian_name,
+        :parent_guardian_phone_number
       )
     end
   end
