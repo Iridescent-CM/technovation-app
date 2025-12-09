3a23c2811af684d32a709526a478884b693d37cb
Vivian Canales <vcnales@gmail.com>
2025-12-09 13:21:19 -0600

    Updated twilio service to save parental consent request record

--- app/services/twilio/api_client.rb
+++ app/services/twilio/api_client.rb
@@ -39,10 +39,18 @@ module Twilio
       end
 
       begin
-        client.messages.create(
+        response = client.messages.create(
           body: message_body,
           to: parent_guardian_phone_number,
           from: technovation_phone_number
+        )
+
+        student_profile.parental_consent_requests.create(
+          delivery_method: :sms,
+          external_message_id: response.sid,
+          recipient: response.to,
+          sent_at: Time.now,
+          season: Season.current.year
         )
       rescue Twilio::REST::RestError => e
         error = "[TWILIO] Error sending SMS - #{e.message}"
