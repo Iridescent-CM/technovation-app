6352933ea38ef0ccdcea6509c68875f7cc17fdd7
Vivian Canales <vcnales@gmail.com>
2026-01-27 11:51:53 -0600

    Refactored twilio api client

--- app/jobs/send_signed_consent_text_message_job.rb
+++ app/jobs/send_signed_consent_text_message_job.rb
@@ -3,10 +3,9 @@ class SendSignedConsentTextMessageJob < ActiveJob::Base
 
   def perform(account_id:, message_type:, delivery_method: :whatsapp)
     account = Account.find(account_id)
-    Twilio::ApiClient.new.send_signed_consent_text_message(
-      account: account,
-      message_type: message_type,
-      delivery_method: delivery_method
-    )
+
+    Twilio::ApiClient
+      .new(account: account, delivery_method: delivery_method)
+      .send_signed_consent_text_message(message_type: message_type)
   end
 end
--- app/services/twilio/api_client.rb
+++ app/services/twilio/api_client.rb
@@ -3,6 +3,8 @@ module Twilio
     include Rails.application.routes.url_helpers
 
     def initialize(
+      account:,
+      delivery_method: :whatsapp,
       api_account_sid: ENV.fetch("TWILIO_ACCOUNT_SID"),
       api_auth_token: ENV.fetch("TWILIO_AUTH_TOKEN"),
       technovation_phone_number: ENV.fetch("TWILIO_PHONE_NUMBER"),
@@ -12,6 +14,8 @@ module Twilio
       secondary_error_notifier: IntegrationErrorNotifier
     )
 
+      @account = account
+      @delivery_method = delivery_method
       @api_account_sid = api_account_sid
       @api_auth_token = api_auth_token
       @technovation_phone_number = technovation_phone_number
@@ -22,19 +26,14 @@ module Twilio
       @client = Twilio::REST::Client.new(api_account_sid, api_auth_token)
     end
 
-    def send_parental_consent_text_message(account:, delivery_method:)
-      token = account.consent_token
+    def send_parental_consent_text_message
       consent_url = edit_parental_consent_url(
-        token: token,
+        token: consent_token,
         notification_method: "text_message",
         host: host
       )
 
-      parent_guardian_name = account.student_profile.parent_guardian_name
-      student_name = account.student_profile.full_name
-
       send_text_message(
-        account: account,
         content_sid: ENV.fetch("TWILIO_PARENTAL_CONSENT_CONTENT_SID"),
         content_variables: {
           "1" => parent_guardian_name,
@@ -42,24 +41,20 @@ module Twilio
           "3" => consent_url
         },
         message_type: :parental_consent,
-        delivery_method: delivery_method
       )
     end
 
-    def send_signed_consent_text_message(account:, message_type:, delivery_method:)
-      token = account.consent_token
+    def send_signed_consent_text_message(message_type:)
       signed_consents_url = signed_consents_url(
-        token: token,
+        token: consent_token,
         host: host
       )
 
       content_sid = message_type == :signed_parental_consent ?
-        ENV.fetch("TWILIO_SIGNED_PARENTAL_CONSENT_CONTENT_SID") : ENV.fetch("TWILIO_SIGNED_MEDIA_CONSENT_CONTENT_SID")
-      parent_guardian_name = account.student_profile.parent_guardian_name
-      student_name = account.student_profile.full_name
+        ENV.fetch("TWILIO_SIGNED_PARENTAL_CONSENT_CONTENT_SID") :
+        ENV.fetch("TWILIO_SIGNED_MEDIA_CONSENT_CONTENT_SID")
 
       send_text_message(
-        account: account,
         content_sid: content_sid,
         content_variables: {
           "1" => parent_guardian_name,
@@ -67,16 +62,30 @@ module Twilio
           "3" => signed_consents_url
         },
         message_type: message_type,
-        delivery_method: delivery_method
       )
     end
 
     private
 
-    attr_reader :client, :api_account_sid, :api_auth_token, :technovation_phone_number, :host, :logger, :error_notifier, :secondary_error_notifier
+    attr_reader :account, :delivery_method, :client, :api_account_sid, :api_auth_token, :technovation_phone_number, :host, :logger, :error_notifier, :secondary_error_notifier
 
-    def send_text_message(account:, content_sid:, content_variables:, message_type:, delivery_method: :whatsapp)
-      parent_guardian_phone_number = account.student_profile.parent_guardian_phone_number
+    def consent_token
+      account.consent_token
+    end
+
+    def parent_guardian_name
+      account.student_profile.parent_guardian_name
+    end
+
+    def student_name
+      account.student_profile.full_name
+    end
+
+    def parent_guardian_phone_number
+      account.student_profile.parent_guardian_phone_number
+    end
+
+    def send_text_message(content_sid:, content_variables:, message_type:)
       prefix = delivery_method == :whatsapp ? "whatsapp:" : ""
 
       response = client.messages.create(
