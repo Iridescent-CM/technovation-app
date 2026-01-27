2e996df867d28a7713ea4b2cf2767a7a7a1b74ab
Vivian Canales <vcnales@gmail.com>
2026-01-27 12:40:39 -0600

    Updated notification method to delivery method

--- app/controllers/parental_consents_controller.rb
+++ app/controllers/parental_consents_controller.rb
@@ -42,13 +42,17 @@ class ParentalConsentsController < ApplicationController
     @parental_consent = student.parental_consent
 
     if @parental_consent.update(parental_consent_params)
-      if params[:notification_method] == "text_message" && student.parent_guardian_phone_number.present?
-        SendSignedConsentTextMessageJob.perform_later(account_id: student.account.id, message_type: :signed_parental_consent)
+      if %w[sms whatsapp].include?(params[:delivery_method]) && student.parent_guardian_phone_number.present?
+        SendSignedConsentTextMessageJob.perform_later(
+          account_id: student.account.id,
+          message_type: :signed_parental_consent,
+          delivery_method: params[:delivery_method].to_sym,
+        )
       elsif student.parent_guardian_email.present?
         ParentMailer.confirm_parental_consent_finished(student.id).deliver_later
       end
 
-      redirect_to edit_media_consent_path(token: token, notification_method: params[:notification_method])
+      redirect_to edit_media_consent_path(token: token, delivery_method: params[:delivery_method])
     else
       render :edit
     end
@@ -65,7 +69,7 @@ class ParentalConsentsController < ApplicationController
   def parental_consent_params
     params.require(:parental_consent).permit(
       :student_profile_consent_token,
-      :notification_method,
+      :delivery_method,
       :electronic_signature,
       :newsletter_opt_in
     ).tap do |tapped|
