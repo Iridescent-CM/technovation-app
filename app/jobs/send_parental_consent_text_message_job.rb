41cee94dfcaae9328b600f6595a91f67659a9cbe
Vivian Canales <vcnales@gmail.com>
2026-01-27 11:51:53 -0600

    Refactored twilio api client

--- app/jobs/send_parental_consent_text_message_job.rb
+++ app/jobs/send_parental_consent_text_message_job.rb
@@ -4,9 +4,8 @@ class SendParentalConsentTextMessageJob < ActiveJob::Base
   def perform(account_id:, delivery_method: :whatsapp)
     account = Account.find(account_id)
 
-    Twilio::ApiClient.new.send_parental_consent_text_message(
-      account: account,
-      delivery_method: delivery_method
-    )
+    Twilio::ApiClient
+      .new(account: account, delivery_method: delivery_method)
+      .send_parental_consent_text_message
   end
 end
