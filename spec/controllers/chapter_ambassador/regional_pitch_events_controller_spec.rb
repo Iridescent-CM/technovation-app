1e30ad693ca1e1cae00bd01dedebdc8fbbf72651
Vivian Canales <vcnales@gmail.com>
2026-02-20 14:11:01 -0600

    Update test

--- spec/controllers/chapter_ambassador/regional_pitch_events_controller_spec.rb
+++ spec/controllers/chapter_ambassador/regional_pitch_events_controller_spec.rb
@@ -2,23 +2,41 @@ require "rails_helper"
 
 RSpec.describe ChapterAmbassador::RegionalPitchEventsController do
   describe "POST #create" do
-    it "creates an event and redirects to show page" do
-      sign_in(:chapter_ambassador)
+    context "when event creation is enabled" do
+      it "creates an event and redirects to show view" do
+        sign_in(:chapter_ambassador)
+        allow(SeasonToggles).to receive(:create_regional_pitch_event?).and_return(true)
 
-      post :create, params: {
-        regional_pitch_event: FactoryBot.attributes_for(:event)
-      }
+        expect {
+          post :create, params: {
+            regional_pitch_event: FactoryBot.attributes_for(:event)
+          }
+        }.to change { RegionalPitchEvent.count }.by(1)
 
-      event = RegionalPitchEvent.last
+        event = RegionalPitchEvent.last
+        expect(response).to redirect_to(chapter_ambassador_event_path(event))
+      end
+    end
 
-      expect(response).to redirect_to(chapter_ambassador_event_path(event))
+    context "when event creation is disabled" do
+      it "does not create an event and redirects to the events list view" do
+        sign_in(:chapter_ambassador)
+        allow(SeasonToggles).to receive(:create_regional_pitch_event?).and_return(false)
+
+        expect {
+          post :create, params: {
+            regional_pitch_event: FactoryBot.attributes_for(:event)
+          }
+        }.not_to change { RegionalPitchEvent.count }
+
+        expect(response).to redirect_to(chapter_ambassador_events_list_path)
+      end
     end
   end
 
   describe "PATCH #update" do
-    it "updates an event and redirects to the show page " do
+    it "updates an event and redirects to the show page" do
       sign_in(:chapter_ambassador)
-
       event = FactoryBot.create(:event)
 
       patch :update, params: {
